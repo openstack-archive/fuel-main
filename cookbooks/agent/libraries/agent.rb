c2d4b359b75f81b3960a66643a95a7d2abdeef3c
Mike Scherbakov <mihgen@gmail.com>
2012-05-14 00:00:24 +0400

    App/type->json, process response code in agent.

--- cookbooks/agent/libraries/agent.rb
+++ cookbooks/agent/libraries/agent.rb
@@ -15,10 +15,14 @@ module NodeAgent
              :cpu => node["cpu"].to_hash,
              :memory => node["memory"].to_hash
            }
+    headers = {"Content-Type" => "application/json"}
 
     cli = HTTPClient.new
     begin
-      cli.put(url, data.to_json)
+      res = cli.put(url, data.to_json, headers)
+      if res.status < 200 or res.status >= 300
+        Chef::Log.error("HTTP PUT failed: #{res.inspect}")
+      end
     rescue Exception => e
       Chef::Log.error("Error in sending ohai data: #{e.message}")
     end
