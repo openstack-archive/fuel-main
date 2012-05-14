6f48360acb993b84fd3b93b148da946fe65aa3d0
Maxim Kulkin <mkulkin@mirantis.com>
2012-05-14 13:56:23 +0400

    Fixed path to database file

--- nailgun/nailgun/settings.py
+++ nailgun/nailgun/settings.py
@@ -2,6 +2,7 @@
 import os.path
 
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+PROJECT_ROOT = os.path.dirname(SITE_ROOT)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -15,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(SITE_ROOT, 'nailgun.sqlite'),  # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_ROOT, 'nailgun.sqlite'),  # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
