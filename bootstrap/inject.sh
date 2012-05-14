0ff71aff7a0897a5d098b4e02b903217dd147cc4
Vladimir Kozhukalov <kozhukalov@gmail.com>
2012-05-14 15:29:11 +0400

    bootstrap/inject.sh modified in orde to include chef bootstrap agent recipe

--- bootstrap/inject.sh
+++ bootstrap/inject.sh
@@ -23,7 +23,7 @@ iface eth0 inet dhcp
 EOF
 
 # default password for root is r00tme 
-sed -ie '/^root/c\root:$6$oC7haQNQ$LtVf6AI.QKn9Jb89r83PtQN9fBqpHT9bAFLzy.YVxTLiFgsoqlPY3awKvbuSgtxYHx4RUcpUqMotp.WZ0Hwoj.:15441:0:99999:7:::' ${INITRD_LOOP}/etc/shadow
+sed -i -e '/^root/c\root:$6$oC7haQNQ$LtVf6AI.QKn9Jb89r83PtQN9fBqpHT9bAFLzy.YVxTLiFgsoqlPY3awKvbuSgtxYHx4RUcpUqMotp.WZ0Hwoj.:15441:0:99999:7:::' ${INITRD_LOOP}/etc/shadow
 
 
 # root autologin tty1
@@ -32,7 +32,8 @@ cat > ${INITRD_LOOP}/usr/bin/autologin <<EOF
 /bin/login -f root
 EOF
 chmod +x ${INITRD_LOOP}/usr/bin/autologin
-sed -ie '/exec/c\exec /sbin/getty -8 -l /usr/bin/autologin 38400 tty1' ${INITRD_LOOP}/etc/init/tty1.conf
+sed -i -e '/exec/c\exec /sbin/getty -8 -l /usr/bin/autologin 38400 tty1' ${INITRD_LOOP}/etc/init/tty1.conf
+
 
 # sources.list
 cat > ${INITRD_LOOP}/etc/apt/sources.list <<EOF
@@ -45,14 +46,34 @@ EOF
 
 wget --quiet -O - http://apt.opscode.com/packages@opscode.com.gpg.key > ${INITRD_LOOP}/root/opscode.key
 
+# preconfiguring chef package
+chroot ${INITRD_LOOP} /bin/bash <<EOF
+echo "chef chef/chef_server_url string http://localhost:4000" | debconf-set-selections
+EOF
+
+# FIXME 
+# It is necessary to use lxc to configure initrd
+
 chroot ${INITRD_LOOP} /bin/bash <<EOF
 cat /root/opscode.key | apt-key add -
 apt-get update
 apt-get -y install chef
+update-rc.d chef-client disable
 EOF
 
 
+# updating modules.dep in order to fix modprobe errors
+for version in `ls -1 ${INITRD_LOOP}/lib/modules`; do
+    depmod -b ${INITRD_LOOP} $version
+done
 
+# injecting cookbooks and configs for chef-solo
+cp -r ../cookbooks ${INITRD_LOOP}/root
+cp -r ../scripts ${INITRD_LOOP}/root
+
+cat > /etc/cron.d/chef-solo <<EOF
+* * * * * root /usr/bin/chef-solo -l debug -c /root/scripts/solo.rb -j /root/scripts/solo.json
+EOF
 
 
 
