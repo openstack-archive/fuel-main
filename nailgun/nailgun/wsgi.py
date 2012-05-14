029164db6d87ff4892fcb2098eff395cbe47d0a7
Maxim Kulkin <mkulkin@mirantis.com>
2012-05-14 13:57:22 +0400

    Added auto reloading feature to Nailgun WSGI application

--- nailgun/nailgun/wsgi.py
+++ nailgun/nailgun/wsgi.py
@@ -17,6 +17,9 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nailgun.settings")
 
+import monitor
+monitor.start(interval=1.0)
+
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
