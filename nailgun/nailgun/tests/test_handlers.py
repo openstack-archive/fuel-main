4b42444f71d28bdb7d0532298864b48a3b01ba31
Mike Scherbakov <mihgen@gmail.com>
2012-05-15 18:40:31 +0400

    Tests refactoring. Added setUp, tearDown in tests.

--- nailgun/nailgun/tests/test_handlers.py
+++ nailgun/nailgun/tests/test_handlers.py
@@ -3,27 +3,34 @@ from django import http
 from django.test import TestCase
 
 from nailgun.models import Node
-from nailgun.api.handlers import NodeHandler
 
 
 class TestHandlers(TestCase):
 
-    def test_node_metadata_gets_updated(self):
+    def setUp(self):
         self.request = http.HttpRequest()
 
-        node = Node(environment_id=1,
+        self.node = Node(environment_id=1,
                     name="test.server.com",
                     metadata={'key': 'value', 'key2': 'val2'})
-        node.save()
+        self.node.save()
+        self.node_url = '/api/environments/1/nodes/test.server.com'
 
-        handler = NodeHandler()
+    def tearDown(self):
+        self.node.delete()
+
+    def test_node_metadata_gets_updated(self):
         new_meta = {'key': 'new-val', 'abc': 'd'}
 
-        nodes_url = '/api/environments/1/nodes/test.server.com'
-        self.client.put(nodes_url, json.dumps(new_meta), "application/json")
+        self.client.put(self.node_url, json.dumps(new_meta), "application/json")
 
         nodes_from_db = Node.objects.filter(environment_id=1,
                                             name='test.server.com')
         self.assertEquals(len(nodes_from_db), 1)
         self.assertEquals(nodes_from_db[0].metadata, {'key': 'new-val', 'abc': 'd'})
+
+    def test_put_returns_400_if_no_body(self):
+        resp = self.client.put(self.node_url, None, "application/json")
+
+        self.assertEquals(resp.status_code, 400)
 
