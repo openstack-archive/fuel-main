62f0c27c8e8fa29964c3ac2ddf1a733028abf8d2
Maxim Kulkin <mkulkin@mirantis.com>
2012-05-15 15:50:46 +0400

    Cleaned up Cobbler provisioner

--- lib/provision/cobbler.py
+++ lib/provision/cobbler.py
@@ -1,37 +1,24 @@
 import logging
 import xmlrpclib
-from provisioner import ProvisionerException
-from provisioner import Provisioner
 
+from . import Provisioner, ProvisionerException
 
-
-class ProvisionerCobbler(Provisioner):
+class CobblerProvisioner(Provisioner):
     """
-    Cobbler provisioner implementation.
+    Cobbler provisioner.
     """
 
-    def __init__(self, **data):
+    def __init__(self, cobbler_url, cobbler_user, cobbler_password):
         self.logger = logging.getLogger(self.logger_name)
 
+        self.cobbler_url = cobbler_url
+        self.cobbler_user = cobbler_user
+        self.cobbler_password = cobbler_password
 
-        if 'cobbler_url' not in data:
-            self.logger.error('Cobbler url was not provided.')
-            raise ProvisionerException("You must provide cobbler_url.")
+        self.logger.debug('Cobbler config: url="%s", user="%s", password="%s"' % (cobbler_url, cobbler_user, cobbler_password))
 
-        self.logger.debug('Cobbler url provided: %s' % str(data['cobbler_url']))
-        self.cobbler_url = data['cobbler_url']
-
-        if 'cobbler_user' not in data or 'cobbler_password' not in data:
-            self.logger.error('Cobbler credentials were not provided correctly.')
-            raise ProvisionerException("You must provide cobbler_user and cobbler_password.")
-            
-        self.logger.debug('Cobbler credentials: cobbler_user=%s, cobbler_password=%s' % (str(data['cobbler_user']), str(data['cobbler_password'])))
-
-        self.cobbler_user = data['cobbler_user']
-        self.cobbler_password = data['cobbler_password']
-
+        self.server = xmlrpclib.Server(self.cobbler_url)
         try:
-            self.server = xmlrpclib.Server(self.cobbler_url)
             self.token = self.server.login(self.cobbler_user, self.cobbler_password)
         except Exception as e:
             self.logger.error('Error occured while connecting to cobbler server.')
@@ -44,3 +31,4 @@ class ProvisionerCobbler(Provisioner):
 
     def list_nodes(self):
         return self.server.get_systems()
+
