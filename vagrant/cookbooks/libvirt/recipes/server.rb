d5f0c7b4d475e5da7d02adaf0d6739bd616477f8
Maxim Kulkin <mkulkin@mirantis.com>
2012-05-17 18:41:12 +0400

    Added libvirt::server recipe

---
+++ vagrant/cookbooks/libvirt/recipes/server.rb
@@ -0,0 +1,23 @@
+
+package 'libvirt-bin'
+package 'sasl2-bin'
+
+service 'libvirt-bin' do
+  action :start
+end
+
+service 'saslauthd' do
+  action :start
+end
+
+template '/etc/libvirt/libvirtd.conf' do
+  source 'libvirtd.conf.erb'
+  mode 0644
+
+  notifies :restart, resources('service[libvirt-bin]')
+end
+
+execute 'set vagrant sasl2 password' do
+  command "echo vagrant | saslpasswd2 -a libvirt vagrant"
+end
+
