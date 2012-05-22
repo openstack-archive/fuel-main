e047d8945c71c69ce73d94ff0c8c610f6378a579
Vladimir Kozhukalov <kozhukalov@gmail.com>
2012-05-22 19:47:28 +0400

    ISO building component added

---
+++ iso/build.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -x
+set -e
+
+[ X`whoami` = X'root' ] || { echo "You must be root."; exit 1; }
+
+[ -z ${BUILDDIR} ] && BUILDDIR=/var/tmp/build_iso
+echo "BUILDDIR=${BUILDDIR}"
+[ -z ${REPODIR} ] && REPODIR=`dirname $0`/.. 
+echo "REPODIR=${REPODIR}"
+
+SCRIPTDIR=`dirname $0`
+
+[ -z ${ORIGISO} ] && ORIGISO=/var/tmp/ubuntu-12.04-server-amd64.iso
+TEMPDIR=`mktemp -d`
+[ -z ${ISO} ] && ISO=/var/tmp/mirantis-nailgun-ubuntu-12.04-amd64.iso
+
+
+echo "Cleaning iso ..."
+rm -rf ${BUILDDIR}
+mkdir -p ${BUILDDIR}
+
+echo "Copying orig iso into build dir ..."
+mount -o loop ${ORIGISO} ${TEMPDIR} && \
+rsync -a ${TEMPDIR}/ ${BUILDDIR} && chmod -R u+w ${BUILDDIR} && \
+umount ${TEMPDIR}
+
+
+echo "Editing install menu ..."
+cat > ${BUILDDIR}/isolinux/txt.cfg <<EOF 
+default install
+label install
+  menu label ^Mirantis Nailgun
+  kernel /install/vmlinuz
+  append  priority=critical locale=en_US file=/cdrom/preseed/mirantis-nailgun.seed vga=788 initrd=/install/initrd.gz quiet --
+EOF
+
+cat > ${BUILDDIR}/isolinux/menu.cfg <<EOF
+menu hshift 13
+menu width 49
+menu margin 8
+
+menu title Installer boot menu
+include stdmenu.cfg
+include txt.cfg
+EOF
+
+echo "Automating install process ..."
+cat > ${BUILDDIR}/isolinux/lang <<EOF
+en
+EOF
+
+cp ${REPODIR}/iso/mirantis-nailgun.seed ${BUILDDIR}/preseed
+
+echo "Creating repo ..."
+BUILDDIR=${BUILDDIR} REPODIR=${REPODIR} ${SCRIPTDIR}/repo.sh
+
+echo "Injecting files ..."
+BUILDDIR=${BUILDDIR} REPODIR=${REPODIR} ${SCRIPTDIR}/inject.sh
+
+echo "Calculating md5sums ..."
+rm ${BUILDDIR}/md5sum.txt
+(cd ${BUILDDIR}/ && find . -type f -print0 | xargs -0 md5sum | grep -v "boot.cat" | grep -v "md5sum.txt" > md5sum.txt)
+
+
+echo "Building iso image ..."
+mkisofs -r -V "Mirantis Nailgun" \
+            -cache-inodes \
+            -J -l -b isolinux/isolinux.bin \
+            -c isolinux/boot.cat -no-emul-boot \
+            -boot-load-size 4 -boot-info-table \
+            -o ${ISO} ${BUILDDIR}/
+
+
---
+++ iso/repo.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+set -x
+set -e
+
+[ X`whoami` = X'root' ] || { echo "You must be root."; exit 1; }
+
+[ -z ${BUILDDIR} ] && BUILDDIR=/var/tmp/build_iso
+echo "BUILDDIR=${BUILDDIR}"
+[ -z ${REPODIR} ] && REPODIR=`dirname $0`/.. 
+echo "REPODIR=${REPODIR}"
+
+SCRIPTDIR=`dirname $0`
+
+[ -z ${GNUPGHOME} ] && GNUPGHOME=${REPODIR}/gnupg
+
+echo "Rebuilding ubuntu-keyring package in order to inject mirantis gpg key ..."
+KEYRINGBUILDDIR=`mktemp -d`
+mkdir -m 700 ${KEYRINGBUILDDIR}/gnupg
+(cd ${KEYRINGBUILDDIR} && apt-get source ubuntu-keyring)
+package=`find ${KEYRINGBUILDDIR} -type d -name "ubuntu-keyring*" | xargs basename`
+
+cat ${REPODIR}/gnupg/pubring.gpg | \
+    gpg --homedir ${KEYRINGBUILDDIR}/gnupg --import 2>/dev/null
+cat ${KEYRINGBUILDDIR}/${package}/keyrings/ubuntu-archive-keyring.gpg | \
+    gpg --homedir ${KEYRINGBUILDDIR}/gnupg --import 2>/dev/null
+gpg --homedir ${KEYRINGBUILDDIR}/gnupg --export > \
+    ${KEYRINGBUILDDIR}/${package}/keyrings/ubuntu-archive-keyring.gpg
+
+(cd ${KEYRINGBUILDDIR}/${package} && dpkg-buildpackage || true )
+mv ${KEYRINGBUILDDIR}/*.deb ${BUILDDIR}/pool/main/u/ubuntu-keyring
+mv ${KEYRINGBUILDDIR}/*.udeb ${BUILDDIR}/pool/main/u/ubuntu-keyring
+rm -r ${KEYRINGBUILDDIR}
+
+# FIXME
+
+echo "Downloading opscode packages ..."
+MIRRORDOWNLOAD=http://mc0n1-srt.srt.mirantis.net/ubuntu/
+TEMPDOWNLOAD=`mktemp --tmpdir=/var/tmp -d`
+mkdir -p ${TEMPDOWNLOAD}/etc/preferences.d
+mkdir -p ${TEMPDOWNLOAD}/etc/apt.conf.d
+cat > ${TEMPDOWNLOAD}/etc/apt.conf <<EOF
+APT
+{
+  Architecture "amd64";
+  Default-Release "precise";
+  Get::Download-Only "true";
+  Get::AllowUnauthenticated "true";
+};
+
+Dir
+{
+  State "${TEMPDOWNLOAD}/state";
+  State::status "status";
+  
+  Cache::archives "${TEMPDOWNLOAD}/archives";
+  Cache "${TEMPDOWNLOAD}/cache";
+  
+  Etc "${TEMPDOWNLOAD}/etc";
+};
+EOF
+
+cat > ${TEMPDOWNLOAD}/etc/sources.list <<EOF
+deb ${MIRRORDOWNLOAD} precise main
+EOF
+
+mkdir -p ${TEMPDOWNLOAD}/state
+touch ${TEMPDOWNLOAD}/state/status
+
+mkdir -p ${TEMPDOWNLOAD}/archives
+mkdir -p ${TEMPDOWNLOAD}/cache
+
+apt-get -c=${TEMPDOWNLOAD}/etc/apt.conf update
+
+
+
+echo "Updating repository ..."
+
+DISTS=${BUILDDIR}/dists
+
+rm -r ${DISTS}
+
+mkdir -p ${DISTS}/precise/main/binary-amd64  
+mkdir -p ${DISTS}/precise/main/binary-i386  
+mkdir -p ${DISTS}/precise/main/debian-installer/binary-amd64  
+
+(cd ${DISTS} && ln -s ./precise stable)
+(cd ${DISTS} && ln -s ./precise unstable)
+
+(cd ${BUILDDIR} && dpkg-scanpackages -m -a amd64 -tdeb pool | gzip -9c > dists/precise/main/binary-amd64/Packages.gz)
+cat > ${DISTS}/precise/main/binary-amd64/Release <<EOF
+Archive: precise
+Version: 12.04
+Component: main
+Origin: Mirantis
+Label: Mirantis Nailgun
+Architecture: amd64
+EOF
+
+(cd ${BUILDDIR} && dpkg-scanpackages -m -a i386 -tdeb pool | gzip -9c > dists/precise/main/binary-i386/Packages.gz)
+cat > ${DISTS}/precise/main/binary-amd64/Release <<EOF
+Archive: precise
+Version: 12.04
+Component: main
+Origin: Mirantis
+Label: Mirantis Nailgun
+Architecture: i386
+EOF
+
+(cd ${BUILDDIR} && dpkg-scanpackages -m -a amd64 -tudeb pool | gzip -9c > dists/precise/main/debian-installer/binary-amd64/Packages.gz)
+
+TEMPCONF=`mktemp`
+
+cat > ${TEMPCONF} <<EOF
+APT::FTPArchive::Release::Origin "Mirantis";
+APT::FTPArchive::Release::Label "Mirantis Nailgun";
+APT::FTPArchive::Release::Codename "precise";
+APT::FTPArchive::Release::Suite "precise";
+APT::FTPArchive::Release::Version "12.04";
+APT::FTPArchive::Release::Architectures "amd64 i386";
+APT::FTPArchive::Release::Components "main";
+APT::FTPArchive::Release::Description "Mirantis Nailgun Repo";
+EOF
+
+apt-ftparchive -c ${TEMPCONF} release ${DISTS}/precise > ${DISTS}/precise/Release
+
+rm ${TEMPCONF}
+
+echo "Signing Release ..."
+GNUPGHOME=${GNUPGHOME} gpg --yes --output ${DISTS}/precise/Release.gpg -ba ${DISTS}/precise/Release
+
