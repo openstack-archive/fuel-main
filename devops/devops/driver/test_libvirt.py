d28e25c3a5e50bab3a2bf3c0c950e65525824d2a
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-05-25 14:24:44 +0400

    [devops] Added tests for libvirt's XML: handling memory and cdrom

--- devops/devops/driver/test_libvirt.py
+++ devops/devops/driver/test_libvirt.py
@@ -1,18 +1,25 @@
 import unittest
-from lxml import etree
-from devops.model import Network, Node
-from devops.driver.libvirt import Libvirt, LibvirtXMLBuilder
+from devops import xml
+from devops.model import Network, Node, Disk, Cdrom
+from devops.driver.libvirt import Libvirt, LibvirtXMLBuilder, DeploymentSpec
+
 
 class TestLibvirtXMLBuilder(unittest.TestCase):
+    node_spec = DeploymentSpec()
+    node_spec.arch = 'x86_64'
+    node_spec.os_type = 'hvm'
+    node_spec.hypervisor = 'kvm'
+    node_spec.emulator = '/usr/bin/kvm'
+
     def setUp(self):
         self.builder = LibvirtXMLBuilder()
 
     def test_hostonly_network_xml_generation(self):
         network = Network('net1')
         network.id = 'net1'
-        xml = self.builder.build_network_xml(network)
+        doc_xml = self.builder.build_network_xml(network)
 
-        doc = etree.fromstring(xml)
+        doc = xml.parse_string(doc_xml)
 
         self.assertIsNotNone(doc)
         self.assertEqual('network', doc.tag)
@@ -21,6 +28,39 @@ class TestLibvirtXMLBuilder(unittest.TestCase):
         self.assertIsNotNone(e)
         self.assertEqual('net1', e.text)
 
-        # e = doc.find('bridge')
-        # self.assertIsNotNone(e)
-        # self.assertEqual('bridge-net1', e.get('name'))
+    def test_node_memory(self):
+        node = Node('node1')
+        node.id = node.name
+        node.memory = 123
+        
+        doc_xml = self.builder.build_node_xml(node, self.node_spec)
+
+        doc = xml.parse_string(doc_xml)
+
+        memory_element = doc.find('memory')
+        self.assertIsNotNone(memory_element)
+        self.assertEqual('KiB', memory_element['unit'])
+        self.assertEqual(str(123*1024), memory_element.text)
+
+
+    def test_cdrom_disk(self):
+        node = Node('node1')
+        node.id = node.name
+        node.cdrom = Cdrom(isopath='foo.iso')
+
+        doc_xml = self.builder.build_node_xml(node, self.node_spec)
+
+        doc = xml.parse_string(doc_xml)
+
+        cdrom_element = doc.find('devices/disk[@type="file" and @device="cdrom"]')
+        self.assertIsNotNone(cdrom_element)
+
+        cdrom_driver_element = cdrom_element.find('driver')
+        self.assertIsNotNone(cdrom_driver_element)
+        self.assertEqual('qemu', cdrom_driver_element['name'])
+        self.assertEqual('raw', cdrom_driver_element['type'])
+
+        cdrom_source_element = cdrom_element.find('source')
+        self.assertIsNotNone(cdrom_source_element)
+        self.assertEqual('foo.iso', cdrom_source_element['file'])
+
