0fd151df8e6a6e11bf8fc9eb33dd5e763b2953ab
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-05-31 14:58:40 +0400

    [devops] Convert scancodes to libvirt's format (no key release codes)

--- devops/devops/scancodes.py
+++ devops/devops/scancodes.py
@@ -2,158 +2,163 @@
 # Based on http://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html
 # Scancodes < 0x80 - key presses, > 0x80 - key releases
 SCANCODES = {
-    '1': '02 82',
-    '2': '03 83',
-    '3': '04 84',
-    '4': '05 85',
-    '5': '06 86',
-    '6': '07 87',
-    '7': '08 88',
-    '8': '09 89',
-    '9': '0a 8a',
-    '0': '0b 8b',
-    '-': '0c 8c',
-    '=': '0d 8d',
+    '1': 0x02,
+    '2': 0x03,
+    '3': 0x04,
+    '4': 0x05,
+    '5': 0x06,
+    '6': 0x07,
+    '7': 0x08,
+    '8': 0x09,
+    '9': 0x0a,
+    '0': 0x0b,
+    '-': 0x0c,
+    '=': 0x0d,
 
-    'q': '10 90',
-    'w': '11 91',
-    'e': '12 92',
-    'r': '13 93',
-    't': '14 94',
-    'y': '15 95',
-    'u': '16 96',
-    'i': '17 97',
-    'o': '18 98',
-    'p': '19 99',
+    'q': 0x10,
+    'w': 0x11,
+    'e': 0x12,
+    'r': 0x13,
+    't': 0x14,
+    'y': 0x15,
+    'u': 0x16,
+    'i': 0x17,
+    'o': 0x18,
+    'p': 0x19,
 
-    'Q': '2a 10 aa 90',
-    'W': '2a 11 aa 91',
-    'E': '2a 12 aa 92',
-    'R': '2a 13 aa 93',
-    'T': '2a 14 aa 94',
-    'Y': '2a 15 aa 95',
-    'U': '2a 16 aa 96',
-    'I': '2a 17 aa 97',
-    'O': '2a 18 aa 98',
-    'P': '2a 19 aa 99',
+    'Q': (0x2a, 0x10),
+    'W': (0x2a, 0x11),
+    'E': (0x2a, 0x12),
+    'R': (0x2a, 0x13),
+    'T': (0x2a, 0x14),
+    'Y': (0x2a, 0x15),
+    'U': (0x2a, 0x16),
+    'I': (0x2a, 0x17),
+    'O': (0x2a, 0x18),
+    'P': (0x2a, 0x19),
 
-    'a': '1e 9e',
-    's': '1f 9f',
-    'd': '20 a0',
-    'f': '21 a1',
-    'g': '22 a2',
-    'h': '23 a3',
-    'j': '24 a4',
-    'k': '25 a5',
-    'l': '26 a6',
+    'a': 0x1e,
+    's': 0x1f,
+    'd': 0x20,
+    'f': 0x21,
+    'g': 0x22,
+    'h': 0x23,
+    'j': 0x24,
+    'k': 0x25,
+    'l': 0x26,
 
-    'A': '2a 1e aa 9e',
-    'S': '2a 1f aa 9f',
-    'D': '2a 20 aa a0',
-    'F': '2a 21 aa a1',
-    'G': '2a 22 aa a2',
-    'H': '2a 23 aa a3',
-    'J': '2a 24 aa a4',
-    'K': '2a 25 aa a5',
-    'L': '2a 26 aa a6',
+    'A': (0x2a, 0x1e),
+    'S': (0x2a, 0x1f),
+    'D': (0x2a, 0x20),
+    'F': (0x2a, 0x21),
+    'G': (0x2a, 0x22),
+    'H': (0x2a, 0x23),
+    'J': (0x2a, 0x24),
+    'K': (0x2a, 0x25),
+    'L': (0x2a, 0x26),
 
-    ';': '27 a7',
-    '"': '2a 28 aa a8',
-    '\'': '28 a8',
+    ';': 0x27,
+    '"': (0x2a, 0x28),
+    '\'': 0x28,
 
-    '\\': '2b ab',
-    '|': '2a 2b aa 8b',
+    '\\': 0x2b,
+    '|': (0x2a, 0x2b),
 
-    '[': '1a 9a',
-    ']': '1b 9b',
-    '<': '2a 33 aa b3',
-    '>': '2a 34 aa b4',
-    '?': '2a 35 aa b5',
-    '$': '2a 05 aa 85',
-    '+': '2a 0d aa 8d',
+    '[': 0x1a,
+    ']': 0x1b,
+    '<': (0x2a, 0x33),
+    '>': (0x2a, 0x34),
+    '?': (0x2a, 0x35),
+    '$': (0x2a, 0x05),
+    '+': (0x2a, 0x0d),
 
-    'z': '2c ac',
-    'x': '2d ad',
-    'c': '2e ae',
-    'v': '2f af',
-    'b': '30 b0',
-    'n': '31 b1',
-    'm': '32 b2',
+    'z': 0x2c,
+    'x': 0x2d,
+    'c': 0x2e,
+    'v': 0x2f,
+    'b': 0x30,
+    'n': 0x31,
+    'm': 0x32,
 
-    'Z': '2a 2c aa ac',
-    'X': '2a 2d aa ad',
-    'C': '2a 2e aa ae',
-    'V': '2a 2f aa af',
-    'B': '2a 30 aa b0',
-    'N': '2a 31 aa b1',
-    'M': '2a 32 aa b2',
+    'Z': (0x2a, 0x2c),
+    'X': (0x2a, 0x2d),
+    'C': (0x2a, 0x2e),
+    'V': (0x2a, 0x2f),
+    'B': (0x2a, 0x30),
+    'N': (0x2a, 0x31),
+    'M': (0x2a, 0x32),
 
-    ',': '33 b3',
-    '.': '34 b4',
-    '/': '35 b5',
-    ':': '2a 27 aa a7',
-    '%': '2a 06 aa 86',
-    '_': '2a 0c aa 8c',
-    '&': '2a 08 aa 88',
-    '(': '2a 0a aa 8a',
-    ')': '2a 0b aa 8b',
+    ',': 0x33,
+    '.': 0x34,
+    '/': 0x35,
+    ':': (0x2a, 0x27),
+    '%': (0x2a, 0x06),
+    '_': (0x2a, 0x0c),
+    '&': (0x2a, 0x08),
+    '(': (0x2a, 0x0a),
+    ')': (0x2a, 0x0b),
 
-    ' ': '39 b9'
+    ' ': 0x39
 }
 
 SPECIALS = {
-    '<Enter>': '1c 9c',
-    '<Return>': '1c 9c',
-    '<Backspace>': '0e 8e',
-    '<Spacebar>': '39 b9',
-    '<Esc>': '01 81',
-    '<Tab>': '0f 8f',
-    '<KillX>': '1d 38 0e',
-    '<Wait>': 'wait',
+    '<Enter>': 0x1c,
+    '<Return>': 0x1c,
+    '<Backspace>': 0x0e,
+    '<Spacebar>': 0x39,
+    '<Esc>': 0x01,
+    '<Tab>': 0x0f,
+    '<KillX>': (0x1d, 0x38, 0x0e),
+    '<Wait>': ('wait', ),
 
-    '<PageUp>': '49 c9',
-    '<PageDown>': '51 d1',
-    '<Home>': '47 c7',
-    '<End>': '4f cf',
-    '<Insert>': '52 d2',
-    '<Delete>': '53 d3',
-    '<Left>': '4b cb',
-    '<Right>': '4d cd',
-    '<Up>': '48 c8',
-    '<Down>': '50 d0',
+    '<PageUp>': 0x49,
+    '<PageDown>': 0x51,
+    '<Home>': 0x47,
+    '<End>': 0x4f,
+    '<Insert>': 0x52,
+    '<Delete>': 0x53,
+    '<Left>': 0x4b,
+    '<Right>': 0x4d,
+    '<Up>': 0x48,
+    '<Down>': 0x50,
 
-    '<F1>': '3b',
-    '<F2>': '3c',
-    '<F3>': '3d',
-    '<F4>': '3e',
-    '<F5>': '3f',
-    '<F6>': '40',
-    '<F7>': '41',
-    '<F8>': '42',
-    '<F9>': '43',
-    '<F10>': '44'
+    '<F1>': 0x3b,
+    '<F2>': 0x3c,
+    '<F3>': 0x3d,
+    '<F4>': 0x3e,
+    '<F5>': 0x3f,
+    '<F6>': 0x40,
+    '<F7>': 0x41,
+    '<F8>': 0x42,
+    '<F9>': 0x43,
+    '<F10>': 0x44
 }
+
+__all__ = ['from_string']
+
+def iterable(a):
+    if a is None: return tuple() 
+    return a if isinstance(a, (tuple, list)) else (a,)
 
 def from_string(s):
     "from_string(s) - Convert string of chars into string of corresponding scancodes."
-    scancodes = ''
+
+    scancodes = []
 
     while len(s) > 0:
         if s[0] == '<' and s.find('>') > 0:
             special_end = s.find('>')+1
             special = s[0:special_end]
-            special_scancodes = SPECIALS.get(special)
-            if special_scancodes:
-                scancodes += special_scancodes + ' '
-
             s = s[special_end:]
+
+            codes = SPECIALS.get(special)
         else:
             key = s[0]
-            key_scancodes = SCANCODES.get(key)
-            if key_scancodes:
-                scancodes += key_scancodes + ' '
-
             s = s[1:]
 
-    return scancodes.strip()
+            codes = SCANCODES.get(key)
+            
+        scancodes += iterable(codes)
+
+    return scancodes
 
--- devops/devops/test_scancodes.py
+++ devops/devops/test_scancodes.py
@@ -5,13 +5,21 @@ class TestScancodes(unittest.TestCase):
 
     def test_abc(self):
         codes = scancodes.from_string('abc')
-        self.assertEqual('1e 9e 30 b0 2e ae', codes)
+        self.assertEqual([0x1e, 0x30, 0x2e], codes)
 
     def test_ABC(self):
         codes = scancodes.from_string('ABC')
-        self.assertEqual('2a 1e aa 9e 2a 30 aa b0 2a 2e aa ae', codes)
+        self.assertEqual([0x2a, 0x1e, 0x2a, 0x30, 0x2a, 0x2e], codes)
 
     def test_specials(self):
         codes = scancodes.from_string('<Esc>a<Up>b')
-        self.assertEqual('01 81 1e 9e 48 c8 30 b0', codes)
+        self.assertEqual([0x01, 0x1e, 0x48, 0x30], codes)
+
+    def test_newlines_are_ignored(self):
+        codes = scancodes.from_string("a\nb")
+        self.assertEqual([0x1e, 0x30], codes)
+
+    def test_wait(self):
+        codes = scancodes.from_string("a<Wait>b")
+        self.assertEqual([0x1e, 'wait', 0x30], codes)
 
