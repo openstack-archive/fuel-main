9f376d6fbe65c479c5164ae95780bceffd285a7a
Vladimir Stepanov <grossho@gmail.com>
2012-06-01 21:05:10 +0400

    change cache-way for installation ipaddr

--- vagrant/cookbooks/devops/recipes/deps.rb
+++ vagrant/cookbooks/devops/recipes/deps.rb
@@ -2,13 +2,15 @@
   'xmlbuilder' => '1.0',
   'PyYAML' => '3.1',
   'lxml' => '2.3.2',
-  'ipaddr' => '2.1.10',
 }.each do |package, version|
   python_pip package do
     version version
     action :install
   end
 
+  cookbook_python_pip 'ipaddr' do
+    version '2.1.10'
+  end
   # httpclient is for node agent
   gem_package "httpclient"
 end
