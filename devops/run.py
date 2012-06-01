ba73e9188a479b037817bc4c999d0eb29c2626ac
Maxim Kulkin <mkulkin@mirantis.com>
2012-06-01 15:53:37 +0400

    [devops] Cleaned up run.py

--- devops/run.py
+++ devops/run.py
@@ -11,12 +11,7 @@ from devops import yaml_config_loader
 INSTALLATION_ISO = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'install.iso'))
 INSTALLATION_ISO_URL = "http://mc0n1-srt.srt.mirantis.net/nailgun-ubuntu-12.04-amd64.last.iso"
 
-if not os.path.exists(INSTALLATION_ISO):
-    print("Downloading installation iso")
-    os.system("wget -O '%s' -c '%s'" % (INSTALLATION_ISO, INSTALLATION_ISO_URL))
-    print("Finished downloading installation iso")
-
-env = yaml_config_loader.load("""
+MASTER_AND_SLAVE_CONFIG = """
 name: 'Sample environment'
 networks:
   - network: internal
@@ -29,24 +24,42 @@ nodes:
     networks: ['external', 'internal']
   - node: slug
     networks: ['internal']
-""" % (INSTALLATION_ISO,))
+""" % (INSTALLATION_ISO,)
 
-gateway = env.node['gateway']
-gateway.vnc = True
 
-print("Creating environment")
+def download_iso():
+    if not os.path.exists(INSTALLATION_ISO):
+        print("Downloading installation iso")
+        os.system("wget -O '%s' -c '%s'" % (INSTALLATION_ISO, INSTALLATION_ISO_URL))
+        print("Finished downloading installation iso")
 
-controller = Controller(Libvirt())
-controller.build_environment(env)
 
-print("Environment created")
+def main():
+    download_iso()
 
-print("Starting node")
-gateway.start()
-print("Waiting node to boot")
-time.sleep(10)
-print("Sending user input")
-gateway.send_keys("""<Esc><Enter>
+    environment = yaml_config_loader.load(MASTER_AND_SLAVE_CONFIG)
+
+    external_network = environment.network['external']
+
+    gateway = environment.node['gateway']
+    gateway.vnc = True
+
+    print("Creating environment")
+
+    controller = Controller(Libvirt())
+    controller.build_environment(environment)
+
+    print("Environment created")
+
+    print("Starting node")
+    gateway.start()
+
+    print("VNC is available on vnc://localhost:%d" % gateway.vnc_port)
+
+    print("Waiting node to boot")
+    time.sleep(10)
+    print("Sending user input")
+    gateway.send_keys("""<Esc><Enter>
 <Wait>
 /install/vmlinuz initrd=/install/initrd.gz
  priority=critical
@@ -58,14 +71,9 @@ gateway.send_keys("""<Esc><Enter>
  netcfg/get_gateway=10.12.0.1
  netcfg/get_nameservers=10.12.0.1
  netcfg/confirm_static=true
- <Enter>
-""")
-print("Finished sending user input")
+ <Enter>""")
+    print("Finished sending user input")
 
-# print("Sleeping 5 seconds")
-# time.sleep(5)
-# 
-# controller.destroy_environment(env)
-# 
-# print("Environment destroyed")
+if __name__ == '__main__':
+    main()
 
