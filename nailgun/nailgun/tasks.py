5349e4d1cde2016a6cd51122c8f357afbac9d835
Nikolay Markov <nmarkov@mirantis.com>
2012-06-13 14:57:53 +0400

    json generating and downloading for deployment

--- nailgun/nailgun/tasks.py
+++ nailgun/nailgun/tasks.py
@@ -3,6 +3,8 @@ import logging
 
 import json
 import paramiko
+import tarfile
+import shutil
 from django.conf import settings
 from celery.task import task
 from celery.task import subtask
@@ -18,6 +20,7 @@ logger = logging.getLogger(__name__)
 def deploy_cluster(cluster_id):
     databag = os.path.join(
         settings.CHEF_CONF_FOLDER,
+        "_".join(["cluster", str(cluster_id)]),
         settings.CHEF_NODES_DATABAG_NAME
     )
 
@@ -27,32 +30,54 @@ def deploy_cluster(cluster_id):
 
     for node in nodes:
         node_json = {}
+        node_solo = {
+            "cluster_id": cluster_id,
+            "run_list": []
+        }
 
         roles_for_node = node.roles.all()
         if not roles_for_node:
             raise Exception("Roles list for node %s is empty" % node.id)
 
+        node_json['cluster_id'] = cluster_id
         for f in node._meta.fields:
-            if f.name == 'cluster':
-                node_json['cluster_id'] = node.cluster_id
-            else:
+            if f.name != 'cluster':
                 node_json[f.name] = getattr(node, f.name)
 
         node_json['roles'] = []
         for role in roles_for_node:
-            node_json['roles'].append({
+            rc = [r.recipe for r in role.recipes.all()]
+            node_solo["run_list"].extend(rc)
+            node_json["roles"].append({
                 "name": role.name,
-                "recipes": [r.recipe for r in role.recipes.all()]
+                "recipes": rc
             })
 
         if not os.path.exists(databag):
-            os.mkdir(databag)
+            os.makedirs(databag)
 
+        # writing to solo
+        with open(
+            os.path.join(
+                settings.CHEF_CONF_FOLDER,
+                "".join([node.id, ".json"])
+            ),
+            "w"
+        ) as entity:
+            entity.write(json.dumps(node_solo))
+
+        # writing to databag
         with open(
             os.path.join(databag, "".join([node.id, ".json"])),
             "w"
         ) as entity:
             entity.write(json.dumps(node_json))
+
+    t = tarfile.open("".join([databag, ".tar.gz"]), "w:gz")
+    t.add(databag, os.path.basename(databag))
+    t.close()
+    # removing databag - it's already packed
+    shutil.rmtree(databag)
 
     job = group(subtask(bootstrap_node, args=(n.id, )) for n in nodes)
     result = job.apply_async()
