456def0267c711f362625d9ffa99de4df7ca469b
Nikolay Markov <nmarkov@mirantis.com>
2012-06-21 16:19:23 +0400

    integration test remastered

---
+++ test/integration/helpers.py
@@ -0,0 +1,135 @@
+import os
+import urllib2
+import logging
+from unittest import TestCase
+import paramiko
+
+logging.basicConfig(format=':%(lineno)d: %(asctime)s %(message)s', level=logging.DEBUG)
+
+class HTTPClient(object):
+    def __init__(self):
+        self.opener = urllib2.build_opener(urllib2.HTTPHandler)
+
+    def get(self, url):
+        req = urllib2.Request(url)
+        return self.opener.open(req).read()
+
+    def post(self, url, data="{}", content_type="application/json"):
+        req = urllib2.Request(url, data=data)
+        req.add_header('Content-Type', content_type)
+        return self.opener.open(req).read()
+
+    def put(self, url, data="{}", content_type="application/json"):
+        req = urllib2.Request(url, data=data)
+        req.add_header('Content-Type', content_type)
+        req.get_method = lambda: 'PUT'
+        return self.opener.open(req).read()
+
+
+class HTTPTestCase(TestCase):
+    """
+    Test case with HTTP
+    """
+    def __init__(self, *args, **kwargs):
+        super(HTTPTestCase, self).__init__(*args, **kwargs)
+        self.client = HTTPClient()
+
+class SSHTestCase(TestCase):
+    """
+    Test case with SSH
+    """
+    def __init__(self, *args, **kwargs):
+        super(SSHTestCase, self).__init__(*args, **kwargs)
+        self.channel = None
+        self.sudo = False
+
+    def connect_ssh(self, host, username, password):
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.host = host
+        self.username = username
+        self.password = password
+        self.ssh_client.connect(host, username=username, password=password)
+        self.sftp_client = self.ssh_client.open_sftp()
+
+    def open_channel(self):
+        if not self.channel:
+            self.channel = self.ssh_client.invoke_shell()
+
+    def aquire_sudo(self):
+        if not self.channel:
+            self.open_channel()
+        if not self.sudo:
+            logging.info("Aquiring sudo")
+            self.channel.send("sudo -s\n")
+            self._recv_until("%s: " % self.username)
+            self.channel.send("%s\n" % self.password)
+            self._recv_until("# ")
+            self.sudo = True
+
+    def exec_cmd(self, command, sudo=False):
+        logging.info("Executing command: '%s'" % command)
+        if not self.channel:
+            self.open_channel()
+        if sudo and not self.sudo:
+            self.aquire_sudo()
+        self.channel.send("%s\n" % command)
+        if sudo or self.sudo:
+            return self._recv_until("# ")
+        else:
+            return self._recv_until("$ ")
+
+    def mkdir(self, path, sudo=False):
+        logging.info("Creating directory: %s" % path)
+        if not sudo:
+            self.sftp_client.mkdir(path)
+        else:
+            self.open_channel()
+            self.aquire_sudo()
+            self.channel.send("mkdir %s\n" % path)
+            self._recv_until("# ")
+
+
+    def rmdir(self, path, sudo=False):
+        logging.info("Removing directory: %s" % path)
+        if not sudo:
+            self.ssh_client.exec_command("rm -rf %s" % path)
+        else:
+            self.open_channel()
+            self.aquire_sudo()
+            self.channel.send("rm -rf %s\n" % path)
+            self._recv_until("# ")          
+
+    def scp(self, frm, to):
+        logging.info("Copying file: %s -> %s" % (frm, to))
+        self.sftp_client.put(frm, to)
+
+    def scp_d(self, frm, to):
+        logging.info("Copying directory: %s -> %s" % (frm, to))
+        remote_root = os.path.join(
+            to,
+            os.path.basename(frm)
+        )
+        for root, dirs, fls in os.walk(frm):
+            rel = os.path.relpath(root, frm)
+            if rel == ".":
+                curdir = remote_root
+            else:
+                curdir = os.path.join(remote_root, rel)
+            self.sftp_client.mkdir(curdir)
+            for fl in fls:
+                self.sftp_client.put(
+                    os.path.join(root, fl),
+                    os.path.join(curdir, fl)
+                )
+
+    def disconnect(self):
+        self.sftp_client.close()
+        self.ssh_client.close()
+
+    def _recv_until(self, data):
+        buff = ""
+        while not buff.endswith(data):
+            resp = self.channel.recv(9999)
+            buff += resp
+        return buff
--- test/integration/test_node.py
+++ test/integration/test_node.py
@@ -10,6 +10,7 @@ from subprocess import Popen, PIPE
 
 from . import ci
 from devops.helpers import wait, tcp_ping, http
+from integration.helpers import HTTPTestCase, SSHTestCase
 
 import paramiko
 
@@ -21,106 +22,52 @@ COOKBOOKS_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "cookbooks"
 SAMPLE_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "scripts", "ci")
 SAMPLE_REMOTE_PATH = "/home/ubuntu"
 
+class StillPendingException(Exception):
+    pass
 
-def read_until(channel, end, log=None):
-    buff = ""
-    while not buff.endswith(end):
-        resp = channel.recv(9999)
-        buff += resp
-    if log:
-        log.write(buff)
-    return buff
-
-# TODO: create helper
-def copy_folder(sftp, local_path, remote_path):
-    remote_root = os.path.join(
-        remote_path,
-        os.path.basename(local_path)
-    )
-    for root, dirs, fls in os.walk(local_path):
-        rel = os.path.relpath(root, local_path)
-        if rel == ".":
-            curdir = remote_root
-        else:
-            curdir = os.path.join(remote_root, rel)
-        sftp.mkdir(curdir)
-        for fl in fls:
-            sftp.put(
-                os.path.join(root, fl),
-                os.path.join(curdir, fl)
-            )
-
-def check_tasks(task):
-    if task['status'] != 'SUCCESS':
-        raise Exception("Task %s failed!\n %s" % 
-                (task['task_id'], str(task)),
-        )
-    if 'subtasks' in task and task['subtasks']:
-        for subtask in task['subtasks']:
-            check_tasks(subtask)
-
-# TODO: create basic class for implementing HTTP client in tests
-class TestNode(TestCase):
+class TestNode(HTTPTestCase, SSHTestCase):
     def test_node(self):
-        # TODO: all in one ssh session
-        host = str(ci.environment.node['admin'].ip_address)
-        opener = urllib2.build_opener(urllib2.HTTPHandler)
-
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(host, username="ubuntu", password="r00tme")
-
         cookbook_remote_path = os.path.join(SAMPLE_REMOTE_PATH, "sample-cook")
         release_remote_path = os.path.join(SAMPLE_REMOTE_PATH, "sample-release.json")
 
-        # removing old cookbooks
-        (sin, sout, serr) = ssh.exec_command(
-            "rm -rf %s" % cookbook_remote_path
-        )
-        (sin, sout, serr) = ssh.exec_command(
-            "rm -rf %s" % os.path.join(SAMPLE_REMOTE_PATH, "cookbooks")
-        )
+        host = str(ci.environment.node['admin'].ip_address)
+        self.connect_ssh(host, "ubuntu", "r00tme")
 
-        (sin, sout, serr) = ssh.exec_command(
-            "rm -rf %s" % os.path.join(SAMPLE_REMOTE_PATH, "solo")
-        )
-
-        sftp = ssh.open_sftp()
-        sftp.put(
+        self.rmdir(cookbook_remote_path)
+        self.rmdir(os.path.join(SAMPLE_REMOTE_PATH, "cookbooks"))
+        self.rmdir(os.path.join(SAMPLE_REMOTE_PATH, "solo"))
+        
+        self.scp(
             os.path.join(SAMPLE_PATH, "sample-release.json"),
             release_remote_path
         )
 
-        sftp.mkdir(
-            os.path.join(SAMPLE_REMOTE_PATH, "solo")
-        )
-        sftp.mkdir(
-            os.path.join(SAMPLE_REMOTE_PATH, "solo/config")
-        )
+        self.mkdir(os.path.join(SAMPLE_REMOTE_PATH, "solo"))
+        self.mkdir(os.path.join(SAMPLE_REMOTE_PATH, "solo/config"))        
 
-        sftp.put(
+        self.scp(
             DEPLOY_PATH,
             os.path.join(SAMPLE_REMOTE_PATH, "deploy")
         )
-        sftp.put(
+        self.scp(
             os.path.join(SOLO_PATH, "solo.json"),
             os.path.join(SAMPLE_REMOTE_PATH, "solo", "config", "solo.json")
         )
-        sftp.put(
+        self.scp(
             os.path.join(SOLO_PATH, "solo.rb"),
             os.path.join(SAMPLE_REMOTE_PATH, "solo", "config", "solo.rb")
         )
 
-        copy_folder(sftp,
+        self.scp_d(
             os.path.join(SAMPLE_PATH, "sample-cook"),
             SAMPLE_REMOTE_PATH
         )
-        copy_folder(sftp,
+        self.scp_d(
             COOKBOOKS_PATH,
             SAMPLE_REMOTE_PATH
         )
 
-        sftp.close()
+        self.aquire_sudo()
 
         commands = [
             "rm -rf /opt/nailgun/nailgun.sqlite",
@@ -142,65 +89,70 @@ class TestNode(TestCase):
             ),
         ]
 
-        chan = ssh.invoke_shell()
-
-        chan.send("sudo -s\n")
-        read_until(chan, "ubuntu: ")
-        chan.send("r00tme\n")
-        read_until(chan, "# ")
-
         for cmd in commands:
-            print "~$ %s" % cmd
-            chan.send(cmd+"\n")
-            #with open("log.html", "a+") as l:
-            #    read_until(chan, "# ", log=l)
-            read_until(chan, "# ")
+            self.exec_cmd(cmd)
 
-        req = urllib2.Request("http://%s:8000/api/clusters" % host,
-                data='{ "name": "MyOwnPrivateCluster", "release": 1 }')
-        req.add_header('Content-Type', 'application/json')
-        resp = json.loads(opener.open(req).read())
+        cluster = json.loads(self.client.post(
+            "http://%s:8000/api/clusters" % host,
+            data='{ "name": "MyOwnPrivateCluster", "release": 1 }'
+        ))
 
-        req = urllib2.Request("http://%s:8000/api/nodes" % host)
-        nodes = json.loads(opener.open(req).read())
+        nodes = json.loads(self.client.get(
+            "http://%s:8000/api/nodes" % host
+        ))
         if len(nodes) == 0:
             raise ValueError("Nodes list is empty")
         node_id = nodes[0]['id']
 
-        req = urllib2.Request("http://%s:8000/api/clusters/1" % host,
-                data='{ "nodes": ["%s"] }' % node_id)
-        req.add_header('Content-Type', 'application/json')
-        req.get_method = lambda: 'PUT'
-        resp = json.loads(opener.open(req).read())
+        resp = json.loads(self.client.put(
+            "http://%s:8000/api/clusters/1" % host,
+            data='{ "nodes": ["%s"] }' % node_id
+        ))
 
-        req = urllib2.Request("http://%s:8000/api/clusters/1" % host)
-        resp = json.loads(opener.open(req).read())
-        if len(resp["nodes"]) == 0:
+        cluster = json.loads(self.client.get(
+            "http://%s:8000/api/clusters/1" % host
+        ))
+        if len(cluster["nodes"]) == 0:
             raise ValueError("Failed to add node into cluster")
 
-        req = urllib2.Request("http://%s:8000/api/nodes/%s" % (host, node_id),
+        resp = json.loads(self.client.put(
+            "http://%s:8000/api/nodes/%s" % (host, node_id),
             data='{ "roles": [1, 2] }'
-        )
-        req.add_header('Content-Type', 'application/json')
-        req.get_method = lambda: 'PUT'
-        resp = json.loads(opener.open(req).read())
+        ))
         if len(resp["roles"]) == 0:
             raise ValueError("Failed to assign roles to node")
 
-        req = urllib2.Request("http://%s:8000/api/clusters/1/chef-config/" % host,
-            data="{}"
-        )
-        req.add_header('Content-Type', 'application/json')
-        task_id = json.loads(opener.open(req).read())['task_id']
+        task = json.loads(self.client.post(
+            "http://%s:8000/api/clusters/1/chef-config/" % host
+        ))
+        task_id = task['task_id']
+
         time.sleep(2)
 
-        req = urllib2.Request("http://%s:8000/api/tasks/%s/" % (host, task_id))
-        resp = json.loads(opener.open(req).read())
-        check_tasks(resp)
+        task = json.loads(self.client.get(
+            "http://%s:8000/api/tasks/%s/" % (host, task_id)
+        ))
+        while True:
+            try:
+                self.check_tasks(task)
+                break
+            except StillPendingException:
+                pass
 
-        chan.send("test -f /tmp/chef_success && echo 'SUCCESS'\n")
-        ret = read_until(chan, "# ")
+        ret = self.exec_cmd("test -f /tmp/chef_success && echo 'SUCCESS'")
         if not "SUCCESS" in ret:
             raise Exception("Recipe failed to execute")
 
-        ssh.close()
+        self.disconnect()
+
+    def check_tasks(self, task):
+        if task['status'] != 'SUCCESS':
+            if task['status'] == 'PENDING':
+                raise StillPendingException("Task %s is still pending")
+            raise Exception(
+                "Task %s failed!\n %s" %
+                (task['task_id'], str(task)),
+            )
+        if 'subtasks' in task and task['subtasks']:
+            for subtask in task['subtasks']:
+                self.check_tasks(subtask)
