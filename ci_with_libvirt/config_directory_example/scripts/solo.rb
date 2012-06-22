95f158b9ad775daeebbb749f329b6ea157b90c4f
Vladimir Kozhukalov <kozhukalov@gmail.com>
2012-06-22 21:03:57 +0400

    deleted ci_with_libvirt

--- ci_with_libvirt/config_directory_example/scripts/solo.rb
+++
@@ -1,5 +0,0 @@
-log_location "/var/log/chef/solo.log"
-file_cache_path "/tmp/chef"
-cookbook_path File.expand_path(File.join(File.dirname(__FILE__), '../cookbooks'))
-log_level :debug
-verbose_logging true
