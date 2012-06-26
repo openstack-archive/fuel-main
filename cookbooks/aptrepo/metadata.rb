7e2f5a6ebc8bf10b8ea50667acfa64399580d048
Vladimir Kozhukalov <kozhukalov@gmail.com>
2012-06-26 18:10:57 +0400

    removed aptrepo cookbook (replaced with repo)

--- cookbooks/aptrepo/metadata.rb
+++
@@ -1,7 +0,0 @@
-maintainer        "Mirantis, Inc."
-maintainer_email  "product@mirantis.com"
-description       "Installs and configures cobbler"
-version           "0.0.1"
-recipe            "aptrepo::default", "Creates script to update ubuntu repo"
-supports          "ubuntu"
-
