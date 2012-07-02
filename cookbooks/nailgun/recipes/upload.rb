70a709215ca95617852a9d99e56328a851a2ed09
Mike Scherbakov <mihgen@gmail.com>
2012-07-02 16:35:34 +0400

    Added cooks/releases auto-upload

---
+++ cookbooks/nailgun/recipes/upload.rb
@@ -0,0 +1,17 @@
+cooks = Dir.glob("#{node[:nailgun][:root]}/os-cookbooks/cookbooks/*").select {|f| File.directory? f}
+cooks.each do |cook|
+  bash "Bash script for cookbook installation #{cook}" do
+    code <<-EOH
+    #{node[:nailgun][:root]}/bin/install_cookbook "#{cook}"
+    EOH
+  end
+end
+
+releases = Dir.glob("#{node[:nailgun][:root]}/os-cookbooks/releases/*.json")
+releases.each do |rls|
+  bash "Bash script for release creation #{rls}" do
+    code <<-EOH
+    #{node[:nailgun][:root]}/bin/create_release "#{rls}"
+    EOH
+  end
+end
