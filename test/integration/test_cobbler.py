33f482060ec82b45aefbb6465af35dce171b2515
Vladimir Kozhukalov <kozhukalov@gmail.com>
2012-07-03 17:21:33 +0400

    fixed test_cobbler

--- test/integration/test_cobbler.py
+++ test/integration/test_cobbler.py
@@ -5,20 +5,11 @@ from . import ci
 
 class TestCobbler:
     def setUp(self):
-        pass
+        self.ip = ci.environment.node['admin'].ip_address
 
-    def test_cobbler_xmlrpc(self):
-        admin_node = ci.environment.node['admin']
-
-        wait(lambda: http(host=admin_node.ip_address, url='/cobbler_api', waited_code=501), timeout=30)
-
-        server = xmlrpclib.Server('http://%s/cobbler_api' % admin_node.ip_address)
-        token = server.login('cobbler', 'cobbler')
-        found = server.find_system({'name':'default'}, token)
-        assert found[0]['name'] == 'default'
- 
     def test_cobbler_alive(self):
-        assert True
+        wait(lambda: http(host=admin_node.ip_address, url='/cobbler_api', waited_code=501), timeout=60)
+        server = xmlrpclib.Server('http://%s/cobbler_api' % self.ip)
+        token = server.login('cobbler', 'cobbler')
+        assert server.ping() == True
 
-    # def test_cobbler_alive(self):
-    #     wait(lambda: ssh)
