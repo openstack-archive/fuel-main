de0e621ad3b5324733afd9f821785f2ec105a6f8
Maxim Kulkin <mkulkin@mirantis.com>
2012-07-04 16:55:28 +0400

    Updated rabbitmq cookbook: added user and vhost resources

--- cookbooks/rabbitmq/attributes/default.rb
+++ cookbooks/rabbitmq/attributes/default.rb
@@ -7,6 +7,6 @@ default[:rabbitmq][:erl_args]  = "+K true +A 30 \
 default[:rabbitmq][:start_args] = ""
 default[:rabbitmq][:logdir] = "/var/log/rabbitmq"
 default[:rabbitmq][:mnesiadir] = "/var/lib/rabbitmq/mnesia"
-default[:rabbitmq][:cluster] = "no"
+
 default[:rabbitmq][:cluster_config] = "/etc/rabbitmq/rabbitmq_cluster.config"
 default[:rabbitmq][:cluster_disk_nodes] = []
