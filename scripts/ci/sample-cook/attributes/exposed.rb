e27620a251e2342a3b45e1da9cd013d2c1234654
Nikolay Markov <nmarkov@mirantis.com>
2012-07-06 12:31:04 +0400

    exposed password not empty

--- scripts/ci/sample-cook/attributes/exposed.rb
+++ scripts/ci/sample-cook/attributes/exposed.rb
@@ -1,2 +1,2 @@
 default[:head_key][:nested_key] = "value"
-default[:service][:password] = ""  # Let's use this for password generation check
+default[:service][:password] = "password"  # Let's use this for password generation check
