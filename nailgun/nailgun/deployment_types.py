e5f841010c35f2151260c7a67f6e0b883d65dead
Vitaly Kramskikh <vkramskikh@gmail.com>
2012-07-20 17:09:05 +0400

    Forgot to add a file

---
+++ nailgun/nailgun/deployment_types.py
@@ -0,0 +1,24 @@
+deployment_types = {}
+
+
+class TypeRegistrator(type):
+    def __init__(cls, name, bases, dct):
+        super(TypeRegistrator, cls).__init__(name, bases, dct)
+        if hasattr(cls, 'id'):
+            deployment_types[cls.id] = cls
+
+
+class BaseDeploymentType(object):
+    __metaclass__ = TypeRegistrator
+
+
+class SimpleDeploymentType(BaseDeploymentType):
+    id = 'simple'
+    name = 'Simple Deployment'
+    description = 'Simple OpenStack deployment'
+
+    @classmethod
+    def assign_roles(cluster):
+        # TODO: replace logic
+        for node in cluster.nodes:
+            node.roles.clear()
