5bdd03a768bf82c7f25f97f015e92603b11fc311
Mike Scherbakov <mihgen@gmail.com>
2012-07-30 13:21:41 +0400

    tcp_ping mocked in test_tasks

--- nailgun/nailgun/tests/test_tasks.py
+++ nailgun/nailgun/tests/test_tasks.py
@@ -36,12 +36,14 @@ class TestTasks(TestCase):
         self.role.delete()
         self.recipe.delete()
 
+    @mock.patch('nailgun.tasks.tcp_ping')
     @mock.patch('nailgun.tasks.SshConnect')
     @mock.patch('nailgun.tasks._provision_node')
-    def test_bootstrap_node(self, pn_mock, ssh_mock):
+    def test_bootstrap_node(self, pn_mock, ssh_mock, tp_mock):
         ssh = ssh_mock.return_value
         ssh.run.return_value = True
         pn_mock.return_value = True
+        tp_mock.return_value = True
 
         self.assertEquals(self.node.status, "ready")
         res = tasks.bootstrap_node.delay(self.node.id)
@@ -49,10 +51,13 @@ class TestTasks(TestCase):
         node = models.Node.objects.get(id=self.node.id)
         self.assertEquals(node.status, "ready")
 
+    @mock.patch('nailgun.tasks.tcp_ping')
     @mock.patch('nailgun.tasks.SshConnect')
-    def test_bootstrap_calls_provision_and_ssh(self, ssh_mock):
+    def test_bootstrap_calls_provision_and_ssh(self, ssh_mock, tp_mock):
         ssh = ssh_mock.return_value
         ssh.run = mock.MagicMock(return_value=True)
+        tp_mock.return_value = True
+
         tasks._provision_node = mock.MagicMock(return_value=None)
         tasks.bootstrap_node(self.node.id)
         self.assertEquals(tasks._provision_node.call_args_list,
@@ -60,30 +65,37 @@ class TestTasks(TestCase):
         self.assertEquals(ssh.run.call_args_list,
                 [call('/opt/nailgun/bin/deploy')])
 
+    @mock.patch('nailgun.tasks.tcp_ping')
     @mock.patch('nailgun.tasks.SshConnect')
-    def test_bootstrap_does_not_call_provision(self, ssh_mock):
+    def test_bootstrap_does_not_call_provision(self, ssh_mock, tp_mock):
         ssh = ssh_mock.return_value
         ssh.run.return_value = True
+        tp_mock.return_value = True
         tasks._provision_node = mock.MagicMock(return_value=None)
+
         tasks.bootstrap_node(self.node.id, installed=True)
         self.assertEquals(tasks._provision_node.call_args_list, [])
 
+    @mock.patch('nailgun.tasks.tcp_ping')
     @mock.patch('nailgun.tasks.SshConnect')
     @mock.patch('nailgun.tasks._provision_node')
-    def test_bootstrap_raises_deploy_error(self, pn_mock, ssh_mock):
+    def test_bootstrap_raises_deploy_error(self, pn_mock, ssh_mock, tp_mock):
         ssh = ssh_mock.return_value
         ssh.run.return_value = False
         pn_mock.return_value = True
+        tp_mock.return_value = True
 
         with self.assertRaises(exceptions.DeployError):
             tasks.bootstrap_node(self.node.id)
 
+    @mock.patch('nailgun.tasks.tcp_ping')
     @mock.patch('nailgun.tasks.SshConnect')
     @mock.patch('nailgun.tasks._provision_node')
-    def test_bootstrap_puts_error_in_task(self, pn_mock, ssh_mock):
+    def test_bootstrap_puts_error_in_task(self, pn_mock, ssh_mock, tp_mock):
         ssh = ssh_mock.return_value
         ssh.run.return_value = False
         pn_mock.return_value = True
+        tp_mock.return_value = True
 
         self.assertEquals(self.node.status, "ready")
         res = tasks.bootstrap_node.delay(self.node.id)
