df4a759d8d98d72afc299b31815f01678df8959d
Andrey Danin <adanin@mirantis.com>
2012-09-14 18:51:07 +0400

    [bootstrap] Rewrite net_probe.py and add net_probe MC agent.

--- bootstrap/netanalyse/net_probe.agent.rb
+++ bootstrap/netanalyse/net_probe.agent.rb
@@ -1,39 +1,20 @@
 module MCollective
   module Agent
     require "json"
-    # An agent to manage the Puppet Daemon
-    #
-    # Configuration Options:
-    #    puppetd.splaytime - Number of seconds within which to splay; no splay
-    #                        by default
-    #    puppetd.statefile - Where to find the state.yaml file; defaults to
-    #                        /var/lib/puppet/state/state.yaml
-    #    puppetd.lockfile  - Where to find the lock file; defaults to
-    #                        /var/lib/puppet/state/puppetdlock
-    #    puppetd.puppetd   - Where to find the puppet agent binary; defaults to
-    #                        /usr/sbin/puppetd
-    #    puppetd.summary   - Where to find the summary file written by Puppet
-    #                        2.6.8 and newer; defaults to
-    #                        /var/lib/puppet/state/last_run_summary.yaml
-    #    puppetd.pidfile   - Where to find puppet agent's pid file; defaults to
-    #                        /var/run/puppet/agent.pid
-    class NetProbe<RPC::Agent
+    class Net_probe<RPC::Agent
       metadata    :name        => "Network Probe Agent",
                   :description => "Check network connectivity between nodes.",
                   :author      => "Andrey Danin",
                   :license     => "MIT",
                   :version     => "0.1",
+                  :url         => "http://mirantis.com",
                   :timeout     => 60
 
+      uid = "5e33c330-fce0-11e1-a21f-0800200c9a66"
+
       action "start_frame_listeners" do
-        input :iflist,
-               :prompt      => "List of interfaces",
-               :description => "List of interface names to start listen frames on it",
-               :type        => :string,
-               :validation  => '^([a-zA-Z]+\d*])(,[a-zA-Z]+\d*])*$',
-               :optional    => false
         validate :iflist, String
-        status, msg = start_frame_listeners(iflist.split(","))
+        status, msg = start_frame_listeners(request[:iflist].split(","))
         reply[:status] = status
         if not status
           reply[:msg] = msg
@@ -41,24 +22,27 @@ module MCollective
       end
 
       action "send_probing_frames" do
-         input :config,
-               :prompt      => "",
-               :description => "",
-               :type        => :string,
-               :optional    => false
-        validate :iflist, String
-        send_probing_frames(:config)
+        validate :interfaces, String
+        config_string = "{\"action\": \"generate\", \"uid\": \"#{uid}\", 
+ \"interfaces\": #{request[:interfaces]}}"
+        send_probing_frames(config_string)
       end
 
       action "get_probing_info" do
-        status, msg = get_probing_info
+        reply[:neigbours] = get_probing_frames
+        reply[:uid] = uid
+      end
+
+      action "echo" do
+        reply[:msg] = request[:msg]
+        reply[:uid] = uid
       end
 
       private
       def start_frame_listeners(iflist)
         begin
           iflist.each do |iface|
-            cmd = "sudo tcpdump -i #{iface} -Z adanin -w /var/tmp/dump-#{iface}.pcap udp port"
+            cmd = "net_probe.py -l #{iface}"
             pid = fork { system(cmd) }
             Process.detach(pid)
           end
@@ -69,12 +53,16 @@ module MCollective
       end
 
       def stop_frame_listeners()
-        system("killall tcpdump")
+        piddir = "/var/run/net_probe"
+        pidfiles = Dir.glob(File.join(piddir, '*'))
+        pidfiles.each do |f|
+          run("kill -INT #{File.basename(f)}")
+        end
       end
 
       def send_probing_frames(config_string)
         config = JSON.load(config_string)
-        cmd = "sudo python /var/tmp/net_probe.py"
+        cmd = "net_probe.py -"
         out = ""
         err = ""
         status = run(cmd, :stdin => JSON.dump(config), :stdout => out, :stderr => err)
@@ -86,15 +74,20 @@ module MCollective
         end
       end
 
-      def get_probing_info()
+      def get_probing_frames()
         stop_frame_listeners
-        file_list = "/var/tmp/dump-*.pcap"
-        out = ""
-        err = ""
-        file_list.each do |file|
-          cmd = "tcpdump -f #{file}"
-          status = run(cmd, :stdout => out, :stderr => err)
+        sleep(1)
+        neigbours = Hash.new
+        pattern = "/var/tmp/net-probe-dump-*"
+        Dir.glob(pattern).each do |file|
+          data = ""
+          open(file) do |f|
+            data += f.gets
+          end
+          p = JSON.load(data)
+          neigbours.merge!(p)
         end
+        return neigbours
       end
     end
   end
--- bootstrap/netanalyse/net_probe.py
+++ bootstrap/netanalyse/net_probe.py
@@ -6,6 +6,7 @@
 #
 import os
 import sys
+import signal
 import json
 
 from optparse import OptionParser
@@ -13,8 +14,46 @@ from scapy.config import *
 conf.logLevel = 40
 from scapy.all import *
 
+usage = """This is a packet generator and analyser.
+
+Run: net_probe -c <config_file>     - use config file
+     net_probe -                        - read config from stdin
+
+     net_probe -i <iface_name> -v <vlan_list> -u <UID>
+                                        - generate frames
+
+     net_probe -l <iface_name> [-d <dump_file>]
+                                        - collect frames from interface
+
+Default <dump_file> is "/var/tmp/net-probe-dump-" plus <iface_name>.
+To stop listening frames send SIGINT to process.
+
+Example run to capture frames:
+net_probe -l eth0 &
+
+Capture frames config file example is:
+{"action": "listen", "interface": "eth0", 
+ "dump_file": "/var/tmp/net-probe-dump-eth0"}
+
+Simple frame generation config file example is:
+{"action": "generate", "uid": "aaa-bb-cccccc", 
+ "interfaces": { "eth0": "1-4094"}}
+
+Full frame generation config file example is:
+{   "action": "generate",
+    "uid": "aaa-bb-cccccc", "cookie": "Some cookie",
+    "src_mac": "11:22:33:44:55:66",
+    "src": "10.0.0.1", "dst": "10.255.255.255",
+    "sport": 4056, "dport": 4057,
+    "interfaces": { 
+        "eth0": "10, 15, 20, 201-210, 301-310, 1000-2000",
+        "eth1": "1-4094"
+    }
+}
+"""
+
 def error(msg):
-    sys.stderr().write(msg)
+    sys.stderr.write(msg)
 
 def parse_vlan_list(vlan_string):
     validate = lambda x: (x > 0) and (x < 4095)
@@ -41,61 +80,44 @@ def parse_vlan_list(vlan_string):
             exit(1)
     return vlan_list
 
-def print_packet(packet):
-    rvlan = packet[Dot1Q].vlan
-    rmsg = str(packet[UDP].payload)[len(cookie):]
-    return "%d %s" % (rvlan, rmsg)
-
-def listen_probe_frames(iface):
+def get_probe_frames(iface):
     fltr = lambda r: UDP in r and str(r[UDP].payload).startswith(cookie)
-    sniff(iface=iface, lfilter=fltr, prn=print_packet)
-
-def send_probe_frame(iface, vlan, data, src_mac="00:00:00:00:00:00",
-                     src="0.0.0.0", dst="0.0.0.0", sport=31337, dport=31337):
-    p = Ether(src=src_mac)/Dot1Q(vlan=vlan)/IP(src=src, dst=dst)
-    p = p/UDP(sport=sport, dport=dport)/data
-    sendp(p, iface=iface)
-
-def analyse_dump(file_list):
+    packets = sniff(iface=iface, lfilter=fltr)
     neigbors = {}
-    for iface, name in file_list:
-        fo = open(name, 'r')
-        neigbors[iface] = {}
-        neigbor_dict = neigbors[iface]
-        for line in fo.readline():
-            vlan, riface, uid = line.split(' ', 2)
-            if not neigbor_dict.has_key(vlan):
-                neigbor_dict[vlan] = {}
-            if not neigbor_dict[vlan].has_key(uid):
-                neigbor_dict[vlan][uid] = [riface]
-            else:
-                iface_list = neigbor_dict[vlan][uid]
-                if riface not in iface_list:
-                    iface_list.append(riface)
-        fo.close()
+    neigbors[iface] = {}
+    neigbor_dict = neigbors[iface]
+    for p in packets:
+        if Dot1Q in p:
+            vlan = p[Dot1Q].vlan
+        else:
+            vlan = 0
+        rmsg = str(p[UDP].payload)[len(cookie):]
+        riface, uid = rmsg.split(' ', 1)
+        if not neigbor_dict.has_key(vlan):
+            neigbor_dict[vlan] = {}
+        if not neigbor_dict[vlan].has_key(uid):
+            neigbor_dict[vlan][uid] = [riface]
+        else:
+            iface_list = neigbor_dict[vlan][uid]
+            if riface not in iface_list:
+                iface_list.append(riface)
     return neigbors
 
-def analyse_pcap_dump(filename):
-    dump = rdpcap(filename)
-    dump = dump.filter(lambda r: UDP in r and str(r[UDP].payload).startswith(cookie))
-    neigbor_dict = {}
-    for packet in dump:
-        if Dot1Q in packet:
-            rvlan = packet[Dot1Q].vlan
-        else:
-            rvlan = 0
-        rmsg = str(packet[UDP].payload)[len(cookie):]
-        riface, ruid = rmsg.split(' ', 1)
-        if not neigbor_dict.has_key(rvlan):
-            neigbor_dict[rvlan] = {}
-        if not neigbor_dict[rvlan].has_key(ruid):
-            neigbor_dict[rvlan][ruid] = (riface,)
-        else:
-            mac_list = neigbor_dict[rvlan][ruid]
-            if rmac not in mac_list:
-                neigbor_dict[rvlan][ruid] = mac_list + (rmac,)
-    return neigbor_dict
+def send_probe_frame(**props):
+    p = Ether(src=props['src_mac'], dst="ff:ff:ff:ff:ff:ff")
+    p = p/Dot1Q(vlan=props['vlan'])/IP(src=props['src'], dst=props['dst'])
+    p = p/UDP(sport=props['sport'], dport=props['dport'])/props['data']
+    sendp(p, iface=props['iface'])
 
+def addpid(piddir):
+    pid = os.getpid()
+    if not os.path.exists(piddir):
+        os.mkdir(piddir)
+    pidfile = os.path.join(piddir, str(pid))
+    fo = open(pidfile, 'w')
+    fo.write('')
+    fo.close()
+    return pidfile
 
 # -------------- main ---------------
 
@@ -109,8 +131,12 @@ parser.add_option("-l", dest="listen_iface")
 opts, args = parser.parse_args()
 
 cookie = "Nailgun:"
-action = ""
-piddir = '/var/run'
+piddir = '/var/run/net_probe'
+dumpname_prefix = "/var/tmp/net-probe-dump-"
+conf = {'src_mac': None,
+         'src': '1.0.0.0', 'dst': '1.0.0.0',
+         'sport': 31337, 'dport': 31337,
+         'cookie': cookie}
 
 if opts.config_file or '-' in args:    
     try:
@@ -118,103 +144,50 @@ if opts.config_file or '-' in args:
             fo = sys.stdin
         else:
             fo = open(opts.config_file, 'r')
-        conf = json.load(fo)
+        config = json.load(fo)
     except IOError:
-        error("Error: can not read config file %s\n" % conf.config_file)
+        error("Error: can not read config file %s\n" % opts.config_file)
         exit(1)
     except ValueError as e:
         error("Error: can not parse config file: %s\n" % e.message)
         exit(1)
-
-    if conf.has_key('uid'):
-        uid = conf['uid']
-    if conf.has_key('cookie'):
-        cookie = conf['cookie']
-    if conf.has_key('action'):
-        action = conf['action']
-
-    prop_list = ('src_mac', 'src', 'dst', 'sport', 'dport')
-    props = {'src_mac': '00:00:00:00:00:00',
-             'src': '0.0.0.0', 'dst': '0.0.0.0',
-             'sport': 31337, 'dport': 31337}
-    for i in prop_list:
-        if conf.has_key(i):
-            props[i] = conf[i]
-    if conf.has_key('interfaces'):
-        interfaces = conf['interfaces']
-    else:
-        error("Error: specify 'interfaces' section in config file.")
-        exit(1)
+    for key in config.keys():
+        conf[key] = config[key]
 
 if opts.uid:
-    uid = opts.uid
-if not uid:
-    error("Error: need to specify uid.\n")
-    exit(1)
+    conf['uid'] = opts.uid
 
 if opts.ifname and opts.vlan_list:
-    action = 'generate'
-    vlist = parse_vlan_list(opts.vlan_list)
-    interfaces[opts.ifname] = vlist
-    interfaces = {}
-
-if opts.dump_file and opts.ifname:
-    action = 'analyse'
-    interfaces = {}
-    interfaces[opts.ifname] = opts.dump_file
+    conf['action'] = 'generate'
+    conf['interfaces'] = {}
+    conf['interfaces'][opts.ifname] = opts.vlan_list
 
 if opts.listen_iface:
-    action = 'listen'
-    listen = opts.listen_iface
+    conf['action'] = 'listen'
+    conf['interface'] = opts.listen_iface
+    if opts.dump_file:
+        conf['dump_file'] = opts.dump_file
+    else:
+        conf['dump_file'] = "%s%s" % (dumpname_prefix, conf['interface'])
 
-if action == 'generate':
-    for iface, vlan_list in interfaces.items():
-        props['iface'] = iface
-        props['data'] = str(''.join((cookie, iface, ' ', uid)))
-        props['vlan'] = parse_vlan_list(vlan_list)
-        send_probe_frame(**props)
-elif action == 'analyse':
-    neigbors = analyse_dump(interfaces)
-    print json.dumps({'uid':uid, 'neigbors':neigbors})
-elif action = 'listen':
-    pid = os.getpid()
-    filename = 'net_probe.pid'
-    pidfile = '/'.join((piddir, filename))
-    fo = open(pidfile, 'a')
-    fo.write('%d\n' % pid)
+if conf['action'] == 'generate':
+    if not conf.has_key('interfaces'):
+        error("Error: specify at least one 'interface-vlans' pair.")
+        exit(1)
+    for iface, vlan_list in conf['interfaces'].items():
+        conf['iface'] = iface
+        conf['data'] = str(''.join((conf['cookie'], iface, ' ', conf['uid'])))
+        conf['vlan'] = parse_vlan_list(vlan_list)
+        send_probe_frame(**conf)
+elif conf['action'] == 'listen':
+    pidfile = addpid(piddir)
+    neigbors = get_probe_frames(conf['interface'])
+    fo = open(conf['dump_file'], 'w')
+    fo.write(json.dumps(neigbors))
     fo.close()
-    listen_probe_frames(listen)
+    os.unlink(pidfile)
 else:
-    print """This is a packet generator and analyser.
-Run: net_probe -c <config_filename>     - use config file.
-     net_probe -                        - read config from stdin.
-     net_probe -i <iface_name> -v <vlan_list> -u <UID>
-                                        - generate frames.
-     net_probe -l <iface_name>          - collect frames from interface.
-     net_probe -d <filename> -i <iface_name> -u <UID>
-                                        - analyse dump.
-
-Simple analyse dumps config file example is:
-{"action": "generate", uid": "aaa-bb-cccccc", 
- "interfaces": { "eth0": "/var/tmp/dump-eth0"}}
-
-Simple frame generation config file example is:
-{"action": "generate", uid": "aaa-bb-cccccc", 
- "interfaces": { "eth0": "1-4094"}}
-
-Full frame generation config file example is:
-{   "uid": "aaa-bb-cccccc", "cookie": "Some cookie",
-    "action": "generate",
-    "src_mac": "11:22:33:44:55:66",
-    "src": "10.0.0.1", "dst": "10.255.255.255",
-    "sport": 4056, "dport": 4057,
-    "interfaces": { 
-        "eth0": "10, 15, 20, 201-210, 301-310, 1000-2000",
-        "eth1": "1-4094"
-    }
-}
-"""
-
+    print usage
 
 
 
