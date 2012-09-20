beba397d896e606f371828a46a112ee7f8f8e5bd
Mike Scherbakov <mihgen@gmail.com>
2012-09-20 14:13:09 +0400

    Removed unused redis2 cookbook

--- cookbooks/redis2/recipes/package.rb
+++
@@ -1,7 +0,0 @@
-# Installs redis from packages
-
-pkg = value_for_platform( [:ubuntu, :debian] => {:default => "redis-server"},
-                         [:centos, :redhat] => {:default => "redis"},
-                         :default => "redis")
-package pkg
-node["redis2"]["daemon"] = "/usr/bin/redis-server"
