875c7abfee573a6811121a60fb865661b122d39b
Mike Scherbakov <mihgen@gmail.com>
2012-09-20 14:12:48 +0400

    Removed unused Django cookbook

--- cookbooks/django/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default[:django][:web_server] = "mod_wsgi"
-default[:django][:version] = "1.4"
-default[:django][:venv] = false
--- cookbooks/django/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-include_recipe "apache2"
-include_recipe "apache2::#{node[:django][:web_server]}"
-
-local_python_pip 'Django' do
-  version node[:django][:version] if node[:django][:version]
-  virtualenv node[:django][:venv]
-end
