005f4192ce04314bb5cf9432ac1ad692f687a32e
Nikolay Markov <nmarkov@mirantis.com>
2012-09-27 18:31:59 +0400

    rpc consumer test simplified

--- nailgun/test/test_rpc_consumer.py
+++ nailgun/test/test_rpc_consumer.py
@@ -12,33 +12,18 @@ from api.models import Node
 
 class TestConsumer(BaseHandlers):
 
-    def setUp(self):
-        super(TestConsumer, self).setUp()
-        self.conn = rpc.create_connection(True)
-        self.rpc_thread = threaded.RPCThread()
-        self.rpc_thread.start()
-
-    def tearDown(self):
-        super(TestConsumer, self).tearDown()
-        self.conn.close()
-
     def test_node_deploy_resp(self):
         node = self.create_default_node()
         node2 = self.create_default_node()
-        rpc.cast('nailgun', {
-            "method": "deploy_resp",
-            "args": {
-                "nodes": {
-                    str(node.id): {"status": "deploying"},
-                    str(node2.id): {"status": "error"}
-                }
+        receiver = threaded.NailgunReceiver()
+
+        receiver.deploy_resp(
+            nodes={
+                str(node.id): {"status": "deploying"},
+                str(node2.id): {"status": "error"}
             }
-        })
-        timer = time.time()
-        while True:
-            self.db.refresh(node)
-            self.db.refresh(node2)
-            if (node.status, node2.status) == ("deploying", "error"):
-                break
-            if time.time() - timer > 5:
-                raise Exception("Resp timeout expired")
+        )
+        self.db.refresh(node)
+        self.db.refresh(node2)
+        if (node.status, node2.status) != ("deploying", "error"):
+            raise Exception("Failed to update nodes")
