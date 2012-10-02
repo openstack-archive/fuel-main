4594fb4680bdc1d91fc7164946c952a08cf09740
Mike Scherbakov <mihgen@gmail.com>
2012-10-02 12:35:42 +0400

    Wiped out deprecated Naily code

--- mcollective/bin/run_simple.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-$LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), "..", "lib")))
-
-require 'naily/mcclient/simple'
-
-client = Naily::MCClient::Simple.new
-client.run
