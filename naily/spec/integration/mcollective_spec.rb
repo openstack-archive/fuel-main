44ed8ae253986c3eb3da33bcf1a7bccffbd7fb28
Mike Scherbakov <mihgen@gmail.com>
2012-10-02 17:40:24 +0400

    Added test for nailyfact (put, get methods)

--- naily/spec/integration/mcollective_spec.rb
+++ naily/spec/integration/mcollective_spec.rb
@@ -15,5 +15,20 @@ context "When MC agent is up and running" do
     stats[0].results[:statuscode].should eql(0)
     stats[0].results[:data][:msg].should eql("Hello, it is my reply: #{data_to_send}")
   end
+
+  it "it should update facts file with new key-value and could get it back" do
+    value_to_send = rand(2**30).to_s
+    node = "admin"
+    mc = rpcclient("nailyfact")
+    mc.progress = false
+    mc.discover(:nodes => [node])
+    stats = mc.put(:key => "role", :value => value_to_send)
+    stats.should have(1).items
+    stats[0].results[:statuscode].should eql(0)
+    stats = mc.get(:key => "role")
+    stats.should have(1).items
+    stats[0].results[:statuscode].should eql(0)
+    stats[0].results[:data][:value].should eql(value_to_send)
+  end
 end
 end
