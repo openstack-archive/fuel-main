465ff8fb8de94ebd5c2acde25a9d6359e306ab50
Mike Scherbakov <mihgen@gmail.com>
2012-10-04 01:40:40 +0400

    Fixes in puppet master installation

--- cookbooks/nailgun/recipes/naily.rb
+++ cookbooks/nailgun/recipes/naily.rb
@@ -7,8 +7,11 @@ Dir.glob("#{node[:nailgun][:root]}/naily/agent/*").each do |agent|
   end
 end
 
-link "#{node[:nailgun][:root]}/naily/puppet/modules/nailytest" do
-  to "/etc/puppet/modules/nailytest"
+# Chef's link resource doesn't process directory link, so we end up with bash script
+bash "Link Puppet test module" do
+  code <<-EOH
+  ln -sfT `readlink -f "#{node[:nailgun][:root]}/naily/puppet/modules/nailytest"` /etc/puppet/modules/nailytest
+  EOH
 end
 
 link "#{node[:nailgun][:root]}/naily/puppet/manifests/site.pp" do
