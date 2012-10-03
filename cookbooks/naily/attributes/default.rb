ddd48f38e6efdb6cdb22aa1dd2cb4775aadaf7dd
Maxim Kulkin <mkulkin@mirantis.com>
2012-10-03 18:56:15 +0400

    Added naily cookbook

---
+++ cookbooks/naily/attributes/default.rb
@@ -0,0 +1,10 @@
+default[:naily][:user]  = 'naily'
+default[:naily][:group] = 'naily'
+
+default[:naily][:broker_host] = 'localhost'
+default[:naily][:broker_port] = 5672
+default[:naily][:broker_username] = 'guest'
+default[:naily][:broker_password] = 'guest'
+
+default[:naily][:queue] = 'naily'
+
---
+++ cookbooks/naily/recipes/default.rb
@@ -0,0 +1,26 @@
+
+gem_package 'naily'
+
+user  node[:naily][:user]
+group node[:naily][:group]
+
+template '/etc/init.d/naily' do
+  source 'sv-init.erb'
+end
+
+directory '/etc/naily' do
+  owner node[:naily][:user]
+  group node[:naily][:group]
+  mode '0755'
+end
+
+template '/etc/naily/nailyd.conf' do
+  owner node[:naily][:user]
+  group node[:naily][:group]
+  mode '0644'
+end
+
+service 'naily' do
+  action [:enable, :start]
+end
+
