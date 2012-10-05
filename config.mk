8305c254ea84ac4f44fb81c43c27050df71ebf36
Mike Scherbakov <mihgen@gmail.com>
2012-10-05 14:11:52 +0400

    Using new packages mirror

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ ifndef BOOTSTRAP_REBUILD
 BOOTSTRAP_REBUILD:=0
 endif
 
-MIRROR_URL:=http://srv08-srt.srt.mirantis.net/mirror
+MIRROR_URL:=http://srv08-srt.srt.mirantis.net/mirror.new
 ifndef IGNORE_MIRROR
 IGNORE_MIRROR:=0
 endif
@@ -14,7 +14,7 @@ LOCAL_MIRROR:=mirror
 endif
 
 ifndef GOLDEN_MIRROR
-GOLDEN_MIRROR:=srv08-srt.srt.mirantis.net:/var/lib/jenkins/mirror
+GOLDEN_MIRROR:=srv08-srt.srt.mirantis.net:/var/lib/jenkins/mirror.new
 endif
 
 iso.path:=$(BUILD_DIR)/iso/nailgun-ubuntu-12.04-amd64.iso
