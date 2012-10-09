fd61f9fe3f03a9ee7bba8f10f4b48c7d6ca884f3
Vladimir Kozhukalov <kozhukalov@gmail.com>
2012-10-09 17:15:37 +0400

    [nailgun] Added extrasettings for mcollective and puppet

--- nailgun/settings/extrasettings.py
+++ nailgun/settings/extrasettings.py
@@ -13,3 +13,15 @@ home = os.getenv("HOME")
 PATH_TO_SSH_KEY = home and os.path.join(home, ".ssh", "id_rsa") or None
 PATH_TO_BOOTSTRAP_SSH_KEY = home and \
     os.path.join(home, ".ssh", "bootstrap.rsa") or None
+
+MCO_PSKEY = "Gie6iega9ohngaenahthohngu8aebohxah9seidi"
+MCO_STOMPHOST = "localhost"
+MCO_STOMPPORT = "61613"
+MCO_STOMPUSER = "guest"
+MCO_STOMPPASSWORD = "guest"
+
+PUPPET_MASTER_HOST = "localhost"
+
+DNS_DOMAIN = "example.com"
+DNS_SERVERS = "127.0.0.1"
+DNS_SEARCH = "example.com"
