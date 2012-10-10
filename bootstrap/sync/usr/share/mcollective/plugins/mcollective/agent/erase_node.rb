3d826eef5c718394c3eacda328fdf2ea8f95febb
gcon <gcon@Flackpanzer.(none)>
2012-10-10 18:29:43 +0400

    [bootstrap] Fixes after review.

--- bootstrap/sync/usr/share/mcollective/plugins/mcollective/agent/erase_node.rb
+++ bootstrap/sync/usr/share/mcollective/plugins/mcollective/agent/erase_node.rb
@@ -13,37 +13,64 @@ module MCollective
                   :timeout     => 60
 
       action "erase_node" do
-        validate :dev, String
-        if File.exist?(request[:dev])
-          begin
-            header = open(request[:dev]).sysread(512)
-            dev = open(request[:dev], 'w')
-            dev.syswrite("\000"*512)
-            dev.close
+        validate :file, String
+        if File.exist?(request[:file])
+          if request.data.key?(:get_data) and request.data[:get_data] != ''
+            header = get_data(request[:file], 512)
+            reply[:erased_data] = Base64.encode64(header)
+          end
+          request_reboot = request.data.key?(:reboot) and request.data[:reboot] != ''
+          if request.data.key?(:dry_run) and request.data[:dry_run] != ''
             reply[:status] = true
-          rescue Exception => e
-            reply[:status] = false
-            reply[:error] = e.message
-          ensure
-            reply[:header] = Base64.encode64(header)
+            reply[:dry_run] = true
+            if request_reboot
+              reply[:reboot] = true
+            end
+          else
+            begin
+              erase_data(request[:file], 512)
+              reply[:status] = true
+              if request_reboot
+                reboot
+                reply[:reboot] = true
+              end
+            rescue Exception => e
+              reply[:status] = false
+              reply[:error_msg] = e.message
+            end
           end
         else
           reply[:status] = false
-          reply[:error] = "File \"#{request[:dev]}\" not exist."
+          reply[:error_msg] = "File \"#{request[:file]}\" not exist."
         end
       end
 
       action "reboot_node" do
-        out = ""
-        err = ""
-        cmd = "shutdown -r 5"
-        status = run(cmd, :stdout => out, :stderr => err)
-        if status != 0
-          reply[:errors] = "ERROR WHILE RUN COMMAND:\n#{cmd}\nSTDOUT:\n#{out}\nSTDERR:\n#{err}"
-        end
-        reply[:status] = status
+        reboot
       end
 
+      private
+
+      def reboot
+        cmd = "/bin/sleep 5; /sbin/shutdown -r"
+        pid = fork { system(cmd) }
+        Process.detach(pid)
+      end
+
+      def get_data(file, length, offset=0)
+        fd = open(file)
+        fd.seek(offset)
+        ret = fd.sysread(length)
+        fd.close
+        return ret
+      end
+
+      def erase_data(file, length, offset=0)
+        fd = open(file, 'w')
+        fd.seek(offset)
+        ret = fd.syswrite("\000"*length)
+        fd.close
+      end
 
     end
   end
