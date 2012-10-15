184e8d8f86f14fe3ce717e2b403cbf6ab3dc5b0e
Mike Scherbakov <mihgen@gmail.com>
2012-10-15 17:14:21 +0400

    [astute] Get rid of renaming attributes

--- astute/lib/astute/deployer.rb
+++ astute/lib/astute/deployer.rb
@@ -7,15 +7,15 @@ module Astute
   module Deployer
     private
 
-    def self.wait_until_puppet_done(puppetd, previous_runs)
+    def self.wait_until_puppet_done(puppetd, previous_run_status)
       # Wait for first node is done, than check the next one
       # Load to mcollective is reduced by checking only one machine at time in a set
       # In fact we need to know if whole set of machines finished deployment
-      previous_runs.each do |res|
-        prev_run = res['ts']
+      previous_run_status.each do |res|
+        prev_run = res.results[:data][:lastrun]
         last_run = prev_run
         while last_run == prev_run
-          puppetd.discover(:nodes => [res['sender']])
+          puppetd.discover(:nodes => [res.results[:sender]])
           puppet_status = puppetd.status
           # logging to false, otherwise we get a message every second
           last_run = puppet_status[0].results[:data][:lastrun]
@@ -34,10 +34,6 @@ module Astute
       puppetd = MClient.new(ctx, "puppetd", macs)
       puppet_status = puppetd.status
 
-      # In results :lastrun we get the time when Puppet finished it's work last time
-      previous_runs = puppet_status.map { |res| {'sender' => res.results[:sender],
-                                                 'ts' => res.results[:data][:lastrun]} }
-
       puppetd.runonce
 
       Astute.logger.debug "Waiting for puppet to finish deployment on all nodes (timeout = #{PUPPET_TIMEOUT} sec)..."
@@ -46,7 +42,7 @@ module Astute
         # Yes, we polling here and yes, it's temporary.
         # As a better implementation we can later use separate queue to get result, ex. http://www.devco.net/archives/2012/08/19/mcollective-async-result-handling.php
         # or we can rewrite puppet agent not to fork, and increase ttl for mcollective RPC.
-        wait_until_puppet_done(puppetd, previous_runs)
+        wait_until_puppet_done(puppetd, puppet_status)
       end
       time_spent = Time.now - time_before
       Astute.logger.info "#{ctx.task_id}: Spent #{time_spent} seconds on puppet run for following nodes(macs): #{nodes.map {|n| n['mac']}.join(',')}"
