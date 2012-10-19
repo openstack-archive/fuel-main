06fa2ba323858185ed81853cd50244959e914706
Mike Scherbakov <mihgen@gmail.com>
2012-10-19 10:58:23 +0400

    [astute] Changed paths in setup-env.sh

--- astute/setup-env.sh
+++ astute/setup-env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for agent in `ls mcollective/agent/`; do
+for agent in `ls ../mcagent/`; do
     echo "Linking agent $agent of mcollective.."
-    ln -sf `readlink -f mcollective/agent/$agent` /usr/libexec/mcollective/mcollective/agent/$agent
+    ln -sf `readlink -f ../mcagent/$agent` /usr/libexec/mcollective/mcollective/agent/$agent
 done
-ln -sfT `readlink -f puppet/modules/nailytest` /etc/puppet/modules/nailytest
-ln -sf `readlink -f puppet/manifests/site.pp` /etc/puppet/manifests/site.pp
+ln -sfT `readlink -f ../puppet/nailytest` /etc/puppet/modules/nailytest
+ln -sf `readlink -f ../puppet/nailytest/examples/site.pp` /etc/puppet/manifests/site.pp
 ln -sf `readlink -f ../bootstrap/sync/usr/bin/net_probe.py` /usr/bin/net_probe.py
 uuidgen > /etc/bootif  # for net_probe plugin
 service mcollective restart
