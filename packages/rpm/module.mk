39126b84a195fd1fbb7e659a9c1f861793af5349
Alwex <ashaposhnikov@mirantis.com>
2012-10-25 11:05:15 +0000

    MIRROR_DIR is clean out now.

--- packages/rpm/module.mk
+++ packages/rpm/module.mk
@@ -12,12 +12,7 @@ SRC_URLS:=$(addprefix $(MIRROR_URL)/sources/,$(SRC_FILES))
 endif
 
 export SRC_URLS
-
-ifdef MIRROR_DIR
-SRC_DIR:=$(MIRROR_DIR)/sources/
-else
 SRC_DIR:=$/SOURCES/
-endif
 
 $/RPMS/x86_64/cirros-uec-0.3.0-1.x86_64.rpm: $/SOURCES/cirros-0.3.0-x86_64-uec.tar.gz packages/rpm/specs/cirros-0.3.0.spec
 	rpmbuild -vv --define "_topdir `readlink -f $/`" -ba packages/rpm/specs/cirros-0.3.0.spec
