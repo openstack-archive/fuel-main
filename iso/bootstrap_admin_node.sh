4f69c54ab0891ef654c37cd62b0957b2f98965cb
Alwex <ashaposhnikov@mirantis.com>
2012-11-02 08:49:07 +0000

    Added logging of puppet run on first boot.

--- iso/bootstrap_admin_node.sh
+++ iso/bootstrap_admin_node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-puppet apply --modulepath=/etc/puppet/modules /etc/puppet/modules/nailgun/examples/site.pp
+puppet apply --logdest /var/log/puppet/firstboot.log --modulepath=/etc/puppet/modules /etc/puppet/modules/nailgun/examples/site.pp
 
 sed -i "/bootstrap_admin_node.sh/d" /etc/rc.local
