cf60ad1a8c1c290e814d7c6e322a3aba829390a0
Dmitry Pyzhov <dpyzhov@mirantis.com>
2012-11-08 17:03:50 +0400

    PRD-24 Integration tests to check if Naily, puppetmaster are alive

---
+++ test/integration/test_nailyd.py
@@ -0,0 +1,26 @@
+import logging
+
+import xmlrpclib
+
+from test.integration.base import Base
+from test.helpers import SSHClient
+
+
+class TestNailyd(Base):
+    def __init__(self, *args, **kwargs):
+        super(TestNailyd, self).__init__(*args, **kwargs)
+        self.remote = SSHClient()
+
+    def setUp(self):
+        logging.info('Admin node ip: %s' % self.get_admin_node_ip())
+        self.ip = self.get_admin_node_ip()
+
+    def tearDown(self):
+        pass
+
+    def test_nailyd_alive(self):
+        self.remote.connect_ssh(self.ip, 'root', 'r00tme')
+        ps_output = self.remote.execute('ps ax')['stdout']
+        naily_processes = filter(lambda x: '/usr/bin/nailyd' in x, ps_output)
+        logging.debug("Found naily processes: %s" % naily_processes)
+        self.assertEquals(len(naily_processes), 1)
---
+++ test/integration/test_puppetmaster.py
@@ -0,0 +1,34 @@
+import logging
+
+import xmlrpclib
+from devops.helpers import wait, tcp_ping
+
+from test.integration.base import Base
+from test.helpers import SSHClient
+
+
+class TestPuppetMaster(Base):
+    def __init__(self, *args, **kwargs):
+        super(TestPuppetMaster, self).__init__(*args, **kwargs)
+        self.remote = SSHClient()
+
+    def setUp(self):
+        logging.info('Admin node ip: %s' % self.get_admin_node_ip())
+        self.ip = self.get_admin_node_ip()
+
+    def tearDown(self):
+        pass
+
+    def test_puppetmaster_alive(self):
+        wait(
+            lambda: tcp_ping(self.ip, 8140),
+            timeout=5
+        )
+        self.remote.connect_ssh(self.ip, 'root', 'r00tme')
+        ps_output = self.remote.execute('ps ax')['stdout']
+        pm_processes = filter(
+            lambda x: '/usr/sbin/puppetmasterd' in x,
+            ps_output
+        )
+        logging.debug("Found puppet master processes: %s" % pm_processes)
+        self.assertEquals(len(pm_processes), 4)
