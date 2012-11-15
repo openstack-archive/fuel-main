a1c0fd0d85dd4dbaff80b88f2cf8c0243a6f73c7
Mike Scherbakov <mihgen@gmail.com>
2012-11-15 17:16:27 +0400

    [nailgun] Check correctness of nodes/status when deploy/remove

--- nailgun/nailgun/test/test_cluster_changes_handler.py
+++ nailgun/nailgun/test/test_cluster_changes_handler.py
@@ -59,3 +59,42 @@ class TestHandlers(BaseHandlers):
 
         nailgun.task.task.rpc.cast.assert_called_once_with(
             'naily', msg)
+
+    def test_deploy_and_remove_cast_with_correct_nodes_and_statuses(self):
+        nailgun.task.task.rpc = Mock()
+        cluster = self.create_cluster_api()
+
+        n_ready = self.create_default_node(cluster_id=cluster['id'],
+                                           status='ready')
+        n_added = self.create_default_node(cluster_id=cluster['id'],
+                                           pending_addition=True,
+                                           status='discover')
+        n_removed = self.create_default_node(cluster_id=cluster['id'],
+                                             pending_deletion=True,
+                                             status='error')
+
+        nailgun.task.task.Cobbler = Mock()
+        resp = self.app.put(
+            reverse(
+                'ClusterChangesHandler',
+                kwargs={'cluster_id': cluster['id']}),
+            headers=self.default_headers
+        )
+
+        # remove_nodes method call
+        n_rpc = nailgun.task.task.rpc.cast. \
+            call_args_list[0][0][1]['args']['nodes']
+        self.assertEquals(len(n_rpc), 1)
+        n_removed_rpc = [n for n in n_rpc if n['uid'] == n_removed.id][0]
+        # object is found, so we passed the right node for removal
+        self.assertIsNotNone(n_removed_rpc)
+
+        # deploy method call
+        n_rpc = nailgun.task.task.rpc.cast. \
+            call_args_list[1][0][1]['args']['nodes']
+        self.assertEquals(len(n_rpc), 2)
+        n_ready_rpc = [n for n in n_rpc if n['uid'] == n_ready.id][0]
+        n_added_rpc = [n for n in n_rpc if n['uid'] == n_added.id][0]
+
+        self.assertEquals(n_ready_rpc['status'], 'ready')
+        self.assertEquals(n_added_rpc['status'], 'provisioning')
