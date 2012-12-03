c4d5fcce9daec412d8eb04eacfd3b25418b733e7
Dmitry Pyzhov <dpyzhov@mirantis.com>
2012-12-03 19:13:28 +0400

    Put fuel puppet modules into /etc/puppet/modules

--- iso/bootstrap_admin_node.sh
+++ iso/bootstrap_admin_node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-puppet apply --modulepath=/etc/puppet/modules /etc/puppet/modules/nailgun/examples/site.pp 2>&1 | tee /var/log/puppet/firstboot.log
+puppet apply --modulepath=/opt/nailgun_puppet /opt/nailgun_puppet/nailgun/examples/site.pp 2>&1 | tee /var/log/puppet/firstboot.log
 
 sed -i --follow-symlinks "/bootstrap_admin_node.sh/d" /etc/rc.local
