ef40ee2cb0df2c4e83b052131b7b8dbf8b0c09ce
Mike Scherbakov <mihgen@gmail.com>
2013-01-10 18:20:02 +0400

    [astute] Added multinode_compute test for nailyfact deployment_engine

---
+++ astute/spec/unit/nailyfact_deploy_spec.rb
@@ -0,0 +1,103 @@
+#!/usr/bin/env rspec
+require File.join(File.dirname(__FILE__), "..", "spec_helper")
+
+describe "NailyFact DeploymentEngine" do
+  context "When deploy is called, it" do
+    before(:each) do
+      @ctx = mock
+      @ctx.stubs(:task_id)
+      reporter = mock
+      @ctx.stubs(:reporter).returns(reporter)
+      reporter.stubs(:report)
+      @deploy_engine = Astute::DeploymentEngine::NailyFact.new(@ctx)
+    end
+
+    it "should call valid method depends on attrs" do
+      nodes = [{'uid' => 1}]
+      attrs = {'deployment_mode' => 'ha_compute'}
+      attrs_modified = attrs.merge({'some' => 'somea'})
+      
+      @deploy_engine.expects(:attrs_ha_compute).with(nodes, attrs).returns(attrs_modified)
+      @deploy_engine.expects(:deploy_ha_compute).with(nodes, attrs_modified)
+      # All implementations of deploy_piece go to subclasses
+      @deploy_engine.respond_to?(:deploy_piece).should be_true
+      @deploy_engine.deploy(nodes, attrs)
+    end
+
+    it "should raise an exception if deployment mode is unsupported" do
+      nodes = [{'uid' => 1}]
+      attrs = {'deployment_mode' => 'unknown'}
+      expect {@deploy_engine.deploy(nodes, attrs)}.to raise_exception(/Method attrs_unknown is not implemented/)
+    end
+
+    it "multinode_compute deploy should not raise any exception" do
+      data = {"args" =>
+                {"attributes" =>
+                  {"storage_network_range" => "172.16.0.0/24", "auto_assign_floating_ip" => false,
+                   "mysql" => {"root_password" => "Z2EqsZo5"},
+                   "keystone" => {"admin_token" => "5qKy0i63", "db_password" => "HHQ86Rym", "admin_tenant" => "admin"},
+                   "nova" => {"user_password" => "h8RY8SE7", "db_password" => "Xl9I51Cb"},
+                   "glance" => {"user_password" => "nDlUxuJq", "db_password" => "V050pQAn"},
+                   "rabbit" => {"user" => "nova", "password" => "FLF3txKC"},
+                   "management_network_range" => "192.168.0.0/24",
+                   "public_network_range" => "240.0.1.0/24",
+                   "fixed_network_range" => "10.0.0.0/24",
+                   "floating_network_range" => "240.0.0.0/24",
+                   "deployment_mode" => "multinode_compute"},
+               "task_uuid" => "19d99029-350a-4c9c-819c-1f294cf9e741",
+               "nodes" => [{"mac" => "52:54:00:0E:B8:F5", "status" => "provisioning",
+                            "uid" => "devnailgun.mirantis.com", "error_type" => nil,
+                            "network_data" => [{"gateway" => "192.168.0.1",
+                                                "name" => "management", "dev" => "eth0",
+                                                "brd" => "192.168.0.255", "netmask" => "255.255.255.0",
+                                                "vlan" => 102, "ip" => "192.168.0.2/24"},
+                                               {"gateway" => "240.0.1.1",
+                                                "name" => "public", "dev" => "eth0",
+                                                "brd" => "240.0.1.255", "netmask" => "255.255.255.0",
+                                                "vlan" => 101, "ip" => "240.0.1.2/24"},
+                                               {"name" => "floating", "dev" => "eth0", "vlan" => 120},
+                                               {"name" => "fixed", "dev" => "eth0", "vlan" => 103},
+                                               {"name" => "storage", "dev" => "eth0", "vlan" => 104}],
+                            "id" => 1,
+                            "ip" => "10.20.0.200",
+                            "role" => "controller"},
+                           {"mac" => "52:54:00:50:91:DD", "status" => "provisioning",
+                            "uid" => 2, "error_type" => nil,
+                            "network_data" => [{"gateway" => "192.168.0.1",
+                                                "name" => "management", "dev" => "eth0",
+                                                "brd" => "192.168.0.255", "netmask" => "255.255.255.0",
+                                                "vlan" => 102, "ip" => "192.168.0.3/24"},
+                                               {"gateway" => "240.0.1.1",
+                                                "name" => "public", "dev" => "eth0",
+                                                "brd" => "240.0.1.255", "netmask" => "255.255.255.0",
+                                                "vlan" => 101, "ip" => "240.0.1.3/24"},
+                                               {"name" => "floating", "dev" => "eth0", "vlan" => 120},
+                                               {"name" => "fixed", "dev" => "eth0", "vlan" => 103},
+                                               {"name" => "storage", "dev" => "eth0", "vlan" => 104}],
+                            "id" => 2,
+                            "ip" => "10.20.0.221",
+                            "role" => "compute"},
+                           {"mac" => "52:54:00:C3:2C:28", "status" => "provisioning",
+                            "uid" => 3, "error_type" => nil,
+                            "network_data" => [{"gateway" => "192.168.0.1",
+                                                "name" => "management", "dev" => "eth0",
+                                                "brd" => "192.168.0.255", "netmask" => "255.255.255.0",
+                                                "vlan" => 102, "ip" => "192.168.0.4/24"},
+                                               {"gateway" => "240.0.1.1",
+                                                "name" => "public", "dev" => "eth0",
+                                                "brd" => "240.0.1.255", "netmask" => "255.255.255.0",
+                                                "vlan" => 101, "ip" => "240.0.1.4/24"},
+                                               {"name" => "floating", "dev" => "eth0", "vlan" => 120},
+                                               {"name" => "fixed", "dev" => "eth0", "vlan" => 103},
+                                               {"name" => "storage", "dev" => "eth0", "vlan" => 104}],
+                            "id" => 3,
+                            "ip" => "10.20.0.68",
+                            "role" => "compute"}]},
+              "method" => "deploy",
+              "respond_to" => "deploy_resp"}
+      Astute::Metadata.expects(:publish_facts).times(data['args']['nodes'].size)
+      Astute::PuppetdDeployer.expects(:deploy).twice  # we got two calls, one for controller, and another for all computes
+      @deploy_engine.deploy(data['args']['nodes'], data['args']['attributes'])
+    end
+  end
+end
