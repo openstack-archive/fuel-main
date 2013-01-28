66e2e4a105a0d0fbac1e6102e81288a7e859bbba
Vladimir Kozhukalov <kozhukalov@gmail.com>
2013-01-28 14:00:32 +0400

    Removed commit_sha from puppet

--- puppet/nailgun/manifests/venv.pp
+++ puppet/nailgun/manifests/venv.pp
@@ -41,7 +41,6 @@ class nailgun::venv(
   $database_port = "3306"
   $database_user = "nailgun"
   $database_passwd = "nailgun"
-  $commit_sha = ""
 
   file { "/etc/nailgun":
     ensure => directory,
