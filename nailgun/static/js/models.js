8dd119da6d754618b17bc3365e2a0d9a8415006b
Julia Kirnosova <jkirnosova@mirantis.com>
2013-01-28 11:12:29 +0400

    Disabled Deploy Changes button for new clusters

--- nailgun/static/js/models.js
+++ nailgun/static/js/models.js
@@ -36,7 +36,7 @@ define(function() {
             return this.get('tasks') && this.get('tasks').where(options)[0];
         },
         hasChanges: function() {
-            return this.get('nodes').hasChanges() || this.get('changes').length;
+            return this.get('nodes').hasChanges() || (this.get('changes').length && this.get('nodes').currentNodes().length);
         },
         needsRedeployment: function() {
             return this.get('nodes').where({pending_addition: false, status: 'error'}).length;
