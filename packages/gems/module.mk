fb132b91c455cfbca590ee2470872d062f13c12c
Vladimir Kozhukalov <kozhukalov@gmail.com>
2013-02-07 12:34:22 +0400

    Added packages subdirectory into gems targets

--- packages/gems/module.mk
+++ packages/gems/module.mk
@@ -13,10 +13,10 @@ $(BUILD_DIR)/packages/gems/build.done: \
 	(cd $(LOCAL_MIRROR_GEMS) && gem generate_index gems)
 	$(ACTION.TOUCH)
 
-astute: $(BUILD_DIR)/gems/astute-$(ASTUTE_VERSION).gem
+astute: $(BUILD_DIR)/packages/gems/astute-$(ASTUTE_VERSION).gem
 astute_version:
 	@echo $(ASTUTE_VERSION)
 
-naily: $(BUILD_DIR)/gems/naily-$(NAILY_VERSION).gem
+naily: $(BUILD_DIR)/packages/gems/naily-$(NAILY_VERSION).gem
 naily_version:
 	@echo $(NAILY_VERSION)
