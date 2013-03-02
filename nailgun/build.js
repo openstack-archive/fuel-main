1b40d5bf3be0a5b6881b2f30aea1c67867f6f47d
Vitaly Kramskikh <vkramskikh@gmail.com>
2013-03-02 17:50:17 +0400

    Working build.js file

---
+++ nailgun/build.js
@@ -0,0 +1,9 @@
+({
+    baseUrl: ".",
+    appDir: "static",
+    dir: "/tmp/static",
+    mainConfigFile: "static/js/main.js",
+    modules: [{name: "js/main"}],
+    waitSeconds: 60,
+    optimize: "uglify2"
+})
