0beb0a4ce4c395dad2ed4d84511803c7e6d51388
Vitaly Kramskikh <vkramskikh@gmail.com>
2013-03-12 20:00:30 +0400

    Fix for stange UI tests error

--- nailgun/ui_tests/test_cluster_settings.js
+++ nailgun/ui_tests/test_cluster_settings.js
@@ -26,6 +26,7 @@ casper.then(function() {
         this.test.assertExists('.dismiss-settings-dialog', 'Dismiss changes dialog appears if there are changes and user is going to leave the tab');
         this.click('.btn-return');
     });
+    this.waitWhileSelector('.dismiss-settings-dialog');
     this.then(function() {
         this.click('.btn-revert-changes');
         this.test.assertExists('.btn-apply-changes:disabled', 'Save settings button is disabled again after changes were cancelled');
@@ -36,6 +37,7 @@ casper.then(function() {
     this.test.comment('Testing OpenStack settings: save changes');
     this.click('input[type=checkbox]');
     this.click('.btn-apply-changes');
+    this.waitForSelector('.btn-load-defaults:not(:disabled)');
     this.test.assertExists('.btn-revert-changes:disabled', 'Cancel changes button is disabled after changes were saved successfully');
 });
 
