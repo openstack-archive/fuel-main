faf9638bc69dc79c7fdc9294cc309c40ca57d518
Andrey Danin <gcon.monolake@gmail.com>
2013-04-11 21:11:01 +0400

    [integr.test] Fix process names in test_nailyd_alive

--- fuelweb_test/integration/test_nailyd.py
+++ fuelweb_test/integration/test_nailyd.py
@@ -21,6 +21,11 @@ class TestNailyd(Base):
     def test_nailyd_alive(self):
         self.remote.connect_ssh(self.ip, 'root', 'r00tme')
         ps_output = self.remote.execute('ps ax')['stdout']
-        naily_processes = filter(lambda x: '/usr/bin/nailyd' in x, ps_output)
-        logging.debug("Found naily processes: %s" % naily_processes)
-        self.assertEquals(len(naily_processes), 1)
+        naily_processes = filter(lambda x: 'naily master' in x, ps_output)
+        logging.debug("Found %d naily master processes: %s" %
+                      (len(naily_processes), naily_processes))
+        self.assertEqual(1, len(naily_processes))
+        naily_processes = filter(lambda x: 'naily worker' in x, ps_output)
+        logging.debug("Found %d naily worker processes: %s" %
+                      (len(naily_processes), naily_processes))
+        self.assertEqual(True, len(naily_processes) > 1)
