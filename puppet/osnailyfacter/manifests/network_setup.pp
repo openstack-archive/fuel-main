ae6400a8a36002754758decec685ba4d2d7bd2e7
Dmitry Pyzhov <dpyzhov@mirantis.com>
2013-04-21 01:55:20 +0400

    Aliases in puppet variables

--- puppet/osnailyfacter/manifests/network_setup.pp
+++ puppet/osnailyfacter/manifests/network_setup.pp
@@ -1,6 +1,38 @@
-class osnailyfacter::network_setup {
-
-include puppet-network
-        create_resources(network_config,parsejson($network_data))
+define setup_interfaces (
+  $interface = $name,
+  $network_settings
+) {
+  $ipaddr = $network_settings[$interface]['ipaddr']
+  $gateway = $network_settings[$interface]['gateway']
+  notify{"${interface} => ${ipaddr}, ${gateway}":}
+  l23network::l3::ifconfig{$interface:
+    ipaddr        => $ipaddr,
+    gateway       => $gateway,
+    check_by_ping => 'none'
+  }
 }
 
+
+
+define check_base_interfaces (
+  $interface = $name,
+) {
+  $b_iface = split($interface, '.')
+  if size($b_iface) > 1 {
+    if ! defined(L23network::L3::Ifconfig[$n_iface]) {
+      l23network::l3::ifconfig{$b_iface:
+        ipaddr        => 'none',
+      }
+    }
+    L23network::L3::Ifconfig<| title == $b_iface |> ->
+    L23network::L3::Ifconfig<| title == $interface |>
+  }
+}
+
+class osnailyfacter::network_setup (
+  interfaces = keys(parsejson($network_data)),
+  network_settings = parsejson($network_data)
+) {
+  setup_interfaces{$interfaces: network_settings=>$network_settings} ->
+  check_base_interfaces{$interfaces:}
+}
