664b39d6712a7116b2a5327aba36aa298cd6954b
Vitaly Kramskikh <vkramskikh@gmail.com>
2013-05-20 17:50:12 +0400

    Fixture with real-world nodes info by Ryan and Mike

--- nailgun/nailgun/test/test_fixture_uploading.py
+++ nailgun/nailgun/test/test_fixture_uploading.py
@@ -16,7 +16,7 @@ class TestFixture(BaseHandlers):
 
     def test_upload_working(self):
         check = self.db.query(Node).all()
-        self.assertEqual(len(list(check)), 10)
+        self.assertEqual(len(list(check)), 8)
 
     def test_custom_fixture(self):
         data = u'''[{
