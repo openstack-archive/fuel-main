1773cf7b7a33c59a67a82701ddf6307ec10a1ec8
Andrey Danin <gcon.monolake@gmail.com>
2013-07-08 17:46:48 +0400

    [net_probe] Restore shabang at first line.

--- packages/rpm/nailgun-net-check/net_probe.py
+++ packages/rpm/nailgun-net-check/net_probe.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 #    Copyright 2013 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -12,8 +13,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-
-#!/usr/bin/python
 #
 # Generate and send Ethernet packets to specified interfaces.
 # Collect data from interfaces.
