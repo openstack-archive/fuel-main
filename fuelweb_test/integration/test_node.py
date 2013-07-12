1c96b7f7d9b6d2862ba883a2173fe6d40382fee5
nfedotov <ks74jgb1>
2013-07-12 16:41:28 +0400

    wrong amount of expected smiles

--- fuelweb_test/integration/test_node.py
+++ fuelweb_test/integration/test_node.py
@@ -198,13 +198,13 @@ class TestNode(BaseNodeTestCase):
 
         self.assertClusterReady(
             self.nodes().slaves[0].name,
-            smiles_count=6, networks_count=1, timeout=300)
+            smiles_count=8, networks_count=1, timeout=300)
         self.assertClusterReady(
             self.nodes().slaves[1].name,
-            smiles_count=6, networks_count=1, timeout=300)
+            smiles_count=8, networks_count=1, timeout=300)
         self.assertClusterReady(
             self.nodes().slaves[2].name,
-            smiles_count=6, networks_count=1, timeout=300)
+            smiles_count=8, networks_count=1, timeout=300)
 
 if __name__ == '__main__':
     unittest.main()
