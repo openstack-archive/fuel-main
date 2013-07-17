f830e647ddd1299c507c7ee85ac986380801ce9a
nfedotov <ks74jgb1>
2013-07-17 15:52:47 +0400

    Replace two asserts with one assert(set, set)

--- fuelweb_test/integration/base_node_test_case.py
+++ fuelweb_test/integration/base_node_test_case.py
@@ -312,12 +312,8 @@ class BaseNodeTestCase(BaseTestCase):
         ip = self.get_node_by_devops_node(
             self.ci().environment().node_by_name(node_name))['ip']
         current_ips = self.get_cluster_floating_list(ip)
-        self.assertEqual(len(current_ips), len(expected_ips),
-                         "Floating ips amount. Cluster ip: %s" % ip)
-
-        self.assertTrue(
-            all([ip in expected_ips for ip in current_ips]),
-            'Floating IP list')
+        self.assertEqual(
+            set(expected_ips), set(current_ips), 'Floating IP list')
 
     @logwrap
     def get_private_keys(self):
