018a194a45b46ae994520ab5b21839f993bddf0f
Andrey Korolyov <andrey@xdel.ru>
2013-07-18 15:32:12 +0400

    repo purge - let`s see if everything breaks

--- mirror/centos/yum_repos.mk
+++ mirror/centos/yum_repos.mk
@@ -56,84 +56,14 @@ enabled=0
 priority=10
 endef
 
-define yum_repo_epel
-[epel]
-name=Extra Packages for Enterprise Linux $(CENTOS_MAJOR) - $(CENTOS_ARCH)
-#mirrorlist=https://mirrors.fedoraproject.org/metalink?repo=epel-$(CENTOS_MAJOR)&arch=$(CENTOS_ARCH)
-baseurl=http://mirror.yandex.ru/epel/$(CENTOS_MAJOR)/$(CENTOS_ARCH)
-gpgcheck=0
-enabled=1
-priority=20
-endef
-
 define yum_repo_fuel
-[epel-fuel-grizzly-3.0.1]
+[epel-fuel-grizzly-3.1]
 name=Mirantis OpenStack Custom Packages
-#mirrorlist=http://download.mirantis.com/epel-fuel-grizzly/mirror.internal.list
-baseurl=http://download.mirantis.com/epel-fuel-grizzly-3.0.1/
+#mirrorlist=http://download.mirantis.com/epel-fuel-grizzly-3.1/mirror.internal.list
+baseurl=http://download.mirantis.com/epel-fuel-grizzly-3.1/
 gpgcheck=0
 enabled=1
 priority=1
-endef
-
-define yum_repo_fuel_devel
-[mash-epel-fuel-grizzly]
-name=Epel Fuel Grizzly from Matt
-baseurl=http://osci-koji.srt.mirantis.net/mash/epel-fuel-grizzly/x86_64/
-enabled=0
-gpgcheck=0
-priority=1
-
-[mash-fuel-folsom]
-name=Fuel Folsom from Matt
-baseurl=http://osci-koji.srt.mirantis.net/mash/fuel-folsom/x86_64/
-enabled=0
-gpgcheck=0
-priority=1
-
-[mash-fuel-grizzly-puppet27]
-name=Puppet packages from Matt
-baseurl=http://osci-koji.srt.mirantis.net/mash/epel-fuel-grizzly-puppet27/x86_64/
-enabled=0
-gpgcheck=0
-priority=1
-endef
-
-define yum_repo_puppetlabs
-[puppetlabs]
-name=Puppet Labs Packages
-baseurl=http://yum.puppetlabs.com/el/$(CENTOS_MAJOR)/products/$(CENTOS_ARCH)/
-enabled=1
-gpgcheck=1
-gpgkey=http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs
-priority=1
-endef
-
-define yum_repo_puppetlabs_devel
-[devel_puppetlabs]
-name=Puppet Labs Packages
-baseurl=http://yum.puppetlabs.com/el/$(CENTOS_MAJOR)/devel/$(CENTOS_ARCH)/
-enabled=0
-gpgcheck=1
-gpgkey=http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs
-priority=2
-endef
-
-define yum_repo_rpmforge
-[rpmforge]
-name=RHEL $(CENTOS_RELEASE) - RPMforge.net - dag
-#mirrorlist = http://apt.sw.be/redhat/el$(CENTOS_MAJOR)/en/mirrors-rpmforge
-baseurl=http://apt.sw.be/redhat/el$(CENTOS_MAJOR)/en/$(CENTOS_ARCH)/rpmforge
-gpgcheck=0
-enabled=0
-
-[rpmforge-extras]
-name = RHEL $(CENTOS_RELEASE) - RPMforge.net - extras
-#mirrorlist = http://apt.sw.be/redhat/el$(CENTOS_MAJOR)/en/mirrors-rpmforge-extras
-baseurl = http://apt.sw.be/redhat/el$(CENTOS_MAJOR)/en/$(CENTOS_ARCH)/extras
-gpgcheck = 0
-enabled = 1
-priority=95
 endef
 
 define yum_repo_subscr_manager
--- mirror/rhel/yum_repos.mk
+++ mirror/rhel/yum_repos.mk
@@ -14,16 +14,6 @@ pluginconfpath=$(BUILD_DIR)/mirror/rhel/etc/yum/pluginconf.d
 reposdir=$(BUILD_DIR)/mirror/rhel/etc/yum.repos.d
 endef
 
-define rhel_yum_repo_puppetlabs
-[puppetlabs]
-name=Puppet Labs Packages
-baseurl=http://yum.puppetlabs.com/el/$(CENTOS_MAJOR)/products/$(CENTOS_ARCH)/
-enabled=1
-gpgcheck=1
-gpgkey=http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs
-priority=1
-endef
-
 define rhel_yum_repo_proprietary
 [proprietary]
 name = RHEL $(CENTOS_RELEASE) - Proprietary
@@ -31,42 +21,4 @@ baseurl = $(MIRROR_RHEL)
 gpgcheck = 0
 enabled = 1
 priority=1
-endef
-
-define rhel_yum_repo_rhel
-[rhel-os-30-puddle]
-name=OpenStack-3.0-Puddle
-baseurl=http://srv11-msk.msk.mirantis.net/rhel6/OpenStack-3.0-Puddle
-gpgcheck=0
-enabled=1
-
-[rhel-server-rpms]
-name=rhel-6-server-rpms
-baseurl=http://srv11-msk.msk.mirantis.net/rhel6/rhel-6-server-rpms
-gpgcheck=0
-enabled=1
-
-[rhel-server-optional-rpms]
-name=rhel-6-server-optional-rpms
-baseurl=http://srv11-msk.msk.mirantis.net/rhel6/rhel-6-server-optional-rpms
-gpgcheck=0
-enabled=1
-
-[rhel-ha-rpms]
-name=rhel-ha-for-rhel-6-server-rpms
-baseurl=http://srv11-msk.msk.mirantis.net/rhel6/rhel-ha-for-rhel-6-server-rpms
-gpgcheck=0
-enabled=1
-
-[rhel-lb-rpms]
-name=rhel-lb-for-rhel-6-server-rpms
-baseurl=http://srv11-msk.msk.mirantis.net/rhel6/rhel-lb-for-rhel-6-server-rpms
-gpgcheck=0
-enabled=1
-
-[rhel-rs-rpms]
-name=rhel-rs-for-rhel-6-server-rpms
-baseurl=http://srv11-msk.msk.mirantis.net/rhel6/rhel-rs-for-rhel-6-server-rpms
-gpgcheck=0
-enabled=1
 endef
