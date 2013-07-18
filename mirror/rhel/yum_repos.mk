13337dd3e6946256af2c6d3c56a6be34bf14f90a
Andrey Korolyov <andrey@xdel.ru>
2013-07-18 18:35:53 +0400

    partial revert of the previous comment - some of internal repos should exist

--- mirror/rhel/yum_repos.mk
+++ mirror/rhel/yum_repos.mk
@@ -14,6 +14,44 @@ pluginconfpath=$(BUILD_DIR)/mirror/rhel/etc/yum/pluginconf.d
 reposdir=$(BUILD_DIR)/mirror/rhel/etc/yum.repos.d
 endef
 
+define rhel_yum_repo_rhel
+[rhel-os-30-puddle]
+name=OpenStack-3.0-Puddle
+baseurl=http://srv11-msk.msk.mirantis.net/rhel6/OpenStack-3.0-Puddle
+gpgcheck=0
+enabled=1
+
+[rhel-server-rpms]
+name=rhel-6-server-rpms
+baseurl=http://srv11-msk.msk.mirantis.net/rhel6/rhel-6-server-rpms
+gpgcheck=0
+enabled=1
+
+[rhel-server-optional-rpms]
+name=rhel-6-server-optional-rpms
+baseurl=http://srv11-msk.msk.mirantis.net/rhel6/rhel-6-server-optional-rpms
+gpgcheck=0
+enabled=1
+
+[rhel-ha-rpms]
+name=rhel-ha-for-rhel-6-server-rpms
+baseurl=http://srv11-msk.msk.mirantis.net/rhel6/rhel-ha-for-rhel-6-server-rpms
+gpgcheck=0
+enabled=1
+
+[rhel-lb-rpms]
+name=rhel-lb-for-rhel-6-server-rpms
+baseurl=http://srv11-msk.msk.mirantis.net/rhel6/rhel-lb-for-rhel-6-server-rpms
+gpgcheck=0
+enabled=1
+
+[rhel-rs-rpms]
+name=rhel-rs-for-rhel-6-server-rpms
+baseurl=http://srv11-msk.msk.mirantis.net/rhel6/rhel-rs-for-rhel-6-server-rpms
+gpgcheck=0
+enabled=1
+endef 
+
 define rhel_yum_repo_proprietary
 [proprietary]
 name = RHEL $(CENTOS_RELEASE) - Proprietary
