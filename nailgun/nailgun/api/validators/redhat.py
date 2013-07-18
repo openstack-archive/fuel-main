b142e1b6950241594edf4ee8dcc47aa814d3c2ef
e0ne <e0ne@e0ne.info>
2013-07-18 14:05:41 +0300

    Rmove unused import

--- nailgun/nailgun/api/validators/redhat.py
+++ nailgun/nailgun/api/validators/redhat.py
@@ -15,7 +15,6 @@
 
 from nailgun.api.validators.base import BasicValidator
 from nailgun.errors import errors
-from nailgun.settings import settings
 
 
 class RedHatAcountValidator(BasicValidator):
