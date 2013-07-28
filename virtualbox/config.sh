325c14c5a4eab21c6e583795f9f4262fa02fcfb8
Roman Alekseenkov <ralekseenkov@mirantis.com>
2013-07-27 23:09:47 -0700

    license headers for virtualbox scripts

--- virtualbox/config.sh
+++ virtualbox/config.sh
@@ -1,5 +1,19 @@
 #!/bin/bash 
 
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 # The number of nodes for installing OpenStack on
 #   - for minimal non-HA installation, specify 2 (1 controller + 1 compute)
 #   - for minimal non-HA with Cinder installation, specify 3 (1 ctrl + 1 compute + 1 cinder)
