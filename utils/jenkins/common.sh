1583da2eda1c1ebcd0b059ee093f478b89f93271
Evgeniy L <test0x0000@gmail.com>
2013-08-06 16:15:07 +0400

    Ignore docs folder in astute for license checking

--- utils/jenkins/common.sh
+++ utils/jenkins/common.sh
@@ -2,7 +2,7 @@ function license_check {
     # License information must be in every source file
     cd $WORKSPACE/local_repo
     tmpfile=`tempfile`
-    find nailgun astute naily -regex ".*\.\(rb\|py\|js\)" -type f -print0 | xargs -0 grep -Li License > $tmpfile
+    find nailgun astute naily -not -path "astute/docs/*" -regex ".*\.\(rb\|py\|js\)" -type f -print0 | xargs -0 grep -Li License > $tmpfile
     files_with_no_license=`wc -l $tmpfile | awk '{print $1}'`
     if [ $files_with_no_license -gt 0 ]; then
         echo "ERROR: Found files without license, see files below:"
