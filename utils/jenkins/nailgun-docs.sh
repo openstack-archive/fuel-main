7892539ca33c09bfb6bce07d69f0573e54495be2
Nikolay Markov <nmarkov@mirantis.com>
2013-08-21 13:43:15 +0400

    removed commented code

--- utils/jenkins/nailgun-docs.sh
+++ utils/jenkins/nailgun-docs.sh
@@ -9,5 +9,3 @@ make clean
 make html
 rsync -avz -e ssh --delete _build/html/ fjenkins@fuel-docs.vm.mirantis.net:/home/fjenkins/workspace/fuel-docs.mirantis.com/docs/_build/html/
 cd $WORKSPACE
-
-#zip -r docs docs -x \*.sh -x \*.bat -x \*.jar
