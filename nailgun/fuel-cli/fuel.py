24bb52df2344c1c0f2818ad94c0ecf65b1c62ef5
Alexandr Notchenko <anotchenko@mirantis.com>
2013-08-28 14:44:54 +0400

    table formatter, and api funcs

--- nailgun/fuel-cli/fuel.py
+++ nailgun/fuel-cli/fuel.py
@@ -14,46 +14,98 @@
 #    under the License.
 
 import argparse
+import json
+import urllib2
+
+API_ROOT = "http://127.0.0.1:8000/api/"
+
+
+def format_table(data, **kwargs):
+    rows = [tuple(x.values()) for x in data]
+
+    header = tuple(data[0].keys())
+    number_of_columns = len(header)
+    column_widths = dict(
+        zip(
+            range(number_of_columns),
+            (len(str(x)) for x in header)
+        )
+    )
+
+    for row in rows:
+        column_widths.update(
+            (index, max(column_widths[index], len(str(element))))
+            for index, element in enumerate(row)
+        )
+    row_template = ' | '.join(
+        '%%-%ss' % column_widths[i] for i in range(number_of_columns)
+    )
+
+    return '\n'.join(
+        (row_template % header,
+         '-|-'.join(column_widths[column_index]*'-'
+                    for column_index in range(number_of_columns)),
+         '\n'.join(row_template % x for x in rows))
+    )
+
+
+def json_api_get_request(api):
+    request = urllib2.urlopen(API_ROOT + api)
+    return json.loads(request.read())
 
 
 def release(params):
-    print params
+    if params.list:
+        data = json_api_get_request("releases/")
+        print(format_table(data))
+    if params.rid:
+        try:
+            data = json_api_get_request("releases/" + params.rid + "/")
+            print(format_table([data]))
+        except urllib2.HTTPError:
+            print("Error. No release with such rid!")
+    print(params)
 
 
 def environment(params):
-    print params
+    param_values = params.__dict__.values()
+    param_values.remove("environment")
+    if not any(param_values) or params.list:
+        data = json_api_get_request("clusters/")
+        print(data)
+        # print format_table(data)
 
 
 def node(params):
-    print params
+    print(params)
 
 
 def network(params):
-    print params
+    print(params)
 
 
 def settings(params):
-    print params
+    print(params)
 
 
 def logs(params):
-    print params
+    print(params)
 
 
 def task(params):
-    print params
+    print(params)
 
 
 def notifications(params):
-    print params
+    print(params)
 
 
 def health(params):
-    print params
+    print(params)
 
 
 def deploy(params):
-    print params
+    print(params)
 
 
 actions = {
@@ -63,19 +115,17 @@ actions = {
             "args": ["-l", "--list"],
             "params": {
                 "dest": "list",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "List all available releases.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["-c", "--config"],
             "params": {
                 "dest": "config",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Configure release with --rid",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["--rid"],
@@ -94,19 +144,17 @@ actions = {
             "args": ["-l", "--list"],
             "params": {
                 "dest": "list",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "List all available environments.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["--create"],
             "params": {
                 "dest": "create",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Create a new environment with specific eid and name.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["--eid", "--environment-id"],
@@ -130,19 +178,17 @@ actions = {
             "args": ["--rename", "--environment-rename"],
             "params": {
                 "dest": "rename",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Rename environment with specific eid or ename",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["-d", "--delete", "--environment-delete"],
             "params": {
                 "dest": "delete",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Delete environment with specific eid or ename",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["--nname", "--new-name"],
@@ -161,28 +207,25 @@ actions = {
             "args": ["-l", "--list"],
             "params": {
                 "dest": "list",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "List all nodes.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["--alloc"],
             "params": {
                 "dest": "alloc",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "List only allocated nodes.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["-s", "--set"],
             "params": {
                 "dest": "set",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Set role for specific node.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["--nid", "--node-id"],
@@ -206,10 +249,9 @@ actions = {
             "args": ["-rm", "--remove"],
             "params": {
                 "dest": "remove",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Remove node with specific nid.",
-                "default": None
+                "default": False
             }
         }]
     },
@@ -219,46 +261,41 @@ actions = {
             "args": ["-d", "--default"],
             "params": {
                 "dest": "default",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Open default configuration.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["-m", "--modify"],
             "params": {
                 "dest": "modify",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Modify current configuration.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["-v", "--verify"],
             "params": {
                 "dest": "verify",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Verify current configuration.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["-c", "--cancel"],
             "params": {
                 "dest": "cancel",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Remove current changes in configuration.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["-s", "--save"],
             "params": {
                 "dest": "save",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Save current changes in configuration.",
-                "default": None
+                "default": False
             }
         }]
     },
@@ -268,37 +305,33 @@ actions = {
             "args": ["-d", "--default"],
             "params": {
                 "dest": "default",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Open default configuration.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["-m", "--modify"],
             "params": {
                 "dest": "modify",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Modify current configuration.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["-c", "--cancel"],
             "params": {
                 "dest": "cancel",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Remove current changes in configuration.",
-                "default": None
+                "default": False
             }
         }, {
             "args": ["-s", "--save"],
             "params": {
                 "dest": "save",
-                "action": "store",
-                "type": str,
+                "action": "store_true",
                 "help": "Save current changes in configuration.",
-                "default": None
+                "default": False
             }
         }]
     },
@@ -346,7 +379,8 @@ actions = {
                 "dest": "source",
                 "action": "store",
                 "type": str,
-                "help": "Service to use as log source. e.g. `web backend`, REST, orchestrator",
+                "help": "Service to use as log source. "
+                        "(web backend, REST, orchestrator)",
                 "default": None
             }
         }, {
@@ -399,7 +433,7 @@ if __name__ == '__main__':
     params, other_params = parser.parse_known_args()
     sys.argv.pop(1)
 
-    if not params.action in actions:
+    if params.action not in actions:
         parser.print_help()
         sys.exit(0)
 
