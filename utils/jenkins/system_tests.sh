bf710be6983a7e73b7f9434e9586ae1864bb51c8
Dmitry Ilyin <idv1985@gmail.com>
2013-09-03 14:12:04 +0400

    Refactoring of system_tests

--- utils/jenkins/system_tests.sh
+++ utils/jenkins/system_tests.sh
@@ -1,56 +1,105 @@
 #!/bin/sh
-# System Tests Script
-#
-# It can perform several actions depending on Jenkins JOB_NAME it is ran from.
-# If task name is "iso" it will make iso file
-# Other defined test names will ran nose tests with made iso file
-# Iso file name is taken from job name prefix
-# Task name is taken from job name suffix
-# Separator is one dot '.'
-#
-# For example if task name is:
-# mytest.somestring.iso
-# ISO name: mytest.iso
-# Task name: iso
-# If ran with such JOB_NAME iso file with name mytest.iso will be created
-# If task name is:
-# mytest.somestring.node
-# ISO name: mytest.iso
-# Task name: node
-# If ran with such JOB_NAME node tests will be ran using iso file mytest.iso
-#
-# First you should ran mytest.somestring.iso to create mytest.iso
-# Then you can ran mytest.somestring.node to run tests using mytest.iso
-
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-
-# variable checks
-
-if [ -z "${JENKINS_HOME}" ]; then
-  echo "Error! JENKINS_HOME is not set!"
-  exit 1
-fi
-
-if [ -z "${JOB_NAME}" ]; then
-  echo "Error! JOB_NAME is not set!"
-  exit 1
-fi
-
-if [ -z "${WORKSPACE}" ]; then
-  echo "Error! WORKSPACE is not set!"
-  exit 1
-fi
-
-# global paths
-
-ISO_DIR="${JENKINS_HOME}/iso"
-ISO_NAME="${JOB_NAME%.*}.iso"
-ISO_PATH="${ISO_DIR}/${ISO_NAME}"
-TASK_NAME="${JOB_NAME##*.}"
 
 # functions
 
-function MakeISO {
+GlobalPaths() {
+  # global paths
+
+  ISO_DIR="${JENKINS_HOME}/iso"
+  ISO_NAME="${JOB_NAME%.*}.iso"
+  ISO_PATH="${ISO_DIR}/${ISO_NAME}"
+  TASK_NAME="${JOB_NAME##*.}"
+}
+
+GetoptsVariables() {
+  while getopts ":j:n:w:h" opt; do
+    case $opt in
+      j)
+        JENKINS_HOME="${OPTARG}"
+        ;;
+      n)
+        JOB_NAME="${OPTARG}"
+        ;;
+      w)
+        WORKSPACE="${OPTARG}"
+        ;;
+      h)
+        ShowHelp
+        exit 0
+        ;;
+      \?)
+        echo "Invalid option: -$OPTARG" >&2
+        ShowHelp
+        exit 1
+        ;;
+      :)
+        echo "Option -$OPTARG requires an argument." >&2
+        ShowHelp
+        exit 1
+        ;;
+    esac
+  done
+}
+
+ShowHelp() {
+cat <<EOF
+System Tests Script
+
+It can perform several actions depending on Jenkins JOB_NAME it is ran from
+or it can take variables from arguments if you do need to override these values.
+
+If task name is "iso" it will make iso file
+Other defined test names will ran nose tests with made iso file
+
+Iso file name is taken from job name prefix
+Task name is taken from job name suffix
+Separator is one dot '.'
+
+For example if task name is:
+mytest.somestring.iso
+ISO name: mytest.iso
+Task name: iso
+If ran with such JOB_NAME iso file with name mytest.iso will be created
+If task name is:
+mytest.somestring.node
+ISO name: mytest.iso
+Task name: node
+If ran with such JOB_NAME node tests will be ran using iso file mytest.iso
+
+First you should ran mytest.somestring.iso to create mytest.iso
+Then you can ran mytest.somestring.node to run tests using mytest.iso and other tests too.
+
+You also can override variables with these options:
+
+  -j (path) - JENKINS_HOME
+  -n (name) - JOB_NAME
+  -w (path) - WORKSPACE
+  -h        - Show this help
+
+EOF
+}
+
+CheckVariables() {
+  # variable checks
+
+  if [ -z "${JENKINS_HOME}" ]; then
+    echo "Error! JENKINS_HOME is not set!"
+    exit 1
+  fi
+
+  if [ -z "${JOB_NAME}" ]; then
+    echo "Error! JOB_NAME is not set!"
+    exit 1
+  fi
+
+  if [ -z "${WORKSPACE}" ]; then
+    echo "Error! WORKSPACE is not set!"
+    exit 1
+  fi
+}
+
+MakeISO() {
   # Create iso file to be used in tests
 
   # choose mirror to build iso from
@@ -102,7 +151,7 @@ function MakeISO {
   exit 0
 }
 
-function RunTest {
+RunTest() {
   # Run test selected by task name
 
   # check if iso file exists
@@ -116,12 +165,14 @@ function RunTest {
 
   export ENV_NAME="${JOB_NAME}_system_test"
   export LOGS_DIR="${WORKSPACE}/logs"
+  export ISO_NAME
+  export ISO_PATH
 
   # remove previous garbage
   dos.py erase "${ENV_NAME}"
 
   # run python test set to create environments, deploy and test product
-  nosetests -w "${JOB_NAME}" -s -l DEBUG --with-xunit "${1}"
+  nosetests -w "${WORKSPACE}" -s -l DEBUG --with-xunit "${1}"
 
   # remove created environment
   dos.py destroy "${ENV_NAME}"
@@ -129,29 +180,35 @@ function RunTest {
   exit 0
 }
 
+RouteTasks() {
+  # this selector defines task names that are recognised by this script
+  # and runs corresponding jobs for them
+
+  case "${TASK_NAME}" in
+  admin_node)
+    RunTest "fuelweb_test.integration.test_admin_node"
+    ;;
+  node)
+    RunTest "fuelweb_test.integration.test_node:TestNode"
+    ;;
+  node_ha)
+    RunTest "fuelweb_test.integration.test_node_ha"
+    ;;
+  node_ha2)
+    RunTest "fuelweb_test.integration.test_node_ha2"
+    ;;
+  iso)
+    MakeISO
+    ;;
+  *)
+    echo "Unknown task: ${TASK_NAME}!"
+    exit 1
+    ;;
+  esac
+}
+
 # MAIN
-
-# this selector defines task names that are recognised by this script
-# and runs corresponding jobs for them
-case "${TASK_NAME}" in
-admin_node)
-  RunTest "fuelweb_test.integration.test_admin_node"
-  ;;
-node)
-  RunTest "fuelweb_test.integration.test_node:TestNode"
-  ;;
-node_ha)
-  RunTest "fuelweb_test.integration.test_node_ha"
-  ;;
-node_ha2)
-  RunTest "fuelweb_test.integration.test_node_ha2"
-  ;;
-iso)
-  MakeISO
-  ;;
-*)
-  echo "Unknown task: ${TASK_NAME}!"
-  exit 1
-  ;;
-esac
-
+GetoptsVariables $@
+CheckVariables
+GlobalPaths
+RouteTasks
