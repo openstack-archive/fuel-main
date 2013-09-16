6f189b58959d8876b692e6cfa439ac9767914a2b
Andrey Korolyov <akorolev@mirantis.com>
2013-09-16 19:46:52 +0400

    unbroke make mirror

--- mirror/ubuntu/boot.mk
+++ mirror/ubuntu/boot.mk
@@ -1,11 +1,11 @@
 ISOLINUX_FILES:=netboot.tar.gz
 
 # debian isolinux files
-$(addprefix $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/,$(ISOLINUX_FILES)):
-	@mkdir -p $(ISO_DIR)/isoroot/ubuntu/installer-amd64/current/images/netboot $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)
+$(addprefix $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/,$(ISOLINUX_FILES)):
+	@mkdir -p $(@D)
 	wget -O $@ $(MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/$(@F)
-	tar -xzf $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/$(@F) -C $(ISO_DIR)/isoroot/ubuntu/installer-amd64/current/images/netboot
+	tar -xzf $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/$(@F) -C $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/
 
 $(BUILD_DIR)/mirror/ubuntu/boot.done: \
-		$(addprefix $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/,$(ISOLINUX_FILES))
+		$(addprefix $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/,$(ISOLINUX_FILES))
 	$(ACTION.TOUCH)
