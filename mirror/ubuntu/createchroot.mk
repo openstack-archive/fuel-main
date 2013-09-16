b6e52d4084e26eba121a6d6bef40de9aae8cfaa7
Andrey Korolyov <andrey@xdel.ru>
2013-09-17 02:13:16 +0400

    ubuntu repo creation, finalize

--- mirror/ubuntu/createchroot.mk
+++ mirror/ubuntu/createchroot.mk
@@ -9,4 +9,5 @@ $(BUILD_DIR)/mirror/ubuntu/createchroot.done:
 	sudo mkdir -p $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/repo/conf
 	sudo cp -a $(SOURCE_DIR)/mirror/ubuntu/mkrepo.sh $(SOURCE_DIR)/mirror/ubuntu/distributions $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/repo/conf
 	sudo chroot $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot /bin/bash -c "chmod +x /repo/conf/mkrepo.sh && /repo/conf/mkrepo.sh"
+	sudo mv $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/repo/* $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/ && sudo rm -rf $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/
 	$(ACTION.TOUCH)
