1991597bc1d583b802e2eada6265f811fa62d6da
Alexandr Notchenko <anotchenko@mirantis.com>
2013-09-18 17:03:51 +0400

    added request body test

--- nailgun/nailgun/test/integration/test_cluster_handler.py
+++ nailgun/nailgun/test/integration/test_cluster_handler.py
@@ -250,10 +250,11 @@ class TestHandlers(BaseIntegrationTest):
             nodes_kwargs=[
                 {'pending_addition': True},
                 {'online': False, 'status': 'ready'}])
-
+        cluster = self.env.clusters[0]
         get_resp = self.app.get(
             reverse('ClusterGeneratedData',
-                    kwargs={'cluster_id': self.env.clusters[0].id}),
+                    kwargs={'cluster_id': cluster.id}),
             headers=self.default_headers
         )
         self.assertEquals(get_resp.status, 200)
+        self.datadiff(json.loads(get_resp.body), cluster.attributes.generated)
