8ef70fce64a28002a708f16bd987e479c30a9644
Alexandr Notchenko <anotchenko@mirantis.com>
2013-09-19 15:56:22 +0400

    Fixed pep8 in urls.py ignore rule removed from run_test.sh

--- nailgun/nailgun/api/urls.py
+++ nailgun/nailgun/api/urls.py
@@ -16,145 +16,144 @@
 
 import web
 
-from nailgun.api.handlers.cluster import ClusterHandler
-from nailgun.api.handlers.cluster import ClusterCollectionHandler
-from nailgun.api.handlers.cluster import ClusterChangesHandler
-from nailgun.api.handlers.cluster import ClusterAttributesHandler
 from nailgun.api.handlers.cluster import ClusterAttributesDefaultsHandler
+from nailgun.api.handlers.cluster import ClusterAttributesHandler
+from nailgun.api.handlers.cluster import ClusterChangesHandler
+from nailgun.api.handlers.cluster import ClusterCollectionHandler
 from nailgun.api.handlers.cluster import ClusterGeneratedData
-from nailgun.api.handlers.orchestrator import DefaultProvisioningInfo
-from nailgun.api.handlers.orchestrator import DefaultDeploymentInfo
-from nailgun.api.handlers.orchestrator import ProvisioningInfo
-from nailgun.api.handlers.orchestrator import DeploymentInfo
+from nailgun.api.handlers.cluster import ClusterHandler
+from nailgun.api.handlers.disks import NodeDefaultsDisksHandler
+from nailgun.api.handlers.disks import NodeDisksHandler
+from nailgun.api.handlers.logs import LogEntryCollectionHandler
+from nailgun.api.handlers.logs import LogPackageHandler
+from nailgun.api.handlers.logs import LogSourceByNodeCollectionHandler
+from nailgun.api.handlers.logs import LogSourceCollectionHandler
 
 from nailgun.api.handlers.network_configuration \
     import NetworkConfigurationHandler
 from nailgun.api.handlers.network_configuration \
     import NetworkConfigurationVerifyHandler
 
-from nailgun.api.handlers.redhat import RedHatSetupHandler
-from nailgun.api.handlers.redhat import RedHatAccountHandler
-from nailgun.api.handlers.release import ReleaseHandler
-from nailgun.api.handlers.release import ReleaseCollectionHandler
-
-from nailgun.api.handlers.node import NodesAllocationStatsHandler
-from nailgun.api.handlers.node import NodeHandler
 from nailgun.api.handlers.node import NodeCollectionHandler
+from nailgun.api.handlers.node import NodeHandler
+from nailgun.api.handlers.node import NodesAllocationStatsHandler
 
-from nailgun.api.handlers.disks import NodeDisksHandler
-from nailgun.api.handlers.disks import NodeDefaultsDisksHandler
-from nailgun.api.handlers.disks import NodeVolumesInformationHandler
-
-from nailgun.api.handlers.node import NodeNICsHandler
-from nailgun.api.handlers.node import NodeNICsDefaultHandler
-from nailgun.api.handlers.node import NodeCollectionNICsHandler
 from nailgun.api.handlers.node import NodeCollectionNICsDefaultHandler
+from nailgun.api.handlers.node import NodeCollectionNICsHandler
+from nailgun.api.handlers.node import NodeNICsDefaultHandler
+from nailgun.api.handlers.node import NodeNICsHandler
 from nailgun.api.handlers.node import NodeNICsVerifyHandler
 
-from nailgun.api.handlers.tasks import TaskHandler
-from nailgun.api.handlers.tasks import TaskCollectionHandler
-
-from nailgun.api.handlers.notifications import NotificationHandler
 from nailgun.api.handlers.notifications import NotificationCollectionHandler
+from nailgun.api.handlers.notifications import NotificationHandler
 
-from nailgun.api.handlers.logs import LogEntryCollectionHandler
-from nailgun.api.handlers.logs import LogPackageHandler
-from nailgun.api.handlers.logs import LogSourceCollectionHandler
-from nailgun.api.handlers.logs import LogSourceByNodeCollectionHandler
-
-from nailgun.api.handlers.registration import FuelKeyHandler
-
-from nailgun.api.handlers.version import VersionHandler
+from nailgun.api.handlers.orchestrator import DefaultDeploymentInfo
+from nailgun.api.handlers.orchestrator import DefaultProvisioningInfo
+from nailgun.api.handlers.orchestrator import DeploymentInfo
+from nailgun.api.handlers.orchestrator import ProvisioningInfo
 
 from nailgun.api.handlers.plugin import PluginCollectionHandler
 from nailgun.api.handlers.plugin import PluginHandler
 
+from nailgun.api.handlers.redhat import RedHatAccountHandler
+from nailgun.api.handlers.redhat import RedHatSetupHandler
+from nailgun.api.handlers.registration import FuelKeyHandler
+from nailgun.api.handlers.release import ReleaseCollectionHandler
+from nailgun.api.handlers.release import ReleaseHandler
+
+from nailgun.api.handlers.tasks import TaskCollectionHandler
+from nailgun.api.handlers.tasks import TaskHandler
+
+from nailgun.api.handlers.version import VersionHandler
+
+
 urls = (
     r'/releases/?$',
-    'ReleaseCollectionHandler',
+    ReleaseCollectionHandler.__name__,
     r'/releases/(?P<release_id>\d+)/?$',
-    'ReleaseHandler',
+    ReleaseHandler.__name__,
 
     r'/clusters/?$',
-    'ClusterCollectionHandler',
+    ClusterCollectionHandler.__name__,
     r'/clusters/(?P<cluster_id>\d+)/?$',
-    'ClusterHandler',
+    ClusterHandler.__name__,
     r'/clusters/(?P<cluster_id>\d+)/changes/?$',
-    'ClusterChangesHandler',
+    ClusterChangesHandler.__name__,
     r'/clusters/(?P<cluster_id>\d+)/attributes/?$',
-    'ClusterAttributesHandler',
+    ClusterAttributesHandler.__name__,
     r'/clusters/(?P<cluster_id>\d+)/attributes/defaults/?$',
-    'ClusterAttributesDefaultsHandler',
+    ClusterAttributesDefaultsHandler.__name__,
     r'/clusters/(?P<cluster_id>\d+)/network_configuration/?$',
-    'NetworkConfigurationHandler',
+    NetworkConfigurationHandler.__name__,
     r'/clusters/(?P<cluster_id>\d+)/network_configuration/verify/?$',
-    'NetworkConfigurationVerifyHandler',
+    NetworkConfigurationVerifyHandler.__name__,
+
     r'/clusters/(?P<cluster_id>\d+)/orchestrator/deployment/?$',
-    'DeploymentInfo',
+    DeploymentInfo.__name__,
     r'/clusters/(?P<cluster_id>\d+)/orchestrator/deployment/defaults/?$',
-    'DefaultDeploymentInfo',
+    DefaultDeploymentInfo.__name__,
     r'/clusters/(?P<cluster_id>\d+)/orchestrator/provisioning/?$',
-    'ProvisioningInfo',
+    ProvisioningInfo.__name__,
     r'/clusters/(?P<cluster_id>\d+)/orchestrator/provisioning/defaults/?$',
-    'DefaultProvisioningInfo',
+    DefaultProvisioningInfo.__name__,
     r'/clusters/(?P<cluster_id>\d+)/generated/?$',
-    'ClusterGeneratedData',
+    ClusterGeneratedData.__name__,
 
     r'/nodes/?$',
-    'NodeCollectionHandler',
+    NodeCollectionHandler.__name__,
     r'/nodes/(?P<node_id>\d+)/?$',
-    'NodeHandler',
+    NodeHandler.__name__,
     r'/nodes/(?P<node_id>\d+)/disks/?$',
-    'NodeDisksHandler',
+    NodeDisksHandler.__name__,
     r'/nodes/(?P<node_id>\d+)/disks/defaults/?$',
-    'NodeDefaultsDisksHandler',
+    NodeDefaultsDisksHandler.__name__,
     r'/nodes/(?P<node_id>\d+)/volumes/?$',
     r'NodeVolumesInformationHandler',
     r'/nodes/interfaces/?$',
-    'NodeCollectionNICsHandler',
+    NodeCollectionNICsHandler.__name__,
     r'/nodes/interfaces/default_assignment?$',
-    'NodeCollectionNICsDefaultHandler',
+    NodeCollectionNICsDefaultHandler.__name__,
     r'/nodes/(?P<node_id>\d+)/interfaces/?$',
-    'NodeNICsHandler',
+    NodeNICsHandler.__name__,
     r'/nodes/(?P<node_id>\d+)/interfaces/default_assignment?$',
-    'NodeNICsDefaultHandler',
+    NodeNICsDefaultHandler.__name__,
     r'/nodes/interfaces_verify/?$',
-    'NodeNICsVerifyHandler',
+    NodeNICsVerifyHandler.__name__,
     r'/nodes/allocation/stats/?$',
-    'NodesAllocationStatsHandler',
+    NodesAllocationStatsHandler.__name__,
     r'/tasks/?$',
-    'TaskCollectionHandler',
+    TaskCollectionHandler.__name__,
     r'/tasks/(?P<task_id>\d+)/?$',
-    'TaskHandler',
+    TaskHandler.__name__,
 
     r'/notifications/?$',
-    'NotificationCollectionHandler',
+    NotificationCollectionHandler.__name__,
     r'/notifications/(?P<notification_id>\d+)/?$',
-    'NotificationHandler',
+    NotificationHandler.__name__,
 
     r'/logs/?$',
-    'LogEntryCollectionHandler',
+    LogEntryCollectionHandler.__name__,
     r'/logs/package/?$',
-    'LogPackageHandler',
+    LogPackageHandler.__name__,
     r'/logs/sources/?$',
-    'LogSourceCollectionHandler',
+    LogSourceCollectionHandler.__name__,
     r'/logs/sources/nodes/(?P<node_id>\d+)/?$',
-    'LogSourceByNodeCollectionHandler',
+    LogSourceByNodeCollectionHandler.__name__,
 
     r'/registration/key/?$',
-    'FuelKeyHandler',
+    FuelKeyHandler.__name__,
 
     r'/version/?$',
-    'VersionHandler',
+    VersionHandler.__name__,
 
     r'/plugins/?$',
-    'PluginCollectionHandler',
+    PluginCollectionHandler.__name__,
     r'/plugins/(?P<plugin_id>\d+)/?$',
-    'PluginHandler',
+    PluginHandler.__name__,
     r'/redhat/account/?$',
-    'RedHatAccountHandler',
+    RedHatAccountHandler.__name__,
     r'/redhat/setup/?$',
-    'RedHatSetupHandler',
+    RedHatSetupHandler.__name__,
 )
 
 app = web.application(urls, locals())
