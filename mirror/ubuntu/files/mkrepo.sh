8141fb94ea7a2f8ea7c376a21b9f06360d6b8a50
Andrey Korolyov <akorolev@mirantis.com>
2013-09-30 22:11:12 +0400

    never create files aside
    Change-Id: Icb78876f5b20d7fa754ca4edb536bc11748b3f16

--- mirror/ubuntu/files/mkrepo.sh
+++ mirror/ubuntu/files/mkrepo.sh
@@ -8,7 +8,7 @@ mkdir -p /repo/pool/debian-installer /repo/pool/main
 cd /repo/pool/debian-installer
 # Grab every udeb
 for udeb in $(wget -qO - http://mirror.yandex.ru/ubuntu/dists/precise/main/debian-installer/binary-amd64/Packages.bz2 | bzip2 -cd | sed -ne 's/^Filename: //p'); do
-        wget http://mirror.yandex.ru/ubuntu/$udeb
+        wget -N http://mirror.yandex.ru/ubuntu/$udeb
 done
 # Get rid of urlencoded names
 for i in $(ls | grep %) ; do mv $i $(echo $i | echo -e $(sed 's/%/\\x/g')) ; done
@@ -24,7 +24,7 @@ for i in $(ls | grep %) ; do mv $i $(echo $i | echo -e $(sed 's/%/\\x/g')) ; don
 mkdir -p /repo/indices
 cd /repo/indices
 for idx in override.precise.main override.precise.extra.main override.precise.main.debian-installer ; do
-	wget http://mirror.yandex.ru/ubuntu/indices/$idx
+	wget -N http://mirror.yandex.ru/ubuntu/indices/$idx
 done
 cd /repo
 # Just because apt scan will produce crap
