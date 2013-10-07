bdc5cf002801ff232f60d16f025f1ca2fe3e6fa2
Andrey Korolyov <akorolev@mirantis.com>
2013-10-07 17:27:18 +0400

    proper d-i bootstrap file placement
    Change-Id: I2b0a772195230ca6fed420154f1cea2f760c3996

--- mirror/ubuntu/boot.mk
+++ mirror/ubuntu/boot.mk
@@ -3,7 +3,7 @@ ISOLINUX_FILES:=netboot.tar.gz
 # debian isolinux files
 $(addprefix $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/,$(ISOLINUX_FILES)):
 	@mkdir -p $(@D)
-	wget -O $@ $(MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/$(@F)
+	wget -O $@ $(MIRROR_UBUNTU)/installer-amd64/current/images/netboot/$(@F)
 	tar -xzf $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/$(@F) -C $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/
 
 $(BUILD_DIR)/mirror/ubuntu/boot.done: \
