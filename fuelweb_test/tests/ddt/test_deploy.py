890a35cedf49276418f1f47b99198bffdc323d47
nfedotov <nfedotov@mirantis.com>
2013-10-28 12:30:34 +0400

    Data-driven test.
    - It deploys envirnoment according to provided
    parameters. It does not perform any verification or additional
    runs (network verification, OSTF).
    - Test parameters are stored in Google spreadsheet
    so you need to export following envirnment variables:
    GOOGLE_LOGIN, GOOGLE_PASSWORD, GOOGLE_SPREEDSHEET.
    GOOGLE_SPREEDSHEET is spreadsheet_key, could be found in
    a spreadsheet URL
    
    Change-Id: I0157f4b85059252ae5b2f97769ce1dc7886d390c

---
+++ fuelweb_test/tests/ddt/test_deploy.py
@@ -0,0 +1,110 @@
+import logging
+import types
+from proboscis import test, factory
+from fuelweb_test.helpers.decorators import log_snapshot_on_error
+from fuelweb_test.tests.base_test_case import TestBasic, SetupEnvironment
+from fuelweb_test.tests.ddt.gtestdata import TestParamsList
+
+
+#logging.basicConfig(
+#    format=':%(lineno)d: %(asctime)s %(message)s',
+#    level=logging.DEBUG
+#)
+
+logger = logging.getLogger(__name__)
+
+@factory
+def test_deploy_factory():
+    return [TestDeploy(test_params) for test_params in TestParamsList()]
+
+
+@test(groups=['ddt'])
+class TestDeploy(TestBasic):
+
+    def __init__(self, params):
+        super(TestDeploy, self).__init__()
+        self.params = params
+
+    @test(depends_on=[SetupEnvironment.prepare_release])
+    @log_snapshot_on_error
+    def deploy_environment(self):
+        logger.info(self.params)
+
+        self.env.revert_snapshot("ready")
+        self.env.bootstrap_nodes(
+            self.env.devops_nodes_by_names(self.params.nodes.keys()))
+
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.params.environment['name'],
+            release_name=self.params.environment['release'],
+            mode=self.params.environment['mode'],
+            settings=self.params.environment['settings']
+        )
+        cluster = self.fuel_web.client.get_cluster(cluster_id)
+        nailgun_nodes = \
+            self.fuel_web.update_nodes(cluster_id,  self.params.nodes)
+
+        # update node's interfaces
+        for node in nailgun_nodes:
+            self.fuel_web.update_node_networks(
+                node['id'], self.params.interfaces)
+
+        # update environment attributes
+        attributes = self.fuel_web.client.get_cluster_attributes(cluster_id)
+        attributes = self._update_dict(attributes, self.params.settings)
+        self.fuel_web.client.update_cluster_attributes(cluster_id, attributes)
+
+        # update environment networks
+        networks = self.fuel_web.client.get_networks(cluster_id)
+
+        for net_conf in self.params.networks['networks']:
+            for net in networks['networks']:
+                if net["name"] == net_conf['name']:
+                    net.update(net_conf)
+                    break
+
+        # update neutron parameters
+        if 'neutron_parameters' in self.params.networks:
+            networks['neutron_parameters'].update(
+                self.params.networks['neutron_parameters'])
+        # update nova parameters
+        if 'net_manager' in self.params.networks:
+            networks['net_manager'] = self.params.networks['net_manager']
+
+        self.fuel_web.client.client.put(
+            "/api/clusters/%d/network_configuration/%s" %
+            (cluster_id, cluster['net_provider']), networks
+        )
+
+        # deploy changes
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        self.env.make_snapshot(self.params.environment['name'])
+
+    # Help methods
+    def _update_dict(self, target, source):
+        """
+            Updates nested dictionary values.
+        """
+        def values_are_dicts(dictionary):
+            for v in dictionary.values():
+                if isinstance(v, dict):
+                    return True
+            return False
+
+        for key, value in target.items():
+            if key in source:
+                is_unicode = isinstance(value, unicode)
+                is_string = isinstance(value, str)
+                is_list = isinstance(value, list)
+                if is_unicode or is_string or is_list:
+                    target[key] = source[key]
+                    continue
+
+                if values_are_dicts(value):
+                    target[key] = self._update_dict(target[key], source[key])
+                else:
+                    target[key].update(source[key])
+        return target
+
+
