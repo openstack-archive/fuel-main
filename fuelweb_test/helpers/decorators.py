375fbbb48c343fc3a16788cc542423c9b99c4e66
Nikita Prikazchikov <nprikazchikov@mirantis.com>
2013-10-29 19:23:44 +0400

    Refactoring system tests on python-proboscis
    Change-Id: Iae82bece4923db898836db405cddc81f94526913

---
+++ fuelweb_test/helpers/decorators.py
@@ -0,0 +1,109 @@
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import functools
+import json
+import logging
+import os
+import time
+import urllib2
+from proboscis import SkipTest
+from fuelweb_test.settings import *
+from devops.helpers.helpers import SSHClient
+
+
+def save_logs(url, filename):
+    logging.info('Saving logs to "{}" file'.format(filename))
+    try:
+        with open(filename, 'w') as f:
+            f.write(
+                urllib2.urlopen(url).read()
+            )
+    except urllib2.HTTPError, e:
+        logging.error(e)
+
+
+def log_snapshot_on_error(func):
+    """
+    Decorator to snapshot environment when error occurred in test.
+    And always fetch diagnostic snapshot from master node
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwagrs):
+        status = "pass"
+        try:
+            return func(*args, **kwagrs)
+        except SkipTest:
+            pass
+        except:
+            status = "fail"
+            name = 'error_%s' % func.__name__
+            description = "Failed in method '%s'" % func.__name__
+            logging.debug("Snapshot {} {}".format(name, description))
+            logging.debug(
+                "You could revert this snapshot using [{command}]".format(
+                    command=
+                    "dos.py revert {env} --snapshot-name {name} && "
+                    "dos.py resume {env}".format(
+                        env=ENV_NAME, name=name
+                    )
+                )
+            )
+            if args[0].env is not None:
+                args[0].env.make_snapshot(snapshot_name=name[-50:])
+            raise
+        finally:
+            if LOGS_DIR and False:
+                if not os.path.exists(LOGS_DIR):
+                    os.makedirs(LOGS_DIR)
+
+                env = args[0].env
+                env.get_virtual_environment().resume()
+                task = env.fuel_web.client.generate_logs()
+                task = env.fuel_web.task_wait(task, 60 * 5)
+                url = "http://{}:8000{}".format(
+                    env.get_admin_node_ip(), task['message']
+                )
+                log_file_name = '{status}_{name}-{time}.tar.gz'.format(
+                    status=status,
+                    name=func.__name__,
+                    time=time.strftime("%Y_%m_%d__%H_%M_%S", time.gmtime())
+                )
+                save_logs(url, os.path.join(LOGS_DIR, log_file_name))
+    return wrapper
+
+
+def debug(logger):
+    def wrapper(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            logger.debug(
+                "Calling: {} with args: {} {}".format(
+                    func.__name__, args, kwargs
+                )
+            )
+            result = func(*args, **kwargs)
+            logger.debug(
+                "Done: {} with result: {}".format(func.__name__, result))
+            return result
+        return wrapped
+    return wrapper
+
+
+def json_parse(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        response = func(*args, **kwargs)
+        return json.loads(response.read())
+    return wrapped
--- utils/jenkins/system_tests.sh
+++ utils/jenkins/system_tests.sh
@@ -35,8 +35,8 @@ if you do need to override them.
               Made from iso dir and name if not set.
 -t (name)   - Name of task this script should perform. Should be one of defined ones.
               Taken from Jenkins' job's suffix if not set.
--T (class)  - Overrides what tests should be ran insted of using task system.
-              Set if you want to run custom test.
+-o (str)    - Allows you any extra command line option to run test job if you
+              want to use some parameters.
 -a (str)    - Allows you to path NOSE_ATTR to the test job if you want
               to use some parameters.
 -A (str)    - Allows you to path  NOSE_EVAL_ATTR if you want to enter attributes
@@ -123,7 +123,7 @@ GlobalVariables() {
 }
 
 GetoptsVariables() {
-  while getopts ":w:j:i:t:T:a:A:m:U:r:b:dkKe:h" opt; do
+  while getopts ":w:j:i:t:o:a:A:m:U:r:b:dkKeh" opt; do
     case $opt in
       w)
         WORKSPACE="${OPTARG}"
@@ -137,8 +137,8 @@ GetoptsVariables() {
       t)
         TASK_NAME="${OPTARG}"
         ;;
-      T)
-        TEST_NAME="${OPTARG}"
+      o)
+        TEST_OPTIONS="${TEST_OPTIONS} ${OPTARG}"
         ;;
       a)
         NOSE_ATTR="${OPTARG}"
@@ -322,14 +322,6 @@ CdWorkSpace() {
     fi
 }
 
-RunCustomTest() {
-  # if TEST_NAME is set we override task selector and run this test instead
-  if [ -n "${TEST_NAME}" ]; then
-    RunTest "${TEST_NAME}"
-    exit 0
-  fi
-}
-
 RunTest() {
     # Run test selected by task name
 
@@ -385,12 +377,19 @@ RunTest() {
     if [ -n "${NOSE_EVAL_ATTR}" ]; then
         OPTS="${OPTS} -A ${NOSE_EVAL_ATTR}"
     fi
+    if [ -n "${TEST_OPTIONS}" ]; then
+        OPTS="${OPTS} ${TEST_OPTIONS}"
+    fi
 
     # run python test set to create environments, deploy and test product
     if [ "${DRY_RUN}" = "yes" ]; then
-        echo nosetests -v -w "fuelweb_test" -s -l DEBUG ${OPTS} --with-xunit "${1}"
+        echo export PYTHONPATH="${WORKSPACE} ${PYTHONPATH}"
+        echo python fuelweb_test/run_tests.py -v -s --with-xunit ${OPTS}
     else
-        nosetests -v -w "fuelweb_test" -s -l DEBUG ${OPTS} --with-xunit "${1}"
+        export PYTHONPATH=${WORKSPACE} ${PYTHONPATH}
+        echo ${PYTHONPATH}
+        python fuelweb_test/run_tests.py -v -s --with-xunit ${OPTS}
+
     fi
     ec=$?
 
@@ -439,9 +438,6 @@ CheckVariables
 
 # first we chdir into our working directory unless we dry run
 CdWorkSpace
-
-# run custom test if TEST_NAME is set
-RunCustomTest
 
 # finally we can choose what to do according to TASK_NAME
 RouteTasks
