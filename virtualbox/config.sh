7755e14d08e11c9ccb75fb6e95d751480187f48c
Miroslav Anashkin <manashkin@mirantis.com>
2013-12-12 23:33:54 +0400

    Support for Windows in Virtualbox scripts
    Added support for Cygwin.
    Increased default disk and memory sizes for compatibility with Heat.
    Added checks for non-installed Cygwin components.
    Default NIC type changed to faster AMD PCnet FAST III.
    Added check on network settings application.
    Added trick to increase network configuration stability under Windows.
    Changed hard-coded interface addition part to automatic loop.
    Removed extra whitespaces.
    Rebased to latest commit.
    
    Closes-Bug: #1252314
    Change-Id: I466ebec87506eade5164de7d27d73f10dbbea0d9

--- virtualbox/config.sh
+++ virtualbox/config.sh
@@ -27,9 +27,44 @@ iso_path=`ls -1t iso/*.iso 2>/dev/null | head -1`
 vm_name_prefix=fuel-
 
 # Host interfaces to bridge VMs interfaces with
+# VirtualBox has different virtual NIC naming convention and index base
+# between Windows and Linux/MacOS
 idx=0
-for ip in 10.20.0.1 172.16.0.1 172.16.1.1; do
-  host_nic_name[$idx]=vboxnet$idx
+# Please add the IPs accordingly if you going to create non-default NICs number
+# 10.20.0.1/24   - Fuel Admin network
+# 172.16.0.1/24  - OpenStack Public/External/Floating network
+# 172.16.1.1/24  - OpenStack Fixed/Internal/Private network
+# 192.168.0.1/24 - OpenStack Management network
+# 192.168.1.1/24 - OpenStack Storage network (for Ceph, Swift etc)
+for ip in 10.20.0.1 172.16.0.1 172.16.1.1 ; do
+# VirtualBox for Windows has different virtual NICs naming and indexing
+  if [ $idx -eq 0 ]; then
+    case "$(uname)" in
+      Linux | Darwin)
+        host_nic_name[$idx]=vboxnet$idx
+      ;;
+      CYGWIN*)
+        host_nic_name[$idx]='VirtualBox Host-Only Ethernet Adapter'
+      ;;
+      *)
+        echo "$(uname) is not supported operating system."
+        exit 1
+      ;;
+    esac
+  else
+    case "$(uname)" in
+      Linux | Darwin)
+        host_nic_name[$idx]=vboxnet$idx
+      ;;
+      CYGWIN*)
+        host_nic_name[$idx]='VirtualBox Host-Only Ethernet Adapter #'$((idx+1))
+      ;;
+      *)
+        echo "$(uname) is not supported operating system."
+        exit 1
+      ;;
+    esac
+  fi
   host_nic_ip[$idx]=$ip
   host_nic_mask[$idx]=255.255.255.0
   idx=$((idx+1))
@@ -38,7 +73,7 @@ done
 # Master node settings
 vm_master_cpu_cores=1
 vm_master_memory_mb=1024
-vm_master_disk_mb=25600
+vm_master_disk_mb=65535
 
 # Master node access to the internet through the host system, using VirtualBox NAT adapter
 vm_master_nat_network=192.168.200/24
@@ -59,7 +94,10 @@ vm_slave_cpu_cores=1
 # This section allows you to define RAM size in MB for each slave node.
 # Keep in mind that PXE boot might not work correctly with values lower than 768.
 # You can specify memory size for the specific slaves, other will get default vm_slave_memory_default
-vm_slave_memory_default=1024
+# Fuel 3.2 controllers require 1280 MiB of RAM as absolute minimum due to Heat!
+vm_slave_memory_default=1280
+# You may comment out all the following memory parameters to use default value for each node.
+# It is recommended if you going to try HA configurations.
 vm_slave_memory_mb[1]=1536  # for controller node at least 1.5Gb is required if you also run Ceph and Heat on it
 vm_slave_memory_mb[2]=1024  # for compute node 1GB is recommended, otherwise VM instances in OpenStack may not boot
 vm_slave_memory_mb[3]=1024  # for dedicated Cinder, 768Mb is OK, but Ceph needs 1Gb minimum
@@ -70,6 +108,7 @@ vm_slave_memory_mb[3]=1024  # for dedicated Cinder, 768Mb is OK, but Ceph needs 
 # dialog you will be able to allocate the whole disk or it's part for
 # operating system (Base OS), VMs (Virtual Storage), Ceph or other function,
 # depending on the roles applied to the server.
-vm_slave_first_disk_mb=65536
-vm_slave_second_disk_mb=65536
-vm_slave_third_disk_mb=65536
+# Nodes with combined roles may require more disk space.
+vm_slave_first_disk_mb=65535
+vm_slave_second_disk_mb=65535
+vm_slave_third_disk_mb=65535
--- virtualbox/functions/network.sh
+++ virtualbox/functions/network.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 #    Copyright 2013 Mirantis, Inc.
 #
@@ -17,57 +17,136 @@
 # This file contains the functions to manage host-only interfaces in the system
 
 get_hostonly_interfaces() {
-    echo -e `VBoxManage list hostonlyifs | grep '^Name' | sed 's/^Name\:[ \t]*//' | uniq` 
+  echo -e `VBoxManage list hostonlyifs | grep '^Name' | sed 's/^Name\:[ \t]*//' | uniq | tr "\\n" ","`
 }
 
 is_hostonly_interface_present() {
-    name=$1
-    list=$(get_hostonly_interfaces)
-    
-    # Check that the list of interfaces contains the given interface
-    if [[ $list = *$name* ]]; then
-        return 0
-    else
-        return 1
-    fi
+  name=$1
+# String comparison with IF works different in Cygwin, probably due to encoding.
+# So, reduced Case is used. since it works the same way.
+# Default divider character change is mandatory for Cygwin.
+  case "$(uname)" in
+    CYGWIN*)
+      OIFS=$IFS
+      IFS=","
+      ;;
+    *)
+      ;;
+  esac
+  # Call VBoxManage directly instead of function, due to changed IFS
+  list=(`VBoxManage list hostonlyifs | grep '^Name' | sed 's/^Name\:[ \t]*//' | uniq | tr "\\n" ","`)
+  # Change default divider back
+  case "$(uname)" in
+    CYGWIN*)
+      IFS=$OIFS
+      ;;
+    *)
+      ;;
+  esac
+  # Check that the list of interfaces contains the given interface
+  if [[ $list = *$name* ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+check_if_iface_settings_applied() {
+  name=$1
+  ip=$2
+  mask=$3
+  echo "Verifying interface $name has IP $ip and mask $mask properly set."
+  # Please leave 12 spaces in place - these are placed intentionally
+  case "$(uname)" in
+    CYGWIN*)
+      OIFS=$IFS
+      IFS=","
+      ;;
+    *)
+      ;;
+    esac
+  local new_name=(`VBoxManage list hostonlyifs | egrep -A9 "Name:            $name\$" | awk '/Name/ { $1 = ""; print substr($0, 2) }'`)
+  case "$(uname)" in
+    CYGWIN*)
+      IFS=$OIFS
+      ;;
+    *)
+      ;;
+  esac
+  local new_ip=(`VBoxManage list hostonlyifs | egrep -A9 "Name:            $name\$" | awk '/IPAddress:/ {print $2}'`)
+  local new_mask=(`VBoxManage list hostonlyifs | egrep -A9 "Name:            $name\$" | awk '/NetworkMask:/ {print $2}'`)
+  local new_dhcp=(`VBoxManage list hostonlyifs | egrep -A9 "Name:            $name\$" | awk '/DHCP:/ {print $2}'`)
+  # First verify if we checking correct interface
+  if [[ "$name" != "$new_name" ]]; then
+    echo "Checking $name but found settings for $new_name"
+    return 1
+  fi
+  if [[ $ip != $new_ip ]]; then
+    echo "New IP address $ip does not match the applied one $new_ip"
+    return 1
+  fi
+  if [[ $mask != $new_mask ]]; then
+    echo "New Net Mask $mask does not match the applied one $new_mask"
+    return 1
+  fi
+  if [[ "Disabled" != $new_dhcp ]]; then
+    echo "Failed to disable DHCP for network $name"
+    return 1
+  fi
+  echo "OK."
+  return 0
 }
 
 create_hostonly_interface() {
-    name=$1
-    ip=$2
-    mask=$3
-    echo "Creating host-only interface (name ip netmask): $name  $ip  $mask"
+  name=$1
+  ip=$2
+  mask=$3
+  echo "Creating host-only interface (name ip netmask): $name  $ip  $mask"
 
-    # Exit if the interface already exists (deleting it here is not safe, as VirtualBox creates hostonly adapters sequentially)
-    if is_hostonly_interface_present $name; then
-        echo "Fatal error. Interface $name cannot be created because it already exists. Exiting"
-        exit 1
-    fi
+  # Exit if the interface already exists (deleting it here is not safe, as VirtualBox creates hostonly adapters sequentially)
+  if is_hostonly_interface_present "$name"; then
+    echo "Fatal error. Interface $name cannot be created because it already exists. Exiting"
+    exit 1
+  fi
 
-    VBoxManage hostonlyif create
+  VBoxManage hostonlyif create
 
-    # If it does not exist after creation, let's abort
-    if ! is_hostonly_interface_present $name; then
-        echo "Fatal error. Interface $name does not exist after creation. Exiting"
-        exit 1
-    fi
+  # If it does not exist after creation, let's abort
+  if ! is_hostonly_interface_present "$name"; then
+    echo "Fatal error. Interface $name does not exist after creation. Exiting"
+    exit 1
+  fi
 
-    # Disable DHCP
-    echo "Disabling DHCP server on interface: $name..."
-    VBoxManage dhcpserver remove --ifname $name 2>/dev/null
-
-    # Set up IP address and network mask
-    echo "Configuring IP address $ip and network mask $mask on interface: $name..."
-    VBoxManage hostonlyif ipconfig $name --ip $ip --netmask $mask
+  # Disable DHCP
+  echo "Disabling DHCP server on interface: $name..."
+  # These magic 1 second sleeps around DHCP config are required under Windows/Cygwin
+  # due to VBoxSvc COM server accepts next request before previous one is actually finished.
+  sleep 1s
+  VBoxManage dhcpserver remove --ifname "$name" 2>/dev/null
+  sleep 1s
+  set -x
+  # Set up IP address and network mask
+  echo "Configuring IP address $ip and network mask $mask on interface: $name..."
+  VBoxManage hostonlyif ipconfig "$name" --ip $ip --netmask $mask
+  set +x
+  # Check what we have created actually.
+  # Sometimes VBox occasionally fails to apply settings to the last IFace under Windows
+  if !(check_if_iface_settings_applied "$name" $ip $mask); then
+    echo "Looks like VirtualBox failed to apply settings for interface $name"
+    echo "Sometimes such error happens under Windows."
+    echo "Please run launch.sh one more time."
+    echo "If this error remains after several attempts, then something really went wrong."
+    echo "Aborting."
+    exit 1
+  fi
 }
 
 delete_all_hostonly_interfaces() {
-    list=$(get_hostonly_interfaces)
-
-    # Delete every single hostonly interface in the system
-    for interface in $list; do
-        echo "Deleting host-only interface: $interface..."
-        VBoxManage hostonlyif remove $interface
-    done
+  OIFS=$IFS;IFS=",";list=(`VBoxManage list hostonlyifs | grep '^Name' | sed 's/^Name\:[ \t]*//' | uniq | tr "\\n" ","`);IFS=$OIFS
+  # Delete every single hostonly interface in the system
+  for interface in "${list[@]}"; do
+    echo "Deleting host-only interface: $interface..."
+    VBoxManage hostonlyif remove "$interface"
+  done
 }
 
