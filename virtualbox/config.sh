2509bd064516c9448db35bb703941a23cb84b626
Nikolay Korshenin <nkorshenin@mirantis.com>
2013-12-13 16:33:51 +0400

    Replace Fuel Web with Mirantis OpenStack in scripts and README
    Change-Id: Ic2fd88d2d2ad578ac4ef00ff995fad699e7eaac7
    Closes-Bug: #1258659

--- virtualbox/config.sh
+++ virtualbox/config.sh
@@ -23,7 +23,7 @@ cluster_size=3
 # Get the first available ISO from the directory 'iso'
 iso_path=`ls -1t iso/*.iso 2>/dev/null | head -1`
 
-# Every Fuel Web machine name will start from this prefix  
+# Every Mirantis OpenStack machine name will start from this prefix  
 vm_name_prefix=fuel-web-
 
 # Host interfaces to bridge VMs interfaces with
