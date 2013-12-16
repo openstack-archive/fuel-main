427b2459eb0533d2268b047c299e067027e02a10
Ivan Kolodyazhny <ikolodyazhny@mirantis.com>
2013-12-16 15:20:44 +0200

    Installing nailgun docs dependencies duiring build
    Change-Id: I303637e9f6f92fadb4c7d0b33d20abf0dc530c2d

--- utils/jenkins/nailgun-docs.sh
+++ utils/jenkins/nailgun-docs.sh
@@ -2,12 +2,14 @@
 
 . $WORKSPACE/utils/jenkins/common.sh
 
-nailgun_deps
-
 make clean
 make $WORKSPACE/build/repos/nailgun.done
 
 cd $WORKSPACE/build/repos/nailgun/docs
+
+# Installing nailgun docs dependencies
+sudo pip install -r requirements-docs.txt
+
 make clean
 make html
 rsync -avz -e ssh --delete _build/html/ fjenkins@fuel-docs.vm.mirantis.net:/home/fjenkins/workspace/fuel-docs.mirantis.com/docs/_build/html/
