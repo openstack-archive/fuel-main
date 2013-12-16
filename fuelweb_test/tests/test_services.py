7f8158faea4547e376e8d9a8f8e8b9293bbd2bfd
NastyaUrlapova <aurlapova@mirantis.com>
2013-12-16 17:44:15 +0400

    change netprovider in murano test
    Change-Id: I637bb18775788bacb7f7a99084be4cf64c80ff2e

--- fuelweb_test/tests/test_services.py
+++ fuelweb_test/tests/test_services.py
@@ -121,7 +121,9 @@ class MuranoSimple(TestBasic):
         cluster_id = self.fuel_web.create_cluster(
             name=self.__class__.__name__,
             settings={
-                'murano': True
+                'murano': True,
+                "net_provider": 'neutron',
+                "net_segment_type": 'gre'
             }
         )
         self.fuel_web.update_nodes(
@@ -141,6 +143,7 @@ class MuranoSimple(TestBasic):
         checkers.verify_service(
             self.env.get_ssh_to_remote_by_name("slave-01"),
             service_name='muranoconductor')
+
         self.env.make_snapshot("deploy_murano_simple")
 
     @test(depends_on=[deploy_murano_simple],
