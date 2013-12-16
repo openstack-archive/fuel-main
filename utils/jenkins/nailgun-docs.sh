134692a0a22803b81528408714ed9bab7dc2d047
Ivan Kolodyazhny <ikolodyazhny@mirantis.com>
2013-12-16 11:55:29 +0200

    Move nailgun docs to a new VM
    Change-Id: Ic3b4a0d2797ee22308ba98972f6356cf7bf69daa

--- utils/jenkins/nailgun-docs.sh
+++ utils/jenkins/nailgun-docs.sh
@@ -10,5 +10,5 @@ make $WORKSPACE/build/repos/nailgun.done
 cd $WORKSPACE/build/repos/nailgun/docs
 make clean
 make html
-rsync -avz -e ssh --delete _build/html/ fjenkins@fuel-docs.vm.mirantis.net:/home/fjenkins/workspace/fuel-docs.mirantis.com/docs/_build/html/
+rsync -avz -e ssh --delete _build/html/ jenkins@mos-docs.vm.mirantis.net:/var/www/fuel-dev
 cd $WORKSPACE
