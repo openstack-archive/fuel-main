eefb7016e514230513311196effd052b99a6e865
nfedotov <nfedotov@mirantis.com>
2013-12-20 12:06:09 +0400

    Fix wrong indices of slave nodes
    Change-Id: I78fa9c9d685b09fcfd83eb4b51431596ea48fbc3

--- fuelweb_test/tests/test_ceph.py
+++ fuelweb_test/tests/test_ceph.py
@@ -195,7 +195,7 @@ class CephHA(TestBasic):
             should_fail=4)
 
         # Destroy osd-node
-        self.env.nodes().slaves[-1].destroy()
+        self.env.nodes().slaves[5].destroy()
         check_ceph_health(self.env.get_ssh_to_remote_by_name('slave-01'))
         self.fuel_web.run_ostf(
             cluster_id=cluster_id,
@@ -209,7 +209,7 @@ class CephHA(TestBasic):
             should_fail=4)
 
         # Cold restart
-        self.fuel_web.restart_nodes(self.env.nodes().slaves[:])
+        self.fuel_web.restart_nodes(self.env.nodes().slaves[:4])
 
         check_ceph_health(self.env.get_ssh_to_remote_by_name('slave-01'))
         self.fuel_web.run_ostf(
