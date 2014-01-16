0000c9965def6388a5a2b1c86d7a29dd0ea9af96
Roman Vyalov <rvyalov@mirantis.com>
2014-01-16 21:53:52 +0400

    Add custom install.img for anaconda
    Add firmware to install.img
    Replace path for get install.img
    
    Closes-Bug: #1260492
    Change-Id: Ibe23411ae31effe9b8e52a4b717a36533f651508

--- mirror/centos/boot.mk
+++ mirror/centos/boot.mk
@@ -1,5 +1,5 @@
 ISOLINUX_FILES:=boot.msg grub.conf initrd.img isolinux.bin memtest vesamenu.c32 vmlinuz
-IMAGES_FILES:=efiboot.img efidisk.img install.img
+IMAGES_FILES:=efiboot.img efidisk.img
 EFI_FILES:=BOOTX64.conf BOOTX64.efi splash.xpm.gz
 
 # centos isolinux files
@@ -16,6 +16,9 @@ $(addprefix $(LOCAL_MIRROR_CENTOS_OS_BASEURL)/EFI/BOOT/,$(EFI_FILES)):
 $(addprefix $(LOCAL_MIRROR_CENTOS_OS_BASEURL)/images/,$(IMAGES_FILES)):
 	@mkdir -p $(@D)
 	wget -O $@ $(MIRROR_CENTOS_OS_BASEURL)/images/$(@F)
+
+# get custom centos install.img 
+  wget http://osci-obs.vm.mirantis.net:82/centos-fuel-4.1-stable/install.img $(MIRROR_CENTOS_OS_BASEURL)/images/install.img
 
 $(BUILD_DIR)/mirror/centos/boot.done: \
 		$(addprefix $(LOCAL_MIRROR_CENTOS_OS_BASEURL)/images/,$(IMAGES_FILES)) \
