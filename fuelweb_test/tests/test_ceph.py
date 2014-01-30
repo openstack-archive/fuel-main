2c0e49590826858a7c823714b581bbdc22a6c40f
Anstasiia Naboikina <anaboikina@mirantis.com>
2014-01-30 14:12:44 +0200

    Add tests for vm backed with Ceph live migration
    Added OpenStackClient to perform common
    OpenStack operations, such as creating
    servers, migrating them, making some basic
    checks;
    
    Added basic test for verification of live
    migration of VMs backed with Ceph;
    
    Change-Id: If5789338f68ad35151175afe29830fb9b75f7ecf
    Closes-bug: #1261336

--- fuelweb_test/tests/test_ceph.py
+++ fuelweb_test/tests/test_ceph.py
@@ -13,24 +13,25 @@
 #    under the License.
 
 import logging
+import proboscis
+import time
 
-from proboscis import test, SkipTest
-from proboscis.asserts import assert_true
-
+from fuelweb_test.helpers import os_actions
 from fuelweb_test.helpers.checkers import check_ceph_health
 from fuelweb_test.helpers.decorators import log_snapshot_on_error, debug
 from fuelweb_test import settings
-from fuelweb_test.tests.base_test_case import TestBasic, SetupEnvironment
+from fuelweb_test.tests import base_test_case
 
 logger = logging.getLogger(__name__)
 logwrap = debug(logger)
 
 
-@test(groups=["thread_1", "ceph"])
-class CephCompact(TestBasic):
+@proboscis.test(groups=["thread_1", "ceph"])
+class CephCompact(base_test_case.TestBasic):
 
-    @test(depends_on=[SetupEnvironment.prepare_slaves_3],
-          groups=["ceph_multinode_compact"])
+    @proboscis.test(depends_on=
+                    [base_test_case.SetupEnvironment.prepare_slaves_3],
+                    groups=["ceph_multinode_compact"])
     @log_snapshot_on_error
     def ceph_multinode_compact(self):
         """Deploy ceph in simple mode
@@ -46,7 +47,7 @@ class CephCompact(TestBasic):
 
         """
         if settings.OPENSTACK_RELEASE == settings.OPENSTACK_RELEASE_REDHAT:
-            raise SkipTest()
+            raise proboscis.SkipTest()
 
         self.env.revert_snapshot("ready_with_3_slaves")
 
@@ -78,11 +79,12 @@ class CephCompact(TestBasic):
         self.env.make_snapshot("ceph_multinode_compact")
 
 
-@test(groups=["thread_1", "ceph"])
-class CephCompactWithCinder(TestBasic):
+@proboscis.test(groups=["thread_1", "ceph"])
+class CephCompactWithCinder(base_test_case.TestBasic):
 
-    @test(depends_on=[SetupEnvironment.prepare_release],
-          groups=["ceph_multinode_with_cinder"])
+    @proboscis.test(depends_on=
+                    [base_test_case.SetupEnvironment.prepare_release],
+                    groups=["ceph_multinode_with_cinder"])
     @log_snapshot_on_error
     def ceph_multinode_with_cinder(self):
         """Deploy ceph with cinder in simple mode
@@ -99,7 +101,7 @@ class CephCompactWithCinder(TestBasic):
 
         """
         if settings.OPENSTACK_RELEASE == settings.OPENSTACK_RELEASE_REDHAT:
-            raise SkipTest()
+            raise proboscis.SkipTest()
 
         self.env.revert_snapshot("ready")
         self.env.bootstrap_nodes(self.env.nodes().slaves[:4])
@@ -133,11 +135,12 @@ class CephCompactWithCinder(TestBasic):
         self.env.make_snapshot("ceph_multinode_with_cinder")
 
 
-@test(groups=["thread_1", "ceph"])
-class CephHA(TestBasic):
+@proboscis.test(groups=["thread_1", "ceph"])
+class CephHA(base_test_case.TestBasic):
 
-    @test(depends_on=[SetupEnvironment.prepare_release],
-          groups=["ceph_ha"])
+    @proboscis.test(depends_on=
+                    [base_test_case.SetupEnvironment.prepare_release],
+                    groups=["ceph_ha"])
     @log_snapshot_on_error
     def ceph_ha(self):
         """Deploy ceph with cinder in HA mode
@@ -154,7 +157,7 @@ class CephHA(TestBasic):
 
         """
         if settings.OPENSTACK_RELEASE == settings.OPENSTACK_RELEASE_REDHAT:
-            raise SkipTest()
+            raise proboscis.SkipTest()
 
         self.env.revert_snapshot("ready")
         self.env.bootstrap_nodes(self.env.nodes().slaves[:6])
@@ -190,11 +193,12 @@ class CephHA(TestBasic):
         self.env.make_snapshot("ceph_ha")
 
 
-@test(groups=["thread_1", "ceph"])
-class CephRadosGW(TestBasic):
+@proboscis.test(groups=["thread_1", "ceph"])
+class CephRadosGW(base_test_case.TestBasic):
 
-    @test(depends_on=[SetupEnvironment.prepare_slaves_5],
-          groups=["ceph_rados_gw"])
+    @proboscis.test(
+        depends_on=[base_test_case.SetupEnvironment.prepare_slaves_5],
+        groups=["ceph_rados_gw"])
     @log_snapshot_on_error
     def ceph_rados_gw(self):
         """Deploy ceph with RadosGW for objects
@@ -213,7 +217,7 @@ class CephRadosGW(TestBasic):
 
         """
         if settings.OPENSTACK_RELEASE == settings.OPENSTACK_RELEASE_REDHAT:
-            raise SkipTest()
+            raise base_test_case.SkipTest()
 
         self.env.revert_snapshot("ready_with_5_slaves")
 
@@ -253,6 +257,252 @@ class CephRadosGW(TestBasic):
         radosgw_started = lambda: len(remote.check_call(
             'ps aux | grep "/usr/bin/radosgw -n '
             'client.radosgw.gateway"')['stdout']) == 3
-        assert_true(radosgw_started(), 'radosgw daemon started')
+        proboscis.asserts.assert_true(radosgw_started(), 'radosgw daemon started')
 
         self.env.make_snapshot("ceph_rados_gw")
+        self.env.make_snapshot("ceph_ha")
+
+
+@proboscis.test(groups=["thread_1", "ceph"])
+class VmBackedWithCephMigrationBasic(base_test_case.TestBasic):
+
+    @proboscis.test(depends_on=
+                    [base_test_case.SetupEnvironment.prepare_slaves_3],
+                    groups=["ceph"])
+    @log_snapshot_on_error
+    def migrate_vm_backed_with_ceph(self):
+        """Check VM backed with ceph migration in simple mode
+
+        Scenario:
+            1. Create cluster
+            2. Add 1 node with controller and ceph OSD roles
+            3. Add 2 node with compute and ceph OSD roles
+            4. Deploy the cluster
+            5. Check ceph status
+            6. Run OSTF
+            7. Create a new VM, assign floating ip
+            8. Migrate VM
+            9. Check cluster and server state after migration
+            10. Terminate VM
+
+        Snapshot vm_backed_with_ceph_live_migration
+
+        """
+        if settings.OPENSTACK_RELEASE == settings.OPENSTACK_RELEASE_REDHAT:
+            raise proboscis.SkipTest()
+
+        self.env.revert_snapshot("ready_with_3_slaves")
+
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=settings.DEPLOYMENT_MODE_SIMPLE,
+            settings={
+                'volumes_ceph': True,
+                'images_ceph': True,
+                'ephemeral_ceph': True,
+                'volumes_lvm': False
+            }
+        )
+
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller', 'ceph-osd'],
+                'slave-02': ['compute', 'ceph-osd'],
+                'slave-03': ['compute', 'ceph-osd']
+            }
+        )
+        creds = ("cirros", "test")
+
+        # Cluster deploy
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        # Create new server
+        os = os_actions.OpenStackActions(
+            self.fuel_web.get_nailgun_node_by_name("slave-01")["ip"])
+
+        logger.info("Create new server")
+        srv = os.create_server_for_migration(
+            scenario='./fuelweb_test/helpers/instance_initial_scenario')
+        logger.info("Srv is currently in status: %s" % srv.status)
+
+        if srv.status != "ERROR":
+            logger.info("Assigning floating ip to server")
+            floating_ip = os.assign_floating_ip(srv)
+            srv_host = os.get_srv_host_name(srv)
+            logger.info("Server is on host %s" % srv_host)
+
+            time.sleep(100)
+
+            md5before = os.get_md5sum(
+                "/home/test_file",
+                self.env.get_ssh_to_remote_by_name("slave-01"),
+                floating_ip.ip, creds)
+
+            logger.info("Get available computes")
+            avail_hosts = os.get_hosts_for_migr(srv_host)
+
+            logger.info("Migrating server")
+            new_srv = os.migrate_server(srv, avail_hosts[0])
+            logger.info("Check cluster and server state after migration")
+
+            if new_srv.status != "ACTIVE":
+                raise Exception()
+
+            md5after = os.get_md5sum(
+                "/home/test_file",
+                self.env.get_ssh_to_remote_by_name("slave-01"),
+                floating_ip.ip, creds)
+
+            if md5after not in md5before:
+                logger.info("Md5 checksums don`t match.")
+                logger.info(
+                    "Before migration md5 was equal to: {bef}"
+                    "Now it eqals: {aft}".format(bef=md5before, aft=md5after))
+                raise Exception()
+
+            res = os.execute_through_host(
+                self.env.get_ssh_to_remote_by_name("slave-01"),
+                floating_ip.ip, "ping -q -c3 -w3 %s | grep 'received' |"
+                " grep -v '0 packets received'", creds)
+            logger.info("Ping 8.8.8.8 result on vm is: %s" % res)
+
+            logger.info("Check Ceph health is ok after migration")
+            check_ceph_health(self.env.get_ssh_to_remote_by_name('slave-01'))
+
+            logger.info("Server is now on host %s" %
+                        os.get_srv_host_name(new_srv))
+
+            logger.info("Terminate migrated server")
+            os.delete_instance(new_srv)
+            os.verify_srv_deleted(new_srv)
+        else:
+            logger.info("Instance is in ERROR status")
+            raise Exception()
+
+        self.env.make_snapshot("vm_backed_with_ceph_live_migration")
+
+
+@proboscis.test(groups=["thread_1", "ceph"])
+class VmBackedWithCephMigrationVolume(base_test_case.TestBasic):
+
+    @proboscis.test(depends_on=
+                    [base_test_case.SetupEnvironment.prepare_slaves_3],
+                    groups=["ceph"])
+    @log_snapshot_on_error
+    def migrate_vm_backed_with_ceph(self):
+        """Check VM backed with ceph migration in simple mode
+
+        Scenario:
+            1. Create cluster
+            2. Add 1 node with controller and ceph OSD roles
+            3. Add 2 node with compute and ceph OSD roles
+            4. Deploy the cluster
+            5. Check ceph status
+            8. Create a new VM, assign floating ip
+            9. Create a new volume, attach it to VM
+            10. Create filesystem and mount volume.
+            8. Migrate VM
+            9. Check cluster and server state after migration
+            10. Terminate VM
+
+        Snapshot vm_backed_with_ceph_live_migration_with_volume
+
+        """
+        if settings.OPENSTACK_RELEASE == settings.OPENSTACK_RELEASE_REDHAT:
+            raise proboscis.SkipTest()
+
+        self.env.revert_snapshot("ready_with_3_slaves")
+
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=settings.DEPLOYMENT_MODE_SIMPLE,
+            settings={
+                'volumes_ceph': True,
+                'images_ceph': False,
+                'ephemeral_ceph': True,
+                'volumes_lvm': False
+            }
+        )
+
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['compute', 'ceph-osd'],
+                'slave-03': ['compute', 'ceph-osd']
+            }
+        )
+
+        # Cluster deploy
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        creds = ("cirros", "test")
+
+        # Create new server
+        os = os_actions.OpenStackActions(
+            self.fuel_web.get_nailgun_node_by_name("slave-01")["ip"])
+
+        logger.info("Create new server")
+        srv = os.create_server_for_migration(
+            scenario='./fuelweb_test/helpers/instance_initial_scenario')
+        logger.info("Srv is currently in status: %s" % srv.status)
+
+        if srv.status != "ERROR":
+            logger.info("Assigning floating ip to server")
+            floating_ip = os.assign_floating_ip(srv)
+            srv_host = os.get_srv_host_name(srv)
+            logger.info("Server is on host %s" % srv_host)
+
+            logger.info("Create volume")
+            vol = os.create_volume()
+            logger.info("Attach volume to server")
+            os.attach_volume(vol, srv)
+
+            time.sleep(100)
+            logger.info("Create filesystem and mount volume")
+            os.execute_through_host(
+                self.env.get_ssh_to_remote_by_name('slave-01'),
+                floating_ip.ip, '. /home/mount_volume.sh', creds)
+
+            os.execute_through_host(
+                self.env.get_ssh_to_remote_by_name('slave-01'),
+                floating_ip.ip, 'touch /mnt/file-on-volume', creds)
+
+            logger.info("Get available computes")
+            avail_hosts = os.get_hosts_for_migr(srv_host)
+
+            logger.info("Migrating server")
+            new_srv = os.migrate_server(srv, avail_hosts[0])
+            logger.info("Check cluster and server state after migration")
+
+            if new_srv.status != "ACTIVE":
+                raise Exception()
+
+            logger.info("Mount volume after migration")
+            out = os.execute_through_host(
+                self.env.get_ssh_to_remote_by_name('slave-01'),
+                floating_ip.ip, 'sudo mount /dev/vdb /mnt', creds)
+
+            logger.info("out of mounting volume is: %s" % out)
+
+            if "file-on-volume" not in os.execute_through_host(
+                    self.env.get_ssh_to_remote_by_name('slave-01'),
+                    floating_ip.ip, "ls /mnt", creds):
+                logger.info("File is abscent in /mnt")
+                raise Exception()
+
+            logger.info("Check Ceph health is ok after migration")
+            check_ceph_health(self.env.get_ssh_to_remote_by_name('slave-01'))
+
+            logger.info("Server is now on host %s" %
+                        os.get_srv_host_name(new_srv))
+
+            logger.info("Terminate migrated server")
+            os.delete_instance(new_srv)
+            os.verify_srv_deleted(new_srv)
+        else:
+            logger.info("Instance is in ERROR status")
+            raise Exception()
+
+        self.env.make_snapshot("vm_backed_with_ceph_live_migration_with_volume")
