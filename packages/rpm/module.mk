a36c3add799743bb299b8c9078b7d6878d7db04d
Ivan Kolodyazhny <ikolodyazhny@mirantis.com>
2014-01-30 15:28:29 +0200

    Update dhcp-checker spec to use tar.gz
    Change-Id: I01da5d124429f194003c48c29203df29c247332b

--- packages/rpm/module.mk
+++ packages/rpm/module.mk
@@ -53,23 +53,6 @@ $(BUILD_DIR)/packages/rpm/rpm-nailgun-net-check.done: \
 	sudo sh -c "$${SANDBOX_DOWN}"
 	$(ACTION.TOUCH)
 
-$(BUILD_DIR)/packages/rpm/rpm-dhcp-checker.done: SANDBOX:=$(BUILD_DIR)/packages/rpm/SANDBOX
-$(BUILD_DIR)/packages/rpm/rpm-dhcp-checker.done: export SANDBOX_UP:=$(SANDBOX_UP)
-$(BUILD_DIR)/packages/rpm/rpm-dhcp-checker.done: export SANDBOX_DOWN:=$(SANDBOX_DOWN)
-$(BUILD_DIR)/packages/rpm/rpm-dhcp-checker.done: $(BUILD_DIR)/repos/nailgun.done
-$(BUILD_DIR)/packages/rpm/rpm-dhcp-checker.done: \
-		$(BUILD_DIR)/packages/rpm/prep.done \
-		$(SOURCE_DIR)/packages/rpm/specs/dhcp-checker.spec \
-		$(call find-files,$(BUILD_DIR)/repos/nailgun/dhcp-checker)
-	sudo sh -c "$${SANDBOX_UP}"
-	sudo mkdir -p $(SANDBOX)/tmp/SOURCES
-	sudo cp $(LOCAL_MIRROR_SRC)/* $(SANDBOX)/tmp/SOURCES
-	sudo cp -r $(BUILD_DIR)/repos/nailgun/dhcp-checker/* $(SANDBOX)/tmp/SOURCES/
-	sudo cp $(SOURCE_DIR)/packages/rpm/specs/dhcp-checker.spec $(SANDBOX)/tmp
-	sudo chroot $(SANDBOX) rpmbuild -vv --define "_topdir /tmp" -ba /tmp/dhcp-checker.spec
-	cp $(SANDBOX)/tmp/RPMS/noarch/dhcp_checker-*.rpm $(BUILD_DIR)/packages/rpm/RPMS/x86_64/
-	sudo sh -c "$${SANDBOX_DOWN}"
-	$(ACTION.TOUCH)
 
 $(BUILD_DIR)/packages/rpm/rpm-python-fuelclient.done: SANDBOX:=$(BUILD_DIR)/packages/rpm/SANDBOX
 $(BUILD_DIR)/packages/rpm/rpm-python-fuelclient.done: export SANDBOX_UP:=$(SANDBOX_UP)
@@ -138,7 +121,6 @@ $(BUILD_DIR)/packages/rpm/repo.done: \
 		$(BUILD_DIR)/packages/rpm/rpm-nailgun-net-check.done \
 		$(BUILD_DIR)/packages/rpm/rpm-nailgun-redhat-license.done \
 		$(BUILD_DIR)/packages/rpm/rpm-rbenv-ruby.done \
-		$(BUILD_DIR)/packages/rpm/rpm-dhcp-checker.done \
 		$(BUILD_DIR)/packages/rpm/rpm-fuelmenu.done \
 		$(BUILD_DIR)/packages/rpm/rpm-python-fuelclient.done
 	find $(BUILD_DIR)/packages/rpm/RPMS -name '*.rpm' -exec cp -u {} $(LOCAL_MIRROR_CENTOS_OS_BASEURL)/Packages \;
