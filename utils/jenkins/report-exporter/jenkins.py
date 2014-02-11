453ad2e3b6835174a8f0474c63131350fb6acb9b
nfedotov <nfedotov@mirantis.com>
2014-02-11 18:15:27 +0400

    Script for exporting jenkins test report to Google spreadsheet
    Example: python report-exporter -j my-job-name -s 0AhZjx9R84sqXdGFSdnNhUGhDQlNGN6dMamZMSVlZdFE
    following environment variables should be specified before launching
    JENKINS_HOME - jenkins home page URL
    GOOGLE_LOGIN, GOOGLE_PASSWORD - google account credentials
    
    It exports test report of the last run of a specified job to
    google spreadsheet document. A sheet name will be named <job-name>.
    Sheet format example:
    name        build1    build2    build3   ...
    testname1   pass      fail      fail
    testname2   pass      pass      fail
    ...
    
    Change-Id: Ie90bba9c34f282336bfd957aa37d1af184dfb7d5

---
+++ utils/jenkins/report-exporter/jenkins.py
@@ -0,0 +1,53 @@
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import urllib2
+import urllib
+import urlparse
+import json
+from settings import JENKINS_HOME
+
+
+class JSONResource(object):
+    def __init__(self, url):
+        self.url = url
+
+    def get_data(self):
+        req = urllib2.Request(urlparse.urljoin(self.url, 'api/json'))
+        opener = urllib2.build_opener(urllib2.HTTPHandler)
+        str = opener.open(req).read()
+        return json.loads(str)
+
+
+class Job(JSONResource):
+    def __init__(self, name):
+        url = urlparse.urljoin(JENKINS_HOME, 'job/{0}/'.format(urllib.quote(name)))
+        super(Job, self).__init__(url)
+
+        self.name = name
+
+    @property
+    def builds(self):
+        js = self.get_data()
+        return [Build(b['number'], b['url']) for b in js['builds']]
+
+
+class Build(JSONResource):
+    def __init__(self, number, url):
+        super(Build, self).__init__(url)
+        self.number = number
+
+    @property
+    def test_report(self):
+        return JSONResource(urlparse.urljoin(self.url, 'testReport/')).get_data()
