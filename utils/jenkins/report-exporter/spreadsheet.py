96371a2d3f9b9626cc5538a3eab05679719aeae1
nfedotov <nfedotov@mirantis.com>
2014-02-11 18:15:27 +0400

    Script for exporting jenkins test report to Google spreadsheet
    Example: python report-exporter -j my-job-name -s 0AhZjx9R84sqXdGFSdnNhUGhDQlNGN6dMamZMSVlZdFE
    following environment variables should be specified before launching
    JENKINS_HOME - jenkins home page URL
    GOOGLE_LOGIN, GOOGLE_PASSWORD - google account credentials
    
    It exports test report of the last run of a specified job to
    google spreadsheet document. A sheet name will be named <job-name>.
    Sheet format example:
    name        build1    build2    build3   ...
    testname1   pass      fail      fail
    testname2   pass      pass      fail
    ...
    
    Change-Id: Ie90bba9c34f282336bfd957aa37d1af184dfb7d5

---
+++ utils/jenkins/report-exporter/spreadsheet.py
@@ -0,0 +1,79 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import re
+from gdata.spreadsheet import text_db
+from settings import GOOGLE_LOGIN, GOOGLE_PASSWORD
+
+
+class BuildsDocument(object):
+    def __init__(self, spreadsheet_key):
+        # connect to google docs and get spreadsheet
+        self.gclient = text_db.DatabaseClient(GOOGLE_LOGIN, GOOGLE_PASSWORD)
+        self.gspreadsheet = \
+            self.gclient.GetDatabases(spreadsheet_key=spreadsheet_key)[0]
+
+    def get_sheet(self, name):
+        tables = self.gspreadsheet.GetTables(name=name)
+        if len(tables) == 0:
+            # Create new sheet
+            self.gspreadsheet.client._GetSpreadsheetsClient().AddWorksheet(
+                title=name, row_count=1, col_count=50,
+                key=self.gspreadsheet.spreadsheet_key)
+            tables = self.gspreadsheet.GetTables(name=name)
+        return BuildSheet(tables.pop())
+
+
+class BuildSheet:
+    BUILD_COLUMN = 'b{0}'
+
+    def __init__(self, table):
+        self.table = table
+        self.table.LookupFields()
+        if len(self.table.fields) == 0:
+            # set fields if sheet is empty
+            self.table.SetFields(['name'])
+
+    @property
+    def last_build_column(self):
+        s = self.table.fields[-1]
+        return s if 'name' not in s else None
+
+    def get_build_column_name(self, build_num):
+        """
+        Returns column name of a build.
+        If there is no such column it creates another one
+
+        It turns out name of column could not be a number.
+        For ex: if name is '12' r.content returns {'_cokwr': 'PASSED'}
+        So,we have to append a string to a build number and then
+        use the result string as a column name
+        """
+        name = self.BUILD_COLUMN.format(build_num)
+        if self.last_build_column != name:
+            fields = self.table.fields
+            fields.append(name)
+            self.table.SetFields(fields)
+            self.table.LookupFields()
+        return name
+
+    def get_case_record(self, case_name):
+        """
+        Returns record / row for a test case
+        """
+        name = re.sub('[<>]', '', case_name)
+        r = self.table.FindRecords('name = {0}'.format(name))
+        if len(r) == 0:
+            return self.table.AddRecord({'name': name})
+        return r.pop()
