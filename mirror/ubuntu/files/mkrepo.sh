20fbd5f7661fad9e62bc62147a8d67de503e7ff5
Roman Vyalov <rvyalov@mirantis.com>
2014-03-19 16:00:23 +0400

    Move to ubuntu 12.04.4
    Closes-bug: #1294695
    Change-Id: I68853f0ee776dd9a2a24490d1e4ac66eacc3d6b6

--- mirror/ubuntu/files/mkrepo.sh
+++ mirror/ubuntu/files/mkrepo.sh
@@ -10,17 +10,77 @@ for dir in binary-i386 binary-amd64; do
 done
 mkdir -p /repo/pool/debian-installer /repo/pool/main
 cd /repo/pool/debian-installer
+
+###################
 # Grab every udeb
-for udeb in $(wget -qO - http://mirror.yandex.ru/ubuntu/dists/precise/main/debian-installer/binary-amd64/Packages.bz2 | bzip2 -cd | sed -ne 's/^Filename: //p'); do
-        wget -N http://mirror.yandex.ru/ubuntu/$udeb
+###################
+
+wrkdir=`dirname $(pwd)`/`basename $(pwd)`
+
+rm -f ${wrkdir}/UPackages.tmp ${wrkdir}/override.precise.main ${wrkdir}/override.precise.extra.main ${wrkdir}/override.precise.main.debian-installer
+touch ${wrkdir}/UPackages.tmp ${wrkdir}/override.precise.main ${wrkdir}/override.precise.extra.main ${wrkdir}/override.precise.main.debian-installer
+
+# Prepare temp apt dirs
+[ -d ${wrkdir}/apt.tmp ] && rm -rf ${wrkdir}/apt.tmp
+mkdir -p ${wrkdir}/apt.tmp/lists
+mkdir -p ${wrkdir}/apt.tmp/sources
+mkdir -p ${wrkdir}/apt.tmp/cache
+
+# Extract all specified repos (except backports repo)
+for list in /etc/apt/sources.list /etc/apt/sources.list.d/*.list; do
+  for repo in `cat $list| grep -v backports | sed 's| \+|\||g' | grep "^deb|"`; do
+     repourl=`echo $repo | awk -F '|' '{print $2}'`
+     repodist=`echo $repo | awk -F '|' '{print $3}'`
+     repos=`echo $repo | awk -F '|' '{for(i=4; i<=NF; ++i) {print $i}}'`
+     for repo in $repos; do
+       bz=${repourl}/dists/${repodist}/${repo}/debian-installer/binary-amd64/Packages.bz2
+       echo "deb ${repourl} ${repodist} ${repo}/debian-installer" >> ${wrkdir}/apt.tmp/sources/sources.list
+       wget -qO - $bz | bzip2 -cdq | sed -ne 's/^Package: //p' >> ${wrkdir}/UPackages.tmp
+       # Getting indices
+       wget -N ${repourl}/indices/override.${repodist}.${repo} >> ${wrkdir}/override.precise.main
+       wget -N ${repourl}/indices/override.${repodist}.extra.${repo} ${wrkdir}/override.precise.extra.main
+       wget -N ${repourl}/indices/override.${repodist}.${repo}.debian-installer ${wrkdir}/override.precise.main.debian-installer
+     done
+  done
 done
+
+# Collect all udebs except packages with suffux generic or virtual
+packages=`cat ${wrkdir}/UPackages.tmp | sort -u | egrep -v "generic|virtual"`
+
+# Find latest udebs with suffux generic or virtual
+gvpackages=`cat ${wrkdir}/UPackages.tmp | egrep "generic|virtual" | sed 's|[0-9]\+|[0-9]+|g' | sort -u`
+for i in $gvpackages; do
+  packages="$packages `cat ${wrkdir}/UPackages.tmp | egrep "generic|virtual" | egrep $i | sort -rV | head -1`"
+done
+
+# Update apt temp cache
+apt-get -o Dir::Etc::SourceParts="${wrkdir}/apt.tmp/sources/parts" \
+        -o Dir::Etc::SourceList="${wrkdir}/apt.tmp/sources/sources.list" \
+        -o Dir::State::Lists="${wrkdir}/apt.tmp/lists" \
+        -o Dir::Cache="${wrkdir}/apt.tmp/cache" \
+        update
+
+# Download udebs
+apt-get -o Dir::Etc::SourceParts="${wrkdir}/apt.tmp/sources/parts" \
+        -o Dir::Etc::SourceList="${wrkdir}/apt.tmp/sources/sources.list" \
+        -o Dir::State::Lists="${wrkdir}/apt.tmp/lists" \
+        -o Dir::Cache="${wrkdir}/apt.tmp/cache" \
+        download $packages
+
+rm -f ${wrkdir}/UPackages.tmp
+rm -rf ${wrkdir}/apt.tmp
+
 # Get rid of urlencoded names
 for i in $(ls | grep %) ; do mv $i $(echo $i | echo -e $(sed 's/%/\\x/g')) ; done
-rm -f debootstrap*
+#rm -f debootstrap*
+
 #
 # Borrow right one...
-wget http://download.mirantis.com/precise-grizzly-fuel-3.2/pool/main/d/debootstrap/debootstrap-udeb_1.0.39_all.udeb
+#wget http://download.mirantis.com/precise-grizzly-fuel-3.2/pool/main/d/debootstrap/debootstrap-udeb_1.0.39_all.udeb
+
+##########################################
 # Move all stuff to the our package pool
+##########################################
 mv /var/cache/apt/archives/*deb /repo/pool/main
 cd /repo/pool/main
 # urlencode again
@@ -28,8 +88,9 @@ for i in $(ls | grep %) ; do mv $i $(echo $i | echo -e $(sed 's/%/\\x/g')) ; don
 mkdir -p /repo/indices
 cd /repo/indices
 for idx in override.precise.main override.precise.extra.main override.precise.main.debian-installer ; do
-	wget -N http://mirror.yandex.ru/ubuntu/indices/$idx
+  cat ${wrkdir}/$idx | sort -u > ${wrkdir}/repo/indices/$idx
 done
+rm -f ${wrkdir}/override.precise.main ${wrkdir}/override.precise.extra.main ${wrkdir}/override.precise.main.debian-installer
 cd /repo
 # Just because apt scan will produce crap
 cp -a Release-amd64 Release-i386
