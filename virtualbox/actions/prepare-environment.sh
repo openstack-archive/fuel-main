8bc12de6b7a98910df17bc02f83af09b01a19b10
Andrey Danin <gcon.monolake@gmail.com>
2014-03-20 14:06:31 +0400

    Update a copyright year to 2014
    Change-Id: I5aaae1a725f7ac4cbe110fd4afbdd700bd03184a

--- virtualbox/actions/prepare-environment.sh
+++ virtualbox/actions/prepare-environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # set -x
 
-#    Copyright 2013 Mirantis, Inc.
+#    Copyright 2014 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
