97cc2558cca466ca69e6eebda8ec724aa0a4941a
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-04-03 13:46:07 +0400

    Added docker container refs
    Docker refs allow for download and build
    of docker images to ship with Fuel ISO
    blueprint fuel-containerization-of-services
    Change-Id: I8d0ab5510b5b14d94aadc65d004d3e333f5ff39b

--- packages/module.mk
+++ packages/module.mk
@@ -1,13 +1,15 @@
 include $(SOURCE_DIR)/packages/rpm/module.mk
 include $(SOURCE_DIR)/packages/eggs/module.mk
 include $(SOURCE_DIR)/packages/gems/module.mk
+include $(SOURCE_DIR)/packages/docker/module.mk
 
 .PHONY: packages
 
 $(BUILD_DIR)/packages/build.done: \
 		$(BUILD_DIR)/packages/rpm/build.done \
 		$(BUILD_DIR)/packages/eggs/build.done \
-		$(BUILD_DIR)/packages/gems/build.done
+		$(BUILD_DIR)/packages/gems/build.done \
+		$(BUILD_DIR)/packages/docker/build.done
 	$(ACTION.TOUCH)
 
 packages: $(BUILD_DIR)/packages/build.done
