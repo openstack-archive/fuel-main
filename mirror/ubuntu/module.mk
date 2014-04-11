b1dcb91c4391c87796ea6c7e5507aefaff0ac819
Dmitry Burmistrov <dburmistrov@mirantis.com>
2014-04-11 16:07:20 +0400

    Move to ubuntu 12.04.4
    Closes-bug: #1294695
    
    Change-Id: I73dfff063fd95bc0676ac7aac3cd35d052e7f596

--- mirror/ubuntu/module.mk
+++ mirror/ubuntu/module.mk
@@ -1,8 +1,12 @@
+.PHONY: clean-ubuntu
 # This module downloads ubuntu installation images.
 include $(SOURCE_DIR)/mirror/ubuntu/boot.mk
 include $(SOURCE_DIR)/mirror/ubuntu/createchroot.mk
 
-$(BUILD_DIR)/mirror/ubuntu/build.done: \
+clean-ubuntu:
+	(mount -l | grep -q $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/proc && sudo umount $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/proc) || exit 0
+
+$(BUILD_DIR)/mirror/ubuntu/build.done: clean-ubuntu \
 		$(BUILD_DIR)/mirror/ubuntu/boot.done \
 		$(BUILD_DIR)/mirror/ubuntu/createchroot.done
 	$(ACTION.TOUCH)
