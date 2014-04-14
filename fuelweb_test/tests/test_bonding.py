e1cabcd953bd726774bc5ff84c3a0b8b346f456c
Anastasiia Naboikina <anaboikina@mirantis.com>
2014-04-14 13:11:23 +0300

    Add possibility to add several interfaces on 1 L2
    Modified describe_environment, create_interfaces,
    set_network methods so that they use bonding
    parameters of settings.py module. Added new
    parameters to settings.py that allow bonding.
    
    Closes-Bug: #1301834
    
    Change-Id: Ifd8f99596ce79f08d99bc99743a97df9884a4724

--- fuelweb_test/tests/test_bonding.py
+++ fuelweb_test/tests/test_bonding.py
@@ -92,7 +92,8 @@ class BondingSimple(TestBasic):
             ]
         }
 
-        nets = self.fuel_web.client.get_networks(cluster_id)['networks']
+        net_params = self.fuel_web.client.get_networks(cluster_id)
+        nets = net_params['networks']
         vlan_start = 100
 
         for net in nets:
@@ -110,10 +111,10 @@ class BondingSimple(TestBasic):
 
         cluster = self.fuel_web.client.get_cluster(cluster_id)
         assert_equal(str(cluster['net_provider']), 'neutron')
-        # assert_equal(str(cluster['net_segment_type']), segment_type)
+        assert_equal(str(net_params["networking_parameters"]
+                         ['segmentation_type']), segment_type)
 
-        # TODO(Tatyana) uncomment when it will be implemented)
-        # self.fuel_web.verify_network(cluster_id)
+        self.fuel_web.verify_network(cluster_id)
 
         self.fuel_web.run_ostf(
             cluster_id=cluster_id,
@@ -188,7 +189,8 @@ class BondingSimple(TestBasic):
             ]
         }
 
-        nets = self.fuel_web.client.get_networks(cluster_id)['networks']
+        net_params = self.fuel_web.client.get_networks(cluster_id)
+        nets = net_params['networks']
         vlan_start = 100
 
         for net in nets:
@@ -206,10 +208,10 @@ class BondingSimple(TestBasic):
 
         cluster = self.fuel_web.client.get_cluster(cluster_id)
         assert_equal(str(cluster['net_provider']), 'neutron')
-        # assert_equal(str(cluster['net_segment_type']), segment_type)
+        assert_equal(str(net_params["networking_parameters"]
+                         ['segmentation_type']), segment_type)
 
-        # TODO(Tatyana) uncomment when it will be implemented)
-        # self.fuel_web.verify_network(cluster_id)
+        self.fuel_web.verify_network(cluster_id)
 
         self.fuel_web.run_ostf(
             cluster_id=cluster_id,
@@ -289,7 +291,9 @@ class BondingHA(TestBasic):
             ]
         }
 
-        nets = self.fuel_web.client.get_networks(cluster_id)['networks']
+        net_params = self.fuel_web.client.get_networks(cluster_id)
+        nets = net_params['networks']
+
         vlan_start = 100
 
         for net in nets:
@@ -307,10 +311,10 @@ class BondingHA(TestBasic):
 
         cluster = self.fuel_web.client.get_cluster(cluster_id)
         assert_equal(str(cluster['net_provider']), 'neutron')
-        # assert_equal(str(cluster['net_segment_type']), segment_type)
+        assert_equal(str(net_params["networking_parameters"]
+                         ['segmentation_type']), segment_type)
 
-        # TODO(Tatyana) uncomment when it will be implemented)
-        # self.fuel_web.verify_network(cluster_id)
+        self.fuel_web.verify_network(cluster_id)
 
         self.fuel_web.run_ostf(
             cluster_id=cluster_id,
@@ -387,7 +391,8 @@ class BondingHA(TestBasic):
             ]
         }
 
-        nets = self.fuel_web.client.get_networks(cluster_id)['networks']
+        net_params = self.fuel_web.client.get_networks(cluster_id)
+        nets = net_params['networks']
         vlan_start = 100
 
         for net in nets:
@@ -405,10 +410,10 @@ class BondingHA(TestBasic):
 
         cluster = self.fuel_web.client.get_cluster(cluster_id)
         assert_equal(str(cluster['net_provider']), 'neutron')
-        # assert_equal(str(cluster['net_segment_type']), segment_type)
+        assert_equal(str(net_params["networking_parameters"]
+                         ['segmentation_type']), segment_type)
 
-        # TODO(Tatyana) uncomment when it will be implemented)
-        # self.fuel_web.verify_network(cluster_id)
+        self.fuel_web.verify_network(cluster_id)
 
         self.fuel_web.run_ostf(
             cluster_id=cluster_id,
