1fc55306c3ecfbc6390ef0eba24395f9e13b8af2
Aleksandr Didenko <adidenko@mirantis.com>
2014-04-15 20:52:23 +0300

    Deploy fuel node with docker containers
    DO NOT MERGE PLS. Work in progress.
    
    We need to put fuel-images.tar.lbz and fuel-dockerctl.tgz on the
    ISO and use them in bootstrap_admin_node.sh script.
    
    blueprint fuel-containerization-of-services
    
    Change-Id: Ic57f06bb3534c3e839a39918734018652408bb5d

--- iso/bootstrap_admin_node.sh
+++ iso/bootstrap_admin_node.sh
@@ -55,4 +55,45 @@ sed -i "s%\(^.*able on:\).*$%\1 http://`ip address show $primary | awk '/inet / 
 [ -d /var/lib/hiera ] || mkdir -p /var/lib/hiera
 touch /var/lib/hiera/common.yaml /etc/puppet/hiera.yaml
 
-puppet apply  /etc/puppet/modules/nailgun/examples/site.pp
+# apply puppet
+puppet apply -d -v /etc/puppet/modules/nailgun/examples/host-only.pp
+
+### docker stuff
+dockerctl="/root/fuel-dockerctl/dockerctl"
+images_dir="/var/www/nailgun/docker/images"
+sources_dir="/var/www/nailgun/docker/sources"
+
+# prepare dockerctl utility
+tar -C /root/ -xzf /var/www/nailgun/docker/fuel-dockerctl.tar.gz
+mv /root/fuel-dockerctl/sources /var/www/nailgun/docker/sources
+ln -s /root/fuel-dockerctl/dockerctl /usr/bin/dockerctl
+chmod +x /root/fuel-dockerctl/dockerctl
+
+# extract docker images
+mkdir -p $images_dir $sources_dir
+rm -f $images_dir/*tar
+pushd $images_dir &>/dev/null
+lrzuntar "$images_dir/fuel-images.tar.lrz"
+popd &>/dev/null
+
+# load docker images
+for image in $images_dir/*tar ; do
+    echo "Loading docker image ${image}..."
+    docker load -i "$image"
+done
+
+# clean up extracted images
+rm -f $images_dir/*tar
+
+#FIXME(mattymo) Symlink astute.yaml to correct directory
+ln -s /etc/astute.yaml /etc/fuel/astute.yaml
+
+# Precreate needed directories
+mkdir -p /var/www/nailgun/dump
+for dir in audit cobbler ConsoleKit coredump httpd lxc nailgun naily nginx ntpstats puppet rabbitmq remote rhsm supervisor ; do
+  mkdir -p /var/log/docker-logs/$dir
+done
+rmdir /var/log/remote && ln -s /var/log/docker-logs/remote /var/log/remote
+# build storage containers and run all containers
+$dockerctl build all
+
