123b5d1b0346a6557f1a54ff47659317ca72857b
Roman Vyalov <rvyalov@mirantis.com>
2014-04-16 13:33:31 +0400

    Move to 12.04.4 netboot image
    Update netboot.tar.gz to 12.04.4
    Add template to use fast MSK server for download netboot.tar.gz
    
    Change-Id: Ib27e0ce5552dd8b758fc079ec56474c11273dcd4

--- mirror/ubuntu/boot.mk
+++ mirror/ubuntu/boot.mk
@@ -3,8 +3,9 @@ ISOLINUX_FILES:=netboot.tar.gz
 # debian isolinux files
 $(addprefix $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/,$(ISOLINUX_FILES)):
 	@mkdir -p $(@D)
-	wget -O $@ http://mirror.yandex.ru/ubuntu/dists/precise/main/installer-amd64/current/images/netboot/netboot.tar.gz 
-	tar -xzf $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/$(@F) -C $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/
+	wget -O $@ http://mirror.yandex.ru/ubuntu/dists/precise-updates/main/installer-amd64/current/images/saucy-netboot/netboot.tar.gz
+	#Move to internal server for fast downloads
+	#http://mirrors.msk.mirantis.net/ubuntu/dists/precise-updates/main/installer-amd64/current/images/saucy-netboot/netboot.tar.gzASEURL)/installer-amd64/current/images/netboot/
 
 $(BUILD_DIR)/mirror/ubuntu/boot.done: \
 		$(addprefix $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/,$(ISOLINUX_FILES))
