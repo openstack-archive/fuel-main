60f07bc9f22437421fb7094fe48f62ed3542882f
Dmitry Pyzhov <dpyzhov@mirantis.com>
2014-04-21 16:52:46 +0400

    Get rid of outdated mirrors
    Change-Id: I4469f83384a5d36f3d2f262d808c89cc5ce00022

--- mirror/module.mk
+++ mirror/module.mk
@@ -7,17 +7,13 @@ clean: clean-mirror
 clean-mirror: clean-ubuntu
 	sudo rm -rf $(BUILD_DIR)/mirror
 
-include $(SOURCE_DIR)/mirror/src/module.mk
 include $(SOURCE_DIR)/mirror/centos/module.mk
 include $(SOURCE_DIR)/mirror/ubuntu/module.mk
-include $(SOURCE_DIR)/mirror/rhel/module.mk
 include $(SOURCE_DIR)/mirror/gems/module.mk
 
 $(BUILD_DIR)/mirror/build.done: \
-		$(BUILD_DIR)/mirror/src/build.done \
 		$(BUILD_DIR)/mirror/centos/build.done \
 		$(BUILD_DIR)/mirror/ubuntu/build.done \
-		$(BUILD_DIR)/mirror/rhel/build.done \
 		$(BUILD_DIR)/mirror/gems/build.done
 	$(ACTION.TOUCH)
 
