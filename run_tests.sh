29b3cad58fb9e691ea444754413a9629d91d05a1
NastyaUrlapova <aurlapova@mirantis.com>
2014-04-21 17:26:39 +0400

    Update accrording new flake8
    Change-Id: I49b9898eb6d3770c64fb458a6b63deecd8001a39

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-flake8 --ignore=H302,H802 --exclude bin/,include/,lib/,local/,tmp/ --show-source fuelweb_test fuelweb_ui_test
+flake8 --ignore=H301,H302,H306,H802 --exclude bin/,include/,lib/,local/,tmp/ --show-source fuelweb_test fuelweb_ui_test
 
