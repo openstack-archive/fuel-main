e53c07f7eff3bc47527d912c7ae237f931328a3d
Dmitry Pyzhov <dpyzhov@mirantis.com>
2014-04-25 19:21:44 +0400

    Better nodes removal in virtualbox
    Change-Id: Ic4e0f84994bb9e67f466ac07c65b11ca008248b3

--- virtualbox/functions/vm.sh
+++ virtualbox/functions/vm.sh
@@ -17,7 +17,7 @@
 # This file contains the functions to manage VMs in through VirtualBox CLI
 
 get_vm_base_path() {
-    echo -e `VBoxManage list systemproperties | grep '^Default machine folder' | sed 's/^Default machine folder\:[ \t]*//'` 
+    echo -e `VBoxManage list systemproperties | grep '^Default machine folder' | sed 's/^Default machine folder\:[ \t]*//'`
 }
 
 get_vms_running() {
@@ -40,6 +40,17 @@ is_vm_running() {
     fi
 }
 
+is_vm_present() {
+    name=$1
+    list=$(get_vms_present)
+
+    if [[ $list = *$name* ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 create_vm() {
     name=$1
     nic=$2
@@ -47,8 +58,13 @@ create_vm() {
     memory_mb=$4
     disk_mb=$5
     os=${6:-'RedHat_64'}
-   
-    # Create virtual machine with the right name and type (assuming CentOS) 
+
+    # There is a chance that some files are left from previous VM instance
+    vm_base_path=$(get_vm_base_path)
+    vm_path="$vm_base_path/$name/"
+    rm -rf "$vm_path"
+
+    # Create virtual machine with the right name and type (assuming CentOS)
     VBoxManage createvm --name $name --ostype $os --register
 
     # Set the real-time clock (RTC) operate in UTC time
@@ -116,12 +132,12 @@ delete_vm() {
         VBoxManage controlvm $name poweroff
     fi
 
-    # Virtualbox does not fully delete VM file structure, so we need to delete the corresponding directory with files as well 
     echo "Deleting existing virtual machine $name..."
-    until VBoxManage unregistervm $name --delete
+    while is_vm_present $name
     do
-        echo "retrying"
+        VBoxManage unregistervm $name --delete
     done
+    # Virtualbox does not fully delete VM file structure, so we need to delete the corresponding directory with files as well
     rm -rf "$vm_path"
 }
 
@@ -129,11 +145,11 @@ delete_vms_multiple() {
     name_prefix=$1
     list=$(get_vms_present)
 
-    # Loop over the list of VMs and delete them, if its name matches the given refix 
+    # Loop over the list of VMs and delete them, if its name matches the given refix
     for name in $list; do
         if [[ $name == $name_prefix* ]]; then
             echo "Found existing VM: $name. Deleting it..."
-            delete_vm $name 
+            delete_vm $name
         fi
     done
 }
@@ -149,7 +165,7 @@ start_vm() {
 mount_iso_to_vm() {
     name=$1
     iso_path=$2
- 
+
     # Mount ISO to the VM
     VBoxManage storageattach $name --storagectl "IDE" --port 0 --device 0 --type dvddrive --medium "$iso_path"
 }
