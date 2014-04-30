89ae0e21ff6f15ff1c76850cb60921bfc366209b
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-04-30 18:40:19 +0400

    Moved nailgun syncdb and fixtures to puppet
    This allows puppet to manage the DB operations
    better and retry until the DB is available.
    
    blueprint fuel-containerization-of-services
    
    Change-Id: Ica49d072abb808af5edfe84d1ac4efcd7d79bfe8

--- docker/nailgun/start.sh
+++ docker/nailgun/start.sh
@@ -4,7 +4,4 @@ ln -sf /etc/fuel/version.yaml /etc/nailgun/version.yaml
 #Run puppet to apply custom config
 puppet apply -v /etc/puppet/modules/nailgun/examples/nailgun-only.pp
 
-#Set up nailgun DB
-/usr/bin/nailgun_syncdb
-/usr/bin/nailgun_fixtures
 /usr/bin/supervisord -n
