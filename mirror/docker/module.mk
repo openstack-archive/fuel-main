3d4ce64975446211196ffce925393d1687a46303
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-05-12 19:45:29 +0400

    Skip docker clean if docker is not running
    Change-Id: I40adfe5f7b5ddea7e0d7917f34deb8a8081fe030
    Closes-Bug: #1318633

--- mirror/docker/module.mk
+++ mirror/docker/module.mk
@@ -5,7 +5,7 @@ include $(SOURCE_DIR)/mirror/docker/base-images.mk
 clean: clean-docker
 
 clean-docker:
-	(docker rm -f `docker ps -aq`; docker rmi `docker images -q`) || exit 0
+	pgrep docker && (sudo docker rm -f `sudo docker ps -aq`; sudo docker rmi `sudo docker images -q`) || exit 0
 
 $(BUILD_DIR)/mirror/docker/build.done: \
 		$(BUILD_DIR)/mirror/docker/base-images.done
