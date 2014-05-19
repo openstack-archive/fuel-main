6da775927f97c9d01ac466fd6cf7a44c9ada8ca2
Aleksandr Didenko <adidenko@mirantis.com>
2014-05-19 14:04:44 +0300

    Update cobbler container start script
    Stop httpd and xinetd services before puppet run in order to make
    sure no pids left. Puppet will configure and start needed services
    itself according to manifests.
    
    Closes-bug: #1320834
    Change-Id: I3a338a13a94512676828b006521672a815e7b922

--- docker/cobbler/start.sh
+++ docker/cobbler/start.sh
@@ -4,6 +4,11 @@ mkdir -p /var/log/cobbler/{anamon,kicklog,syslog,tasks}
 #reset authorized_keys file so puppet can a write new one
 rm -f /etc/cobbler/authorized_keys
 
+# Make sure services are not running (no pids, etc), puppet will
+# configure and bring them up.
+/etc/init.d/httpd stop
+/etc/init.d/xinetd stop
+
 #Run puppet to apply custom config
 puppet apply -v /etc/puppet/modules/nailgun/examples/cobbler-only.pp
 #stop cobbler and dnsmasq
@@ -11,7 +16,5 @@ puppet apply -v /etc/puppet/modules/nailgun/examples/cobbler-only.pp
 /etc/init.d/cobblerd stop
 
 #Set up nailgun DB
-/etc/init.d/httpd start
-/etc/init.d/xinetd start
 dnsmasq -d &
 cobblerd -F
