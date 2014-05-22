aec2192bc667d405184d33727a3dff5a16e82696
Aleksandra Fedorova <afedorova@mirantis.com>
2014-05-22 13:44:36 +0400

    Make sheet names version-independent
    Also add logging and several flake8 fixes.
    
    Change-Id: I245b303f14fd7b0c085ee1e1aa2245d573458501

--- utils/jenkins/report-exporter/__main__.py
+++ utils/jenkins/report-exporter/__main__.py
@@ -20,43 +20,41 @@ from spreadsheet import BuildsDocument
 from settings import GOOGLE_SPREADSHEET
 
 logger = logging.getLogger(__package__)
+logger.addHandler(logging.StreamHandler())
 
 parser = OptionParser()
 parser.add_option('-j', '--job-name', dest='job_name',
                   help='Jenkins job name')
 parser.add_option('-s', '--spreadsheet', dest='spreadsheet',
                   help='Google spreadsheet key', default=GOOGLE_SPREADSHEET)
+parser.add_option("-v", "--verbose",
+                  action="store_true", dest="verbose", default=False,
+                  help="Enable debug output")
+
 (options, args) = parser.parse_args()
+
+if options.verbose:
+    logger.setLevel(logging.DEBUG)
 
 # Get data from Jenkins
 job = Job(options.job_name)
-build = None
-# Look for last finished build
-for b in job.builds:
-    if b.get_data()['building']:
-        logger.info(
-            'job #{0} is building. Go to previous build'.format(b.number))
-        continue
-
-    test_report = b.test_report
-    if test_report is None:
-        raise Exception('Test report has not been found. Job: {0}. '
-                        'Build #{1}'.format(options.job_name, b.number))
-
-    build = b
-    break
-
-if build is None:
-    raise Exception('Finished build has not been found. '
-                    'Job: {0}'.format(options.job_name))
+build = job.last_finished_build()
 
 # Open google spreadsheet
 doc = BuildsDocument(options.spreadsheet)
-sheet = doc.get_sheet(job.name)
 
+# Remove leading version-dependent symbols
+sheet_name = job.name[job.name.index("system_test."):]
+logger.info("Sheet %s" % sheet_name)
+
+test_report = build.test_report
+
+sheet = doc.get_sheet(sheet_name)
 gd_client = doc.gspreadsheet.client._GetSpreadsheetsClient()
-gd_sheet = filter(lambda e: job.name in e.title.text,
-                  gd_client.GetWorksheetsFeed(key=options.spreadsheet).entry).pop()
+gd_sheet = filter(
+    lambda e: sheet_name in e.title.text,
+    gd_client.GetWorksheetsFeed(key=options.spreadsheet).entry
+).pop()
 gd_sheet_id = gd_sheet.id.text.rsplit('/', 1)[1]
 gd_rows = \
     {r.title.text: r for r in gd_client.GetListFeed(
