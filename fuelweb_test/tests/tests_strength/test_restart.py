752c059ec10fa5a63ce1b3ad2a881a0e9038866d
Tatyana Leontovich <tleontovich@mirantis.com>
2014-05-27 15:05:38 +0300

    5.1 Fix ceph restart tests
    Disable volume_lvm if ceph is using for volumes
    Delete cinder role if cinder is not use
    Change expected ostf test fail form 0 to 1 after compute node's
    destroyed
    
    Change-Id: I68aafc6bc69ed12b4c4c5fd8dd8d749da1cd0369
    Related-Bug: #1323252

--- fuelweb_test/tests/tests_strength/test_restart.py
+++ fuelweb_test/tests/tests_strength/test_restart.py
@@ -55,7 +55,7 @@ class CephRestart(TestBasic):
             mode=settings.DEPLOYMENT_MODE_SIMPLE,
             settings={
                 'volumes_ceph': True,
-                'images_ceph': True
+                'volumes_lvm': False,
             }
         )
         self.fuel_web.update_nodes(
@@ -89,7 +89,7 @@ class CephHARestart(TestBasic):
         Scenario:
             1. Create cluster
             2. Add 3 nodes with controller and ceph OSD roles
-            3. Add 1 node with cinder and ceph OSD roles
+            3. Add 1 node with ceph OSD roles
             4. Add 2 nodes with compute and ceph OSD roles
             5. Deploy the cluster
             6. Check ceph status
@@ -110,7 +110,8 @@ class CephHARestart(TestBasic):
             mode=settings.DEPLOYMENT_MODE_HA,
             settings={
                 'volumes_ceph': True,
-                'images_ceph': True
+                'images_ceph': True,
+                'volumes_lvm': False,
             }
         )
         self.fuel_web.update_nodes(
@@ -121,7 +122,7 @@ class CephHARestart(TestBasic):
                 'slave-03': ['controller', 'ceph-osd'],
                 'slave-04': ['compute', 'ceph-osd'],
                 'slave-05': ['compute', 'ceph-osd'],
-                'slave-06': ['cinder', 'ceph-osd']
+                'slave-06': ['ceph-osd']
             }
         )
         # Depoy cluster
@@ -141,7 +142,7 @@ class CephHARestart(TestBasic):
         # Destroy compute node
         self.env.nodes().slaves[4].destroy()
         check_ceph_health(self.env.get_ssh_to_remote_by_name('slave-01'))
-        self.fuel_web.run_ostf(cluster_id=cluster_id)
+        self.fuel_web.run_ostf(cluster_id=cluster_id, should_fail=1)
 
         # Cold restart
         self.fuel_web.cold_restart_nodes(self.env.nodes().slaves[:4])
