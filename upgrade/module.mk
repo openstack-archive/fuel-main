b0ff673f9f4b68a82c7e7e2cf9c5f8bb70c318d6
Dmitry Pyzhov <dpyzhov@mirantis.com>
2014-06-18 18:12:35 +0400

    Add config.yaml to upgrade tarball, update package structure
    Change-Id: I596d10833c979cd8941c4771e9f5fdeebe4c9948

--- upgrade/module.mk
+++ upgrade/module.mk
@@ -25,7 +25,8 @@ $(BUILD_DIR)/upgrade/common-part.done: \
 		$(BUILD_DIR)/iso/iso.done
 	rm -f $(BUILD_DIR)/upgrade/common-part.tar
 	mkdir -p $(BUILD_DIR)/upgrade/venv
-	tar cf $(BUILD_DIR)/upgrade/common-part.tar -C $(BUILD_DIR)/repos/fuellib/deployment --xform s:^:upgrade/: puppet
+	tar cf $(BUILD_DIR)/upgrade/common-part.tar -C $(BUILD_DIR)/repos/fuellib/deployment --xform s:^puppet/:upgrade/puppet/modules/: puppet
+	tar rf $(BUILD_DIR)/upgrade/common-part.tar -C $(BUILD_DIR)/repos/fuellib/deployment/puppet/osnailyfacter/examples --xform s:^:upgrade/puppet/manifests/: site.pp
 	# Requires virtualenv, pip, python-dev packages
 	virtualenv $(BUILD_DIR)/upgrade/venv
 	$(BUILD_DIR)/upgrade/venv/bin/pip install -r $(BUILD_DIR)/repos/nailgun/fuel_upgrade_system/fuel_upgrade/requirements.txt
@@ -33,6 +34,7 @@ $(BUILD_DIR)/upgrade/common-part.done: \
 	tar rf $(BUILD_DIR)/upgrade/common-part.tar -C $(BUILD_DIR)/upgrade/venv/lib/python* --xform s:^:upgrade/: site-packages
 	tar rf $(BUILD_DIR)/upgrade/common-part.tar -C $(BUILD_DIR)/upgrade/venv --xform s:^:upgrade/: bin/fuel-upgrade
 	tar rf $(BUILD_DIR)/upgrade/common-part.tar --mode=755 -C $(SOURCE_DIR)/upgrade upgrade.sh
+	tar rf $(BUILD_DIR)/upgrade/common-part.tar -C $(BUILD_DIR)/repos/nailgun/fuel_upgrade_system/fuel_upgrade/fuel_upgrade --xform s:^:upgrade/: config.yaml
 	$(ACTION.TOUCH)
 
 $(BUILD_DIR)/upgrade/fuel-part.done: \
@@ -47,7 +49,8 @@ $(BUILD_DIR)/upgrade/openstack-part.done: \
 		$(BUILD_DIR)/iso/iso.done
 	mkdir -p $(BUILD_DIR)/upgrade
 	rm -f $(BUILD_DIR)/upgrade/openstack-part.tar
-	tar cf $(BUILD_DIR)/upgrade/openstack-part.tar -C $(LOCAL_MIRROR) --xform s:^:upgrade/repos/: centos ubuntu
+	tar cf $(BUILD_DIR)/upgrade/openstack-part.tar -C $(LOCAL_MIRROR) --xform s:^centos/os/x86_64/:upgrade/repos/centos/x86_64/: centos/os/x86_64
+	tar rf $(BUILD_DIR)/upgrade/openstack-part.tar -C $(LOCAL_MIRROR) --xform s:^ubuntu/:upgrade/repos/ubuntu/x86_64/: ubuntu
 	tar rf $(BUILD_DIR)/upgrade/openstack-part.tar -C $(BUILD_DIR)/repos/nailgun/nailgun/nailgun/fixtures --xform s:^:upgrade/config/: openstack.yaml
 	$(ACTION.TOUCH)
 
