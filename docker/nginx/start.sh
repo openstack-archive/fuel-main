9182e08bf6ad43e1c631533e0743f31e2b342adf
Evgeniy L <eli@mirantis.com>
2014-06-25 13:57:42 +0400

    Fix nginx start.sh to set right permissions for all files
    chmod -R doesn't follow symlinks in /var/www/nailgun
    directory, rewrote it with find and -follow key
    
    Change-Id: I43f9b9164d8f0f4d8ddb7d06cc2549145bd135bb
    Closes-bug: #1333631

--- docker/nginx/start.sh
+++ docker/nginx/start.sh
@@ -3,6 +3,6 @@
 rm -rf /var/www/nailgun/dump
 ln -s /dump /var/www/nailgun/
 
-chmod -R 755 /var/www/nailgun
+find /var/www/nailgun -follow -exec chmod 755 {} \;
 puppet apply -v /etc/puppet/modules/nailgun/examples/nginx-only.pp
 nginx -g 'daemon off;'
