86cd7a3cf72c0dc5d7f6c5ba4ad0ab6459681839
Evgeniy L <eli@mirantis.com>
2014-06-25 13:57:42 +0400

    Fix nginx start.sh script, to set right permissions for dump directory
    Run `chmod -R 755 /var/www/nailgun/*`, to
    set permissions for /var/www/nailgun/dump
    which is symlink to /dump. We have to do it,
    because chmod -R doesn't follow symlinks.
    
    Change-Id: I43f9b9164d8f0f4d8ddb7d06cc2549145bd135bb
    Closes-bug: #1333631

--- docker/nginx/start.sh
+++ docker/nginx/start.sh
@@ -4,5 +4,7 @@ rm -rf /var/www/nailgun/dump
 ln -s /dump /var/www/nailgun/
 
 chmod -R 755 /var/www/nailgun
+chmod -R 755 /var/www/nailgun/* 2>/dev/null
+
 puppet apply -v /etc/puppet/modules/nailgun/examples/nginx-only.pp
 nginx -g 'daemon off;'
