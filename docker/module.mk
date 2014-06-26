11c84536588163f6e34e72febe06ac7df594b224
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-06-26 11:47:43 +0400

    Refactor docker build process
    The following changes are included:
    * Remove docker save step
    * Add local docker daemon
    * Use devicemapper storage (CentOS compat)
    * Refactor docker clean for new system
    * Modified tar for devicemapper sparse file
    * Enables parallel make iso builds
    
    Change-Id: I20ef911378bd3e42a16a27f028c8a14d5a750aeb
    Closes-Bug: #133333458

--- docker/module.mk
+++ docker/module.mk
@@ -1,16 +1,21 @@
 .PHONY: docker
 
 ISOROOT:=$(BUILD_DIR)/iso/isoroot
+DOCKERROOT:=$(BUILD_DIR)/docker/dockerroot
+DOCKER_LOGFILE:=$(BUILD_DIR)/docker/docker.log
+
 RANDOM_PORT:=$(shell shuf -i 9000-65000 -n 1)
 
-docker: $(BUILD_DIR)/docker/build.done
+docker: $(BUILD_DIR)/docker/build.done \
+        $(BuiLD_DIR)/docker/daemon-down.done
 
 define build_container
 $(BUILD_DIR)/docker/$1.done: \
 		$(BUILD_DIR)/mirror/build.done \
 		$(BUILD_DIR)/repos/repos.done \
 		$(BUILD_DIR)/iso/isoroot-files.done \
-		$(BUILD_DIR)/docker/base-images.done
+		$(BUILD_DIR)/docker/base-images.done \
+                $(BUILD_DIR)/docker/daemon-up.done
 ifeq ($(DOCKER_PREBUILT),0)
 	(cd $(LOCAL_MIRROR_CENTOS) && python $(SOURCE_DIR)/utils/simple_http_daemon.py $(RANDOM_PORT) /tmp/simple_http_daemon_$(RANDOM_PORT).pid)
 	mkdir -p "$(BUILD_DIR)/docker/containers"
@@ -23,20 +28,31 @@ ifeq ($(DOCKER_PREBUILT),0)
 	sed -e 's/production:.*/production: "docker-build"/' -i $(BUILD_DIR)/docker/$1/etc/fuel/version.yaml
 	cp $(SOURCE_DIR)/docker/docker-astute.yaml $(BUILD_DIR)/docker/$1/etc/fuel/astute.yaml
 	rsync -a $(BUILD_DIR)/repos/fuellib/deployment/puppet/* $(BUILD_DIR)/docker/$1/etc/puppet/modules/
-	sudo docker build -t fuel/$1_$(PRODUCT_VERSION) $(BUILD_DIR)/docker/$1
-	sudo docker save fuel/$1_$(PRODUCT_VERSION) > $(BUILD_DIR)/docker/containers/$1.tar
+	sudo docker $(DOCKER_OPTS) build -t fuel/$1_$(PRODUCT_VERSION) $(BUILD_DIR)/docker/$1
 	kill `cat /tmp/simple_http_daemon_$(RANDOM_PORT).pid`
 endif
 	$$(ACTION.TOUCH)
 endef
 
-$(BUILD_DIR)/docker/base-images.done:
-	find $(LOCAL_MIRROR_DOCKER_BASEURL)/ -regex '.*xz' | xargs -n1 sudo docker load -i
+$(BUILD_DIR)/docker/base-images.done: $(BUILD_DIR)/docker/daemon-up.done
+	find $(LOCAL_MIRROR_DOCKER_BASEURL)/ -regex '.*xz' | xargs -n1 sudo docker $(DOCKER_OPTS) load -i
 	$(ACTION.TOUCH)
 
-$(BUILD_DIR)/docker/busybox.done:
+$(BUILD_DIR)/docker/busybox.done: $(BUILD_DIR)/docker/daemon-up.done
 	mkdir -p "$(BUILD_DIR)/docker/containers"
-	sudo docker save busybox > $(BUILD_DIR)/docker/containers/busybox.tar
+	touch "$(DOCKER_LOGFILE)"
+	sudo DOCKER_LOGFILE="$(DOCKER_LOGFILE)" docker $(DOCKER_OPTS) save busybox > $(BUILD_DIR)/docker/containers/busybox.tar
+	$(ACTION.TOUCH)
+
+$(BUILD_DIR)/docker/daemon-up.done:
+	mkdir -p "$(DOCKERROOT)"
+	(sudo docker -d -g $(DOCKERROOT) -p $(BUILD_DIR)/docker/docker.pid -H=unix://$(BUILD_DIR)/docker/docker.sock -s="devicemapper" &)
+	sleep 3
+	$(ACTION.TOUCH)
+$(BUILD_DIR)/docker/daemon-down.done:
+	kill `cat $(BUILD_DIR)/docker/docker.pid`
+	-sudo umount -l `sudo grep "$(BUILD_DIR)/docker" /proc/mounts | awk '{print $$2}' | sort -r` /dev/notfound
+
 	$(ACTION.TOUCH)
 
 # Lrzip all containers into single archive
@@ -53,9 +69,9 @@ $(BUILD_DIR)/docker/build.done: \
 		$(BUILD_DIR)/docker/rsyslog.done \
 		$(BUILD_DIR)/docker/busybox.done
 ifeq ($(DOCKER_PREBUILT),0)
-	(cd $(BUILD_DIR)/docker/containers && tar cf fuel-images.tar *.tar)
+	(cd $(DOCKERROOT) && sudo tar Scf $(BUILD_DIR)/docker/containers/fuel-images.tar *)
 	lrzip -L2 -U -D -f $(BUILD_DIR)/docker/containers/fuel-images.tar -o $(BUILD_DIR)/docker/fuel-images.tar.lrz
-	rm -f $(BUILD_DIR)/docker/*tar
+	rm -f $(BUILD_DIR)/docker/containers/*tar
 else
 	rm -f $(BUILD_DIR)/docker/fuel-images.tar.lrz
 ifeq ($(wildcard $(DOCKER_PREBUILT_SOURCE)),)
