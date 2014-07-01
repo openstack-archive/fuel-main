79c3c16ba0b0d2ac0c7755742b5aab1bd75ec6de
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2014-07-01 12:07:56 +0400

    Building OS images centos and ubuntu
    Those images are supposed to be used for
    image based provisioning.
    
    Implements: blueprint image-based-provisioning
    Change-Id: I4f038c99d4caf7d2b6e291bff29572b3a45d92c7

---
+++ image/centos/module.mk
@@ -0,0 +1,26 @@
+.PHONY: centos_image clean_centos_image
+
+centos_image: $(BUILD_DIR)/image/centos/build.done
+
+clean_centos_image:
+	-sudo umount $(BUILD_DIR)/image/centos/SANDBOX/proc
+	-sudo umount $(BUILD_DIR)/image/centos/SANDBOX/dev
+	sudo rm -rf $(BUILD_DIR)/image/centos
+
+$(BUILD_DIR)/image/centos/build.done: $(BUILD_DIR)/mirror/build.done
+$(BUILD_DIR)/image/centos/build.done: SANDBOX:=$(BUILD_DIR)/image/centos/SANDBOX
+$(BUILD_DIR)/image/centos/build.done: export SANDBOX_UP:=$(SANDBOX_UP)
+$(BUILD_DIR)/image/centos/build.done: export SANDBOX_DOWN:=$(SANDBOX_DOWN)
+$(BUILD_DIR)/image/centos/build.done:
+	@mkdir -p $(@D)
+	sudo sh -c "$${SANDBOX_UP}"
+	sudo yum -c $(SANDBOX)/etc/yum.conf --installroot=$(SANDBOX) -y --nogpgcheck install tar python-setuptools git python-imgcreate python-argparse
+	sudo cp $(SOURCE_DIR)/image/centos/build_centos_image.py $(SANDBOX)/build_centos_image.py
+	sudo cp $(SOURCE_DIR)/image/centos/centos.ks $(SANDBOX)/centos.ks
+	sudo sed -i -e "s;will_be_substituted_with_centos_repo_baseurl;$(LOCAL_MIRROR_CENTOS_OS_BASEURL);g" $(SANDBOX)/centos.ks
+	sudo chroot $(SANDBOX) python build_centos_image.py -k centos.ks -n centos -e
+	sudo mv $(SANDBOX)/centos.img $(BUILD_DIR)/image/centos/
+	sudo mv $(SANDBOX)/initramfs*.img $(BUILD_DIR)/image/centos/
+	sudo mv $(SANDBOX)/vmlinuz* $(BUILD_DIR)/image/centos/
+	sudo sh -c "$${SANDBOX_DOWN}"
+	$(ACTION.TOUCH)
