9237f476d79385ca44dc00852d2c17043347b89e
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2014-07-01 12:07:56 +0400

    Building OS images centos and ubuntu
    Those images are supposed to be used for
    image based provisioning.
    
    Related-BP: image-based-provisioning
    Change-Id: I4f038c99d4caf7d2b6e291bff29572b3a45d92c7

---
+++ image/module.mk
@@ -0,0 +1,15 @@
+.PHONY: image clean_image
+
+include $(SOURCE_DIR)/image/centos/module.mk
+include $(SOURCE_DIR)/image/ubuntu/module.mk
+
+image: $(BUILD_DIR)/image/build.done
+
+clean: clean_image
+clean_image: clean_centos_image clean_ubuntu_image
+	rm -rf $(BUILD_DIR)/image
+
+$(BUILD_DIR)/image/build.done: \
+		$(BUILD_DIR)/image/centos/build.done \
+		$(BUILD_DIR)/image/ubuntu/build.done \
+	$(ACTION.TOUCH)
---
+++ image/ubuntu/module.mk
@@ -0,0 +1,10 @@
+.PHONY: ubuntu_image clean_ubuntu_image
+
+ubuntu_image: $(BUILD_DIR)/image/ubuntu/build.done
+
+clean_ubuntu_image:
+	sudo rm -rf $(BUILD_DIR)/image/ubuntu
+
+$(BUILD_DIR)/image/ubuntu/build.done: $(BUILD_DIR)/mirror/build.done
+$(BUILD_DIR)/image/ubuntu/build.done:
+	$(ACTION.TOUCH)
