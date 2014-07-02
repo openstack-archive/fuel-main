2f53b5ce31869194a5c06d406efcee0799fa0f58
Igor Kalnitsky <igor@kalnitsky.org>
2014-07-02 13:31:59 +0300

    Generate upgrade.sh based on make target
    upgrade.sh shell script should use a valid arguments for fuel_upgrade script
    based on tarball purpose: fuel upgrade or openstack upgrade or both.
    
    Closes-Bug: #1336365
    
    blueprint upgrade-to-5-0-1
    
    Change-Id: I752bb082ee8e68cddd4bda3d4ac4ea51b5c2f030

---
+++ upgrade/upgrade_template.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname $(readlink -e $0))
+UPGRADE_PATH=$SCRIPT_PATH/upgrade
+UPGRADERS={{UPGRADERS}}
+
+
+function error {
+  local message="$1"
+  local code="${2:-1}"
+
+  echo "${message}"
+
+  exit "${code}"
+}
+
+
+function prepare_upgrade_files {
+  DOCKER_IMAGES_DIR_PATH=$UPGRADE_PATH/images
+  DOCKER_IMAGES_ARCHIVE_PATH=$DOCKER_IMAGES_DIR_PATH/fuel-images.tar.lrz
+
+  pushd $DOCKER_IMAGES_DIR_PATH
+  lrzuntar -f $DOCKER_IMAGES_ARCHIVE_PATH
+  popd
+}
+
+
+function run_upgrade {
+  # decompress images iff the docker upgrader is used
+  if [[ $UPGRADERS == *docker* ]]; then
+    prepare_upgrade_files
+  fi
+
+  # run fuel_upgrade script
+  PYTHONPATH="$UPGRADE_PATH/site-packages" python "$UPGRADE_PATH/bin/fuel-upgrade" --src "$UPGRADE_PATH" $UPGRADERS || error "Upgrade failed" $?
+}
+
+
+run_upgrade
