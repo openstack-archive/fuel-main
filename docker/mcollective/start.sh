b3a05ef3719024537992bdb835029ae03876b543
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-07-04 13:09:02 +0400

    Remove dump directory workaround
    Removes special symlink for dump directory
    that was put in place for nginx and
    mcollective containers.
    
    Change-Id: I519808013460a6ee27f00a955c0eff5e3a1316a0
    Related-Bug: #1332496

--- docker/mcollective/start.sh
+++ docker/mcollective/start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#link dump dir
-rm -rf /var/www/nailgun/dump
-ln -s /dump /var/www/nailgun/
-
 puppet apply -d -v /etc/puppet/modules/mcollective/examples/mcollective-server-only.pp
 sed -e 's/daemonize[[:space:]]*=[[:space:]]*1/daemonize = 0/g' -i /etc/mcollective/server.cfg
 /usr/sbin/mcollectived --pid=/var/run/mcollectived.pid --config=/etc/mcollective/server.cfg
