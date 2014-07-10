924c6af2842c59abde5bfaa53211708ff1501c92
Evgeniy L <eli@mirantis.com>
2014-07-10 20:26:32 +0400

    Clean rpm locks before puppet run in docker containers
    Sometimes there are rpm locks from previous container
    run as result when puppet tries to configure container
    it hangs or just fails.
    
    Change-Id: Ic5c0d07ab70898018f4429b9d13dd75d954dc1e2
    Closes-bug: #1339236

--- docker/rabbitmq/start.sh
+++ docker/rabbitmq/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Clean rpm locks before puppet run.
+# See ticket https://bugs.launchpad.net/fuel/+bug/1339236
+rm -f /var/lib/rpm/__db.*
+rpm --rebuilddb
+
 mkdir -p /var/log/rabbitmq
 chown -R rabbitmq:rabbitmq /var/log/rabbitmq
 
