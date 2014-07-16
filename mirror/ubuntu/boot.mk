37b50682c58444d33bf5fbe17814591af2109e7e
Roman Vyalov <rvyalov@mirantis.com>
2014-07-16 14:28:20 +0400

    Remove mirror yandex.ru from downloading ubuntu kernel
    Closes-bug: #1311095
    Change-Id: I29a6ca7718a881677b45df291ddfd224f04c579b

--- mirror/ubuntu/boot.mk
+++ mirror/ubuntu/boot.mk
@@ -3,7 +3,12 @@ ISOLINUX_FILES:=netboot.tar.gz
 # debian isolinux files
 $(addprefix $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/,$(ISOLINUX_FILES)):
 	@mkdir -p $(@D)
-	wget -O $@ http://mirror.yandex.ru/ubuntu/dists/precise-updates/main/installer-amd64/current/images/saucy-netboot/netboot.tar.gz
+ifeq ($(USE_MIRROR),none)
+	wget -O $@ $(MIRROR_UBUNTU)/ubuntu/dists/precise-updates/main/installer-amd64/current/images/saucy-netboot/netboot.tar.gz
+else
+	wget -O $@ $(MIRROR_UBUNTU)/installer-amd64/current/images/netboot/netboot.tar.gz
+endif
+	
 	tar -xzf $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/$(@F) -C $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/
 
 $(BUILD_DIR)/mirror/ubuntu/boot.done: \
