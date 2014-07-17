6a8e820a54dc47ecd98869cb82f46d27309ce29c
Dmitry Pyzhov <dpyzhov@mirantis.com>
2014-07-17 18:57:37 +0400

    Build iso from artifacts
    Change-Id: I7fec5f934362600f1dbccb809e7122d8ab612efe
    Implements: blueprint build-artifacts

--- fuelweb_test/module.mk
+++ fuelweb_test/module.mk
@@ -1,7 +1,7 @@
 test: test-integration
 
 .PHONY: test-integration
-test-integration: $(BUILD_DIR)/iso/iso.done
+test-integration: $(ISO_PATH)
 	 ENV_NAME=$(ENV_NAME) ISO_PATH=$(abspath $(ISO_PATH)) LOGS_DIR=$(LOGS_DIR) nosetests -l $(LEVEL) $(NOSEARGS) -w $(SOURCE_DIR)/fuelweb_test/integration --with-xunit -s
 
 .PHONY: clean-integration-test
