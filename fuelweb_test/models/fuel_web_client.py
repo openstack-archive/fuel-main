c5d74f393055f4dadd732cb8d84fd4198ccffcc9
Artem Panchenko <apanchenko@mirantis.com>
2014-07-21 20:28:24 +0300

    Sync time on nodes before checking Ceph health
    System test 'ceph_ha' almost always fails while
    checking ceph health due to clock skew. That's
    why an additional time synchronization and restart
    of Ceph service was added to the test.
    
    Change-Id: Ic6397e878fdca747f33e47f05e846d25f061ce7b
    Closes-bug: #1336233

--- fuelweb_test/models/fuel_web_client.py
+++ fuelweb_test/models/fuel_web_client.py
@@ -1026,3 +1026,21 @@ class FuelWebClient(object):
             if release_name in release["name"]:
                 release_ids.append(release['id'])
         return release_ids
+
+    @logwrap
+    def sync_ceph_time(self, cluster_id):
+        cluster_nodes = self.client.list_cluster_nodes(cluster_id)
+        ceph_nodes_ips = [n['ip'] for n in cluster_nodes if 'ceph-osd' in
+                          n['roles']]
+        self.environment.sync_time_admin_node()
+        if OPENSTACK_RELEASE_UBUNTU in OPENSTACK_RELEASE:
+            cmd = 'service ceph-all restart'
+        else:
+            cmd = 'service ceph restart'
+        for node_ip in ceph_nodes_ips:
+            remote = self.environment.get_ssh_to_remote(node_ip)
+            self.environment.sync_node_time(remote)
+            result = remote.execute(cmd)
+            if not result['exit_code'] == 0:
+                raise Exception('Ceph restart failed on {0}: {1}'.
+                                format(node_ip, result['stderr']))
