eb50bae0040a087a27582e9c47d727333ca83299
Stepan Rogov <srogov@mirantis.com>
2014-07-31 13:26:13 +0400

    Create system tests for vcenter support and partialy enhace some classes/functions for our cases
    Added:
    1. In os_actions.py - some functions for nova client
    2. In fuel_web_client.py - support of vCenter deploing
    3. In run_tests.py - import tests for vCenter
    4. In settings.py - import vCenter settings from environment
    5. In base_test_case.py - new deploy case with one slave
    
    Change-Id: I18991dbb4b63bf9f407acdd05422032014889b3d

---
+++ fuelweb_test/tests/test_vcenter.py
@@ -0,0 +1,176 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import time
+
+from proboscis.asserts import assert_true
+from proboscis import test
+from devops.helpers.helpers import wait
+from fuelweb_test import settings
+from fuelweb_test.helpers import os_actions
+from fuelweb_test.helpers.decorators import log_snapshot_on_error
+from fuelweb_test.tests.base_test_case import SetupEnvironment
+from fuelweb_test.tests.base_test_case import TestBasic
+from fuelweb_test import logger
+
+
+@test(groups=["vcenter"])
+class VcenterDeploy(TestBasic):
+    @test(depends_on=[SetupEnvironment.prepare_slaves_1],
+          groups=["smoke", "vcenter_one_node_simple"])
+    @log_snapshot_on_error
+    def vcenter_one_node_simple(self):
+        """Deploy cluster with controller node only
+
+        Scenario:
+            1. Create cluster
+            2. Add 1 node with controller role
+            3. Deploy the cluster
+            4. Validate cluster was set up correctly, there are no dead
+            services, there are no errors in logs
+            5. Create instance and delete instance.
+
+        """
+        self.env.revert_snapshot("ready_with_1_slaves")
+
+        # Configure cluster
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=settings.DEPLOYMENT_MODE_SIMPLE,
+            settings={
+                'use_vcenter': True,
+                'host_ip': settings.VCENTER_IP,
+                'vc_user': settings.VCENTER_USERNAME,
+                'vc_password': settings.VCENTER_PASSWORD,
+                'cluster': settings.VCENTER_CLUSTERS
+            }
+        )
+        logger.info("cluster is {}".format(cluster_id))
+
+        # Add nodes to roles
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {'slave-01': ['controller']}
+        )
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        # Wait until nova-compute get information about clusters
+        time.sleep(60)
+
+        self.fuel_web.run_single_ostf_test(
+            cluster_id=cluster_id, test_sets=['smoke'],
+            test_name=('fuel_health.tests.smoke.'
+                       'test_nova_create_instance_with_connectivity.'
+                       'TestNovaNetwork.test_004_create_servers')
+        )
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_1],
+          groups=["vcenter_multiple_cluster"])
+    @log_snapshot_on_error
+    def vcenter_multiple_cluster(self):
+        """Deploy cluster with controller node only and test Vcenter
+           multiple clusters support feature
+
+        Scenario:
+            1. Create cluster
+            2. Add 1 node with controller role
+            3. Deploy the cluster
+            services, there are no errors in logs
+            4. Check that available at least two hypervisor
+            5. Create 4 instances
+            6. Check connectivity between 2 instances in different hypervisor
+        """
+        self.env.revert_snapshot("ready_with_1_slaves")
+
+        # Configure cluster
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=settings.DEPLOYMENT_MODE_SIMPLE,
+            settings={
+                'use_vcenter': True,
+                'host_ip': settings.VCENTER_IP,
+                'vc_user': settings.VCENTER_USERNAME,
+                'vc_password': settings.VCENTER_PASSWORD,
+                'cluster': settings.VCENTER_CLUSTERS
+            }
+        )
+        logger.info("cluster is {0}".format(cluster_id))
+
+        # Add nodes to roles
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {'slave-01': ['controller']}
+        )
+        # Deploy cluster
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        # Wait until nova-compute get information about clusters
+        time.sleep(60)
+
+        ctrl_ip = self.fuel_web.get_nailgun_node_by_name('slave-01')['ip']
+        logger.info("Controller ip is {}".format(ctrl_ip))
+        os = os_actions.OpenStackActions(ctrl_ip)
+        hypervisors = os.get_hypervisors()
+
+        # Check hypervisor quantity and create instances
+        assert_true(len(hypervisors) > 1, 'Not enoght vCenter clusters.')
+        if len(hypervisors) > 1:
+            logger.info("Create Instances and assign floating ips:")
+            for i in range(1, 5):
+                srv = os.create_server_for_migration()
+                logger.info(os.get_instance_detail(srv).to_dict()['name'])
+                os.assign_floating_ip(srv)
+
+        # Check that there are instanses on each hypervisor
+        time.sleep(30)
+        hypervisors = os.get_hypervisors()
+        for hypervisor in hypervisors:
+            assert_true(os.get_hypervisor_vms_count(hypervisor) != 0,
+                        "No active VMs on " +
+                        os.get_hypervisor_hostanme(hypervisor))
+            logger.info("{} active VMs  on Hypervisor {}".format(
+                        os.get_hypervisor_vms_count(hypervisor),
+                        os.get_hypervisor_hostanme(hypervisor))
+
+        # Get instances ips from different hypervisors
+        servers_for_check = {}
+        ips_for_check = []
+        servers = os.get_servers()
+        for server in servers:
+            if os.get_srv_hypervisor_name(server) not in servers_for_check:
+                servers_for_check[os.get_srv_hypervisor_name(server)] = {}
+                server_detail = os.get_instance_detail(server).to_dict()
+                for net_prefs in server_detail['addresses']['novanetwork']:
+                    if net_prefs['OS-EXT-IPS:type'] == 'floating' and \
+                       net_prefs['addr'] not in ips_for_check and \
+                       len(ips_for_check) == 0:
+                        ips_for_check.append(net_prefs['addr'])
+                    if net_prefs['OS-EXT-IPS:type'] == 'fixed' and \
+                       len(ips_for_check) == 1:
+                        ips_for_check.append(net_prefs['addr'])
+
+        # Wait until vm is booted
+        wait(
+            lambda: not node_ssh.execute(
+            'curl -s -m1 http://' + ips_for_check[0]  +
+            ':22 |grep -iq "[a-z]"')['exit_code'], interval=10, timeout=100)
+
+        # Check server's connectivity
+        ssh = self.env.get_ssh_to_remote_by_name("slave-01")
+        res = os.execute_through_host(ssh, ips_for_check[0],
+                                      "ping -q -c3 " + ips_for_check[1] +
+                                      " | grep received | grep -v '0% packet \
+                                      loss'")
+        assert_true(res == "", "Error in Instances network connectivity.\n" +
+                    str(res))
