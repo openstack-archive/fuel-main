8a76fb3d1b08dcf33c2bddf5e248359bdae1301d
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-08-11 18:53:01 +0400

    Add fuel-centos-build container for fuel/centos
    fuel/centos will now be built from CentOS6
    container dynamically with current package set
    instead of being periodically rebuilt. It uses
    ami-creator, which only runs on CentOS. It is
    run inside Docker so it can be used on an
    Ubuntu build host. After the image is created,
    img2docker loads the content into Docker as a
    Docker image.
    
    Change-Id: Ibff08d309fe091cc40d053781b7556fd36ebf173
    Closes-Bug: #1336911

--- mirror/docker/base-images.mk
+++ mirror/docker/base-images.mk
@@ -1,9 +1,10 @@
-BASE_IMAGE_FILES:=fuel-centos.tar.xz busybox.tar.xz nsenter.tar.xz
+BASE_IMAGE_FILES:=centos.tar.xz busybox.tar.xz nsenter.tar.xz
 
 # docker base image files
 $(addprefix $(LOCAL_MIRROR_DOCKER_BASEURL)/,$(BASE_IMAGE_FILES)):
 	@mkdir -p $(@D)
-	wget -nv -O $@ $(MIRROR_DOCKER_BASEURL)/$(@F)
+	#Fix ugly hack and get centos.tar.xz in mirror
+	wget -nv -O $@ $(MIRROR_DOCKER_BASEURL)/$(@F) || wget -nv -O https://github.com/CentOS/sig-cloud-instance-images/blob/684a5ab43827c8316810e5d2abe6ce60e2d68e6e/docker/centos-6-20150102_1408-docker.tar.xz
 
 $(BUILD_DIR)/mirror/docker/base-images.done: \
 		$(addprefix $(LOCAL_MIRROR_DOCKER_BASEURL)/,$(BASE_IMAGE_FILES))
