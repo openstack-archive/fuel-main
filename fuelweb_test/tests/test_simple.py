e76032e6c406702c7774d929ce0c98443dc9eb6f
asledzinskiy <asledzinskiy@mirantis.com>
2014-08-19 11:05:42 +0300

    Add check that fixed network cidr is default value on slave
    Change-Id: I419f432a9717ae7d47cfc962b5d7888562e33b0c
    Closes-Bug: #1358369

--- fuelweb_test/tests/test_simple.py
+++ fuelweb_test/tests/test_simple.py
@@ -108,6 +108,7 @@ class SimpleFlat(TestBasic):
         self.fuel_web.deploy_cluster_wait(cluster_id)
         self.fuel_web.assert_cluster_ready(
             'slave-01', smiles_count=6, networks_count=1, timeout=300)
+        self.fuel_web.check_fixed_network_cidr(cluster_id, self.env.get_ssh_to_remote_by_name('slave-01'))
 
         self.fuel_web.verify_network(cluster_id)
 
