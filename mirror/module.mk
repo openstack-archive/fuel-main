b1c955d476b497218eaf565c820a95a43aed86ae
Sergey Kulanov <sergey@kulanov.org.ua>
2014-08-24 16:49:19 +0300

    Remove sudo from make process (Makefile)
    Done:
      1. sudo was removed for docker operations, by adding user
         to docker group.
      2. RPM-packages prepared with mock ($SOURCE/packages/rpm)
      3. initramfs prepared with mock ($SOURCE/bootstrap)
      4. OpenStack packages build with
         mock ($SOURCE/packages/openstack/rpm)
      5. Ubuntu mirror prepared in docker environment ($SOURCE/mirror/ubuntu/)
    
    Notes:
     - Ask user to re-login, in order to correctly process groups' membership
     - Fix issue (pinning) with multistrap version  (#1312654)
     - Fix npm global installation permission issue (#1337343)
     - Ensure nodejs installation from chris-lea (out-of-the-box nodejs should
       be also «ok», as it was mentioned https://review.openstack.org/#/c/117212/)
     - Fix lrzip periodical issue with infinite loop (removed -U option)
     - Ensure bc package is present (for building img file)
     - Old files saved for checking and comparison (draft) should be removed
       in future, while new files include mock: module_mock.mk, mock_config.mk
    
    ToDo:
      - Need to finish with openstack createrepo as there is no access to internal Mirantis
        resources (repo: http://mirrors.msk.mirantis.net/centos/) from outside
      - We still have several places with sudo, because we are working with files
        which are belongs to root. We still have sudo in such processes as:
           * preparing initramfs;
           * preparing ubuntu mirror;
           * in several make targets: img, clean
    
    Additional information:
      - mock (1.1.33-1) is currently used in Ubuntu 14.04. But it has some bugs,
        so we are fixing them in prepare-build-env.sh (create mock user, and system folders)
      - we use three different mock environments (rpm, bootstrap, openstack),
        for each environment we need to define 3 configuration files, which can be customized:
        site-defaults.cfg, logging.ini, fuel-$(PRODUCT_VERSION)-$(CENTOS_ARCH).cfg
      - Optimization should be performed: mock optimization, reusing the same environments,
        do not clear cache after build, enable ccache, use tmpfs, check  “file” options for mock in
        order to pass files to chroot environment, etc (should be discussed)
      - New Ubuntu mirror ($SOURCE/mirror/ubuntu/createrepo_docker.mk) approach is used for
        preparing ubuntu packages. So some part of functionality was implemented in docker phase,
        so for now make process should follow: centos->docker->ubuntu. New helper script was
        created ($SOURCE/mirror/docker/files/prepare_fuel_ubuntu.sh) in oder to prepare docker base
        image fuel/ubuntu on some third server and publicate this image just near fuel/centos and
        busybox (Note: In order to activate this feature you need to make changes in
           $SOURCE/mirror/ubuntu/module.mk:
    	   - include $(SOURCE_DIR)/mirror/ubuntu/createchroot.mk
    	   + include $(SOURCE_DIR)/mirror/ubuntu/createchroot_docker.mk
    
    Change-Id: I0fcaf9f78888467e24b6dc79e79c62c860b9f642
    Related-bug: #1348599

--- mirror/module.mk
+++ mirror/module.mk
@@ -5,7 +5,7 @@ mirror: $(BUILD_DIR)/mirror/build.done
 clean: clean-mirror
 
 clean-mirror: clean-ubuntu
-	sudo rm -rf $(BUILD_DIR)/mirror
+	rm -rf $(BUILD_DIR)/mirror
 
 include $(SOURCE_DIR)/mirror/centos/module.mk
 include $(SOURCE_DIR)/mirror/ubuntu/module.mk
@@ -13,6 +13,6 @@ include $(SOURCE_DIR)/mirror/docker/module.mk
 
 $(BUILD_DIR)/mirror/build.done: \
 		$(BUILD_DIR)/mirror/centos/build.done \
-		$(BUILD_DIR)/mirror/ubuntu/build.done \
-		$(BUILD_DIR)/mirror/docker/build.done
+		$(BUILD_DIR)/mirror/docker/build.done \
+		$(BUILD_DIR)/mirror/ubuntu/build.done
 	$(ACTION.TOUCH)
