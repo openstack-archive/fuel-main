e86cf49fd6699f687903452f663f93e74aada327
Dmitry Pyzhov <dpyzhov@mirantis.com>
2014-08-27 22:34:31 +0400

    New target 'make vbox-script'
    Closes-Bug: #1362563
    Change-Id: I789c5fabff1c418349b288079d9787a08ea9a411

---
+++ virtualbox.mk
@@ -0,0 +1,11 @@
+all: vbox-scripts
+
+########################
+# VBOX-SCRIPTS ARTIFACT
+########################
+vbox-scripts: $(VBOX_SCRIPTS_PATH)
+
+$(VBOX_SCRIPTS_PATH): \
+		$(call find-files,$(SOURCE_DIR)/virtualbox)
+	mkdir -p $(@D)
+	cd $(SOURCE_DIR) && zip -r $@ virtualbox
