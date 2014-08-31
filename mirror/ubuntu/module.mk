af992d90283be6a1b5047773381fbe1049e6d0bb
Alexei Sheplyakov <asheplyakov@mirantis.com>
2014-08-31 10:56:40 +0400

    Add mirror-ubuntu target
    So it's easy to rebuild the Ubuntu mirror without rebuilding the whole ISO
    
    Change-Id: Ie683dc0d03e253dc6986ba3650e529796ffd5dc7

--- mirror/ubuntu/module.mk
+++ mirror/ubuntu/module.mk
@@ -4,6 +4,7 @@ include $(SOURCE_DIR)/mirror/ubuntu/boot.mk
 include $(SOURCE_DIR)/mirror/ubuntu/createchroot.mk
 
 clean: clean-ubuntu
+mirror-ubuntu: $(BUILD_DIR)/mirror/ubuntu/build.done
 
 clean-ubuntu:
 	(mount -l | grep -q $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/proc && sudo umount $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/proc) || exit 0
