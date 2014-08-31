872b4395f178709f75c7678e36abaa95330914a1
Alexei Sheplyakov <asheplyakov@mirantis.com>
2014-08-31 10:56:40 +0400

    Add mirror-ubuntu and mirror-centos targets
    Useful for updating the Ubuntu (CentOS) packages mirror without rebuilding
    the whole ISO (and without re-deploying the Fuel master node).
    
    blueprint support-ubuntu-trusty
    Change-Id: Ie683dc0d03e253dc6986ba3650e529796ffd5dc7

--- mirror/centos/module.mk
+++ mirror/centos/module.mk
@@ -7,3 +7,7 @@ $(BUILD_DIR)/mirror/centos/build.done: \
 		$(BUILD_DIR)/mirror/centos/repo.done \
 		$(BUILD_DIR)/mirror/centos/boot.done
 	$(ACTION.TOUCH)
+
+mirror-centos: $(BUILD_DIR)/mirror/centos/build.done
+
+.PHONY: mirror-centos
