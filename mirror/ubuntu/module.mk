7e6c1d582f9463c1333881de304d95a7d14be362
Alexei Sheplyakov <asheplyakov@mirantis.com>
2014-08-31 10:56:40 +0400

    Add mirror-ubuntu and mirror-centos targets
    Useful for updating the Ubuntu (CentOS) packages mirror without rebuilding
    the whole ISO (and without re-deploying the Fuel master node).
    
    Change-Id: Ie683dc0d03e253dc6986ba3650e529796ffd5dc7

--- mirror/ubuntu/module.mk
+++ mirror/ubuntu/module.mk
@@ -1,9 +1,10 @@
-.PHONY: clean-ubuntu
+.PHONY: clean-ubuntu mirror-ubuntu
 # This module downloads ubuntu installation images.
 include $(SOURCE_DIR)/mirror/ubuntu/boot.mk
 include $(SOURCE_DIR)/mirror/ubuntu/createchroot.mk
 
 clean: clean-ubuntu
+mirror-ubuntu: $(BUILD_DIR)/mirror/ubuntu/build.done
 
 clean-ubuntu:
 	(mount -l | grep -q $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/proc && sudo umount $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/proc) || exit 0
