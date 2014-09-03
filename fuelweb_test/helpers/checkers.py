be1bfe01c630385e3492bce072ab0ed772b76a4c
NastyaUrlapova <aurlapova@mirantis.com>
2014-09-03 16:37:56 +0400

    Change according lrz archive
    Change-Id: I13969d43c26a30538244c4d489ef4b5673ab287b
    Blueprint: upgrade-lrzip

--- fuelweb_test/helpers/checkers.py
+++ fuelweb_test/helpers/checkers.py
@@ -13,6 +13,7 @@
 #    under the License.
 import hashlib
 import json
+import os
 import re
 import traceback
 
@@ -27,8 +28,6 @@ from devops.error import TimeoutError
 from devops.helpers.helpers import wait
 from devops.helpers.helpers import _wait
 
-
-import os
 from time import sleep
 
 
@@ -259,8 +258,10 @@ def check_tarball_exists(node_ssh, name, path):
 
 @logwrap
 def untar(node_ssh, name, path):
+    filename, ext = os.path.splitext(name)
+    cmd = "tar -xpvf" if ext.endswith("tar") else "lrzuntar"
     result = ''.join(node_ssh.execute(
-        'cd {0} && tar -xpvf {1}'.format(path, name))['stdout'])
+        'cd {0} && {2} {1}'.format(path, name, cmd))['stdout'])
     logger.debug('Result from tar command is {0}'.format(result))
 
 
