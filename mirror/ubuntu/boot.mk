19850a88a36c9f08c24cd0052329d4323af2345a
Albert <asyriy@mirantis.com>
2014-09-04 19:16:05 +0300

    Ubuntu with kernel 3.13.0-26 build
    Change-Id: I374f67f58d3e88146c8da5d71fee40ca213d3033

--- mirror/ubuntu/boot.mk
+++ mirror/ubuntu/boot.mk
@@ -4,7 +4,7 @@ ISOLINUX_FILES:=netboot.tar.gz
 $(addprefix $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/,$(ISOLINUX_FILES)):
 	@mkdir -p $(@D)
 ifeq ($(USE_MIRROR),none)
-	wget -O $@ $(MIRROR_UBUNTU)/ubuntu/dists/precise-updates/main/installer-amd64/current/images/saucy-netboot/netboot.tar.gz
+	wget -O $@ $(MIRROR_UBUNTU)/ubuntu/dists/precise-updates/main/installer-amd64/current/images/trusty-netboot/netboot.tar.gz
 else
 	wget -O $@ $(MIRROR_UBUNTU)/installer-amd64/current/images/netboot/netboot.tar.gz
 endif
