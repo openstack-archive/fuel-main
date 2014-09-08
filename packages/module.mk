bd393eb8016991492d420860d6b24fc3bf5e59fd
Vitaly Parakhin <vparakhin@mirantis.com>
2014-09-08 14:15:49 +0300

    Add building of Fuel deb packages during ISO build.
    1) Add routines for building deb packages and adding them to local mirror.
    2) Add commands for creating Ubuntu-based build sandbox.
    3) Update deb specs for Fuel components.
    
    Change-Id: If098654d1158aca6676f9cbb3fc3100fc2e8674d

--- packages/module.mk
+++ packages/module.mk
@@ -1,10 +1,11 @@
 include $(SOURCE_DIR)/packages/rpm/module.mk
+include $(SOURCE_DIR)/packages/deb/module.mk
 
 .PHONY: packages
 
 ifneq ($(BUILD_PACKAGES),0)
 $(BUILD_DIR)/packages/build.done: \
-		$(BUILD_DIR)/packages/rpm/build.done
+		$(BUILD_DIR)/packages/rpm/build.done $(BUILD_DIR)/packages/deb/build.done  
 endif
 
 $(BUILD_DIR)/packages/build.done:
