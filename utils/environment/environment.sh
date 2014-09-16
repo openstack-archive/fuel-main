9f92d6a649d68b303538daf43adb342d2d0bb8c0
Egor Kotko <ykotko@mirantis.com>
2014-09-16 18:10:02 +0300

    Tool for environment snapshoting
    In cases we cannot reproduce some issues
    we need to have ability to store whole
    devops environment
    
    blueprint environment-snapshot
    
    Change-Id: I5b12406fc5c37c291c124552e38794b08c6bfd02

---
+++ utils/environment/environment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+env_name="$2"
+
+case "$1" in
+"" | "-h" | "--help") echo -e "NO PARAMETERS\n""USAGE:     $0 <zip/unzip> <ENV_NAME>\n<ENV_NAME> - common name's part of all virtual machines\n"; exit 65;;
+
+zip*) ./lib/zip.sh $env_name;;
+
+unzip*) ./lib/unzip.sh $env_name;;
+
+* ) echo "Wrong argument";;
+esac
