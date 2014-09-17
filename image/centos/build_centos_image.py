b53c3b59cabd0092b80cb6516d3a95ec44e44377
Alexander Gordeev <agordeev@mirantis.com>
2014-09-17 20:13:24 +0400

    Add ability to build separate fs images
    This patch is required to build images for every separate mountpoint needed
    both for centos and ubuntu.
    Eg. /boot, /var, /var/lib, etc.
    File system support is limited to ext2, ext3, ext4 only.
    No XFS support mainly because of unexpectly consumed free space and absence of
    working resizing tools due to that.
    
    Controlled by SEPARATE_IMAGES environment variable. String format should be
    space separated list with comma separated mountpoint and filesystem type. Eg.:
    SEPARATE_IMAGES="/boot,ext2 /,ext4 /var,ext3"
    Defaults to "/boot,ext2 /,ext4". Notice, that at least separate /boot is
    required for operating of image based provisioning.
    
    Implements: blueprint image-based-provisioning
    Change-Id: I650052154fe7b483aaba89389e9a09f80fc1543e

--- image/centos/build_centos_image.py
+++ image/centos/build_centos_image.py
@@ -22,8 +22,13 @@ import sys
 import tempfile
 
 import imgcreate
+from imgcreate.errors import MountError
 from imgcreate.fs import BindChrootMount
+from imgcreate.fs import ExtDiskMount
+from imgcreate.fs import SparseLoopbackDisk
+from imgcreate.fs import makedirs
 from imgcreate.kickstart import FirewallConfig
+import yaml
 # this monkey patch is for avoiding anaconda bug with firewall configuring
 FirewallConfig.apply = lambda x, y: None
 
@@ -51,7 +56,8 @@ class ImageCreator(imgcreate.LoopImageCreator):
     ]
 
     def __init__(self, ksfile, name, tmpdir=None,
-                 cachedir=None, export_kernel=False):
+                 cachedir=None, export_kernel=False,
+                 separate_images=None, output_file=None):
         super(ImageCreator, self).__init__(
             imgcreate.read_kickstart(ksfile), name, '/')
         self.tmpdir = tmpdir or tempfile.gettempdir()
@@ -62,9 +68,24 @@ class ImageCreator(imgcreate.LoopImageCreator):
         self.export_kernel = export_kernel
         self.MODULES.extend(imgcreate.kickstart.get_modules(self.ks))
 
+        self.__selinux_mountpoint = "/sys/fs/selinux"
+
         # this will mount mirror into chroot and then unmount
+        self.__output_file = output_file
         self.__ensure_builddir()
         self.__bindmounts = [BindChrootMount('/mirror', self._instroot)]
+        self.__separate_images = {}
+        self.__separate_images_disks = []
+        self.__imgdir = None
+        for ent in separate_images.split():
+            mountpoint, fs_type = ent.split(',')
+            self.__separate_images[mountpoint] = fs_type
+
+    def __get_image(self):
+        if self.__imgdir is None:
+            self.__imgdir = self._mkdtemp()
+        return self.__imgdir + "/" + self.name + ".img"
+    _image = property(__get_image)
 
     def _get_fstab(self):
         s = 'LABEL=_/ / %s defaults 0 0\n' % self._fstype
@@ -72,7 +93,7 @@ class ImageCreator(imgcreate.LoopImageCreator):
         return s
 
     def _get_kernel_options(self):
-        s = imgcreate.kickstart.get_kernel_args(self.ks, default = 'ro')
+        s = imgcreate.kickstart.get_kernel_args(self.ks, default='ro')
         return s
 
     def _create_bootconfig(self):
@@ -108,7 +129,7 @@ timeout=%(timeout)s
         for filename in os.listdir(self._instroot + '/boot'):
             if filename.startswith('initr') or filename.startswith('vmlinuz'):
                 shutil.copyfile(self._instroot + '/boot/' + filename,
-                                os.path.join(os.getcwd(),filename))
+                                os.path.join(os.getcwd(), filename))
 
     def _custom_dracut_conf(self):
         LOG.info('Preparing dracut configuration')
@@ -123,6 +144,89 @@ drivers+=' %(modules)s '
         with open(filename, 'w') as f:
             f.write(config)
 
+    def _get_image_name(self, mountpoint):
+        return self._mkdtemp() + "/" + self.name + \
+            mountpoint.replace('/', '-') + ".img"
+
+    def mount(self, base_on=None, cachedir=None):
+        self.__ensure_builddir()
+        makedirs(self._instroot)
+        makedirs(self._outdir)
+        self._mount_instroot(base_on)
+
+        self.__fstype = self.__separate_images.pop('/')
+        key = lambda x: len(x.rstrip('/').split('/'))
+        for mountpoint in sorted(self.__separate_images.keys(), key=key):
+            makedirs(self._instroot + mountpoint)
+            loop_disk = ExtDiskMount(
+                SparseLoopbackDisk(self._get_image_name(mountpoint),
+                                   4 * 1024L * 1024 * 1024),
+                self._instroot + mountpoint,
+                self.__separate_images[mountpoint],
+                4096,  # blocksize
+                self.fslabel,
+                self.tmpdir)
+            self.__separate_images_disks.append(loop_disk)
+            try:
+                loop_disk.mount()
+            except MountError, e:
+                raise CreatorError("Failed to loopback mount '%s' : %s" %
+                                   (self._get_image_name(mountpoint), e))
+        self.__separate_images_disks.reverse()
+        self.__separate_images_disks.append(self._LoopImageCreator__instloop)
+
+        for d in ("/dev/pts", "/etc", "/boot", "/var/log", "/var/cache/yum",
+                  "/sys", "/proc"):
+            makedirs(self._instroot + d)
+
+        cachesrc = cachedir or (self.__builddir + "/yum-cache")
+        makedirs(cachesrc)
+        # bind mount system directories into _instroot
+        for (f, dest) in [("/sys", None), ("/proc", None),
+                          ("/dev/pts", None), ("/dev/shm", None),
+                          (self.__selinux_mountpoint,
+                           self.__selinux_mountpoint),
+                          (cachesrc, "/var/cache/yum")]:
+            if os.path.exists(f):
+                self.__bindmounts.append(BindChrootMount(f, self._instroot,
+                                                         dest))
+            else:
+                logging.warn("Skipping (%s,%s) because source doesn't exist." %
+                             (f, dest))
+
+        self._do_bindmounts()
+        self.__create_selinuxfs()
+        self.__create_minimal_dev()
+        os.symlink("/proc/self/mounts", self._instroot + "/etc/mtab")
+        self.__write_fstab()
+
+    def _yaml_data(self):
+        data = []
+        for disk in self.__separate_images_disks:
+            filename = os.path.basename(disk.disk.lofile)
+            mountpoint = filename[len(self.name):-4].replace('-', '/')
+            if not mountpoint:
+                mountpoint = '/'
+            data.append({mountpoint: {'filename': filename + '.gz',
+                                      'container': 'gzip',
+                                      'format': disk.fstype}})
+        with open(self.__output_file, 'wb') as f:
+            f.write(yaml.dump(data))
+
+    def _stage_final_image(self):
+        for disk in self.__separate_images_disks:
+            disk.cleanup()
+            minsize = disk._ExtDiskMount__resize_to_minimal()
+            disk.disk.truncate(minsize)
+            shutil.move(disk.disk.lofile, self._outdir + "/" +
+                        os.path.basename(disk.disk.lofile))
+
+    def unmount(self):
+        self.__destroy_selinuxfs()
+        self._undo_bindmounts()
+        for disk in self.__separate_images_disks:
+            disk.cleanup()
+
     def create(self):
         self.mount(cachedir=self.cachedir)
         self._custom_dracut_conf()
@@ -132,6 +236,7 @@ drivers+=' %(modules)s '
             self._custom_export_kernel()
         self.unmount()
         self.package()
+        self._yaml_data()
 
 
 def parse_args():
@@ -157,6 +262,15 @@ def parse_args():
         '-e', '--export', dest='export', action='store_true',
         help='export kernel and miniroot out of image', default=True
     )
+    parser.add_argument(
+        '-s', '--separate-images', dest='separate_images', action='store',
+        type=str, help='separate images string', default='/boot,ext2 /,ext4'
+    )
+    parser.add_argument(
+        '-O', '--output-file', dest='output_file', action='store',
+        type=str, required=False, default="profile.yaml",
+        help='Yaml file to store image metadata',
+    )
     return parser
 
 
@@ -166,12 +280,13 @@ def main():
 
     creator = ImageCreator(
         params.kickstart, params.name,
-        tmpdir=params.tmp, cachedir=params.cache, export_kernel=params.export)
+        tmpdir=params.tmp, cachedir=params.cache, export_kernel=params.export,
+        separate_images=params.separate_images, output_file=params.output_file)
 
     try:
         creator.create()
     except imgcreate.CreatorError, e:
-        LOG.error('Error: %s' %(e,))
+        LOG.error('Error: %s' % (e,))
         return 1
     finally:
         creator.cleanup()
