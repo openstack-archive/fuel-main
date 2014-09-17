9760fc2897782fe4cfcc3da884378510263a0cd0
Alexander Gordeev <agordeev@mirantis.com>
2014-09-17 20:13:24 +0400

    Add ability to build separate fs images for ubuntu
    This ability required to build images for every separate mountpoint needed.
    Eg. /boot, /var, /var/lib, etc.
    File system support is limited to ext2, ext3, ext4 only. No XFS.
    
    Implements: blueprint image-based-provisioning
    Change-Id: I650052154fe7b483aaba89389e9a09f80fc1543e

--- image/ubuntu/module.mk
+++ image/ubuntu/module.mk
@@ -7,23 +7,67 @@ $(ARTS_DIR)/$(TARGET_UBUNTU_IMG_ART_NAME): $(BUILD_DIR)/images/$(TARGET_UBUNTU_I
 
 TARGET_UBUNTU_DEP_FILE:=$(call find-files,$(DEPS_DIR_CURRENT)/$(TARGET_UBUNTU_IMG_ART_NAME))
 
+#NOTE: ubuntu-minimal package depends on: adduser apt apt-utils bzip2 console-setup
+#      debconf debconf-i18n eject gnupg ifupdown initramfs-tools iproute iputils-ping
+#      isc-dhcp-client kbd less locales lsb-release makedev mawk module-init-tools
+#      net-tools netbase netcat-openbsd ntpdate passwd procps python resolvconf
+#      rsyslog sudo tzdata ubuntu-keyring udev upstart ureadahead vim-tiny whiptail
+
+space:=
+space+=
+PKGS_INCLUDE:=\
+bash-completion,\
+bind9-host,\
+cloud-init,\
+cron,\
+curl,\
+daemonize,\
+dnsutils,\
+file,\
+gcc,\
+gdisk,\
+grub-pc,\
+grub-pc-bin,\
+linux-firmware,\
+linux-firmware-nonfree,\
+linux-headers-$(UBUNTU_INSTALLER_KERNEL_VERSION),\
+linux-image-$(UBUNTU_INSTALLER_KERNEL_VERSION)-generic,\
+lvm2,\
+make,\
+mdadm,\
+mlocate,\
+nailgun-net-check,\
+ntp,\
+openssh-client,\
+openssh-server,\
+perl,\
+rsync,\
+telnet,\
+ubuntu-minimal,\
+util-linux,\
+virt-what,\
+wget
+
+TMP_CHROOT:=$(BUILD_DIR)/image/tmp/ubuntu_chroot
+#TODO(agordeev): allow to specify SEPARATE_IMAGES during build, don't hardcode it
+SEPARATE_IMAGES:=/boot,ext4 /,ext4
+
 ifdef TARGET_UBUNTU_DEP_FILE
 $(BUILD_DIR)/images/$(TARGET_UBUNTU_IMG_ART_NAME): $(TARGET_UBUNTU_DEP_FILE)
 	$(ACTION.COPY)
 else
 $(BUILD_DIR)/images/$(TARGET_UBUNTU_IMG_ART_NAME): $(BUILD_DIR)/mirror/build.done
+$(BUILD_DIR)/images/$(TARGET_UBUNTU_IMG_ART_NAME): export SEPARATE_FS_IMAGES=$(SEPARATE_IMAGES)
+$(BUILD_DIR)/images/$(TARGET_UBUNTU_IMG_ART_NAME): export TMP_BUILD_DIR=$(BUILD_DIR)/image/tmp
+$(BUILD_DIR)/images/$(TARGET_UBUNTU_IMG_ART_NAME): export TMP_BUILD_IMG_DIR=$(BUILD_DIR)/image/ubuntu
+$(BUILD_DIR)/images/$(TARGET_UBUNTU_IMG_ART_NAME): export TMP_CHROOT_DIR=$(TMP_CHROOT)
+$(BUILD_DIR)/images/$(TARGET_UBUNTU_IMG_ART_NAME): export IMG_SUFFIX=$(UBUNTU_IMAGE_RELEASE)_$(UBUNTU_ARCH)
+$(BUILD_DIR)/images/$(TARGET_UBUNTU_IMG_ART_NAME): export DEBOOTSRAP_PARAMS=--no-check-gpg --arch=$(UBUNTU_ARCH) --include=$(subst $(space),,$(PKGS_INCLUDE)) $(UBUNTU_RELEASE) $(TMP_CHROOT) file://$(LOCAL_MIRROR)/ubuntu
 $(BUILD_DIR)/images/$(TARGET_UBUNTU_IMG_ART_NAME):
 	@mkdir -p $(@D)
 	mkdir -p $(BUILD_DIR)/image/ubuntu
-	truncate -s 1G $(BUILD_DIR)/image/ubuntu/ubuntu_$(UBUNTU_IMAGE_RELEASE)_$(UBUNTU_ARCH).img
-	mkfs.ext4 -F $(BUILD_DIR)/image/ubuntu/ubuntu_$(UBUNTU_IMAGE_RELEASE)_$(UBUNTU_ARCH).img
-	mkdir $(BUILD_DIR)/image/ubuntu/mnt
-	sudo mount $(BUILD_DIR)/image/ubuntu/ubuntu_$(UBUNTU_IMAGE_RELEASE)_$(UBUNTU_ARCH).img $(BUILD_DIR)/image/ubuntu/mnt -o loop
-# FIXME(kozhukalov): remove particular kernel version
-	sudo debootstrap --no-check-gpg --arch=$(UBUNTU_ARCH) --include=sudo,adduser,locales,openssh-server,file,less,kbd,curl,rsync,bash-completion,ubuntu-minimal,linux-image-$(UBUNTU_INSTALLER_KERNEL_VERSION)-generic,linux-headers-$(UBUNTU_INSTALLER_KERNEL_VERSION),util-linux,ntp,ntpdate,virt-what,grub-pc-bin,grub-pc,cloud-init $(UBUNTU_RELEASE) $(BUILD_DIR)/image/ubuntu/mnt file://$(LOCAL_MIRROR)/ubuntu
-#	sudo rm -fr $(BUILD_DIR)/image/ubuntu/mnt/boot
-	sudo umount -f $(BUILD_DIR)/image/ubuntu/ubuntu_$(UBUNTU_IMAGE_RELEASE)_$(UBUNTU_ARCH).img
-	gzip -f $(BUILD_DIR)/image/ubuntu/ubuntu_$(UBUNTU_IMAGE_RELEASE)_$(UBUNTU_ARCH).img
-	rm -fr $(BUILD_DIR)/image/ubuntu/mnt
+	touch $(BUILD_DIR)/image/ubuntu/profile.yaml
+	bash ./image/ubuntu/create_separate_images.sh
+	find $(BUILD_DIR)/image/ubuntu -name '*img' -exec gzip -f {} \;
 	tar cf $@ -C $(BUILD_DIR)/image/ubuntu .
 endif
