2d5087fc8275676dfd736bc062b67a9e12617171
Sergey Kulanov <skulanov@mirantis.com>
2014-09-25 15:57:44 +0300

    Reducing the size of upgrade tarball
    * implemented building diff mirrors
    * defined diff mirror artifacts
    * put diff mirrors into upgrade tarball
    
    Implements-blueprint: generating-update-mirrors
    Change-Id: Ic208b28e87a1505fe12317494ad327841c9a0964

---
+++ mirror/diff_mirror_module.mk
@@ -0,0 +1,130 @@
+.PHONY: diff-mirror centos-diff-repo ubuntu-diff-repo
+
+diff-mirror: centos-diff-repo ubuntu-diff-repo
+
+#############################
+# CENTOS DIFF MIRROR ARTIFACT
+#############################
+
+define build_diff_centos_repo
+# 1 - new version
+# 2 - old version
+
+# if old version is empty line, then we don't need to build diff repo
+ifneq ($2,)
+DIFF_CENTOS_REPO_ART_NAME:=$(DIFF_CENTOS_REPO_ART_BASE)-$1-$2.tar
+centos-diff-repo: $(ARTS_DIR)/$$(DIFF_CENTOS_REPO_ART_NAME)
+.DELETE_ON_ERROR: $(BUILD_DIR)/mirror/$$(DIFF_CENTOS_REPO_ART_NAME)
+
+DIFF_CENTOS_DEP_FILE:=$(call find-files,$(DEPS_DIR)/$1/$$(DIFF_CENTOS_REPO_ART_NAME))
+ifneq ($$(DIFF_CENTOS_DEP_FILE),)
+$(ARTS_DIR)/$$(DIFF_CENTOS_REPO_ART_NAME): $$(DIFF_CENTOS_DEP_FILE)
+	@mkdir -p $$(@D)
+	cp $$< $$@
+else
+$(ARTS_DIR)/$$(DIFF_CENTOS_REPO_ART_NAME): $(BUILD_DIR)/mirror/$$(DIFF_CENTOS_REPO_ART_NAME)
+	@mkdir -p $$(@D)
+	cp $$< $$@
+endif
+
+CENTOS_DEP_FILE:=$(call find-files,$(DEPS_DIR)/$1/$(CENTOS_REPO_ART_NAME))
+ifneq ($$(CENTOS_DEP_FILE),)
+$(BUILD_DIR)/mirror/$$(DIFF_CENTOS_REPO_ART_NAME): NEWDIR=$(BUILD_DIR)/mirror/$1/centos-repo/Packages
+$(BUILD_DIR)/mirror/$$(DIFF_CENTOS_REPO_ART_NAME): $(BUILD_DIR)/mirror/centos_repo_$1.done
+$(BUILD_DIR)/mirror/centos_repo_$1.done: $$(CENTOS_DEP_FILE)
+	mkdir -p $(BUILD_DIR)/mirror/$1
+	tar xf $$(CENTOS_DEP_FILE) -C $(BUILD_DIR)/mirror/$1
+	@mkdir -p $$(@D)
+	touch $$@
+else
+$(BUILD_DIR)/mirror/$$(DIFF_CENTOS_REPO_ART_NAME): NEWDIR=$(LOCAL_MIRROR_CENTOS_OS_BASEURL)/Packages
+$(BUILD_DIR)/mirror/$$(DIFF_CENTOS_REPO_ART_NAME): $(BUILD_DIR)/mirror/centos_repo_$1.done
+$(BUILD_DIR)/mirror/centos_repo_$1.done: \
+		$(BUILD_DIR)/mirror/build.done \
+		$(BUILD_DIR)/packages/build.done \
+		$(BUILD_DIR)/openstack/build.done
+	@mkdir -p $$(@D)
+	touch $$@
+endif
+$(BUILD_DIR)/mirror/$$(DIFF_CENTOS_REPO_ART_NAME): OLDDIR=$(BUILD_DIR)/mirror/$2/centos-repo/Packages
+$(BUILD_DIR)/mirror/$$(DIFF_CENTOS_REPO_ART_NAME): DIFFDIR=$(DIFF_MIRROR_CENTOS_BASE)-$1-$2/os/$(CENTOS_ARCH)/Packages
+$(BUILD_DIR)/mirror/$$(DIFF_CENTOS_REPO_ART_NAME): | $(LOCAL_MIRROR_CENTOS_OS_BASEURL)/comps.xml
+#	unpacking old version centos mirror
+	mkdir -p $(BUILD_DIR)/mirror/$2
+	tar xf $(DEPS_DIR)/$2/$(CENTOS_REPO_ART_NAME) -C $(BUILD_DIR)/mirror/$2
+#	copying packages which differ from those in old version
+	mkdir -p $$(DIFFDIR)
+	/bin/bash $(SOURCE_DIR)/mirror/create_diff_mirrors.sh $$(NEWDIR) $$(OLDDIR) $$(DIFFDIR)
+#	creating diff mirror
+	cp $(LOCAL_MIRROR_CENTOS_OS_BASEURL)/comps.xml $$(DIFFDIR)/../comps.xml
+	createrepo -g $$(DIFFDIR)/../comps.xml -o $$(DIFFDIR)/../ $$(DIFFDIR)/../
+	rpm -qi -p $$(DIFFDIR)/*.rpm | $(SOURCE_DIR)/iso/pkg-versions.awk > $(DIFF_MIRROR_CENTOS_BASE)-$1-$2/centos-versions.yaml
+	tar cf $$@ -C $(DIFF_MIRROR_CENTOS_BASE)-$1-$2 --xform s:^:centos_updates-$1-$2/: .
+endif # ifneq ($2,)
+endef # build_diff_centos_repo
+
+$(foreach diff,$(UPGRADE_VERSIONS),$(eval $(call build_diff_centos_repo,$(shell echo $(diff) | awk -F':' '{print $$1}'),$(shell echo $(diff) | awk -F':' '{print $$2}'))))
+
+#############################
+# UBUNTU DIFF MIRROR ARTIFACT
+#############################
+
+define build_diff_ubuntu_repo
+# 1 - new version
+# 2 - old version
+
+# if old version is empty line, then we don't need to build diff repo
+ifneq ($2,)
+DIFF_UBUNTU_REPO_ART_NAME:=$(DIFF_UBUNTU_REPO_ART_BASE)-$1-$2.tar
+ubuntu-diff-repo: $(ARTS_DIR)/$$(DIFF_UBUNTU_REPO_ART_NAME)
+.DELETE_ON_ERROR: $(BUILD_DIR)/mirror/$$(DIFF_UBUNTU_REPO_ART_NAME)
+
+DIFF_UBUNTU_DEP_FILE:=$(call find-files,$(DEPS_DIR)/$1/$$(DIFF_UBUNTU_REPO_ART_NAME))
+ifneq ($$(DIFF_UBUNTU_DEP_FILE),)
+$(ARTS_DIR)/$$(DIFF_UBUNTU_REPO_ART_NAME): $$(DIFF_UBUNTU_DEP_FILE)
+	@mkdir -p $$(@D)
+	cp $$< $$@
+else
+$(ARTS_DIR)/$$(DIFF_UBUNTU_REPO_ART_NAME): $(BUILD_DIR)/mirror/$$(DIFF_UBUNTU_REPO_ART_NAME)
+	@mkdir -p $$(@D)
+	cp $$< $$@
+endif
+
+UBUNTU_DEP_FILE:=$(call find-files,$(DEPS_DIR)/$1/$(UBUNTU_REPO_ART_NAME))
+ifneq ($$(UBUNTU_DEP_FILE),)
+$(BUILD_DIR)/mirror/$$(DIFF_UBUNTU_REPO_ART_NAME): NEWDIR=$(BUILD_DIR)/mirror/$1/ubuntu-repo/Packages
+$(BUILD_DIR)/mirror/$$(DIFF_UBUNTU_REPO_ART_NAME): $(BUILD_DIR)/mirror/ubuntu_repo_$1.done
+$(BUILD_DIR)/mirror/ubuntu_repo_$1.done: $$(UBUNTU_DEP_FILE)
+	mkdir -p $(BUILD_DIR)/mirror/$1
+	tar xf $$(UBUNTU_DEP_FILE) -C $(BUILD_DIR)/mirror/$1
+	@mkdir -p $$(@D)
+	touch $$@
+else
+$(BUILD_DIR)/mirror/$$(DIFF_UBUNTU_REPO_ART_NAME): NEWDIR=$(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/Packages
+$(BUILD_DIR)/mirror/$$(DIFF_UBUNTU_REPO_ART_NAME): $(BUILD_DIR)/mirror/ubuntu_repo_$1.done
+$(BUILD_DIR)/mirror/ubuntu_repo_$1.done: \
+		$(BUILD_DIR)/mirror/build.done \
+		$(BUILD_DIR)/packages/build.done \
+		$(BUILD_DIR)/openstack/build.done
+	@mkdir -p $$(@D)
+	touch $$@
+endif
+
+$(BUILD_DIR)/mirror/$$(DIFF_UBUNTU_REPO_ART_NAME): OLDDIR=$(BUILD_DIR)/mirror/$2/ubuntu-repo/pool/main
+$(BUILD_DIR)/mirror/$$(DIFF_UBUNTU_REPO_ART_NAME): DIFFDIR=$(DIFF_MIRROR_UBUNTU_BASE)-$1-$2/pool/main
+$(BUILD_DIR)/mirror/$$(DIFF_UBUNTU_REPO_ART_NAME):
+#	unpacking old version ubuntu mirror
+	mkdir -p $(BUILD_DIR)/mirror/$2
+	tar xf $(DEPS_DIR)/$2/$(UBUNTU_REPO_ART_NAME) -C $(BUILD_DIR)/mirror/$2
+#	copying packages which differ from those in old version
+	mkdir -p $$(DIFFDIR)
+	/bin/bash $(SOURCE_DIR)/mirror/create_diff_mirrors.sh $$(NEWDIR) $$(OLDDIR) $$(DIFFDIR)
+#	creating diff mirror
+	dpkg-scanpackages -m $$(DIFFDIR) > $$(DIFFDIR)/../Packages
+	gzip -9c $$(DIFFDIR)/../Packages > $$(DIFFDIR)/../Packages.gz
+	cat $$(DIFFDIR)/../Packages | $(SOURCE_DIR)/iso/pkg-versions.awk > $(DIFF_MIRROR_UBUNTU_BASE)-$1-$2/ubuntu-versions.yaml
+	tar cf $$@ -C $(DIFF_MIRROR_UBUNTU_BASE)-$1-$2 --xform s:^:ubuntu_updates-$1-$2/: .
+endif # ifneq ($2,)
+endef # build_diff_ubuntu_repo
+
+$(foreach diff,$(UPGRADE_VERSIONS),$(eval $(call build_diff_ubuntu_repo,$(shell echo $(diff) | awk -F':' '{print $$1}'),$(shell echo $(diff) | awk -F':' '{print $$2}'))))
