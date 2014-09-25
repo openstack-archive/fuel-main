0e730ba95092ad713c323bc1c2fd401d76187c1e
Alexei Sheplyakov <asheplyakov@mirantis.com>
2014-09-25 18:49:13 +0400

    mkrepo.sh: bail out if packages are not installable
    ... instead of producing a broken local mirror. Also provide a hint
    what exactly went wrong (which package had unmet dependencies, etc).
    
    Closes-Bug: #1368137
    
    Change-Id: Ifa7e86d706bea025b2deb3f9ace1be2909110e72

--- mirror/ubuntu/files/mkrepo.sh
+++ mirror/ubuntu/files/mkrepo.sh
@@ -22,9 +22,27 @@ linux-image-${UBUNTU_INSTALLER_KERNEL_VERSION}
 linux-headers-${UBUNTU_INSTALLER_KERNEL_VERSION}
 EOF
 
-cat /requirements-deb.txt | while read pkg; do apt-get --print-uris --yes install $pkg | grep ^\' | cut -d\' -f2 >/downloads_$pkg.list; done
+has_apt_errors=''
+rm -f /apt-errors.log
+while read pkg; do
+	downloads_list="/downloads_${pkg}.list"
+	if ! apt-get --print-uris --yes -qq install $pkg >"${downloads_list}" 2>>"/apt-errors.log"; then
+		echo "package $pkg can not be installed" >>/apt-errors.log
+		# run apt-get once more to get a verbose error message
+		apt-get --print-uris --yes install $pkg >>/apt-errors.log 2>&1 || true
+		has_apt_errors='yes'
+	fi
+	sed -i "${downloads_list}" -n -e "s/^'\([^']\+\)['].*$/\1/p"
+done < /requirements-deb.txt
+
+if [ -n "$has_apt_errors" ]; then
+	echo 'some packages are not installable' >&2
+	cat < /apt-errors.log >&2
+	exit 1
+fi
+
 cat /downloads_*.list | sort | uniq > /repo/download/download_urls.list
-rm /downloads_*.list
+rm /downloads_*.list /apt-errors.log
 (cat /repo/download/download_urls.list | xargs -n1 -P4 wget -nv -P /repo/download/) || exit 1
 mv /var/cache/apt/archives/*deb /repo/download/
 # Make structure and mocks for multiarch
