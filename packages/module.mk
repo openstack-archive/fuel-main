24fcdd180193dc30e8dec82289f0bf147f5767d6
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-09-26 15:21:56 +0400

    Improve reliability of preseed download in initrd
    Occassionally Ubuntu fails to download its
    configuration and the installation fails.
    This patch enables preseed scripts to attempt
    10 times to download the configuration
    before failing
    
    blueprint 100-nodes-support
    
    Change-Id: I4dcee04d221c77bbdf82ea280ac836b73654a86e

--- packages/module.mk
+++ packages/module.mk
@@ -1,13 +1,16 @@
 include $(SOURCE_DIR)/packages/rpm/module.mk
+include $(SOURCE_DIR)/packages/deb/module.mk
 
 .PHONY: packages
 
 ifneq ($(BUILD_PACKAGES),0)
 $(BUILD_DIR)/packages/build.done: \
+		$(BUILD_DIR)/packages/deb/build.done \
 		$(BUILD_DIR)/packages/rpm/build.done
 endif
 
-$(BUILD_DIR)/packages/build.done:
+$(BUILD_DIR)/packages/build.done: \
+		$(BUILD_DIR)/packages/deb/build.done
 	$(ACTION.TOUCH)
 
 packages: $(BUILD_DIR)/packages/build.done
