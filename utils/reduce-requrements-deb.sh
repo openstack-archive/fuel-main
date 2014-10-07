229030f4885e7f9a2e6623f1354407cdf1fc727b
Alexei Sheplyakov <asheplyakov@mirantis.com>
2014-10-07 18:25:10 +0400

    requirements-deb.txt: omit redundant packages
    * don't list packages pulled by ubuntu-minimal/ubuntu-standard metapackages
    * don't list the dependencies of build-essential metapackage
    * skip qemu-kvm dependencies
    * skip rabbitmq-server dependencies (erlang runtime)
    * don't list apache2 runtime
    * skip mysql dependencies
    * skip aptitude dependencies
    * don't list cloud-init dependencies
    * skip cinder dependencies
    * don't list nova-* dependencies
    * skip guestmount dependencies
    * don't list neutron-* dependencies
    * omit pacemaker and corosync dependencies
    * skip openstack-dashboard dependencies
    * skip git dependencies
    * skip libgtk2.0 dependencies (not sure why we need libgtk itself)
    
    Also added a script which helps to reduce dependencies.
    
    Note: the obtained requirements-deb.txt still contains redundant and
    extraneous entries, this issue will be handled later on.
    
    Change-Id: Ic0d143a6f6e2eae01b721bae4fd3b6b748558c3b

---
+++ utils/reduce-requrements-deb.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+set -e
+myname="reduce-requirements-deb"
+initial_pkgs="$@"
+
+if [ -z "$initial_pkgs" ]; then
+	echo "$myname: no initial packages specified"
+	exit 0
+fi
+
+cd "$(dirname $0)/.."
+
+cfg_UBUNTU_RELEASE=`sed -n -e '/^UBUNTU_RELEASE/ { s/^UBUNTU_RELEASE:=//p }' config.mk`
+cfg_MIRROR_UBUNTU=`sed -n -e '/^MIRROR_UBUNTU.[=]http/ { s/^MIRROR_UBUNTU.[=]//p }' config.mk`
+PRODUCT_VERSION=`sed -n -e '/^PRODUCT_VERSION/ { s/^PRODUCT_VERSION:=//p }' config.mk`
+MIRROR_FUEL_UBUNTU="http://osci-obs.vm.mirantis.net:82/ubuntu-fuel-${PRODUCT_VERSION}-stable/reprepro"
+
+if [ -z "$MIRROR_UBUNTU" ]; then
+	MIRROR_UBUNTU="$cfg_MIRROR_UBUNTU"
+fi
+if [ -z "$UBUNTU_RELEASE" ]; then
+	UBUNTU_RELEASE="$cfg_UBUNTU_RELEASE"
+fi
+
+rm -rf germinate seeds </dev/null >/dev/null
+mkdir germinate seeds
+
+touch seeds/blacklist
+touch seeds/supported
+cat > seeds/STRUCTURE << EOF
+required:
+supported:
+EOF
+
+for pkg in $initial_pkgs; do
+	echo " * $pkg"
+done > seeds/required
+
+old_pwd="`pwd`"
+cd germinate
+
+germinate -v \
+	-m "$MIRROR_UBUNTU" \
+	-m "$MIRROR_FUEL_UBUNTU" \
+	-d $UBUNTU_RELEASE \
+	-a amd64 \
+	-c main,universe,multiverse \
+	-s seeds \
+	-S "file://${old_pwd}"
+
+cd ..
+
+sed -n -e '/^--------/,/^---------/ { s/^\([a-z][^ \t]\+\).*$/\1/p }' germinate/required.depends > packages-expanded.tmp
+sort < packages-expanded.tmp > packages-expanded.txt
+sort < requirements-deb.txt > requirements-deb-sorted.txt
+comm -23 requirements-deb-sorted.txt packages-expanded.txt > requirements-deb-reduced.txt
