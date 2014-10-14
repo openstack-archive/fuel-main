6c05620900272f2d8aac5da54c073e0ff2d8b136
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-10-14 13:03:12 +0400

    Set permissions on nailgun files installed
    Includes a workaround in nginx start.sh script to
    reset permissions on /usr/share/nailgun/static
    because Docker resets it to 700.
    
    Change-Id: I9172a888cd72ded9ce16897b1e779d994b9802e7
    Closes-Bug: #1380950

--- docker/nginx/start.sh
+++ docker/nginx/start.sh
@@ -5,8 +5,10 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
+#Correct permissions after docker clobbers them on export
 chmod -R 755 /var/www/nailgun
 chmod -R 755 /var/www/nailgun/* 2>/dev/null
+chmod -R 755 /usr/share/nailgun/static 2>/dev/null
 
 puppet apply -v /etc/puppet/modules/nailgun/examples/nginx-only.pp
 nginx -g 'daemon off;'
