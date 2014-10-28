16bac7103958428f7ed72967fce62518ae30a7f4
asledzinskiy <asledzinskiy@mirantis.com>
2014-10-28 17:45:58 +0200

    Run upgrade script with --password option
    - Add --password option to upgrade
    script run
    - Move keystone credentials to settings.py
    
    Change-Id: Iae715e6c6034211d69dd4ea82502c3f3d8e9b14f
    Closes-Bug: #1386165

--- fuelweb_test/settings.py
+++ fuelweb_test/settings.py
@@ -253,3 +253,7 @@ LOCAL_MIRROR_CENTOS = os.environ.get('LOCAL_MIRROR_CENTOS',
                                      '/var/www/nailgun/centos/x86_64')
 
 UPDATE_TIMEOUT = os.environ.get('UPDATE_TIMEOUT', 3600)
+
+DEFAULT_KEYSTONE_CREDS = {'username': 'admin',
+                          'password': 'admin',
+                          'tenant_name': 'admin'}
