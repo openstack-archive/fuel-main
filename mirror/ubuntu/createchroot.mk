a7ec0d95f35b39b8f5d68c3d87fd1d0abee19d2b
Dmitry Borodaenko <dborodaenko@mirantis.com>
2014-11-04 03:38:24 +0000

    Revert "automatically guess the Debian-installer's kernel version"
    Failed to build ISO with this commit:
    http://jenkins-product.srt.mirantis.net:8080/view/6.0/job/6.0.iso/71/console
    
    This reverts commit 41d9e60a6c347021ed8db18a637902ca524d020d.
    
    Change-Id: Iad63c6b36e32abeb7923e205396459acc049c10b

--- mirror/ubuntu/createchroot.mk
+++ mirror/ubuntu/createchroot.mk
@@ -8,11 +8,7 @@ define insert_ubuntu_version
 	$(empty_line)
 endef
 
-di_initrd_img:=$(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/ubuntu-installer/amd64/initrd.gz
-di_kernel_modules_dir=$(shell zcat $(di_initrd_img) | cpio --list 'lib/modules/*/kernel')
-UBUNTU_INSTALLER_KERNEL_VERSION=$(strip $(patsubst lib/modules/%/kernel,%,$(di_kernel_modules_dir)))
-
-$(BUILD_DIR)/mirror/ubuntu/createchroot.done: 
+$(BUILD_DIR)/mirror/ubuntu/createchroot.done:
 	mkdir -p $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot
 	mkdir -p $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/proc
 	cp $(SOURCE_DIR)/mirror/ubuntu/multistrap.conf $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/
