2102c176152be482e04d072f2af0afc48741ec9d
Alexei Sheplyakov <asheplyakov@mirantis.com>
2014-11-06 19:32:41 +0300

    ubuntu: use kernel 3.13 (lts-trusty)
    * packages/deb/debian-boot/01_mount_target_proc.sh: the linux-image
      pre-install script uses /proc/cpuinfo to query CPU features without
      checking if /proc has been mounted. This script mounts proc in /target
      before installing the kernel itself.
    
    * packages/deb/debian-boot/initrd.mk: include the helper script for
      mounting /proc into initramfs.
    
    * config.mk: use lts-trusty (Linux 3.13) kernel and the corresponding
      netboot variant (netboot-trusty) by default.
    
    Change-Id: Id6a2578e36f5f303ffa05ed4d702b119964f62a4
    Related-bug: #1390044
    Related-bug: #1383641

--- packages/deb/debian-boot/initrd.mk
+++ packages/deb/debian-boot/initrd.mk
@@ -1,8 +1,19 @@
 NETBOOT_PATH=$(LOCAL_MIRROR)/ubuntu/installer-amd64/current/images/netboot/ubuntu-installer/amd64
+
+# script which mounts /proc in /target. linux-image* preinst script uses
+# /proc without checking if its mounted, hence this work around:
+hook_script:=$(SOURCE_DIR)/packages/deb/debian-boot/01_mount_target_proc.sh
+# Debian installer runs scripts located in this directory before installing
+# the kernel (and after the base has been installed)
+hook_target_dir:=/usr/lib/post-base-installer.d
+
 $(BUILD_DIR)/packages/deb/debian-boot/initrd.done:\
 		$(BUILD_DIR)/mirror/ubuntu/boot.done
 	mkdir -p $(@D)
 	cd $(@D) && gunzip -c $(NETBOOT_PATH)/initrd.gz | sudo cpio -di
 	cd $(@D) && sudo patch -p1 < $(SOURCE_DIR)/packages/deb/debian-boot/preseed-retry.patch
+	# copy helper script into a directory where the installer expects to find it
+	sudo mkdir -p $(@D)$(hook_target_dir)
+	sudo cp $(hook_script) $(@D)$(hook_target_dir) 
 	cd $(@D) && sudo find . | sudo cpio --create --format='newc' > $(BUILD_DIR)/packages/deb/debian-boot/initrd.gz
 	$(ACTION.TOUCH)
