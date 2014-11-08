726f2264bc94e8c65900f84349d883205093609e
Dmitry Burmistrov <dburmistrov@mirantis.com>
2014-11-08 19:30:00 +0300

    DO NOT MERGE. Dirty hack
    Change-Id: Ice9bd09ce74adc96fbcb677a4d9e3e42fa187e0d

--- packages/module.mk
+++ packages/module.mk
@@ -1,11 +1,10 @@
 include $(SOURCE_DIR)/packages/rpm/module.mk
-include $(SOURCE_DIR)/packages/deb/module.mk
 
 .PHONY: packages
 
 ifneq ($(BUILD_PACKAGES),0)
 $(BUILD_DIR)/packages/build.done: \
-		$(BUILD_DIR)/packages/rpm/build.done $(BUILD_DIR)/packages/deb/build.done  
+		$(BUILD_DIR)/packages/rpm/build.done  
 endif
 
 $(BUILD_DIR)/packages/build.done:
