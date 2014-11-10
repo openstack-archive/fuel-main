c2bf68b071c684e0a1475e800cbb8f4587ba80ba
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2014-11-10 16:50:57 +0300

    Added target OS images into upgrade tarball
    Implements: blueprint image-based-provisioning
    Change-Id: Ib8d7520e4689009da18892166bb774021ae46588

--- iso/module.mk
+++ iso/module.mk
@@ -198,8 +198,8 @@ $(ISOROOT)/bootstrap/bootstrap.rsa: $(SOURCE_DIR)/bootstrap/ssh/id_rsa ;
 ########################
 $(BUILD_DIR)/iso/isoroot-image.done: $(BUILD_DIR)/image/build.done
 	mkdir -p $(ISOROOT)/targetimages
-	tar xf $(BUILD_DIR)/images/$(TARGET_CENTOS_IMG_ART_NAME) -C $(ISOROOT)/targetimages
-	tar xf $(BUILD_DIR)/images/$(TARGET_UBUNTU_IMG_ART_NAME) -C $(ISOROOT)/targetimages
+	tar xf $(ARTS_DIR)/$(TARGET_CENTOS_IMG_ART_NAME) -C $(ISOROOT)/targetimages
+	tar xf $(ARTS_DIR)/$(TARGET_UBUNTU_IMG_ART_NAME) -C $(ISOROOT)/targetimages
 	$(ACTION.TOUCH)
 
 ########################
