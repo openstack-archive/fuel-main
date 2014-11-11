fefb54d6e4325f734245e851613e51570d07cded
Alexei Sheplyakov <asheplyakov@mirantis.com>
2014-11-11 17:23:26 +0300

    packages: make it possible to (re)build only Debian/RPM packages
    - (re)building Debian packages does not create RPMs mirror any more.
    - Building Debian packages can be suppressed by setting the
      BUILD_DEB_PACKAGES variable to zero.
    
    * packages/rpm/module.mk:
      - make sure sources are prepared before building a deb package, i.e. add
        an explicit dependency.
    
    * packages/deb/module.mk:
      - building Debian packages does not require RPMs mirror, adjust
        the dependencies accordingly.
      - don't build packages if BUILD_DEB_PACKAGES is zero.
    
    Related-bug: #1390416
    Change-Id: I049342e7619bd3be80bc2e67c89ad77618b67b59

--- packages/module.mk
+++ packages/module.mk
@@ -1,3 +1,72 @@
+# Usage:
+# (eval (call prepare_file_source,package_name,file_name,source_path))
+# Note: dependencies for deb targets are also specified here to make
+# sure the source is ready before the build is started.
+define prepare_file_source
+$(BUILD_DIR)/packages/source_$1.done: $(BUILD_DIR)/packages/sources/$1/$2
+$(BUILD_DIR)/packages/sources/$1/$2: $(call find-files,$3)
+	mkdir -p $(BUILD_DIR)/packages/sources/$1
+	cp $3 $(BUILD_DIR)/packages/sources/$1/$2
+endef
+
+# Usage:
+# (eval (call prepare_python_source,package_name,file_name,source_path))
+# Note: dependencies for deb targets are also specified here to make
+# sure the source is ready before the build is started.
+define prepare_python_source
+$(BUILD_DIR)/packages/source_$1.done: $(BUILD_DIR)/packages/sources/$1/$2
+$(BUILD_DIR)/packages/sources/$1/$2: $(call find-files,$3)
+	mkdir -p $(BUILD_DIR)/packages/sources/$1
+ifeq ($1,nailgun)
+	cd $3 && npm install && grunt build --static-dir=compressed_static
+	rm -rf $3/static
+	mv $3/compressed_static $3/static
+endif
+	cd $3 && python setup.py sdist -d $(BUILD_DIR)/packages/sources/$1
+endef
+
+# Usage:
+# (eval (call prepare_tgz_source,package_name,file_name,source_path))
+# Note: dependencies for deb targets are also specified here to make
+# sure the source is ready before the build is started.
+define prepare_tgz_source
+$(BUILD_DIR)/packages/source_$1.done: $(BUILD_DIR)/packages/sources/$1/$2
+$(BUILD_DIR)/packages/sources/$1/$2: $(call find-files,$3)
+	mkdir -p $(BUILD_DIR)/packages/sources/$1
+	cd $3 && tar zcf $(BUILD_DIR)/packages/sources/$1/$2 *
+endef
+
+# Usage:
+# (eval (call prepare_ruby21_source,package_name,file_name,source_path))
+# Note: dependencies for deb targets are also specified here to make
+# sure the source is ready before the build is started.
+define prepare_ruby21_source
+$(BUILD_DIR)/packages/source_$1.done: $(BUILD_DIR)/packages/sources/$1/$2
+$(BUILD_DIR)/packages/sources/$1/$2: $(call find-files,$3)
+	mkdir -p $(BUILD_DIR)/packages/sources/$1
+	cd $3 && gem build *.gemspec && cp $2 $(BUILD_DIR)/packages/sources/$1/$2
+endef
+
+$(eval $(call prepare_file_source,fencing-agent,fencing-agent.rb,$(BUILD_DIR)/repos/nailgun/bin/fencing-agent.rb))
+$(eval $(call prepare_file_source,fencing-agent,fencing-agent.cron,$(BUILD_DIR)/repos/nailgun/bin/fencing-agent.cron))
+$(eval $(call prepare_python_source,fuel-agent,fuel-agent-0.1.0.tar.gz,$(BUILD_DIR)/repos/nailgun/fuel_agent))
+$(eval $(call prepare_file_source,fuel-agent,fuel-agent.conf,$(BUILD_DIR)/repos/nailgun/fuel_agent/etc/fuel-agent/fuel-agent.conf.sample))
+$(eval $(call prepare_tgz_source,fuel-agent,fuel-agent-cloud-init-templates.tar.gz,$(BUILD_DIR)/repos/nailgun/fuel_agent/cloud-init-templates))
+$(eval $(call prepare_python_source,fuel-ostf,fuel-ostf-0.1.tar.gz,$(BUILD_DIR)/repos/ostf))
+$(eval $(call prepare_python_source,fuelmenu,fuelmenu-0.1.tar.gz,$(BUILD_DIR)/repos/nailgun/fuelmenu))
+$(eval $(call prepare_file_source,nailgun-agent,agent,$(BUILD_DIR)/repos/nailgun/bin/agent))
+$(eval $(call prepare_file_source,nailgun-agent,nailgun-agent.cron,$(BUILD_DIR)/repos/nailgun/bin/nailgun-agent.cron))
+$(eval $(call prepare_tgz_source,nailgun-mcagents,mcagents.tar.gz,$(BUILD_DIR)/repos/astute/mcagents))
+$(eval $(call prepare_tgz_source,ruby21-nailgun-mcagents,nailgun-mcagents.tar.gz,$(BUILD_DIR)/repos/astute/mcagents))
+$(eval $(call prepare_python_source,nailgun-net-check,nailgun-net-check-0.2.tar.gz,$(BUILD_DIR)/repos/nailgun/network_checker))
+$(eval $(call prepare_python_source,python-tasklib,tasklib-0.1.0.tar.gz,$(BUILD_DIR)/repos/nailgun/tasklib))
+$(eval $(call prepare_python_source,nailgun,nailgun-0.1.0.tar.gz,$(BUILD_DIR)/repos/nailgun/nailgun))
+$(eval $(call prepare_python_source,python-fuelclient,fuelclient-0.2.tar.gz,$(BUILD_DIR)/repos/nailgun/fuelclient))
+$(eval $(call prepare_python_source,shotgun,Shotgun-0.1.0.tar.gz,$(BUILD_DIR)/repos/nailgun/shotgun))
+$(eval $(call prepare_file_source,nailgun-redhat-license,get_redhat_licenses,$(SOURCE_DIR)/packages/rpm/nailgun-redhat-license/get_redhat_licenses))
+$(eval $(call prepare_file_source,ruby21-rubygem-astute,astute.conf,$(SOURCE_DIR)/packages/rpm/astute.conf))
+$(eval $(call prepare_ruby21_source,ruby21-rubygem-astute,astute-0.0.2.gem,$(BUILD_DIR)/repos/astute))
+
 include $(SOURCE_DIR)/packages/rpm/module.mk
 include $(SOURCE_DIR)/packages/deb/module.mk
 
@@ -5,10 +74,14 @@ include $(SOURCE_DIR)/packages/deb/module.mk
 
 ifneq ($(BUILD_PACKAGES),0)
 $(BUILD_DIR)/packages/build.done: \
-		$(BUILD_DIR)/packages/rpm/build.done $(BUILD_DIR)/packages/deb/build.done  
+		$(BUILD_DIR)/packages/rpm/build.done \
+		$(BUILD_DIR)/packages/deb/build.done
 endif
 
 $(BUILD_DIR)/packages/build.done:
 	$(ACTION.TOUCH)
 
 packages: $(BUILD_DIR)/packages/build.done
+
+packages-deb: $(BUILD_DIR)/packages/deb/build.done
+packages-rpm: $(BUILD_DIR)/packages/rpm/build.done
