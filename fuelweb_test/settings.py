49b290c34cda76432333a90f9587944e6c048379
Egor Kotko <ykotko@mirantis.com>
2014-11-11 14:24:54 +0200

    External dns and ntp check
    Shutdown admin interface on master node.
    Check dns resolution from controller.
    Check sync ntp from controller.
    
    Change-Id: I73f3775cb1fbd220fb9d9bdb81f8cdb92e09b599
    Implements: blueprint external-dns-ntp-check

--- fuelweb_test/settings.py
+++ fuelweb_test/settings.py
@@ -259,3 +259,6 @@ IMAGE_PROVISIONING = os.environ.get('IMAGE_PROVISIONING', 'false') == 'true'
 KEYSTONE_CREDS = {'username': os.environ.get('KEYSTONE_USERNAME', 'admin'),
                   'password': os.environ.get('KEYSTONE_PASSWORD', 'admin'),
                   'tenant_name': os.environ.get('KEYSTONE_TENANT', 'admin')}
+
+EXTERNAL_DNS = os.environ.get('EXTERNAL_DNS', '208.67.220.220')
+EXTERNAL_NTP = os.environ.get('EXTERNAL_NTP', 'ua.pool.ntp.org')
