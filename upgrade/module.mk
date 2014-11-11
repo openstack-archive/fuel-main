a13e12cb3d11544a256051453e1569131d37983d
Nikolay Markov <nmarkov@mirantis.com>
2014-11-11 13:00:21 +0300

    Upgrade uses virtualenv
    Related-Bug: #1341564
    
    Change-Id: Ie93034ee209234bf811ceaa622bc599aac3bc3bb

--- upgrade/module.mk
+++ upgrade/module.mk
@@ -52,28 +52,28 @@ $(BUILD_DIR)/upgrade/$(OPENSTACK_YAML_ART_NAME): $(BUILD_DIR)/repos/nailgun.done
 	cp $(BUILD_DIR)/repos/nailgun/nailgun/nailgun/fixtures/openstack.yaml $@
 
 ########################
-# UPGRADE VENV
+# UPGRADE DEPENDENCIES
 ########################
-$(BUILD_DIR)/upgrade/venv.done: \
+$(BUILD_DIR)/upgrade/deps.done: \
 		$(BUILD_DIR)/repos/nailgun.done
-	mkdir -p $(BUILD_DIR)/upgrade/venv
+	mkdir -p $(BUILD_DIR)/upgrade/deps
 	virtualenv $(BUILD_DIR)/upgrade/venv
 #	Requires virtualenv, pip, python-dev packages
-	$(BUILD_DIR)/upgrade/venv/bin/pip install -r $(BUILD_DIR)/repos/nailgun/fuel_upgrade_system/fuel_upgrade/requirements.txt
-	$(BUILD_DIR)/upgrade/venv/bin/pip install $(BUILD_DIR)/repos/nailgun/fuel_upgrade_system/fuel_upgrade
+	$(BUILD_DIR)/upgrade/venv/bin/pip install -r $(BUILD_DIR)/repos/nailgun/fuel_upgrade_system/fuel_upgrade/requirements.txt --download $(BUILD_DIR)/upgrade/deps --no-use-wheel
+	cd $(BUILD_DIR)/repos/nailgun/fuel_upgrade_system/fuel_upgrade && $(BUILD_DIR)/upgrade/venv/bin/python setup.py sdist --dist-dir $(BUILD_DIR)/upgrade/deps
+	rm -rf ./fuel_upgrade.egg-info
 
 ########################
 # COMMON PART
 ########################
 $(BUILD_DIR)/upgrade/common-part.tar: \
 		$(BUILD_DIR)/repos/fuellib.done \
-		$(BUILD_DIR)/upgrade/venv.done
+		$(BUILD_DIR)/upgrade/deps.done
 	mkdir -p $(@D)
 	rm -f $@
 	tar cf $@ -C $(BUILD_DIR)/repos/fuellib/deployment --xform s:^puppet:upgrade/puppet/modules: puppet
 	tar rf $@ -C $(BUILD_DIR)/repos/fuellib/deployment/puppet/osnailyfacter/examples --xform s:^:upgrade/puppet/manifests/: site.pp
-	tar rf $@ -C $(BUILD_DIR)/upgrade/venv/lib/python* --xform s:^:upgrade/: site-packages
-	tar rf $@ -C $(BUILD_DIR)/upgrade/venv --xform s:^:upgrade/: bin/fuel-upgrade
+	tar rf $@ -C $(BUILD_DIR)/upgrade --xform s:^:upgrade/: deps
 	sed 's/{{UPGRADERS}}/${UPGRADERS}/g' $(SOURCE_DIR)/upgrade/upgrade_template.sh > $(BUILD_DIR)/upgrade/upgrade.sh
 	tar rf $@ --mode=755 -C $(BUILD_DIR)/upgrade upgrade.sh
 
