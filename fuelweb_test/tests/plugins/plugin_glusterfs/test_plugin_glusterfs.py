01fb624f366c831fefe5a4774a6b4faa685819c2
Tatyana Leontovich <tleontovich@mirantis.com>
2014-11-11 21:35:56 +0000

    WIP: Add base tests for lbaas plugin
    Add test for lbaas plugin:
    I.* deploy master node
      * upload plugin on it using path expoerted in PLUGIN_PATH var
      * install plugin
      * enable it
      * deploy cluster with  neutron
      * check that lbaas agent is running
      * create pool/vip
      * assert that vip in Active state
      * run OSTF
    
    II. * Deploy neutron with lbaas
        * Reset cluster and add node
        * redeploy and check lbaas
    
    Add template for plugin_example
    * deploy neutron simple with example plugin
    * deploy ha nova with example plugin
    * deploy ha neutron with example plugin
      add nodes, re-deploy cluster
      verify that plugin works on added nodes
    
    and glusterfs plugin
    
    Change-Id: Ib19864d777f0439cd2ba172a8a9bd14abe09a201

---
+++ fuelweb_test/tests/plugins/plugin_glusterfs/test_plugin_glusterfs.py
@@ -0,0 +1,106 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import os
+
+from proboscis.asserts import assert_equal
+from proboscis import test
+from fuelweb_test import logger
+
+from fuelweb_test.helpers.decorators import log_snapshot_on_error
+from fuelweb_test.helpers import checkers
+from fuelweb_test.settings import DEPLOYMENT_MODE_SIMPLE
+from fuelweb_test.settings import GLUSTER_CLUSTER_ENDPOINT
+from fuelweb_test.settings import PLUGIN_PATH
+from fuelweb_test.tests.base_test_case import SetupEnvironment
+from fuelweb_test.tests.base_test_case import TestBasic
+
+
+@test(groups=["plugins"])
+class GlusterfsPlugin(TestBasic):
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_3],
+          groups=["deploy_neutron_glusterfs_simple"])
+    @log_snapshot_on_error
+    def deploy_neutron_glusterfs_simple(self):
+        """Deploy cluster in simple mode with glusterfs plugin
+
+        Scenario:
+            1. Upload plugin to the master node
+            2. Install plugin
+            3. Create cluster
+            4. Add 1 node with controller role
+            5. Add 1 nodes with compute role
+            6. Add 1 nodes with cinder role
+            7. Deploy the cluster
+            8. Run network verification
+            9. Check plugin health
+            10. Run OSTF
+
+        Snapshot deploy_neutron_example_simple
+
+        """
+        self.env.revert_snapshot("ready_with_3_slaves")
+
+        # copy plugin to the master node
+
+        checkers.upload_tarball(
+            self.env.get_admin_remote(), PLUGIN_PATH, '/var')
+
+        # install plugin
+
+        checkers.install_plugin_check_code(
+            self.env.get_admin_remote(),
+            plugin=os.path.basename(PLUGIN_PATH))
+
+        segment_type = 'vlan'
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE_SIMPLE,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": segment_type,
+            }
+        )
+
+        attr = self.fuel_web.client.get_cluster_attributes(cluster_id)
+        if 'external_glusterfs' in attr['editable']:
+            plugin_enabled = attr['editable']['external_glusterfs']['metadata']
+            plugin_enabled['enabled'] = True
+            plugin_data = attr['editable']['external_glusterfs']['endpoint']
+            plugin_data['value'] = GLUSTER_CLUSTER_ENDPOINT
+
+        self.fuel_web.client.update_cluster_attributes(cluster_id, attr)
+
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['compute'],
+                'slave-03': ['cinder']
+            }
+        )
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+        # TODO tleontovich: Check that conf is updated,
+        # packages installed and if possible storage placed in glusterfs
+
+        cluster = self.fuel_web.client.get_cluster(cluster_id)
+        assert_equal(str(cluster['net_provider']), 'neutron')
+
+        self.fuel_web.verify_network(cluster_id)
+
+        # add verification here
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id)
+
+        self.env.make_snapshot("deploy_neutron_glusterfs_simple", is_make=True)
