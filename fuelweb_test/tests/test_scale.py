74872302fb793bb96e78b1da5e2c9e82e5b07afb
Dmitry Tyzhnenko <dtyzhnenko@mirantis.com>
2014-11-12 12:57:42 +0200

    Add systest for check readiness env
    Scenario:
      1. Create cluster
      2. Add 3 nodes with controller and ceph role
      3. Add 5 nodes with compute and ceph roles
      4. Add 1 nodes with mongo roles
      5. Deploy the cluster
      6. Verify smiles count
      7. Verify ceilometer api is running
    
    Change-Id: I1d1ac09bd847a4578359b6e8ec895653def595fb
    Closes-bug: #1391812

--- fuelweb_test/tests/test_scale.py
+++ fuelweb_test/tests/test_scale.py
@@ -1,4 +1,4 @@
-#    Copyright 2014 Mirantis, Inc.
+#    Copyright 2013 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -14,153 +14,30 @@
 
 from proboscis.asserts import assert_equal
 from proboscis import test
-from proboscis import SkipTest
 
 from fuelweb_test import logger as LOGGER
 from fuelweb_test.helpers import checkers
 from fuelweb_test.helpers.decorators import log_snapshot_on_error
-from fuelweb_test import settings
-from fuelweb_test.tests import base_test_case
 from fuelweb_test.helpers import os_actions
 from fuelweb_test.settings import DEPLOYMENT_MODE_HA
 from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
 
-@test(groups=["huge_environments"])
-class HugeEnvironments(base_test_case.TestBasic):
-    @test(depends_on=[base_test_case.SetupEnvironment.prepare_release],
-          groups=["nine_nodes_mixed"])
+
+@test(groups=["scale_ha", "scale_ha_neutron_gre"])
+class ScaleNeutronGreHaCephCeilo(TestBasic):
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_10],
+          groups=["deploy_scale_neutron_gre_ha_ceph_ceilo",
+                  "scale_ha_neutron_gre"])
     @log_snapshot_on_error
-    def nine_nodes_mixed(self):
-        """Deploy cluster with mixed roles on 9 nodes in HA mode
-
-        Scenario:
-            1. Create cluster
-            2. Add 4 nodes as controllers with ceph OSD roles
-            3. Add 5 nodes as compute with ceph OSD and mongo roles
-            4. Turn on Sahara and Ceilometer
-            5. Deploy the cluster
-            6. Check networks and OSTF
-
-        Snapshot None
-
-        """
-        if settings.OPENSTACK_RELEASE == settings.OPENSTACK_RELEASE_REDHAT:
-            raise SkipTest()
-
-        self.env.revert_snapshot("ready")
-        self.env.bootstrap_nodes(self.env.nodes().slaves[:9])
-
-        cluster_id = self.fuel_web.create_cluster(
-            name=self.__class__.__name__,
-            mode=settings.DEPLOYMENT_MODE_HA,
-            settings={
-                'volumes_ceph': True,
-                'images_ceph': True,
-                'volumes_lvm': False,
-                'sahara': True,
-                'ceilometer': True
-            }
-        )
-        self.fuel_web.update_nodes(
-            cluster_id,
-            {
-                'slave-01': ['controller', 'mongo'],
-                'slave-02': ['controller', 'mongo'],
-                'slave-03': ['controller', 'mongo'],
-                'slave-04': ['controller', 'mongo'],
-                'slave-05': ['compute', 'ceph-osd'],
-                'slave-06': ['compute', 'ceph-osd'],
-                'slave-07': ['compute', 'ceph-osd'],
-                'slave-08': ['compute', 'ceph-osd'],
-                'slave-09': ['compute', 'ceph-osd']
-            }
-        )
-        # Cluster deploy
-        self.fuel_web.deploy_cluster_wait(cluster_id,
-                                          timeout=120 * 60,
-                                          interval=30)
-
-        self.fuel_web.run_ostf(
-            cluster_id=cluster_id)
-
-    @test(depends_on=[base_test_case.SetupEnvironment.prepare_release],
-          groups=["nine_nodes_separate_roles"])
-    @log_snapshot_on_error
-    def nine_nodes_separate_roles(self):
-        """Deploy cluster with separate roles on 9 nodes in HA mode with GRE
-
-        Scenario:
-            1. Create cluster
-            2. Add 3 nodes as controllers
-            3. Add 2 nodes as compute
-            4. Add 1 node as cinder and 1 as mongo
-            5. Add 2 nodes as ceph
-            6. Turn on Sahara and Ceilometer
-            7. Deploy the cluster
-            8. Check networks and OSTF
-
-        Snapshot None
-
-        """
-        if settings.OPENSTACK_RELEASE == settings.OPENSTACK_RELEASE_REDHAT:
-            raise SkipTest()
-
-        self.env.revert_snapshot("ready")
-        self.env.bootstrap_nodes(self.env.nodes().slaves[:9])
-
-        cluster_id = self.fuel_web.create_cluster(
-            name=self.__class__.__name__,
-            mode=settings.DEPLOYMENT_MODE_HA,
-            settings={
-                'volumes_ceph': True,
-                'images_ceph': False,
-                'volumes_lvm': False,
-                'sahara': True,
-                'ceilometer': True,
-                'net_provider': 'neutron',
-                'net_segment_type': 'gre'
-            }
-        )
-        self.fuel_web.update_nodes(
-            cluster_id,
-            {
-                'slave-01': ['controller'],
-                'slave-02': ['controller'],
-                'slave-03': ['controller'],
-                'slave-04': ['compute'],
-                'slave-05': ['compute'],
-                'slave-06': ['cinder'],
-                'slave-07': ['mongo'],
-                'slave-08': ['ceph-osd'],
-                'slave-09': ['ceph-osd'],
-            }
-        )
-        # Cluster deploy
-        self.fuel_web.deploy_cluster_wait(cluster_id,
-                                          timeout=120 * 60,
-                                          interval=30)
-
-        self.fuel_web.run_ostf(
-            cluster_id=cluster_id,
-            test_sets=['ha', 'smoke', 'sanity'],
-            should_fail=1)
-
-
-@test(groups=["huge_environments", "huge_ha_neutron", "huge_scale"])
-class HugeHaNeutron(TestBasic):
-
-    @test(depends_on=[SetupEnvironment.prepare_slaves_9],
-          groups=["huge_ha_neutron_ceph_ceilo",
-                  "huge_ha_neutron_gre_ceph_ceilo"])
-    @log_snapshot_on_error
-    def deploy_neutron_gre_ha_ceph_ceilo(self):
+    def deploy_neutron_gre_ha(self):
         """Deploy cluster in HA mode with Neutron GRE
 
         Scenario:
             1. Create cluster
             2. Add 3 nodes with controller and ceph role
-            3. Add 5 nodes with compute and ceph roles
+            3. Add 6 nodes with compute and ceph roles
             4. Add 1 nodes with compute and mongo roles
             5. Deploy the cluster
             6. Verify smiles count
@@ -169,13 +46,10 @@ class HugeHaNeutron(TestBasic):
         Snapshot deploy_scale_neutron_gre_ha_ceph_ceilo
 
         """
-        self.env.revert_snapshot("ready_with_9_slaves")
+        self.env.revert_snapshot("ready_with_10_slaves")
 
         data = {
             'ceilometer': True,
-            'volumes_ceph': True,
-            'images_ceph': True,
-            'volumes_lvm': False,
             'net_provider': 'neutron',
             'net_segment_type': 'gre',
             'tenant': 'haGreCephScale',
@@ -199,7 +73,8 @@ class HugeHaNeutron(TestBasic):
                 'slave-06': ['compute', 'ceph-osd'],
                 'slave-07': ['compute', 'ceph-osd'],
                 'slave-08': ['compute', 'ceph-osd'],
-                'slave-09': ['mongo'],
+                'slave-09': ['compute', 'ceph-osd'],
+                'slave-10': ['mongo'],
             }
         )
         nailgun_nodes = self.fuel_web.client.list_cluster_nodes(cluster_id)
@@ -241,47 +116,48 @@ class HugeHaNeutron(TestBasic):
                 data['password'],
                 data['tenant'])
             self.fuel_web.assert_cluster_ready(
-                os_conn, smiles_count=17, networks_count=2, timeout=300)
+                os_conn, smiles_count=18, networks_count=2, timeout=300)
 
         partitions = checkers.get_mongo_partitions(
-            self.env.get_ssh_to_remote_by_name("slave-09"), "vda5")
+            self.env.get_ssh_to_remote_by_name("slave-10"), "vda5")
         assert_equal(partitions[0].rstrip(), mongo_disk_gb,
                      'Mongo size {0} before deployment is not equal'
                      ' to size after {1}'.format(mongo_disk_gb, partitions))
 
         self.env.make_snapshot("deploy_scale_neutron_gre_ha_ceph_ceilo")
 
-    @test(depends_on=[SetupEnvironment.prepare_slaves_9],
-          groups=["huge_ha_neutron_ceph_ceilo",
-                  "huge_ha_neutron_vlan_ceph_ceilo"])
+
+@test(groups=["scale_ha", "scale_ha_neutron_vlan"])
+class ScaleNeutronVlanHaCephCeilo(TestBasic):
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_10],
+          groups=["deploy_scale_neutron_vlan_ha_ceph_ceilo",
+                  "scale_ha_neutron_vlan"])
     @log_snapshot_on_error
-    def deploy_neutron_vlan_ha_ceph_ceilo(self):
+    def deploy_neutron_gre_ha(self):
         """Deploy cluster in HA mode with Neutron VLAN
 
         Scenario:
             1. Create cluster
             2. Add 3 nodes with controller and ceph role
-            3. Add 5 nodes with compute and ceph roles
+            3. Add 6 nodes with compute and ceph roles
             4. Add 1 nodes with mongo roles
             5. Deploy the cluster
             6. Verify smiles count
             7. Verify ceilometer api is running
 
-        Snapshot None
+        Snapshot deploy_scale_neutron_gre_ha_ceph_ceilo
 
         """
-        self.env.revert_snapshot("ready_with_9_slaves")
+        self.env.revert_snapshot("ready_with_10_slaves")
 
         data = {
             'ceilometer': True,
-            'volumes_ceph': True,
-            'images_ceph': True,
-            'volumes_lvm': False,
             'net_provider': 'neutron',
             'net_segment_type': 'vlan',
-            'tenant': 'haGreCephHuge',
-            'user': 'haGreCephHuge',
-            'password': 'haGreCephHuge'
+            'tenant': 'haGreCephScale',
+            'user': 'haGreCephScale',
+            'password': 'haGreCephScale'
         }
         cluster_id = self.fuel_web.create_cluster(
             name=self.__class__.__name__,
@@ -299,7 +175,8 @@ class HugeHaNeutron(TestBasic):
                 'slave-06': ['compute', 'ceph-osd'],
                 'slave-07': ['compute', 'ceph-osd'],
                 'slave-08': ['compute', 'ceph-osd'],
-                'slave-09': ['mongo'],
+                'slave-09': ['compute', 'ceph-osd'],
+                'slave-10': ['mongo'],
             }
         )
         nailgun_nodes = self.fuel_web.client.list_cluster_nodes(cluster_id)
@@ -341,10 +218,12 @@ class HugeHaNeutron(TestBasic):
                 data['password'],
                 data['tenant'])
             self.fuel_web.assert_cluster_ready(
-                os_conn, smiles_count=17, networks_count=2, timeout=300)
+                os_conn, smiles_count=18, networks_count=2, timeout=300)
 
         partitions = checkers.get_mongo_partitions(
-            self.env.get_ssh_to_remote_by_name("slave-09"), "vda5")
+            self.env.get_ssh_to_remote_by_name("slave-10"), "vda5")
         assert_equal(partitions[0].rstrip(), mongo_disk_gb,
                      'Mongo size {0} before deployment is not equal'
                      ' to size after {1}'.format(mongo_disk_gb, partitions))
+
+        self.env.make_snapshot("deploy_scale_neutron_vlan_ha_ceph_ceilo")
