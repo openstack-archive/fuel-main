394589d3b21b1c9772c1d4654844380767ba7b93
Dmitry Tyzhnenko <dtyzhnenko@mirantis.com>
2014-11-12 12:57:42 +0200

    Add systest for check readiness env
    Scenario:
      1. Create cluster
      2. Add 3 nodes with controller and ceph role
      3. Add 3 nodes with compute and ceph roles
      4. Add 3 nodes with mongo roles
      5. Deploy the cluster
      6. Verify smiles count
      7. Run OSTF
    
    Change-Id: I1d1ac09bd847a4578359b6e8ec895653def595fb
    Closes-bug: #1391812

--- fuelweb_test/tests/tests_strength/test_huge_environments.py
+++ fuelweb_test/tests/tests_strength/test_huge_environments.py
@@ -18,11 +18,12 @@ from proboscis import test
 from fuelweb_test.helpers.decorators import log_snapshot_on_error
 from fuelweb_test import settings
 from fuelweb_test.tests import base_test_case
+from fuelweb_test.helpers import os_actions
 
 
 @test(groups=["huge_environments"])
 class HugeEnvironments(base_test_case.TestBasic):
-    @test(depends_on=[base_test_case.SetupEnvironment.prepare_release],
+    @test(depends_on=[base_test_case.SetupEnvironment.prepare_slaves(9)],
           groups=["nine_nodes_mixed"])
     @log_snapshot_on_error
     def nine_nodes_mixed(self):
@@ -42,8 +43,7 @@ class HugeEnvironments(base_test_case.TestBasic):
         if settings.OPENSTACK_RELEASE == settings.OPENSTACK_RELEASE_REDHAT:
             raise SkipTest()
 
-        self.env.revert_snapshot("ready")
-        self.env.bootstrap_nodes(self.env.nodes().slaves[:9])
+        self.env.revert_snapshot("ready_with_9_slaves")
 
         cluster_id = self.fuel_web.create_cluster(
             name=self.__class__.__name__,
@@ -78,7 +78,7 @@ class HugeEnvironments(base_test_case.TestBasic):
         self.fuel_web.run_ostf(
             cluster_id=cluster_id)
 
-    @test(depends_on=[base_test_case.SetupEnvironment.prepare_release],
+    @test(depends_on=[base_test_case.SetupEnvironment.prepare_slaves(9)],
           groups=["nine_nodes_separate_roles"])
     @log_snapshot_on_error
     def nine_nodes_separate_roles(self):
@@ -100,8 +100,7 @@ class HugeEnvironments(base_test_case.TestBasic):
         if settings.OPENSTACK_RELEASE == settings.OPENSTACK_RELEASE_REDHAT:
             raise SkipTest()
 
-        self.env.revert_snapshot("ready")
-        self.env.bootstrap_nodes(self.env.nodes().slaves[:9])
+        self.env.revert_snapshot("ready_with_9_slaves")
 
         cluster_id = self.fuel_web.create_cluster(
             name=self.__class__.__name__,
@@ -139,3 +138,158 @@ class HugeEnvironments(base_test_case.TestBasic):
             cluster_id=cluster_id,
             test_sets=['ha', 'smoke', 'sanity'],
             should_fail=1)
+
+
+@test(groups=["huge_environments", "huge_ha_neutron", "huge_scale"])
+class HugeHaNeutron(base_test_case.TestBasic):
+
+    @test(depends_on=[base_test_case.SetupEnvironment.prepare_slaves(9)],
+          groups=["huge_ha_neutron_gre_ceph_ceilometer_rados"])
+    @log_snapshot_on_error
+    def huge_ha_neutron_gre_ceph_ceilometer_rados(self):
+        """Deploy cluster in HA mode with Neutron GRE, RadosGW
+
+        Scenario:
+            1. Create cluster
+            2. Add 3 nodes with controller and ceph role
+            3. Add 3 nodes with compute and ceph roles
+            4. Add 3 nodes with compute and mongo roles
+            5. Deploy the cluster
+            6. Verify smiles count
+            7. Run OSTF
+
+        Snapshot None
+
+        """
+        self.env.revert_snapshot("ready_with_9_slaves")
+
+        data = {
+            'volumes_lvm': False,
+            'volumes_ceph': True,
+            'images_ceph': True,
+            'objects_ceph': True,
+            'net_provider': 'neutron',
+            'net_segment_type': 'gre',
+            'tenant': 'haGreCephHugeScale',
+            'user': 'haGreCephHugeScale',
+            'password': 'haGreCephHugeScale'
+        }
+
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=settings.DEPLOYMENT_MODE_HA,
+            settings=data
+        )
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller', 'ceph-osd'],
+                'slave-02': ['controller', 'ceph-osd'],
+                'slave-03': ['controller', 'ceph-osd'],
+                'slave-04': ['compute', 'ceph-osd'],
+                'slave-05': ['compute', 'ceph-osd'],
+                'slave-06': ['compute', 'ceph-osd'],
+                'slave-07': ['mongo'],
+                'slave-08': ['mongo'],
+                'slave-09': ['mongo']
+            }
+        )
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        os_conn = os_actions.OpenStackActions(
+            self.fuel_web.get_public_vip(cluster_id),
+            data['user'],
+            data['password'],
+            data['tenant'])
+        self.fuel_web.assert_cluster_ready(
+            os_conn, smiles_count=15, networks_count=2, timeout=300)
+
+        self.fuel_web.run_ostf(cluster_id=cluster_id,
+                               test_sets=['ha', 'smoke', 'sanity'])
+
+        test_class_main = ('fuel_health.tests.platform_tests.'
+                           'test_ceilometer.'
+                           'CeilometerApiPlatformTests')
+        tests_names = ['test_check_alarm_state',
+                       'test_create_sample']
+        test_classes = ['{0}.{1}'.format(test_class_main, test_name)
+                        for test_name in tests_names]
+        for test_name in test_classes:
+            self.fuel_web.run_single_ostf_test(
+                cluster_id=cluster_id, test_sets=['platform_tests'],
+                test_name=test_name, timeout=60 * 20)
+
+    @test(depends_on=[base_test_case.SetupEnvironment.prepare_slaves(9)],
+          groups=["huge_ha_neutron_vlan_ceph_ceilometer_rados"])
+    @log_snapshot_on_error
+    def huge_ha_neutron_vlan_ceph_ceilometer_rados(self):
+        """Deploy cluster in HA mode with Neutron VLAN, RadosGW
+
+        Scenario:
+            1. Create cluster
+            2. Add 3 nodes with controller and ceph role
+            3. Add 3 nodes with compute and ceph roles
+            4. Add 3 nodes with mongo roles
+            5. Deploy the cluster
+            6. Verify smiles count
+            7. Run OSTF
+
+        Snapshot None
+
+        """
+        self.env.revert_snapshot("ready_with_9_slaves")
+
+        data = {
+            'ceilometer': True,
+            'volumes_ceph': True,
+            'images_ceph': True,
+            'volumes_lvm': False,
+            'net_provider': 'neutron',
+            'net_segment_type': 'vlan',
+            'tenant': 'haVlanCephHugeScale',
+            'user': 'haVlanCephHugeScale',
+            'password': 'haVlanCephHugeScale'
+        }
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=settings.DEPLOYMENT_MODE_HA,
+            settings=data
+        )
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller', 'ceph-osd'],
+                'slave-02': ['controller', 'ceph-osd'],
+                'slave-03': ['controller', 'ceph-osd'],
+                'slave-04': ['compute', 'ceph-osd'],
+                'slave-05': ['compute', 'ceph-osd'],
+                'slave-06': ['compute', 'ceph-osd'],
+                'slave-07': ['mongo'],
+                'slave-08': ['mongo'],
+                'slave-09': ['mongo']
+            }
+        )
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        os_conn = os_actions.OpenStackActions(
+            self.fuel_web.get_public_vip(cluster_id),
+            data['user'],
+            data['password'],
+            data['tenant'])
+        self.fuel_web.assert_cluster_ready(
+            os_conn, smiles_count=15, networks_count=2, timeout=300)
+
+        self.fuel_web.run_ostf(cluster_id=cluster_id,
+                               test_sets=['ha', 'smoke', 'sanity'])
+
+        test_class_main = ('fuel_health.tests.platform_tests.'
+                           'test_ceilometer.'
+                           'CeilometerApiPlatformTests')
+        tests_names = ['test_check_alarm_state',
+                       'test_create_sample']
+        test_classes = ['{0}.{1}'.format(test_class_main, test_name)
+                        for test_name in tests_names]
+        for test_name in test_classes:
+            self.fuel_web.run_single_ostf_test(
+                cluster_id=cluster_id, test_sets=['platform_tests'],
+                test_name=test_name, timeout=60 * 20)
