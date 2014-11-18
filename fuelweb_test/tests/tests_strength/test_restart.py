8bb9b413520bef9a9ed65e8a5960003312ddea6c
asledzinskiy <asledzinskiy@mirantis.com>
2014-11-18 17:00:51 +0200

    Add test on repeatable provision after cluster deletion
    - Add test that deploy cluster, delete it, create new cluster,
    snapshot env and then 10 times tries to provision new cluster
    
    Implement blueprint image-provisioning-in-system-tests
    
    Change-Id: Ibcdd480c1dc3d9cd35521813e937306670ea913b

--- fuelweb_test/tests/tests_strength/test_restart.py
+++ fuelweb_test/tests/tests_strength/test_restart.py
@@ -17,6 +17,7 @@ from fuelweb_test.helpers.decorators import log_snapshot_on_error
 from fuelweb_test import logger
 from fuelweb_test import ostf_test_mapping as map_ostf
 from fuelweb_test import settings
+from fuelweb_test.settings import DEPLOYMENT_MODE_HA
 from fuelweb_test.settings import DEPLOYMENT_MODE_SIMPLE
 from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
@@ -226,3 +227,78 @@ class SimpleFlatRestart(TestBasic):
 
         self.fuel_web.run_ostf(
             cluster_id=cluster_id)
+
+
+@test(groups=["repeatable_image_based"])
+class RepeatableImageBased(TestBasic):
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_5],
+          groups=["repeatable_image_based"])
+    @log_snapshot_on_error
+    def repeatable_image_based(self):
+        """Deploy cluster after deletion many times
+
+        Scenario:
+            1. Create HA cluster
+            2. Add 3 controllers and 2 computes
+            3. Deploy the cluster
+            4. Delete cluster
+            5. Create another HA cluster
+            6. Create snapshot of environment
+            7. Revert snapshot and try provision cluster 10 times
+
+        """
+        segment_type = 'gre'
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE_HA,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": segment_type,
+                'tenant': 'haGre',
+                'user': 'haGre',
+                'password': 'haGre'
+            }
+        )
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['controller'],
+                'slave-03': ['controller'],
+                'slave-04': ['compute'],
+                'slave-05': ['compute']
+            }
+        )
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+        self.fuel_web.client.delete_cluster(cluster_id)
+        for node in self.fuel_web.client.list_nodes():
+            wait(lambda: self.fuel_web.is_node_discovered(node),
+                 timeout=10 * 60, interval=15)
+
+        segment_type = 'vlan'
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE_HA,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": segment_type
+            }
+        )
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['controller'],
+                'slave-03': ['controller'],
+                'slave-04': ['compute'],
+                'slave-05': ['compute']
+            }
+        )
+
+        self.env.make_snapshot("deploy_after_delete", is_make=True)
+
+        for i in range(0, 10):
+            self.env.revert_snapshot("deploy_after_delete")
+            cluster_id = self.fuel_web.get_last_created_cluster()
+            self.fuel_web.provisioning_cluster_wait(cluster_id)
