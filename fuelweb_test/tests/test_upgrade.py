6b0acba8be5fbec12dd4e2d15eba48baf5ffacef
asledzinskiy <asledzinskiy@mirantis.com>
2014-11-18 16:37:18 +0200

    Add wait until nailgun will be operable to rollback tests
    Change-Id: I34ae6388fe023cad929c1c039d4f7643658548be
    Closes-Bug: #1392393

--- fuelweb_test/tests/test_upgrade.py
+++ fuelweb_test/tests/test_upgrade.py
@@ -17,6 +17,7 @@ import os
 from proboscis.asserts import assert_equal
 from proboscis import test
 from proboscis import SkipTest
+from devops.helpers.helpers import _wait
 
 from fuelweb_test.helpers import checkers
 from fuelweb_test.helpers.decorators import log_snapshot_on_error
@@ -283,6 +284,8 @@ class RollbackFuelMaster(base_test_data.TestBasic):
         checkers.check_upgraded_containers(self.env.get_admin_remote(),
                                            hlp_data.UPGRADE_FUEL_TO,
                                            hlp_data.UPGRADE_FUEL_FROM)
+        _wait(lambda: self.fuel_web.get_nailgun_node_by_devops_node(
+            self.env.nodes().slaves[0]), timeout=120)
         self.fuel_web.wait_nodes_get_online_state(self.env.nodes().slaves[:5])
         self.fuel_web.assert_nodes_in_ready_state(cluster_id)
         self.fuel_web.assert_fuel_version(hlp_data.UPGRADE_FUEL_FROM)
@@ -344,6 +347,8 @@ class RollbackFuelMaster(base_test_data.TestBasic):
         checkers.check_upgraded_containers(self.env.get_admin_remote(),
                                            hlp_data.UPGRADE_FUEL_TO,
                                            hlp_data.UPGRADE_FUEL_FROM)
+        _wait(lambda: self.fuel_web.get_nailgun_node_by_devops_node(
+            self.env.nodes().slaves[0]), timeout=120)
         self.fuel_web.wait_nodes_get_online_state(self.env.nodes().slaves[:3])
         self.fuel_web.assert_nodes_in_ready_state(cluster_id)
         self.fuel_web.assert_fuel_version(hlp_data.UPGRADE_FUEL_FROM)
