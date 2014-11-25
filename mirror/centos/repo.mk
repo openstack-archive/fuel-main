5b9f476054551500fb9725a6438f4833f76d222b
Igor Gnatenko <ignatenko@mirantis.com>
2014-11-25 19:53:36 +0300

    Evaluate yum conflicts without duplicates
    Enables Fedora to download packages more reliably.
    
    Change-Id: I01ed8ce4863c8ed0fa75e7a6795ec7096ba14793
    Partial-Bug: #1396618
    blueprint build-iso-on-fedora
    Signed-off-by: Igor Gnatenko <ignatenko@mirantis.com>

--- mirror/centos/repo.mk
+++ mirror/centos/repo.mk
@@ -60,13 +60,17 @@ $(BUILD_DIR)/mirror/centos/yum.done: \
 	rm -rf $(LOCAL_MIRROR_CENTOS_OS_BASEURL)/Packages/*.i686.rpm
 	# Yumdownloader workaround number two:
 	# yumdownloader should fail if some packages are missed
-	test `grep "No Match" $(BUILD_DIR)/mirror/centos/yumdownloader.log | wc -l` = 0
+	pushd $(BUILD_DIR)/mirror/centos/
+	test `grep "No Match" yumdownloader.log | wc -l` = 0
 	# Yumdownloader workaround number three:
 	# We have exactly four downloading conflicts: django, mysql, kernel-headers and kernel-lt-firmware
-	test `grep "conflicts with" $(BUILD_DIR)/mirror/centos/yumdownloader.log | grep -v '^[[:space:]]' | wc -l` -le 9
+	sed -ne "/conflicts with/ {p}" < yumdownloader.log > conflicts.list.tmp
+	sort -u < conflicts.list.tmp > conflicts.list
+	test `wc -l conflicts.list` -le 9
 	# Yumdownloader workaround number four:
 	# yumdownloader should fail if some errors appears
-	test `grep "Errno" $(BUILD_DIR)/mirror/centos/yumdownloader.log | wc -l` = 0
+	test `grep "Errno" yumdownloader.log | wc -l` = 0
+	popd
 	$(ACTION.TOUCH)
 
 show-yum-urls-centos: \
