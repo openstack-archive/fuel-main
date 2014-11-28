758c8591d489e30d9ed59645b89cd68c8912c131
Igor Gnatenko <ignatenko@mirantis.com>
2014-11-28 19:07:35 +0300

    Use aptly instead of custom scripts for building repos
    Currently, we are building ubuntu mirror by 'multistrap', then
    mkrepo.sh which copying .deb from /var/cache/, downloading
    other .deb and .udeb and creating local mirror.
    
    First point is multistrap is not available on RHEL, SUSE, etc.
    Second point is building chroot, configuring pkgs, directly
    downloading packages is slow and we can't maintenance this easy.
    
    Now we will use one docker container, run 'aptly' inside, do mirror all
    .udeb and .deb from requirements-deb.txt and pushing it to local_mirror/
    
    blueprint build-iso-on-fedora
    Signed-off-by: Igor Gnatenko <ignatenko@mirantis.com>
    
    Change-Id: Ieb23c70f31633f651abc1f8700e039700dca669c

--- packages/deb/module.mk
+++ packages/deb/module.mk
@@ -5,8 +5,6 @@ include $(SOURCE_DIR)/packages/deb/debian-boot/module.mk
 clean: clean-deb
 
 clean-deb:
-	-sudo umount $(BUILD_DIR)/packages/deb/SANDBOX/proc
-	-sudo umount $(BUILD_DIR)/packages/deb/SANDBOX/dev
 	sudo rm -rf $(BUILD_DIR)/packages/deb
 
 # Usage:
@@ -22,10 +20,7 @@ $(BUILD_DIR)/packages/deb/$1.done: export SANDBOX_UBUNTU_UP:=$$(SANDBOX_UBUNTU_U
 $(BUILD_DIR)/packages/deb/$1.done: export SANDBOX_UBUNTU_DOWN:=$$(SANDBOX_UBUNTU_DOWN)
 $(BUILD_DIR)/packages/deb/$1.done: $(BUILD_DIR)/repos/repos.done
 	mkdir -p $(BUILD_DIR)/packages/deb/packages $(BUILD_DIR)/packages/deb/sources
-#	create sandbox if does not exist
-	test -e $$(SANDBOX_UBUNTU)/etc/debian_version || sudo sh -c "$$$${SANDBOX_UBUNTU_UP}"
-#	mount proc if not mounted
-	mountpoint -q $$(SANDBOX_UBUNTU)/proc || sudo mount -t proc sandbox_ubuntu_proc $$(SANDBOX_UBUNTU)/proc
+	mkdir -p $$(SANDBOX_UBUNTU)
 	sudo mkdir -p $$(SANDBOX_UBUNTU)/tmp/$1
 ifeq ($1,$(filter $1,nailgun-net-check python-tasklib))
 	tar zxf $(BUILD_DIR)/packages/sources/$1/$(subst python-,,$1)*.tar.gz -C $(BUILD_DIR)/packages/deb/sources
@@ -33,16 +28,22 @@ ifeq ($1,$(filter $1,nailgun-net-check python-tasklib))
 endif
 	sudo cp -r $(BUILD_DIR)/packages/sources/$1/* $$(SANDBOX_UBUNTU)/tmp/$1/
 	sudo cp -r $(SOURCE_DIR)/packages/deb/specs/$1/* $$(SANDBOX_UBUNTU)/tmp/$1/
-	dpkg-checkbuilddeps $(SOURCE_DIR)/packages/deb/specs/$1/debian/control 2>&1 | sed 's/^dpkg-checkbuilddeps: Unmet build dependencies: //g' | sed 's/([^()]*)//g;s/|//g' | sudo tee $$(SANDBOX_UBUNTU)/tmp/$1.installdeps
-	sudo chroot $$(SANDBOX_UBUNTU) /bin/bash -c "cat /tmp/$1.installdeps | xargs apt-get -y install"
-	sudo chroot $$(SANDBOX_UBUNTU) /bin/bash -c "cd /tmp/$1 ; DEB_BUILD_OPTIONS=nocheck debuild -us -uc -b -d"
+	sudo docker run --rm \
+		-v $$(SANDBOX_UBUNTU):/mnt \
+		-v $(SOURCE_DIR):/opt \
+		-v $$(SANDBOX_UBUNTU)/cache:/var/cache
+		ubuntu:$(UBUNTU_RELEASE) /bin/bash -c " \
+			apt-get update; \
+			apt-get install -y build-essential devscripts; \
+			dpkg-checkbuilddeps /opt/packages/deb/specs/$1/debian/control 2>&1 | sed 's/^dpkg-checkbuilddeps: Unmet build dependencies: //g' | sed 's/([^()]*)//g;s/|//g' | tee /mnt/tmp/$1.installdeps; \
+			cat /mnt/tmp/$1.installdeps | xargs apt-get -y install; \
+			cd /mnt/tmp/$1 ; DEB_BUILD_OPTIONS=nocheck debuild -us -uc -b -d"
 	cp $$(SANDBOX_UBUNTU)/tmp/*$1*.deb $(BUILD_DIR)/packages/deb/packages
 	sudo rm -rf $$(SANDBOX_UBUNTU)/tmp/*
-	sudo sh -c "$$$${SANDBOX_UBUNTU_DOWN}"
 	$$(ACTION.TOUCH)
 
 $(BUILD_DIR)/packages/deb/$1-repocleanup.done: $(BUILD_DIR)/mirror/ubuntu/build.done
-	sudo find $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/pool/main -regex '.*$1_[^-]+-[^-]+.*' -delete
+	sudo find $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/public/upstream/pool/main -regex '.*$1_[^-]+-[^-]+.*' -delete
 	$$(ACTION.TOUCH)
 endef
 
@@ -53,10 +54,10 @@ $(eval $(call build_deb,nailgun-agent))
 $(eval $(call build_deb,python-tasklib))
 
 $(BUILD_DIR)/packages/deb/repo.done:
-	sudo find $(BUILD_DIR)/packages/deb/packages -name '*.deb' -exec cp -u {} $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/pool/main \;
+	sudo find $(BUILD_DIR)/packages/deb/packages -name '*.deb' -exec cp -u {} $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/public/upstream/pool/main \;
 	echo "Applying fix for upstream bug in dpkg..."
 	-sudo patch -N /usr/bin/dpkg-scanpackages < $(SOURCE_DIR)/packages/dpkg.patch
-	sudo $(SOURCE_DIR)/regenerate_ubuntu_repo.sh $(LOCAL_MIRROR_UBUNTU_OS_BASEURL) $(UBUNTU_RELEASE)
+	sudo $(SOURCE_DIR)/regenerate_ubuntu_repo.sh $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/public/upstream $(UBUNTU_RELEASE)
 	$(ACTION.TOUCH)
 
 ifneq (0,$(strip $(BUILD_DEB_PACKAGES)))
