3aab16667f47dd8384904e27f70f7a87ba15f4ee
Aleksandra Fedorova <afedorova@mirantis.com>
2014-12-09 21:49:12 +0300

    Switch to stable/6.0 branch
    Change-Id: Icc0045828e4140a38ea9fceaff5fb4aaa0f7b03b

--- config.mk
+++ config.mk
@@ -146,10 +146,10 @@ define set_vars
 endef
 
 # Repos and versions
-FUELLIB_COMMIT?=master
-NAILGUN_COMMIT?=master
-ASTUTE_COMMIT?=master
-OSTF_COMMIT?=master
+FUELLIB_COMMIT?=stable/6.0
+NAILGUN_COMMIT?=stable/6.0
+ASTUTE_COMMIT?=stable/6.0
+OSTF_COMMIT?=stable/6.0
 
 FUELLIB_REPO?=https://github.com/stackforge/fuel-library.git
 NAILGUN_REPO?=https://github.com/stackforge/fuel-web.git
