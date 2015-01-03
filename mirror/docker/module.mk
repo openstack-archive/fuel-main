297ae727351f0798e1736b92ebe3566040fb742f
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2015-01-03 01:36:12 +0100

    Remove pidfile on TERM signal
    Change-Id: I19a9764d5e08bf8745dca8077faf951fb83c14a0
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- mirror/docker/module.mk
+++ mirror/docker/module.mk
@@ -5,6 +5,21 @@ include $(SOURCE_DIR)/mirror/docker/base-images.mk
 clean: clean-docker
 
 clean-docker:
+	for i in $(BUILD_DIR)/docker/run/*.lock; do \
+		LOCKFILE=$(BUILD_DIR)/docker/run/$${i}; \
+		PIDFILE=$(BUILD_DIR)/docker/run/$${i%%.lock}; \
+		if [[ $$(ps -o args= -p `cat $${PIDFILE}) =~ "simple_http_daemon" ]]; then \
+			kill `cat $${PIDFILE}`; \
+			sleep 1; \
+		else \
+			rm -f $${PIDFILE} $${LOCKFILE}; \
+			continue; \
+		fi; \
+		if [ -f $${PIDFILE} ]; then \
+			kill -9 `cat $${PIDFILE}`; \
+			rm -f $${PIDFILE} $${LOCKFILE}; \
+		fi \
+	done
 	timeout -k5 4 sudo docker ps && sudo docker rm -f `sudo docker ps -a | awk '/fuel/ {print $$1}'` || true
 	timeout -k5 4 sudo docker images && sudo docker rmi -f `sudo docker images | awk '/fuel/ { print $$3; }'` || true
 
