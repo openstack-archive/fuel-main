3e92d9c8d404adc6f7aa2aed53ceaf64d0377160
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2015-01-03 01:36:12 +0100

    Refactor SimpleHTTP daemon for parallel execution
    * Remove timeout. SimpleHTTP Daemon will run as long as required.
    * Define SIGTERM event with stop() function. This allows to shutdown
      server properly and remove pidfile.
    * Add extra check whether port is already opened.
    * Add extra check for stale pidfile.
    * Create PID files in "/tmp/simple_http_daemon" rather than '/tmp'
    * Cleanup simple_http_daemon in case of make failures.
    
    Change-Id: I19a9764d5e08bf8745dca8077faf951fb83c14a0
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- utils/simple_http_daemon.py
+++ utils/simple_http_daemon.py
@@ -17,20 +17,27 @@
 import sys
 import os
 import time
+import socket
 import daemon
-import daemon.pidlockfile
+from daemon.pidlockfile import PIDLockFile
+from lockfile import LockTimeout
+from signal import SIGTERM
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-
 class SimpleHTTPDaemon:
     def __init__(self, address='0.0.0.0', port='9001',
-                 pid_file='/var/run/simplehttpd.pid', ttl=600):
+                 pid_file='/var/run/simplehttpd.pid'):
         self.address = address
         self.port = port
-        self.pid_file = pid_file
-        self.ttl = ttl
-        self.end = 0
+        self.pid = PIDLockFile(pid_file)
+        if self.pid.read_pid() != None and os.getpid() != self.pid.read_pid():
+            try:
+                os.kill(self.pid.read_pid(), 0)
+                print 'Process already running!'
+                sys.exit(1)
+            except OSError:
+                self.pid.break_lock()
 
     def run_http_server(self):
         HandlerClass = SimpleHTTPRequestHandler
@@ -38,19 +45,24 @@ class SimpleHTTPDaemon:
         Protocol = "HTTP/1.0"
         server_address = (self.address, self.port)
         HandlerClass.protocol_version = Protocol
-        httpd = ServerClass(server_address, HandlerClass)
-        while time.time() < self.end:
-            httpd.handle_request()
+        self.httpd = ServerClass(server_address, HandlerClass)
+        self.httpd.serve_forever()
 
     def start(self):
-        self.end = time.time() + self.ttl
         context = daemon.DaemonContext(
-            working_directory=os.getcwd(),
-            umask=0o002,
-            pidfile=daemon.pidlockfile.PIDLockFile(self.pid_file)
-        )
+                pidfile=self.pid,
+                working_directory=os.getcwd(),
+                signal_map={
+                    SIGTERM: self.stop
+                }
+            )
         with context:
             self.run_http_server()
+
+    def stop(self):
+        self.httpd.shutdown()
+        self.pid.release()
+        sys.exit(0)
 
 if __name__ == "__main__":
 
@@ -59,15 +71,17 @@ if __name__ == "__main__":
     else:
         port = 9001
 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex(('127.0.0.1',port))
+    if result == 0:
+        print 'Port already opened'
+        sys.exit(1)
+    sock.close()
+
     if sys.argv[2:]:
         pid = sys.argv[2]
     else:
         pid = '/var/run/simplehttpd.pid'
 
-    if sys.argv[3:]:
-        timeout = int(sys.argv[3])
-    else:
-        timeout = sys.maxint
-
-    server = SimpleHTTPDaemon('0.0.0.0', port, pid, timeout)
+    server = SimpleHTTPDaemon('0.0.0.0', port, pid)
     server.start()
