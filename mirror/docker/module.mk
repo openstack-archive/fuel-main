b62c9c4cfd7e0284fc2163e6d4bff40d65783120
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2015-01-03 01:36:12 +0100

    Refactor SimpleHTTP daemon for parallel execution
    * Remove timeout. SimpleHTTP Daemon will run as long as required.
    * Define SIGTERM event with stop() function. This allows to shutdown
      server properly and remove pidfile.
    * Add extra check whether port is already opened.
    * Add extra check for stale pidfile.
    * Create PID files in "/tmp/simple_http_daemon" rather than '/tmp'
    * Cleanup simple_http_daemon in case of make failures.
    
    Change-Id: I19a9764d5e08bf8745dca8077faf951fb83c14a0
    Signed-off-by: Sergii Golovatiuk <sgolovatiuk@mirantis.com>

--- mirror/docker/module.mk
+++ mirror/docker/module.mk
@@ -5,6 +5,21 @@ include $(SOURCE_DIR)/mirror/docker/base-images.mk
 clean: clean-docker
 
 clean-docker:
+	for i in /tmp/simple_http_daemon/*.lock; do \
+		LOCKFILE=$${i}; \
+		PIDFILE=$${i%%.lock}; \
+		if ps -o args= -p `cat $${PIDFILE}`| grep --quiet simple_http_daemon; then \
+			kill `cat $${PIDFILE}`; \
+			sleep 1; \
+		else \
+			rm -f $${PIDFILE} $${LOCKFILE}; \
+			continue; \
+		fi; \
+		if [ -f $${PIDFILE} ]; then \
+			kill -9 `cat $${PIDFILE}`; \
+			rm -f $${PIDFILE} $${LOCKFILE}; \
+		fi \
+	done
 	timeout -k5 4 sudo docker ps && sudo docker rm -f `sudo docker ps -a | awk '/fuel/ {print $$1}'` || true
 	timeout -k5 4 sudo docker images && sudo docker rmi -f `sudo docker images | awk '/fuel/ { print $$3; }'` || true
 
