237526521e94c2ae884e4c22449e166e0457e6e6
Alexei Sheplyakov <asheplyakov@mirantis.com>
2015-01-05 14:45:43 +0300

    mirror/ubuntu: speed up downloading the packages
    Create an alternative APT config/state instead of making a chroot.
    Calculate packages' dependencies in parallel.
    
    blueprint support-ubuntu-trusty
    Change-Id: I27545b9f3bc1393803b953919ea7f077c23f3245

--- mirror/ubuntu/files/mkrepo.sh
+++ mirror/ubuntu/files/mkrepo.sh
@@ -28,63 +28,13 @@ requirements_add_essential_pkgs () {
 	# All essential packages are already installed, so ask dpkg for a list
 	dpkg-query -W -f='${Package} ${Essential}\n' > /tmp/essential.pkgs
 	sed -i /tmp/essential.pkgs -n -e 's/\([^ ]\+\).*yes$/\1/p'
-	cat /tmp/essential.pkgs >> /requirements-deb.txt
+	cat /tmp/essential.pkgs /requirements-deb.txt | sort -u > /tmp/requirements-deb.txt
+	mv /tmp/requirements-deb.txt /requirements-deb.txt
 }
 
-# Note: apt-get install --print-uris package
-# is not going to print anything if the package is already installed. Thus
-# the base packages will be omitted if we use the main APT/dpkg settings.
-# Pretend that no package has been installed by creating an alternative APT
-# state and configuration directories.
-# Previously we used to copy all debs from the APT cache which is unreliable:
-# - a wrong version of the package might be included
-# - multiple revisions of the same package might be included
-apt_altstate="/apt-altstate"
-rm -rf "$apt_altstate"
-apt_lists_dir="$apt_altstate/var/lib/apt/lists"
-apt_cache_dir="$apt_altstate/var/cache/apt"
-null_dpkg_status="$apt_altstate/var/lib/dpkg/status"
-
-mkdir -p "$apt_lists_dir"
-mkdir -p "$apt_cache_dir"
-mkdir -p "${null_dpkg_status%/*}"
-touch "${null_dpkg_status}"
-
-apt_altstate_opts="-o APT::Get::AllowUnauthenticated=1"
-apt_altstate_opts="${apt_altstate_opts} -o Dir::State::Lists=${apt_lists_dir}"
-apt_altstate_opts="${apt_altstate_opts} -o Dir::State::status=${null_dpkg_status}"
-apt_altstate_opts="${apt_altstate_opts} -o Dir::Cache=${apt_cache_dir}"
-
-if ! apt-get $apt_altstate_opts update; then
-	echo "mkrepo.sh: failed to populate alt apt state"
-	exit 1
-fi
-
 requirements_add_essential_pkgs
-has_apt_errors=''
-rm -f /apt-errors.log
-while read pkg; do
-	downloads_list="/downloads_${pkg}.list"
-	if ! apt-get $apt_altstate_opts --print-uris --yes -qq install $pkg >"${downloads_list}" 2>>"/apt-errors.log"; then
-		echo "package $pkg can not be installed" >>/apt-errors.log
-		# run apt-get once more to get a verbose error message
-		apt-get $apt_altstate_opts --print-uris --yes install $pkg >>/apt-errors.log 2>&1 || true
-		has_apt_errors='yes'
-	fi
-	sed -i "${downloads_list}" -n -e "s/^'\([^']\+\)['].*$/\1/p"
-done < /requirements-deb.txt
+make -j4 -f /repo/mkrepo.mk deb_download || exit 1
 
-if [ -n "$has_apt_errors" ]; then
-	echo 'some packages are not installable' >&2
-	cat < /apt-errors.log >&2
-	exit 1
-fi
-
-rm -rf "$apt_altstate"
-
-cat /downloads_*.list | sort | uniq > /repo/download/download_urls.list
-rm /downloads_*.list /apt-errors.log
-(cat /repo/download/download_urls.list | xargs -n1 -P4 wget -nv -P /repo/download/) || exit 1
 # Make structure and mocks for multiarch
 for dir in binary-i386 binary-amd64; do 
 	mkdir -p /repo/dists/${UBUNTU_RELEASE}/main/$dir /repo/dists/${UBUNTU_RELEASE}/main/debian-installer/$dir
@@ -97,16 +47,13 @@ cd /repo/pool/debian-installer
 # Grab every udeb
 ###################
 
-wrkdir=`dirname $(pwd)`/`basename $(pwd)`
+apt_udeb_dir=/tmp/apt.udeb
+rm -rf "${apt_udeb_dir}"
+mkdir -p "${apt_udeb_dir}/lists"
+mkdir -p "${apt_udeb_dir}/sources"
+mkdir -p "${apt_udeb_dir}/cache"
+touch ${apt_udeb_dir}/UPackages.tmp ${apt_udeb_dir}/override.${UBUNTU_RELEASE}.main ${apt_udeb_dir}/override.${UBUNTU_RELEASE}.extra.main
 
-rm -f ${wrkdir}/UPackages.tmp ${wrkdir}/override.${UBUNTU_RELEASE}.main ${wrkdir}/override.${UBUNTU_RELEASE}.extra.main
-touch ${wrkdir}/UPackages.tmp ${wrkdir}/override.${UBUNTU_RELEASE}.main ${wrkdir}/override.${UBUNTU_RELEASE}.extra.main
-
-# Prepare temp apt dirs
-[ -d ${wrkdir}/apt.tmp ] && rm -rf ${wrkdir}/apt.tmp
-mkdir -p ${wrkdir}/apt.tmp/lists
-mkdir -p ${wrkdir}/apt.tmp/sources
-mkdir -p ${wrkdir}/apt.tmp/cache
 
 # Extract all specified repos (except backports repo)
 for list in /etc/apt/sources.list; do
@@ -115,50 +62,43 @@ for list in /etc/apt/sources.list; do
      repodist=`echo $repo | awk -F '|' '{print $3}'`
      repos=`echo $repo | awk -F '|' '{for(i=4; i<=NF; ++i) {print $i}}'`
      for repo in $repos; do
-       echo "deb ${repourl} ${repodist} ${repo}/debian-installer" >> ${wrkdir}/apt.tmp/sources/sources.list
+       echo "deb ${repourl} ${repodist} ${repo}/debian-installer" >> "${apt_udeb_dir}/sources/sources.list"
        packagesfile=`wget -nv -qO - ${repourl}/dists/${repodist}/Release | \
                      egrep '[0-9a-f]{64}' | \
                      grep ${repo}/debian-installer/binary-amd64/Packages.bz2 | \
                      awk '{print $3}'`
        if [ -n "$packagesfile" ]; then
          bz=${repourl}/dists/${repodist}/$packagesfile
-         wget -nv -qO - $bz | bzip2 -cdq | sed -ne 's/^Package: //p' >> ${wrkdir}/UPackages.tmp
+         wget -nv -qO - $bz | bzip2 -cdq | sed -ne 's/^Package: //p' >> ${apt_udeb_dir}/UPackages.tmp
        else
          bz=${repourl}/dists/${repodist}/${repo}/debian-installer/binary-amd64/Packages
-         wget -nv -qO - $bz | sed -ne 's/^Package: //p' >> ${wrkdir}/UPackages.tmp
+         wget -nv -qO - $bz | sed -ne 's/^Package: //p' >> ${apt_udeb_dir}/UPackages.tmp
        fi
        # Getting indices
-       wget -nv -O - ${repourl}/indices/override.${repodist}.${repo} >> ${wrkdir}/override.${UBUNTU_RELEASE}.main
-       wget -nv -O - ${repourl}/indices/override.${repodist}.extra.${repo} >> ${wrkdir}/override.${UBUNTU_RELEASE}.extra.main
-       wget -nv -O - ${repourl}/indices/override.${repodist}.${repo}.debian-installer >> ${wrkdir}/override.${UBUNTU_RELEASE}.main.debian-installer
+       wget -nv -O - ${repourl}/indices/override.${repodist}.${repo} >> ${apt_udeb_dir}/override.${UBUNTU_RELEASE}.main
+       wget -nv -O - ${repourl}/indices/override.${repodist}.extra.${repo} >> ${apt_udeb_dir}/override.${UBUNTU_RELEASE}.extra.main
+       wget -nv -O - ${repourl}/indices/override.${repodist}.${repo}.debian-installer >> ${apt_udeb_dir}/override.${UBUNTU_RELEASE}.main.debian-installer
      done
   done
 done
 
 # Collect all udebs except packages with suffux generic or virtual
-packages=`cat ${wrkdir}/UPackages.tmp | sort -u | egrep -v "generic|virtual"`
+packages=`sort -u < ${apt_udeb_dir}/UPackages.tmp | egrep -v "generic|virtual"`
 
 # Find modules for ${UBUNTU_INSTALLER_KERNEL_VERSION} kernel (installer runs with this version)
-for package in `cat ${wrkdir}/UPackages.tmp | egrep "generic|virtual" | grep ${UBUNTU_INSTALLER_KERNEL_VERSION}`; do
+for package in `cat ${apt_udeb_dir}/UPackages.tmp | egrep "generic|virtual" | grep ${UBUNTU_INSTALLER_KERNEL_VERSION}`; do
   packages="$packages $package"
 done
 
+apt_udeb_opts="-o Dir::Etc::SourceParts="${apt_udeb_dir}/sources/parts""
+apt_udeb_opts="${apt_udeb_opts} -o Dir::Etc::SourceList="${apt_udeb_dir}/sources/sources.list""
+apt_udeb_opts="${apt_udeb_opts} -o Dir::State::Lists="${apt_udeb_dir}/lists""
+apt_udeb_opts="${apt_udeb_opts} -o Dir::Cache="${apt_udeb_dir}/cache""
+
 # Update apt temp cache
-apt-get -o Dir::Etc::SourceParts="${wrkdir}/apt.tmp/sources/parts" \
-        -o Dir::Etc::SourceList="${wrkdir}/apt.tmp/sources/sources.list" \
-        -o Dir::State::Lists="${wrkdir}/apt.tmp/lists" \
-        -o Dir::Cache="${wrkdir}/apt.tmp/cache" \
-        update
-
+apt-get ${apt_udeb_opts} update
 # Download udebs
-apt-get -o Dir::Etc::SourceParts="${wrkdir}/apt.tmp/sources/parts" \
-        -o Dir::Etc::SourceList="${wrkdir}/apt.tmp/sources/sources.list" \
-        -o Dir::State::Lists="${wrkdir}/apt.tmp/lists" \
-        -o Dir::Cache="${wrkdir}/apt.tmp/cache" \
-        download $packages
-
-rm -f ${wrkdir}/UPackages.tmp
-rm -rf ${wrkdir}/apt.tmp
+apt-get ${apt_udeb_opts} download $packages
 
 # Get rid of urlencoded names
 for i in $(ls | grep %) ; do mv $i $(echo $i | echo -e $(sed 's/%/\\x/g')) ; done
@@ -187,9 +127,8 @@ for i in $(ls | grep %) ; do mv $i $(echo $i | echo -e $(sed 's/%/\\x/g')) ; don
 mkdir -p /repo/indices
 cd /repo/indices
 for idx in override.${UBUNTU_RELEASE}.main override.${UBUNTU_RELEASE}.extra.main override.${UBUNTU_RELEASE}.main.debian-installer; do
-  cat ${wrkdir}/$idx | sort -u > /repo/indices/$idx
+  cat ${apt_udeb_dir}/$idx | sort -u > /repo/indices/$idx
 done
-rm -f ${wrkdir}/override.${UBUNTU_RELEASE}.main ${wrkdir}/override.${UBUNTU_RELEASE}.extra.main ${wrkdir}/override.${UBUNTU_RELEASE}.main.debian-installer
 cd /repo
 # Just because apt scan will produce crap
 cp -a Release-amd64 Release-i386
@@ -208,5 +147,3 @@ for pkg_file in `find dists -type f -name Packages`; do
 	sort_packages_file $pkg_file
 done
 apt-ftparchive -c apt-ftparchive-release.conf release dists/${UBUNTU_RELEASE}/ > dists/${UBUNTU_RELEASE}/Release
-# some cleanup...
-rm -rf apt-ftparchive*conf Release-amd64 Release-i386 mkrepo.sh preferences
