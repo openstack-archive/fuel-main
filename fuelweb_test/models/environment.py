776aae232d343f5e852236af466405508a842ded
Dennis Dmitriev <ddmitriev@mirantis.com>
2015-01-06 00:55:03 +0200

    Try change Keystone credentials before using protected Nailgun API
    If 'KEYSTONE_PASSWORD' changed, HTTPClient initialazes with a new
    password. Nailgun API cannot be used on reverted environment until
    Keystone password be changed from default to new one.
    
    - use _wait() until Keystone password be changed
    - use apropriate fuel cli options for 5.x.x version.
    
    Change-Id: Ibe06ac9630485a9589788948d70f972c88da5371
    Closes-Bug: #1407660

--- fuelweb_test/models/environment.py
+++ fuelweb_test/models/environment.py
@@ -361,10 +361,10 @@ class EnvironmentModel(object):
                 logger.info('Admin node started second time.')
                 self.nodes().admin.await(
                     self.admin_net, timeout=10 * 60, by_port=8000)
-                _wait(self._fuel_web.client.get_releases, timeout=120)
+                _wait(self._fuel_web.get_nailgun_version, timeout=120)
 
             self.set_admin_ssh_password()
-            self.set_admin_keystone_password()
+            _wait(self.set_admin_keystone_password, timeout=120)
 
             self.sync_time_admin_node()
 
@@ -405,12 +405,12 @@ class EnvironmentModel(object):
                            settings.SSH_CREDENTIALS['password']))
 
     def set_admin_keystone_password(self):
+        cmd = 'fuel --os-username {0} --os-password {1} release'
         remote = self.get_admin_remote()
         try:
             self.execute_remote_cmd(
-                remote, 'fuel --user {0} --password {1} release'
-                .format(settings.KEYSTONE_CREDS['username'],
-                        settings.KEYSTONE_CREDS['password']))
+                remote, cmd.format(settings.KEYSTONE_CREDS['username'],
+                                   settings.KEYSTONE_CREDS['password']))
         except AssertionError:
             self.execute_remote_cmd(
                 remote, 'fuel user --newpass {0} --change-password'
