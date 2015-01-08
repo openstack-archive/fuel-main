13ce28448ca0d5ac4d397bdf6c17098187fe01eb
asledzinskiy <asledzinskiy@mirantis.com>
2015-01-08 18:23:51 +0200

    Add check on swift rings health
    - Add check that rings balance value is less
    than 100 to ha_flat and neutron_gre_ha tests
    
    Change-Id: I26f56d6375ae1756da0211d493af5d685d0d8241
    Closes-Bug: #1405783

--- fuelweb_test/helpers/checkers.py
+++ fuelweb_test/helpers/checkers.py
@@ -816,3 +816,15 @@ def check_stats_private_info(collector_remote, postgres_actions,
 def check_kernel(kernel, expected_kernel):
     assert_equal(kernel, expected_kernel,
                  "kernel version is wrong, it is {0}".format(kernel))
+
+
+def check_swift_ring(remote):
+    for builder in ['object', 'account', 'container']:
+        res = ''.join(remote.execute(
+            "swift-ring-builder /etc/swift/{0}.builder".format(
+                builder))['stdout'])
+        logger.debug("swift ring builder output is {0}".format(res))
+        balance = re.search('(\d+.\d+) balance', res).group(1)
+        assert_true(int(float(balance)) < 100,
+                    "swift ring builder is not ok,"
+                    " balance is {0}".format(balance))
