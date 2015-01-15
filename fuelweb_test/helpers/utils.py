e1646f52fd293a7c93d8330f9a7cf31b73b56acf
Dennis Dmitriev <ddmitriev@mirantis.com>
2015-01-15 15:41:15 +0200

    Store astute.yaml to the settings.LOGS_DIR
    * If a systest passed, store astute.yaml from each active node to
    the settings.LOGS_DIR, using name of the systest method.
    For example:
    ./logs/deploy_ha_vlan-slave-01.yaml
    ./logs/deploy_ha_vlan-slave-02.yaml
    ./logs/deploy_ha_vlan-slave-03.yaml
    
    * To make nodes available after tests, enable resuming for nodes
    after making every snapshot.
    
    * Log to the console output the test method name and the scenario
    from the docstring of the method. Using the decorator
    log_snapshot_on_error for this, as it is always present in all
    systest methods.
    
    Change-Id: Id95a5eaa8807e3a4f4b7afd5dcdebc6be963d75b
    Closes-Bug: #1401601

--- fuelweb_test/helpers/utils.py
+++ fuelweb_test/helpers/utils.py
@@ -86,3 +86,18 @@ def pull_out_logs_via_ssh(admin_remote, name,
                               "wasn't saved on local host"))
     except Exception:
         logger.error(traceback.format_exc())
+
+
+@logwrap
+def store_astute_yaml(env, func_name):
+    for node in env.nodes().slaves:
+        nailgun_node = env.fuel_web.get_nailgun_node_by_devops_node(node)
+        if node.driver.node_active(node) and nailgun_node['roles']:
+            try:
+                remote = env.get_ssh_to_remote_by_name(node.name)
+                if not remote.download('/etc/astute.yaml', '{0}/{1}-{2}.yaml'
+                                       .format(settings.LOGS_DIR,
+                                               func_name, node.name)):
+                    logger.error("Downloading of 'astute.yaml' failed.")
+            except Exception:
+                logger.error(traceback.format_exc())
