332fef6a6771924f9f2760da9fa434aad9a4bf14
NastyaUrlapova <aurlapova@mirantis.com>
2015-01-16 11:22:16 +0300

    Changes for smoke|bvt according a new product strategy.
    Change-Id: I6a8b2b6fdd51e128d24d5574bc7692a37da8e798

--- fuelweb_test/tests/test_neutron.py
+++ fuelweb_test/tests/test_neutron.py
@@ -22,7 +22,7 @@ from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
 
 
-@test(groups=["thread_1", "neutron", "bvt_1"])
+@test(groups=["thread_1", "neutron", "smoke_neutron"])
 class NeutronGre(TestBasic):
 
     @test(depends_on=[SetupEnvironment.prepare_slaves_3],
