3f5bce14861b81af7a4a66f51c3b7ad36681f5bf
Igor Belikov <ibelikov@mirantis.com>
2015-01-21 18:54:04 +0300

    Remove obsolete jenkins jobs configuration
    * Remove deprecated jenkins jobs configuration files from utils/jenkins/fuel-ci
    * Replace contents of README.md with information about current location of
     jenkins jobs configuration
    
    Change-Id: Ibb25d74f6910427258c632588d82af878b8529c9
    Closes-Bug: #1408225

--- utils/jenkins/fuel-ci/fuel-jenkins/scripts/fuel_systest_env_havana.sh
+++
@@ -1,8 +0,0 @@
-cd $WORKSPACE
-
-export TEST_NAME=fuelweb_test.integration.test_node:TestNode.test_simple_cluster_flat
-export VENV_PATH=/home/jenkins/workspace/venv-nailgun-tests
-cp /home/jenkins/workspace/iso/run_tests.py ./fuelweb_test/run_tests.py
-cp /home/jenkins/workspace/iso/test_pullrequest.py ./fuelweb_test/tests/test_pullrequest.py
-
-sh -x "/home/jenkins/workspace/system_tests.sh" -t test -i /home/jenkins/workspace/iso/fuel_pull_havana.iso -o --group=setup
