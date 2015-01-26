818770d644e1340f869e973e5a634ebde667118f
Aleksandra Fedorova <afedorova@mirantis.com>
2015-01-26 19:36:35 +0300

    Fix fuelmain_sha in version.yaml
    Closes-Bug: #1413246
    
    Change-Id: I88d4ac5d889989d50a034b2ac4491e5dfd814fd8

--- repos.mk
+++ repos.mk
@@ -38,7 +38,7 @@ $(BUILD_DIR)/repos/repos.done:
 		repo_commit_id=`git --git-dir=$(BUILD_DIR)/repos/$$repo/.git rev-parse --verify HEAD`; \
 		echo "  $${repo}_sha: \"$${repo_commit_id}\""; \
 	done > $${version_yaml}.tmp; \
-	fuel_main_commit_id=`git rev-parse --verify HEAD`; \
+	fuelmain_commit_id=`git rev-parse --verify HEAD`; \
 	echo "  fuelmain_sha: \"${fuelmain_commit_id}\"" >> $${version_yaml}.tmp; \
 	mv $${version_yaml}.tmp $${version_yaml}
 	$(ACTION.TOUCH)
