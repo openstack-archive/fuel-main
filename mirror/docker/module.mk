01583bdca6449bac54a09d1e4a63dabf9ce4bec1
Alexei Sheplyakov <asheplyakov@mirantis.com>
2015-01-27 10:28:59 +0300

    mirror/docker: fix the cleanup rule
    After the commit 59b10c0d543af17eb88af43360df0f31337db68b ("Refactor Docker
    cleanup") no docker containers/images get actually removed, so building ISO
    exhausts the disk space quite quickly. To fix the problem
    - Properly quote the special characters so awk command is valid
    - Use xargs -r instead of reinventing it
    
    Change-Id: Ie35906edde2fc04d5c3adef0578d1efb4e9a9d64

--- mirror/docker/module.mk
+++ mirror/docker/module.mk
@@ -5,8 +5,8 @@ include $(SOURCE_DIR)/mirror/docker/base-images.mk
 clean: clean-docker
 
 clean-docker:
-	sudo sh -c 'docker ps && docker rm -f `docker ps -a | awk "/fuel/ {print $$1}"` 2>/dev/null || true'
-	sudo sh -c 'docker images && docker rmi -f `docker images | awk "/fuel/ { print $$3; }"` 2>/dev/null || true'
+	-sudo sh -c "docker ps -a | awk '/fuel/ { print \$$1; }' | xargs --no-run-if-empty docker rm -f"
+	-sudo sh -c "docker images | awk '/fuel/ { print \$$3; }' | xargs --no-run-if-empty docker rmi -f"
 
 $(BUILD_DIR)/mirror/docker/build.done: \
 		$(BUILD_DIR)/mirror/docker/base-images.done
