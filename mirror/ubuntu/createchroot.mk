ce27237fb87a7d2fd180075384f4fe75c8b40e22
Alexei Sheplyakov <asheplyakov@mirantis.com>
2015-01-31 14:30:13 +0300

    mirror/ubuntu: make the deb package download easier to debug
    At the moment debugging package download issues (finding out why certain
    package is missing or has the dependency problems, etc) involves a lot of
    guesswork sinse the temporary chroot gets removed. Apparently the only for
    removing is that the directory holding the chroot gets copied to the ISO.
    Create the chroot in the `build' directory rather than in local_mirror so
    that removing the chroot is not mandatory any more.
    
    * createchroot.mk:
     - introduce CHROOT_DIR (per target) variable and make chroot there,
       put CHROOT_DIR into BUILD_DIR.
     - copy only useful files (pool, dists, indices) to the local_mirror
     - don't remove the chroot, it might be very useful for debugging.
    
    * mkrepo.sh: store APT configs/state in a different directory in order
      to not pollute the ISO with temporary files
    
    blueprint support-ubuntu-trusty
    
    Change-Id: Ia10f82dc79933e41a489b6e4f5d5523ce3c50d9a

--- mirror/ubuntu/createchroot.mk
+++ mirror/ubuntu/createchroot.mk
@@ -2,7 +2,7 @@
 APT_CONF_TEMPLATES := apt-ftparchive-deb.conf apt-ftparchive-udeb.conf apt-ftparchive-release.conf Release-amd64
 empty_line:=
 define insert_ubuntu_version
-	sed -i $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/repo/$(1) \
+	sed -i $(1) \
 		-e 's|@@UBUNTU_RELEASE@@|$(UBUNTU_RELEASE)|g' \
 		-e 's|@@UBUNTU_RELEASE_NUMBER@@|$(UBUNTU_RELEASE_NUMBER)|g'
 	$(empty_line)
@@ -38,49 +38,51 @@ endef
 
 $(BUILD_DIR)/mirror/ubuntu/createchroot.done: export APT_SOURCES_LIST:=$(apt_sources_list)
 $(BUILD_DIR)/mirror/ubuntu/createchroot.done: export POLICY_RC_D:=$(policy_rc_d)
+$(BUILD_DIR)/mirror/ubuntu/createchroot.done: export CHROOT_DIR:=$(BUILD_DIR)/mirror/ubuntu/chroot
 
 $(BUILD_DIR)/mirror/ubuntu/createchroot.done:
-	mkdir -p $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot
+	mkdir -p $(CHROOT_DIR)
 	# Prevent services from being started inside the staging chroot
-	policy_rc_d="$(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/usr/sbin/policy-rc.d"; \
+	policy_rc_d="$(CHROOT_DIR)/usr/sbin/policy-rc.d"; \
 	mkdir -p "$${policy_rc_d%/*}"; \
 	echo "$${POLICY_RC_D}" > "$${policy_rc_d}"; \
 	chmod 755 "$${policy_rc_d}"
-	mkdir -p $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/etc/init.d
+	mkdir -p $(CHROOT_DIR)/etc/init.d
 	# Avoid base packages' configuration errors by preventing the postinst
 	# scripts from fiddling with the services' start order.
 	# Suppresses the scary error messages
 	# "Errors were while processing: <long-list-of-base-packages>'
 	# and makes it possible to install non-trivial packages in the chroot
-	touch $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/etc/init.d/.legacy-bootordering
+	touch $(CHROOT_DIR)/etc/init.d/.legacy-bootordering
 	# copy the mirroring script and the list of the packages
-	mkdir -p $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/repo
-	rsync -a $(SOURCE_DIR)/mirror/ubuntu/files/ $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/repo/
-	cp -a $(SOURCE_DIR)/requirements-deb.txt $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot
-	$(foreach f,$(APT_CONF_TEMPLATES),$(call insert_ubuntu_version,$(f)))
-	chmod +x $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/repo/mkrepo.sh
+	mkdir -p $(CHROOT_DIR)/repo
+	rsync -a $(SOURCE_DIR)/mirror/ubuntu/files/ $(CHROOT_DIR)/repo/
+	cp -a $(SOURCE_DIR)/requirements-deb.txt $(CHROOT_DIR)
+	$(foreach f,$(APT_CONF_TEMPLATES),$(call insert_ubuntu_version,$(CHROOT_DIR)/repo/$(f)))
+	chmod +x $(CHROOT_DIR)/repo/mkrepo.sh
 	# bootstrap Ubuntu
 	sudo debootstrap --no-check-gpg --arch=$(UBUNTU_ARCH) \
 		--variant=minbase --include=apt-utils,wget,bzip2,make \
-		$(UBUNTU_RELEASE) $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot $(MIRROR_UBUNTU)
-	sudo rm -f $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/etc/apt/sources.list.d/*.list
-	echo "$${APT_SOURCES_LIST}" | sudo tee $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/etc/apt/sources.list
-	echo 'APT::Get::AllowUnauthenticated 1;' | sudo tee $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/etc/apt/apt.conf.d/02mirantis-unauthenticated
-	sudo cp -a $(SOURCE_DIR)/mirror/ubuntu/files/preferences $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/etc/apt
-	sudo rm -f $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/etc/resolv.conf
-	sudo cp /etc/resolv.conf $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/etc/resolv.conf
+		$(UBUNTU_RELEASE) $(CHROOT_DIR) $(MIRROR_UBUNTU)
+	sudo rm -f $(CHROOT_DIR)/etc/apt/sources.list.d/*.list
+	echo "$${APT_SOURCES_LIST}" | sudo tee $(CHROOT_DIR)/etc/apt/sources.list
+	echo 'APT::Get::AllowUnauthenticated 1;' | sudo tee $(CHROOT_DIR)/etc/apt/apt.conf.d/02mirantis-unauthenticated
+	sudo cp -a $(SOURCE_DIR)/mirror/ubuntu/files/preferences $(CHROOT_DIR)/etc/apt
+	sudo rm -f $(CHROOT_DIR)/etc/resolv.conf
+	sudo cp /etc/resolv.conf $(CHROOT_DIR)/etc/resolv.conf
 	extra_env=""; \
 	if [ -n "$$HTTP_PROXY" ] || [ -n "$$http_proxy" ]; then \
 		HTTP_PROXY="$${HTTP_PROXY:-$${http_proxy}}"; \
-		echo "Acquire::http { Proxy \"$$HTTP_PROXY\"; };" | sudo tee $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/etc/apt/apt.conf.d/03-use-proxy; \
+		echo "Acquire::http { Proxy \"$$HTTP_PROXY\"; };" | sudo tee $(CHROOT_DIR)/etc/apt/apt.conf.d/03-use-proxy; \
 		extra_env="HTTP_PROXY=$${HTTP_PROXY} http_proxy=$${HTTP_PROXY}"; \
 	fi; \
-	sudo chroot $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot env \
+	sudo chroot $(CHROOT_DIR) env \
 		UBUNTU_RELEASE='$(UBUNTU_RELEASE)' \
 		UBUNTU_INSTALLER_KERNEL_VERSION='$(UBUNTU_INSTALLER_KERNEL_VERSION)' \
 		UBUNTU_KERNEL_FLAVOR='$(UBUNTU_KERNEL_FLAVOR)' \
 		$$extra_env \
 		/repo/mkrepo.sh
-	sudo rsync -a --delete $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot/repo/* $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/
-	sudo rm -rf $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/chroot
+	rsync -a --delete $(CHROOT_DIR)/repo/pool/ $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/pool/
+	rsync -a --delete $(CHROOT_DIR)/repo/dists/ $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/dists/
+	rsync -a --delete $(CHROOT_DIR)/repo/indices/ $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/indices/
 	$(ACTION.TOUCH)
