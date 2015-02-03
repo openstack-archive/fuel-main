542f12c696af40cdff6089cee2e490a27f29530c
Albert <asyriy@mirantis.com>
2015-02-03 17:43:29 +0200

    ubuntu: kernel 3.13 backport from Fuel 6.0
    Change-Id: I1de88f400adc0b851d0f5eca0202e1e209c9afa8

--- mirror/ubuntu/boot.mk
+++ mirror/ubuntu/boot.mk
@@ -1,16 +1,25 @@
 ISOLINUX_FILES:=netboot.tar.gz
 
-# debian isolinux files
-$(addprefix $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/,$(ISOLINUX_FILES)):
-	@mkdir -p $(@D)
+LOCAL_NETBOOT_DIR:=$(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot
+LOCAL_NETBOOT_TGZ:=$(LOCAL_NETBOOT_DIR)/$(ISOLINUX_FILES)
+NETBOOT_URL:=$(MIRROR_UBUNTU)/installer-amd64/current/images/netboot/netboot.tar.gz
 ifeq ($(USE_MIRROR),none)
-	wget -O $@ $(MIRROR_UBUNTU)/ubuntu/dists/precise-updates/main/installer-amd64/current/images/saucy-netboot/netboot.tar.gz
-else
-	wget -O $@ $(MIRROR_UBUNTU)/installer-amd64/current/images/netboot/netboot.tar.gz
+	NETBOOT_URL:=$(MIRROR_UBUNTU)/ubuntu/dists/$(UBUNTU_RELEASE)-updates/main/installer-amd64/current/images/$(UBUNTU_NETBOOT_FLAVOR)/netboot.tar.gz
 endif
-	
-	tar -xzf $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/$(@F) -C $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/
 
-$(BUILD_DIR)/mirror/ubuntu/boot.done: \
-		$(addprefix $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot/,$(ISOLINUX_FILES))
+# debian isolinux files
+$(LOCAL_NETBOOT_TGZ):
+	@mkdir -p $(@D)
+	wget -nv -O $@.tmp $(NETBOOT_URL)
+	mv $@.tmp $@
+	tar -xzf $@ -C $(@D)
+
+$(BUILD_DIR)/mirror/ubuntu/boot.done: $(LOCAL_NETBOOT_TGZ)
 	$(ACTION.TOUCH)
+
+di_initrd_img:=$(LOCAL_NETBOOT_DIR)/ubuntu-installer/amd64/initrd.gz
+di_kernel_modules_dir=$(shell zcat $(di_initrd_img) | cpio --list 'lib/modules/*/kernel')
+UBUNTU_INSTALLER_KERNEL_VERSION=$(strip $(patsubst lib/modules/%/kernel,%,$(di_kernel_modules_dir)))
+
+$(BUILD_DIR)/ubuntu_installer_kernel_version.mk: $(LOCAL_NETBOOT_TGZ)
+	echo 'UBUNTU_INSTALLER_KERNEL_VERSION:=$(UBUNTU_INSTALLER_KERNEL_VERSION)' > $@
