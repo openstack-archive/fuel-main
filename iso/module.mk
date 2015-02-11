93299a3e5f255edbf46bebca3c017aa3d2cfb4d2
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-02-11 16:05:01 +0300

    Add packaging for Docker containers
    In order to safely package containers, yum update -y
    was added to each container.
    Containers are now built and stored in an archive,
    which is packed into a new rpm fuel-docker-images.
    
    blueprint package-docker-images
    
    Change-Id: Ib583e41ec07c9d60eb993216c00ef2f0ba2ca110

--- iso/module.mk
+++ iso/module.mk
@@ -47,7 +47,8 @@ CENTOS_DEP_FILE:=$(call find-files,$(DEPS_DIR_CURRENT)/$(CENTOS_REPO_ART_NAME))
 
 ifdef CENTOS_DEP_FILE
 $(BUILD_DIR)/iso/isoroot-centos.done: \
-		$(BUILD_DIR)/iso/isoroot-dotfiles.done
+		$(BUILD_DIR)/iso/isoroot-dotfiles.done \
+		$(BUILD_DIR)/packages/rpm/fuel-docker-images.done
 	mkdir -p $(ISOROOT)
 	tar xf $(CENTOS_DEP_FILE) -C $(ISOROOT) --xform s:^centos-repo/::
 	createrepo -g $(ISOROOT)/comps.xml \
@@ -114,11 +115,8 @@ $(ISOROOT)/puppet-slave.tgz: $(BUILD_DIR)/puppet/$(PUPPET_ART_NAME)
 # DOCKER
 ########################
 # DOCKER_ART_NAME is defined in /docker/module.mk
-$(ISOROOT)/docker.done: $(BUILD_DIR)/docker/build.done
-	mkdir -p $(ISOROOT)/docker/images
-	cp $(BUILD_DIR)/docker/$(DOCKER_ART_NAME) $(ISOROOT)/docker/images/$(DOCKER_ART_NAME)
-	cp -a $(BUILD_DIR)/docker/sources $(ISOROOT)/docker/sources
-	cp -a $(BUILD_DIR)/docker/utils $(ISOROOT)/docker/utils
+$(ISOROOT)/docker.done: $(BUILD_DIR)/docker/build.done \
+		$(BUILD_DIR)/packages/rpm/fuel-docker-images.done
 	$(ACTION.TOUCH)
 
 ########################
