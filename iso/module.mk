817063fb16e77eca5ed085bc14cb6f4576e97758
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-02-11 20:09:55 +0300

    Add to ISO ability to boot from USB
    Change-Id: I7b2dc2757c28881e02ed3bdb6edb7568af7bcf4d
    Closes-Bug: #1387033

--- iso/module.mk
+++ iso/module.mk
@@ -248,11 +248,13 @@ $(ISO_PATH): $(BUILD_DIR)/iso/isoroot.done
 	sudo sed -r -i -e "s/netmask=[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/netmask=$(MASTER_NETMASK)/" $(BUILD_DIR)/iso/isoroot-mkisofs/isolinux/isolinux.cfg
 	sudo sed -r -i -e "s/gw=[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/gw=$(MASTER_GW)/" $(BUILD_DIR)/iso/isoroot-mkisofs/isolinux/isolinux.cfg
 	sudo sed -r -i -e "s/will_be_substituted_with_PRODUCT_VERSION/$(PRODUCT_VERSION)/" $(BUILD_DIR)/iso/isoroot-mkisofs/isolinux/isolinux.cfg
+	sudo sed -r -i -e "s/will_be_substituted_with_ISO_VOLUME_ID/$(ISO_VOLUME_ID)/g" $(BUILD_DIR)/iso/isoroot-mkisofs/isolinux/isolinux.cfg
 	mkisofs -r -V $(ISO_VOLUME_ID) -p $(ISO_VOLUME_PREP) \
 		-J -T -R -b isolinux/isolinux.bin \
 		-no-emul-boot \
 		-boot-load-size 4 -boot-info-table \
 		-x "lost+found" -o $@ $(BUILD_DIR)/iso/isoroot-mkisofs
+	isohybrid $@
 	implantisomd5 $@
 
 # IMGSIZE is calculated as a sum of iso size plus
