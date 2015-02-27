a78a8594621d44f99154ed49d0c1e83eca1f9697
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-02-27 14:40:04 +0300

    Replace python webserver with apache in container
    Apache can serve repo requests better than the simple
    HTTP server, plus docker can dynamically allocate a random
    host port far more effectively than randomly selecting one.
    
    This patch offers better stability when building Fuel.
    
    Change-Id: I83efa2b5100663b3de117e15c0eea47288842953
    Closes-Bug: #1421610

--- docker/fuel-centos-build/start.sh
+++ docker/fuel-centos-build/start.sh
@@ -5,10 +5,15 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
-#Create loop devices if needed for ami-creator to setup image
+# Install necessary packages
+yum --quiet install -y sudo ami-creator livecd-tools python-imgcreate python-setuptools
+
+sed -i '/requiretty/s/^/#/g' /etc/sudoers
+
+# Create loop devices if needed for ami-creator to setup image
 for loopdev in `seq 1 9`; do
   mknod "/dev/loop${loopdev}" -m0660 b 7 ${loopdev} || :
 done
+(cd /repo && python /usr/local/bin/simple_http_daemon.py 80 /var/run/simple_http_daemon.pid) &
 cd /export
 ami-creator -c /root/fuel-centos.ks -n fuel-centos
-
