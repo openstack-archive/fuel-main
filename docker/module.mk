6e98b27194f44b50ebe13a60e3a634f8db9a313d
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-02-27 14:40:04 +0300

    Replace python webserver with apache in container
    Apache can serve repo requests better than the simple
    HTTP server, plus docker can dynamically allocate a random
    host port far more effectively than randomly selecting one.
    
    This patch offers better stability when building Fuel.
    
    Change-Id: I83efa2b5100663b3de117e15c0eea47288842953
    Closes-Bug: #1421610

--- docker/module.mk
+++ docker/module.mk
@@ -40,20 +40,22 @@ $(BUILD_DIR)/docker/$1.done: \
 		$(BUILD_DIR)/repos/repos.done \
 		$(BUILD_DIR)/packages/rpm/build.done \
 		$(BUILD_DIR)/iso/isoroot-files.done \
-		$(BUILD_DIR)/docker/base-images.done
-	(cd $(LOCAL_MIRROR_CENTOS) && python $(SOURCE_DIR)/utils/simple_http_daemon.py $(RANDOM_PORT) /tmp/simple_http_daemon_$(RANDOM_PORT).pid)
-	mkdir -p "$(BUILD_DIR)/docker/containers"
-	rm -rf $(BUILD_DIR)/docker/$1
-	cp -a $(SOURCE_DIR)/docker/$1 $(BUILD_DIR)/docker/$1
-	sed -e "s/_PORT_/$(RANDOM_PORT)/" -i $(BUILD_DIR)/docker/$1/Dockerfile
-	mkdir -p $(BUILD_DIR)/docker/$1/etc/puppet/modules/
-	mkdir -p $(BUILD_DIR)/docker/$1/etc/fuel
-	cp $(BUILD_DIR)/iso/isoroot/version.yaml $(BUILD_DIR)/docker/$1/etc/fuel/version.yaml
-	sed -e 's/production:.*/production: "docker-build"/' -i $(BUILD_DIR)/docker/$1/etc/fuel/version.yaml
-	cp $(SOURCE_DIR)/docker/docker-astute.yaml $(BUILD_DIR)/docker/$1/etc/fuel/astute.yaml
-	rsync -a $(BUILD_DIR)/repos/fuellib/deployment/puppet/* $(BUILD_DIR)/docker/$1/etc/puppet/modules/
-	sudo docker build --force-rm -t fuel/$1_$(PRODUCT_VERSION) $(BUILD_DIR)/docker/$1
-	kill `cat /tmp/simple_http_daemon_$(RANDOM_PORT).pid`
+		$(BUILD_DIR)/docker/base-images.done \
+		$(BUILD_DIR)/docker/fuel-centos.done
+	REPO_CONT_ID=`sudo docker -D run -d -p 80 -v $(LOCAL_MIRROR_CENTOS):/var/www/html/centos fuel/centos /usr/sbin/apachectl -d -DFOREGROUND` && \
+	RANDOM_PORT=`sudo docker port $$REPO_CONT_ID 80` && \
+	mkdir -p "$(BUILD_DIR)/docker/containers" && \
+	rm -rf $(BUILD_DIR)/docker/$1 && \
+	cp -a $(SOURCE_DIR)/docker/$1 $(BUILD_DIR)/docker/$1 && \
+	sed -e "s/_PORT_/$(RANDOM_PORT)/" -i $(BUILD_DIR)/docker/$1/Dockerfile && \
+	mkdir -p $(BUILD_DIR)/docker/$1/etc/puppet/modules/ && \
+	mkdir -p $(BUILD_DIR)/docker/$1/etc/fuel && \
+	cp $(BUILD_DIR)/iso/isoroot/version.yaml $(BUILD_DIR)/docker/$1/etc/fuel/version.yaml && \
+	sed -e 's/production:.*/production: "docker-build"/' -i $(BUILD_DIR)/docker/$1/etc/fuel/version.yaml && \
+	cp $(SOURCE_DIR)/docker/docker-astute.yaml $(BUILD_DIR)/docker/$1/etc/fuel/astute.yaml && \
+	rsync -a $(BUILD_DIR)/repos/fuellib/deployment/puppet/* $(BUILD_DIR)/docker/$1/etc/puppet/modules/ && \
+	sudo docker build --force-rm -t fuel/$1_$(PRODUCT_VERSION) $(BUILD_DIR)/docker/$1 && \
+	sudo docker -D rm -f $$REPO_CONT_ID
 	$$(ACTION.TOUCH)
 endef
 
@@ -66,14 +68,16 @@ $(BUILD_DIR)/docker/fuel-centos.done: \
 		$(BUILD_DIR)/docker/base-images.done \
 		$(BUILD_DIR)/mirror/centos/build.done \
 		$(BUILD_DIR)/packages/rpm/build.done
-	(cd $(LOCAL_MIRROR_CENTOS) && python $(SOURCE_DIR)/utils/simple_http_daemon.py $(RANDOM_PORT) /tmp/simple_http_daemon_$(RANDOM_PORT).pid)
-	rm -rf $(BUILD_DIR)/docker/fuel-centos-build
-	cp -a $(SOURCE_DIR)/docker/fuel-centos-build $(BUILD_DIR)/docker/fuel-centos-build
-	sed -e "s/_PORT_/$(RANDOM_PORT)/" -i $(BUILD_DIR)/docker/fuel-centos-build/Dockerfile
-	sudo docker build -t fuel/fuel-centos-build $(BUILD_DIR)/docker/fuel-centos-build
-	mkdir -p "$(BUILD_DIR)/docker/centos/output"
-	echo "Generating fuel/centos base image. Refer to $(BUILD_DIR)/docker/fuel-centos-build.log if it fails."
-	sudo docker -D run --rm -a stdout -a stderr -i -t --privileged -v $(LOCAL_MIRROR_CENTOS)/os/x86_64/:/repo:ro -v $(BUILD_DIR)/docker/centos/output:/export fuel/fuel-centos-build 2>&1 > $(BUILD_DIR)/docker/fuel-centos-build.log
+	REPO_CONT_ID=`sudo docker -D run -d -p 80 -v $(LOCAL_MIRROR_CENTOS):/var/www/html/centos centos bash -c "yum install -y httpd;/usr/sbin/apachectl -d -DFOREGROUND"` && \
+	RANDOM_PORT=`sudo docker port $$REPO_CONT_ID 80` && \
+	rm -rf $(BUILD_DIR)/docker/fuel-centos-build && \
+	cp -a $(SOURCE_DIR)/docker/fuel-centos-build $(BUILD_DIR)/docker/fuel-centos-build && \
+	sed -e "s/_PORT_/$$RANDOM_PORT/" -i $(BUILD_DIR)/docker/fuel-centos-build/Dockerfile && \
+	sudo docker build -t fuel/fuel-centos-build $(BUILD_DIR)/docker/fuel-centos-build && \
+	sudo docker rm -f $$REPO_CONT_ID && \
+	mkdir -p "$(BUILD_DIR)/docker/centos/output" && \
+	echo "Generating fuel/centos base image. Refer to $(BUILD_DIR)/docker/fuel-centos-build.log if it fails." && \
+	sudo docker -D run --rm -a stdout -a stderr -i -t --privileged -v $(LOCAL_MIRROR_CENTOS)/os/x86_64/:/repo:ro -v $(BUILD_DIR)/docker/centos/output:/export fuel/fuel-centos-build 2>&1 > $(BUILD_DIR)/docker/fuel-centos-build.log && \
 	sudo $(SOURCE_DIR)/docker/fuel-centos-build/img2docker.sh $(BUILD_DIR)/docker/centos/output/fuel-centos.img fuel/centos
 	$(ACTION.TOUCH)
 
