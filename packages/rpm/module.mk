77c521ff405a416a950198ef38f44b78790b1e1f
Sergey Kulanov <skulanov@mirantis.com>
2015-02-27 16:55:24 +0300

    [WIP] Implement rpm package building in mock env
    1) Flags added to config.mk:
       * USE_MOCK?=0 - mock usage (disabled by default);
       * USE_MOCK_WITH_LOCAL_MIRROR?=0 - build local centos mirror
         otherwise use mirror defined in USE_MIRROR
         (external mirror is used by default);
    
    Examples:
       USE_MOCK=1 USE_MOCK_WITH_LOCAL_MIRROR=1 make python-fuelclient
         * build:
            - local centos mirror;
            - python-fuelclient (using local centos mirror);
    or
       USE_MOCK=1 USE_MIRROR=msk make python-fuelclient
         * build:
            - python-fuelclient (using msk mirror)
    
    Related-bug: #1348599
    Related-bug: #1427239
    
    Change-Id: I1298af3eb8e1b1ad516ba0797f133097807759eb

--- packages/rpm/module.mk
+++ packages/rpm/module.mk
@@ -12,57 +12,6 @@ clean-rpm:
 
 RPM_SOURCES:=$(BUILD_DIR)/packages/rpm/SOURCES
 
-$(BUILD_DIR)/packages/rpm/buildd.tar.gz: SANDBOX_PACKAGES:=ruby rpm-build tar python-setuptools python-pbr
-$(BUILD_DIR)/packages/rpm/buildd.tar.gz: SANDBOX:=$(BUILD_DIR)/packages/rpm/SANDBOX/buildd
-$(BUILD_DIR)/packages/rpm/buildd.tar.gz: export SANDBOX_UP:=$(SANDBOX_UP)
-$(BUILD_DIR)/packages/rpm/buildd.tar.gz: export SANDBOX_DOWN:=$(SANDBOX_DOWN)
-
-$(BUILD_DIR)/packages/rpm/buildd.tar.gz: $(BUILD_DIR)/mirror/centos/repo.done
-	sh -c "$${SANDBOX_UP}"
-	sh -c "$${SANDBOX_DOWN}"
-	sudo tar czf $@.tmp -C $(SANDBOX) .
-	mv $@.tmp $@
-
-
-# Usage:
-# (eval (call build_rpm,package_name))
-define build_rpm
-$(BUILD_DIR)/packages/rpm/repo.done: $(BUILD_DIR)/packages/rpm/$1.done
-$(BUILD_DIR)/packages/rpm/repo.done: $(BUILD_DIR)/packages/rpm/$1-repocleanup.done
-
-# You can use package name as a target, for example: make ruby21-rubygem-astute
-# It will build astute rpm package
-$1: $(BUILD_DIR)/packages/rpm/$1.done
-
-$(BUILD_DIR)/packages/rpm/$1.done: $(BUILD_DIR)/mirror/centos/repo.done
-$(BUILD_DIR)/packages/rpm/$1.done: $(BUILD_DIR)/packages/source_$1.done
-$(BUILD_DIR)/packages/rpm/$1.done: $(BUILD_DIR)/packages/rpm/buildd.tar.gz
-
-
-$(BUILD_DIR)/packages/rpm/$1.done: SANDBOX:=$(BUILD_DIR)/packages/rpm/SANDBOX/$1
-$(BUILD_DIR)/packages/rpm/$1.done: export SANDBOX_UP:=$$(SANDBOX_UP)
-$(BUILD_DIR)/packages/rpm/$1.done: export SANDBOX_DOWN:=$$(SANDBOX_DOWN)
-$(BUILD_DIR)/packages/rpm/$1.done: \
-		$(SOURCE_DIR)/packages/rpm/specs/$1.spec \
-		$(BUILD_DIR)/repos/repos.done
-	mkdir -p $(BUILD_DIR)/packages/rpm/RPMS/x86_64
-	mkdir -p $$(SANDBOX) && \
-	sudo tar xzf $(BUILD_DIR)/packages/rpm/buildd.tar.gz -C $$(SANDBOX) && \
-	sudo mount --bind /proc $$(SANDBOX)/proc && \
-	sudo mount --bind /dev $$(SANDBOX)/dev && \
-	mkdir -p $$(SANDBOX)/tmp/SOURCES && \
-	sudo cp -r $(BUILD_DIR)/packages/sources/$1/* $$(SANDBOX)/tmp/SOURCES && \
-	sudo cp $(SOURCE_DIR)/packages/rpm/specs/$1.spec $$(SANDBOX)/tmp && \
-	sudo chroot $$(SANDBOX) rpmbuild --nodeps -vv --define "_topdir /tmp" -ba /tmp/$1.spec
-	cp $$(SANDBOX)/tmp/RPMS/*/$1-*.rpm $(BUILD_DIR)/packages/rpm/RPMS/x86_64
-	sudo sh -c "$$$${SANDBOX_DOWN}"
-	$$(ACTION.TOUCH)
-
-$(BUILD_DIR)/packages/rpm/$1-repocleanup.done: $(BUILD_DIR)/mirror/centos/repo.done
-	find $(LOCAL_MIRROR_CENTOS_OS_BASEURL)/Packages -regex '.*/$1-[^-]+-[^-]+' -delete
-	$$(ACTION.TOUCH)
-endef
-
 fuel_rpm_packages:=\
 fencing-agent \
 fuel-agent \
@@ -80,14 +29,13 @@ nailgun-redhat-license \
 python-fuelclient \
 ruby21-rubygem-astute
 
+ifeq ($(USE_MOCK),1)
+include $(SOURCE_DIR)/packages/rpm/module_mock.mk
+$(eval $(foreach pkg,$(fuel_rpm_packages),$(call build_rpm_in_mock,$(pkg))$(NEWLINE)))
+else
+include $(SOURCE_DIR)/packages/rpm/module_sandbox.mk
 $(eval $(foreach pkg,$(fuel_rpm_packages),$(call build_rpm,$(pkg))$(NEWLINE)))
-
-
-$(BUILD_DIR)/packages/rpm/repo.done:
-	find $(BUILD_DIR)/packages/rpm/RPMS -name '*.rpm' -exec cp -u {} $(LOCAL_MIRROR_CENTOS_OS_BASEURL)/Packages \;
-	createrepo -g $(LOCAL_MIRROR_CENTOS_OS_BASEURL)/comps.xml \
-		-o $(LOCAL_MIRROR_CENTOS_OS_BASEURL) $(LOCAL_MIRROR_CENTOS_OS_BASEURL)
-	$(ACTION.TOUCH)
+endif
 
 $(BUILD_DIR)/packages/rpm/fuel-docker-images.done: SANDBOX:=$(BUILD_DIR)/packages/rpm/SANDBOX/fuel-docker-images
 $(BUILD_DIR)/packages/rpm/fuel-docker-images.done: export SANDBOX_DOWN:=$(SANDBOX_DOWN)
