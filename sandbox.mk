e442134799b19633d68ced3f81fcc300076825b8
Vladimir Kuklin <vkuklin@mirantis.com>
2015-03-02 14:56:31 +0300

    Refactor how we are doing packaging
    This commit is a squashed version of commits that
    implement packaging of fuel components. In order to
    make our packaging process better I had to introduce
    some new macros and do additional cleanup.
    
    The main paradigm that we are following with this commit is
    the following:
    
    1 GIT REPO -> 1 PACKAGING SPEC -> N PACKAGES
    
    Though it would be ideal to have only one package per git repo,
    but this is left for further releases
    
    This change is very essential to make updates process easier
    as we provide only one type of artifacts that a user can fetch -
    distro-specific package
    
    This particular commit does not introduce new DEB packages
    (such as packages of OCF scripts and other binaries that
    we currently put onto the slave nodes with puppet instead of
    installing them as packages) which will be introduced as
    a part of whole packaging initiative a little bit later
    
    1) Add prepare_git_source macro
    This change is important to always
    checkout the code from git working
    or we can end up with garbage getting
    into package source code and add
    version.txt file to source archives
    
    2) Remove nailgun-redhat-license package
    This package is obsolete - we do not need
    it.
    
    3) Package fuel-library into RPM package
    
    4) Delete old packages and misc stuff
    
    This change is a little cleanup of python-tasklib
    and other leftovers
    
    5) Change RPM sandbox configuration
    
    Change RPM sandbox configuration to use upstream mirrors
    to be able to install nailgun build dependencies
    
    6) Alter Nailgun spec
    
    a) Change nailgun spec to use NPM during package
    build
    b) Switch nailgun to prepare_git_source
    c) Package all packages of fuel-web repo using
    only nailgun.spec
    
    7) Alter RPM sandbox preparation to install build-deps
    
    8) Package astute and mcagents using astute.spec
    
    9) Include all built RPMs
    
    Include all RPMS that were built inside the sandbox
    
    10) Package fuel-ostf with git-archive
    
    11) Build python-fuelclient using git_archive macro
    
    12) Move specs to particular git repositories
    
    13) Rename fuel-library and ostf to make macros easier
    14)Adjust build scripts to fetch specs from particular
    fuel repositories
    
    FUEL DEB-packaging related code
    
    15) Restore part of make system code for DEB packages building
    
    16) Nailgun debian spec files for all nailgun-based packages
    
    17) Clean up SANDBOX for Ubuntu generation
    
    18) Clean up some package building scripts
    
    19) Put PACKAGE_VERSION variable into the top config.mk
    
    20) Add dockerctl and change fuel-library tar.tg path
    
    21) Move deb rules to particula repos
    
    22) Allow to rebuild deb repo with reprepro on-the-fly
    
    24) Fix repocleanup stanza to parse source packages metadata
    
    25) Debmirror whole repos in order not to break reprepro
    
    26) Make docker image build depend on late RPM packages
    or it may lead to race conditions
    
    27) Clean up old deb packages with one shot instead
    of parallel run
    
    Change-Id: I15ae4d0abe51c72b28793e1a1ef30fec0f668b73
    Blueprint: package-fuel-components

--- sandbox.mk
+++ sandbox.mk
@@ -4,13 +4,32 @@ name=Mirantis mirror
 baseurl=file://$(LOCAL_MIRROR_CENTOS_OS_BASEURL)
 gpgcheck=0
 enabled=1
+priority=1
 endef
-
+define yum_upstream_repo
+[upstream]
+name=Upstream mirror
+baseurl=http://mirrors-local-msk.msk.mirantis.net/centos-6.1/6.5/os/x86_64/
+gpgcheck=0
+priority=2
+[upstream-updates]
+name=Upstream mirror
+baseurl=http://mirrors-local-msk.msk.mirantis.net/centos-6.1/6.5/updates/x86_64/
+gpgcheck=0
+priority=2
+endef
+define yum_epel_repo
+[epel]
+name=epel mirror
+baseurl=http://mirror.yandex.ru/epel/6/x86_64/
+gpgcheck=0
+priority=3
+endef
 define sandbox_yum_conf
 [main]
 cachedir=$(SANDBOX)/cache
 keepcache=0
-debuglevel=6
+debuglevel=2
 logfile=$(SANDBOX)/yum.log
 exclude=*.i686.rpm
 exactarch=1
@@ -22,7 +41,7 @@ pluginconfpath=$(SANDBOX)/etc/yum/pluginconf.d
 reposdir=$(SANDBOX)/etc/yum.repos.d
 endef
 
-SANDBOX_PACKAGES:=bash
+SANDBOX_PACKAGES:=bash yum
 
 define SANDBOX_UP
 echo "Starting SANDBOX up"
@@ -32,6 +51,8 @@ $(sandbox_yum_conf)
 EOF
 cp /etc/resolv.conf $(SANDBOX)/etc/resolv.conf
 cat > $(SANDBOX)/etc/yum.repos.d/base.repo <<EOF
+$(yum_upstream_repo)
+$(yum_epel_repo)
 $(yum_local_repo)
 EOF
 sudo rpm -i --root=$(SANDBOX) `find $(LOCAL_MIRROR_CENTOS_OS_BASEURL) -name "centos-release*rpm" | head -1` || \
@@ -48,5 +69,62 @@ endef
 define SANDBOX_DOWN
 sudo umount $(SANDBOX)/proc || true
 sudo umount $(SANDBOX)/dev || true
+endef
+
+
+define apt_sources_list
+deb http://$(MIRROR_UBUNTU)/pkgs/ubuntu $(UBUNTU_RELEASE) main universe multiverse restricted
+deb http://$(MIRROR_UBUNTU)/pkgs/ubuntu $(UBUNTU_RELEASE)-updates main universe multiverse restricted
+deb http://$(MIRROR_UBUNTU)/pkgs/ubuntu $(UBUNTU_RELEASE)-security main universe multiverse restricted
+deb $(MIRROR_UBUNTU_METHOD)://$(MIRROR_FUEL_UBUNTU)/fwm/$(PRODUCT_VERSION)/ubuntu $(PRODUCT_NAME)$(PRODUCT_VERSION) main restricted
+deb $(MIRROR_UBUNTU_METHOD)://$(MIRROR_FUEL_UBUNTU)/fwm/$(PRODUCT_VERSION)/ubuntu $(PRODUCT_NAME)$(PRODUCT_VERSION)-security main restricted
+deb $(MIRROR_UBUNTU_METHOD)://$(MIRROR_FUEL_UBUNTU)/fwm/$(PRODUCT_VERSION)/ubuntu $(PRODUCT_NAME)$(PRODUCT_VERSION)-proposed main restricted
+deb $(MIRROR_UBUNTU_METHOD)://$(MIRROR_FUEL_UBUNTU)/fwm/$(PRODUCT_VERSION)/ubuntu $(PRODUCT_NAME)$(PRODUCT_VERSION)-updates main restricted
+deb $(MIRROR_UBUNTU_METHOD)://$(MIRROR_FUEL_UBUNTU)/fwm/$(PRODUCT_VERSION)/ubuntu $(PRODUCT_NAME)$(PRODUCT_VERSION)-holdback main restricted
+$(if $(EXTRA_DEB_REPOS),$(subst |,$(newline)deb ,deb $(EXTRA_DEB_REPOS)))
+endef
+
+
+
+define SANDBOX_UBUNTU_UP
+echo "SANDBOX_UBUNTU_UP: start"
+mkdir -p $(SANDBOX_UBUNTU)
+mkdir -p $(SANDBOX_UBUNTU)/usr/sbin
+cat > $(SANDBOX_UBUNTU)/usr/sbin/policy-rc.d <<EOF
+#!/bin/sh
+# suppress services start in the staging chroots
+exit 101
+EOF
+chmod 755 $(SANDBOX_UBUNTU)/usr/sbin/policy-rc.d
+mkdir -p $(SANDBOX_UBUNTU)/etc/init.d
+touch $(SANDBOX_UBUNTU)/etc/init.d/.legacy-bootordering
+echo "Running debootstrap"
+sudo debootstrap --no-check-gpg --arch=$(UBUNTU_ARCH) $(UBUNTU_RELEASE) $(SANDBOX_UBUNTU) http://$(MIRROR_UBUNTU)/pkgs/ubuntu/
+sudo cp /etc/resolv.conf $(SANDBOX_UBUNTU)/etc/resolv.conf
+echo "Generating utf8 locale"
+sudo chroot $(SANDBOX_UBUNTU) /bin/sh -c 'locale-gen en_US.UTF-8; dpkg-reconfigure locales'
+echo "Preparing directory for chroot local mirror"
+sudo mkdir -p $(SANDBOX_UBUNTU)/etc/apt/preferences.d/
+sudo cp $(SOURCE_DIR)/mirror/ubuntu/files/preferences $(SANDBOX_UBUNTU)/etc/apt/preferences.d/
+echo "Configuring apt sources.list: deb file:///tmp/apt $(UBUNTU_RELEASE) main"
+cat > $(BUILD_DIR)/mirror/ubuntu/sources.list << EOF
+$(apt_sources_list)
+EOF
+sudo cp $(BUILD_DIR)/mirror/ubuntu/sources.list $(SANDBOX_UBUNTU)/etc/apt/
+echo "Allowing using unsigned repos"
+sudo mkdir -p /etc/apt/apt.conf.d/
+echo "APT::Get::AllowUnauthenticated 1;" | sudo tee $(SANDBOX_UBUNTU)/etc/apt/apt.conf.d/02mirantis-unauthenticated
+echo "Updating apt package database"
+sudo chroot $(SANDBOX_UBUNTU) bash -c "(mkdir -p '$${TEMP}'; mkdir -p /tmp/user/0)" 
+sudo chroot $(SANDBOX_UBUNTU) apt-get update
+echo "Installing additional packages: $(SANDBOX_DEB_PKGS)"
+sudo chroot $(SANDBOX_UBUNTU) apt-get dist-upgrade --yes
+test -n "$(SANDBOX_DEB_PKGS)" && sudo chroot $(SANDBOX_UBUNTU) apt-get install --yes $(SANDBOX_DEB_PKGS)
+echo "SANDBOX_UBUNTU_UP: done"
+endef
+	
+define SANDBOX_UBUNTU_DOWN
+	if mountpoint -q $(SANDBOX_UBUNTU)/proc; then sudo umount $(SANDBOX_UBUNTU)/proc; fi
+	sudo umount $(SANDBOX_UBUNTU)/tmp/apt || true
 endef
 
