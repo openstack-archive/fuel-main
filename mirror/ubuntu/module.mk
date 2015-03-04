67b1f8f7d2b6cda69a729e42fe5ea45808fd029b
Vitaly Parakhin <vparakhin@mirantis.com>
2015-03-04 15:55:07 +0200

    Separate MOS Ubuntu from upstream
    1) Change mirroring procedure for Ubuntu:
    
       - USE_MIRROR=none: rsync the whole mirror from build server
       - USE_MIRROR=<location>: mirror only required MOS distro
    
    New mirror scheme assumes that MOS Ubuntu mirror is located
    by the http://<mos_mirror_host>/mos/ubuntu/ address
    
    2) Remove procedure of building Fuel deb packages
    3) Remove building of deb-based IBP images
    4) Remove deb-based chroots
    
    Implements: separate-mos-from-linux
    Change-Id: Icd10aea6ebddb27976e88ceb930be9afac0241ad

--- mirror/ubuntu/module.mk
+++ mirror/ubuntu/module.mk
@@ -1,24 +1,11 @@
 .PHONY: mirror-ubuntu
 # This module downloads ubuntu installation images.
-include $(SOURCE_DIR)/mirror/ubuntu/boot.mk
+#include $(SOURCE_DIR)/mirror/ubuntu/boot.mk
+
+include $(SOURCE_DIR)/mirror/ubuntu/mirror.mk
 
 mirror-ubuntu: $(BUILD_DIR)/mirror/ubuntu/build.done
 
-$(BUILD_DIR)/mirror/ubuntu/repo.done: $(BUILD_DIR)/ubuntu_installer_kernel_version.mk
-	$(MAKE) -f $(SOURCE_DIR)/mirror/ubuntu/repo.mk \
-		MIRROR_UBUNTU=$(MIRROR_UBUNTU) \
-		MIRROR_FUEL_UBUNTU=$(MIRROR_FUEL_UBUNTU) \
-		EXTRA_DEB_REPOS='$(EXTRA_DEB_REPOS)' \
-		UBUNTU_RELEASE=$(UBUNTU_RELEASE) \
-		UBUNTU_RELEASE_NUMBER=$(UBUNTU_RELEASE_NUMBER) \
-		UBUNTU_ARCH=$(UBUNTU_ARCH) \
-		UBUNTU_KERNEL_FLAVOR=$(UBUNTU_KERNEL_FLAVOR) \
-		LOCAL_MIRROR_UBUNTU=$(LOCAL_MIRROR_UBUNTU) \
-		SOURCE_DIR=$(SOURCE_DIR) \
-		BUILD_DIR=$(BUILD_DIR)
-	$(ACTION.TOUCH)
-
 $(BUILD_DIR)/mirror/ubuntu/build.done: \
-		$(BUILD_DIR)/mirror/ubuntu/boot.done \
-		$(BUILD_DIR)/mirror/ubuntu/repo.done
+		$(BUILD_DIR)/mirror/ubuntu/mirror.done
 	$(ACTION.TOUCH)
