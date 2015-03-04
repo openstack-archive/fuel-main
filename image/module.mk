e0af0e6881e448b777152a0c285036688e471cb4
Vitaly Parakhin <vparakhin@mirantis.com>
2015-03-04 15:55:07 +0200

    Separate MOS Ubuntu from upstream
    1) Change mirroring procedure for Ubuntu:
    
       - USE_MIRROR=none: rsync the whole mirror from build server
       - USE_MIRROR=<location>: mirror only required MOS distro
    
    New mirror scheme assumes that MOS Ubuntu mirror is located
    by the http://<mos_mirror_host>/mos/ubuntu/ address
    
    2) Remove procedure of building Fuel deb packages
    3) Remove building of deb-based IBP images
    4) Remove deb-based chroots
    5) Adjust upgrade and create diff mirrors modules
    
    Implements: separate-mos-from-linux
    Change-Id: Icd10aea6ebddb27976e88ceb930be9afac0241ad

--- image/module.mk
+++ image/module.mk
@@ -1,23 +1,17 @@
 .PHONY: image
 
 include $(SOURCE_DIR)/image/centos/module.mk
-include $(SOURCE_DIR)/image/ubuntu/module.mk
 
 ########################
 # TARGET IMAGE ARTIFACT
 ########################
 
 image: $(BUILD_DIR)/image/build.done
-image-ubuntu: $(BUILD_DIR)/image/ubuntu/build.done
 image-centos: $(BUILD_DIR)/image/centos/build.done
-
-$(BUILD_DIR)/image/ubuntu/build.done: $(ARTS_DIR)/$(TARGET_UBUNTU_IMG_ART_NAME)
-	$(ACTION.TOUCH)
 
 $(BUILD_DIR)/image/centos/build.done: $(ARTS_DIR)/$(TARGET_CENTOS_IMG_ART_NAME)
 	$(ACTION.TOUCH)
 
 $(BUILD_DIR)/image/build.done: \
-	$(BUILD_DIR)/image/ubuntu/build.done \
 	$(BUILD_DIR)/image/centos/build.done
 	$(ACTION.TOUCH)
