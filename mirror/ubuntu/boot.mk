6e3a12806e928ff3cc2589a6766ba8e9a0e23ae0
Albert <asyriy@mirantis.com>
2015-03-05 18:26:46 +0200

    LP Bug fix 1428650. Update the kernel version using during installing ubuntu
    Change-Id: I114b6369cf1df19801c705322c70fa7f6168fcf4

--- mirror/ubuntu/boot.mk
+++ mirror/ubuntu/boot.mk
@@ -2,10 +2,10 @@ ISOLINUX_FILES:=netboot.tar.gz
 
 LOCAL_NETBOOT_DIR:=$(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/installer-amd64/current/images/netboot
 LOCAL_NETBOOT_TGZ:=$(LOCAL_NETBOOT_DIR)/$(ISOLINUX_FILES)
-NETBOOT_URL:=$(MIRROR_UBUNTU)/installer-amd64/current/images/netboot/netboot.tar.gz
-ifeq ($(USE_MIRROR),none)
+#NETBOOT_URL:=$(MIRROR_UBUNTU)/installer-amd64/current/images/netboot/netboot.tar.gz
+#ifeq ($(USE_MIRROR),none)
 	NETBOOT_URL:=$(MIRROR_UBUNTU)/ubuntu/dists/$(UBUNTU_RELEASE)-updates/main/installer-amd64/current/images/$(UBUNTU_NETBOOT_FLAVOR)/netboot.tar.gz
-endif
+#endif
 
 # debian isolinux files
 $(LOCAL_NETBOOT_TGZ):
