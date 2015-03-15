24dc249e5c8dbbe8b3606a25da5ca5a4bd6a2425
Sergey Kulanov <skulanov@mirantis.com>
2015-03-15 20:22:13 +0200

    [WIP] Allow to build only packages source
    * Use "make sources" to get sources for building packages in docker
    * Use "make clean-sources" to clean repos and sources
    
    ToDO:
      need to refactor sources target
    
    Change-Id: Ieef6728535f435bb7190c4cf0f90d19903a7ef8b

--- packages/rpm/module.mk
+++ packages/rpm/module.mk
@@ -1,6 +1,10 @@
-.PHONY: clean clean-rpm
+.PHONY: clean clean-rpm sources clean-sources
 
 clean: clean-rpm
+
+clean-sources:
+	rm -rf $(BUILD_DIR)/packages
+	rm -rf $(BUILD_DIR)/repos
 
 clean-rpm:
 	-mount | grep '$(BUILD_DIR)/packages/rpm/SANDBOX' | while read entry; do \
@@ -113,3 +117,20 @@ $(BUILD_DIR)/packages/rpm/fuel-docker-images.done: \
 
 $(BUILD_DIR)/packages/rpm/build.done: $(BUILD_DIR)/packages/rpm/repo.done
 	$(ACTION.TOUCH)
+
+sources: $(BUILD_DIR)/packages/source_fencing-agent.done \
+	$(BUILD_DIR)/packages/source_fuel-agent.done \
+	$(BUILD_DIR)/packages/source_fuel-image.done \
+	$(BUILD_DIR)/packages/source_fuel-provisioning-scripts.done \
+	$(BUILD_DIR)/packages/source_fuelmenu.done \
+	$(BUILD_DIR)/packages/source_nailgun-mcagents.done \
+	$(BUILD_DIR)/packages/source_ruby21-nailgun-mcagents.done \
+	$(BUILD_DIR)/packages/source_nailgun-net-check.done \
+	$(BUILD_DIR)/packages/source_python-tasklib.done \
+	$(BUILD_DIR)/packages/source_nailgun.done \
+	$(BUILD_DIR)/packages/source_shotgun.done \
+	$(BUILD_DIR)/packages/source_fuel-ostf.done \
+	$(BUILD_DIR)/packages/source_nailgun-agent.done \
+	$(BUILD_DIR)/packages/source_nailgun-redhat-license.done \
+	$(BUILD_DIR)/packages/source_python-fuelclient.done \
+	$(BUILD_DIR)/packages/source_ruby21-rubygem-astute.done
