751ee89da68a53b810724f209a63289276d5bf8d
Sergey Kulanov <skulanov@mirantis.com>
2015-03-26 15:08:05 +0200

    Implement fuel packages building on CI slave
    Input data:
    -----------
    Input data for building packages are (make sources):
     - prepared source code;
     - specs;
    
    Docker approach:
    ~~~~~~~~~~~~~~~~
    1) Prepare two docker images for building fuel rpm and deb packages
    Code implemetation is based on SANDBOX approach from make system:
    * create_rpmbuild_env.sh - create fuel/rpmbuild_env docker image
      - build_rpm_in_docker.sh - runs building rpm packages
        inside rpmbuild_env docker image
    * create_debbuild_env.sh - create fuel/debbuild_env docker image
      - build_deb_in_docker.sh - runs building debian packages
        inside debbuild_env docker image
    * build_deb.sh and build_rpm.sh - building packages examples on
      how to invoke docker
    
    Separate Makefile defined, which can help to speed up building
    packages in parallel:
    Examples:
      * make help           - to get list of variables
      * make packages -j8   - build all rpm and deb packages
    
    Pure mock approach:
    ~~~~~~~~~~~~~~~~~~~
    Build RPM packages using pure mock ( need pigz, lzop pkgs)
    Mock configuration stored in mock/configs
    Examples:
      * make astute         - build astute rpm package(s)
      * make packages -j5   - build rpm package in parallel
    
    Change-Id: Ic97dbe5bbef3bc79eadb91cb221bcae5734f4ce8
    Related-bug: #1427239
    Related-bug: #1433653
    Related-blueprint:  package-fuel-components

---
+++ utils/build_fuel_packages/docker/build_rpm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This is an example how can we use docker rpmbuild_env to build RPM packages
+# in Fuel project
+
+set -ex
+
+PACKAGES_TO_BUILD="astute fuel-library6.1 fuel-ostf nailgun python-fuelclient"
+SOURCE_PATH=${HOME}/test/fuel-main/build/packages/sources
+SPEC_FILE_PATH=${HOME}/test/fuel-main/build/repos
+RESULT_DIR=/tmp/packages
+
+rm -rf ${RESULT_DIR}
+mkdir -p ${RESULT_DIR}
+
+for pckgs in ${PACKAGES_TO_BUILD}; do
+docker run --privileged --rm -v ${SOURCE_PATH}/${pckgs}:/opt/sandbox/SOURCES \
+           -v ${SPEC_FILE_PATH}/${pckgs}/specs/${pckgs}.spec:/opt/sandbox/${pckgs}.spec \
+           -v ${RESULT_DIR}:/opt/sandbox/RPMS \
+           -u ${UID} \
+           fuel/rpmbuild_env /bin/bash /opt/sandbox/build_rpm_in_docker.sh
+done
