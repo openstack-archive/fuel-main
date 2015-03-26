88b67c18496e25899aa410e13517790b890a3f92
Sergey Kulanov <skulanov@mirantis.com>
2015-03-26 15:08:05 +0200

    Implement fuel packages building on CI slave
    Input data:
    -----------
    Input data for building packages are (make sources):
     - prepared source code;
     - specs;
    
    Docker approach:
    ~~~~~~~~~~~~~~~~
    1) Prepare two docker images for building fuel rpm and deb packages
    Code implemetation is based on SANDBOX approach from make system:
    * create_rpmbuild_env.sh - create fuel/rpmbuild_env docker image
      - build_rpm_in_docker.sh - runs building rpm packages
        inside rpmbuild_env docker image
    * create_debbuild_env.sh - create fuel/debbuild_env docker image
      - build_deb_in_docker.sh - runs building debian packages
        inside debbuild_env docker image
    * build_deb.sh and build_rpm.sh - building packages examples on
      how to invoke docker
    
    Separate Makefile defined, which can help to speed up building
    packages in parallel:
    Examples:
      * make help           - to get list of variables
      * make packages -j8   - build all rpm and deb packages
    
    Pure mock approach:
    ~~~~~~~~~~~~~~~~~~~
    Build RPM packages using pure mock ( need pigz, lzop pkgs)
    Mock configuration stored in mock/configs
    Examples:
      * make astute         - build astute rpm package(s)
      * make packages -j5   - build rpm package in parallel
    
    Change-Id: Ic97dbe5bbef3bc79eadb91cb221bcae5734f4ce8
    Related-bug: #1427239
    Related-bug: #1433653
    Related-blueprint:  package-fuel-components

---
+++ utils/build_fuel_packages/docker/build_deb_in_docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script runs inside docker env, please check build_deb.sh file
+# for required input data
+
+set -ex
+
+mkdir -p /tmp/package
+
+# since we have specs in repo we can prepare for build faster
+# just unpack prepared sources
+tar -xzf /opt/sandbox/SOURCES/*gz -C /tmp/package
+
+sudo apt-get update
+
+dpkg-checkbuilddeps /tmp/package/debian/control 2>&1 | \
+  sed 's/^dpkg-checkbuilddeps: Unmet build dependencies: //g' | \
+  sed 's/([^()]*)//g;s/|//g' | tee /tmp/package.installdeps
+
+/bin/sh -c "cat /tmp/package.installdeps | xargs --no-run-if-empty sudo apt-get -y install"
+/bin/sh -c "cd /tmp/package ; DEB_BUILD_OPTIONS=nocheck debuild -us -uc -b -d"
+cp -v /tmp/*.deb /opt/sandbox/DEB
