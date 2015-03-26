831ecd9f7e007a49bb2c3613994ef95b35ebe49e
Sergey Kulanov <skulanov@mirantis.com>
2015-03-26 15:08:05 +0200

    Implement fuel packages building in docker environment
    Prepare two docker images for building fuel rpm and deb packages
    Code implemetation is based on SANDBOX approach from make system:
    * create_rpmbuild_env.sh - create fuel/rpmbuild_env docker image
    * create_debbuild_env.sh - create fuel/debbuild_env docker image
      - build_deb_in_docker.sh - runs building debian packages
        inside debbuild_env docker image
    * build_deb.sh and build_rpm.sh - building packages examples
    
    Input data for building packages are:
     - preapred source code;
     - specs;
    
    Change-Id: Ic97dbe5bbef3bc79eadb91cb221bcae5734f4ce8
    Related-bug: #1427239
    Related-bug: #1433653
    Related-blueprint:  package-fuel-components

---
+++ utils/build_fuel_packages/create_debbuild_env.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+set -ex
+
+# This script will create docker image, that can be used
+# for building deb packages
+
+# Some part of the code was adopted from
+# https://github.com/docker/docker/blob/master/contrib/mkimage.sh
+
+# docker image name
+TAG=fuel/debbuild_env
+# packages
+SANDBOX_PACKAGES="wget bzip2 apt-utils build-essential python-setuptools devscripts debhelper fakeroot"
+
+# ubuntu mirror
+MIRROR=${UBUNTU_MIRROR:-http://mirror.fuel-infra.org/fwm/6.1/ubuntu/}
+UBUNTU_RELEASE=trusty
+
+# path where we create our chroot and build docker
+TMPDIR=/var/tmp/docker_ubuntu
+
+# we need to add user who is going to build packages, usually jenkins
+GID=$(id -g)
+nGID=$(id -gn)
+nUID=$(id -un)
+
+BUILD_USER=${JENKINS_USER:-$nGID}
+BUILD_GROUP=${JENKINS_GROUP:-$nUID}
+BUILD_UID=${JENKINS_UID:-$GID}
+BUILD_GID=${JENKINS_GID:-$UID}
+
+mkdir -p "${TMPDIR}"
+
+# let's make all stuff on tmpfs
+sudo mount -n -t tmpfs -o size=2048M docker_chroot "${TMPDIR}"
+
+# creating chroot env
+dir="$(mktemp -d ${TMPDIR}/docker-image.XXXXXXXXXX)"
+
+rootfsDir="${dir}/rootfs"
+sudo mkdir -p "${rootfsDir}"
+
+sudo mkdir -p ${rootfsDir}/usr/sbin
+sudo bash -c "cat > ${rootfsDir}/usr/sbin/policy-rc.d" <<EOF
+#!/bin/sh
+# suppress services start in the staging chroots
+exit 101
+EOF
+sudo chmod 755 ${rootfsDir}/usr/sbin/policy-rc.d
+sudo mkdir -p ${rootfsDir}/etc/init.d
+sudo touch ${rootfsDir}/etc/init.d/.legacy-bootordering
+
+echo "Running debootstrap"
+sudo debootstrap --no-check-gpg --arch=amd64 ${UBUNTU_RELEASE} ${rootfsDir} ${MIRROR}
+sudo cp /etc/resolv.conf ${rootfsDir}/etc/resolv.conf
+echo "Generating utf8 locale"
+sudo chroot ${rootfsDir} /bin/sh -c 'locale-gen en_US.UTF-8; dpkg-reconfigure locales'
+
+echo "Allowing using unsigned repos"
+echo "APT::Get::AllowUnauthenticated 1;" | sudo tee ${rootfsDir}/etc/apt/apt.conf.d/02mirantis-unauthenticated
+echo "Updating apt package database"
+sudo chroot ${rootfsDir} apt-get update
+echo "Installing additional packages: ${SANDBOX_PACKAGES})"
+test -n "${SANDBOX_PACKAGES}" && sudo chroot ${rootfsDir} apt-get install --yes ${SANDBOX_PACKAGES}
+echo "chroot: done"
+
+# put building script in docker
+sudo mkdir ${rootfsDir}/opt/sandbox
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+sudo cp ${DIR}/build_deb_in_docker.sh ${rootfsDir}/opt/sandbox
+
+# let's pack rootfs
+tarFile="${dir}/rootfs.tar.gz"
+sudo touch "${tarFile}"
+
+sudo tar --numeric-owner -caf "${tarFile}" -C "${rootfsDir}" --transform='s,^./,,' .
+
+# prepare for building docker
+cat > "${dir}/Dockerfile" <<'EOF'
+FROM scratch
+ADD rootfs.tar.gz /
+EOF
+
+# prepare for building docker
+cat > "${dir}/Dockerfile" <<EOF
+FROM scratch
+ADD rootfs.tar.gz /
+
+RUN groupadd --gid ${BUILD_GID} ${BUILD_GROUP} && \
+    useradd --system --uid ${BUILD_UID} --gid ${BUILD_GID} --home /opt/sandbox --shell /bin/bash ${BUILD_USER} && \
+    mkdir -p /opt/sandbox && \
+    chown -R ${BUILD_UID}:${BUILD_GID} /opt/sandbox
+EOF
+
+# cleaning rootfs
+sudo rm -rf "$rootfsDir"
+
+# creating docker image
+docker build -t "${TAG}" "${dir}"
+
+# cleaning all
+rm -rf "${dir}"
+sudo umount "${TMPDIR}"
+
+# saving image
+# docker 1.3.2 has issues with xz, so we use gz
+if [ "${SAVE_DOCKER_IMAGE}" == "yes" ]; then
+  docker save "${TAG}" | gzip > ${ARTS_DIR:-/var/tmp}/fuel-debbuild_env.tar.gz
+fi
