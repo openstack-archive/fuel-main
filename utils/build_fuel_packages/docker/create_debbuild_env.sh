14f0c87b433634940ae2da814b6eff58dc1db571
Sergey Kulanov <skulanov@mirantis.com>
2015-03-26 15:08:05 +0200

    Implement fuel packages building on CI slave
    Input data:
    -----------
    Input data for building packages are (make sources):
     - prepared source code;
     - specs;
    
    Docker approach:
    ~~~~~~~~~~~~~~~~
    1) Prepare two docker images for building fuel rpm and deb packages
    Code implemetation is based on SANDBOX approach from make system:
    * create_rpmbuild_env.sh - create fuel/rpmbuild_env docker image
      - build_rpm_in_docker.sh - runs building rpm packages
        inside rpmbuild_env docker image
    * create_debbuild_env.sh - create fuel/debbuild_env docker image
      - build_deb_in_docker.sh - runs building debian packages
        inside debbuild_env docker image
    * build_deb.sh and build_rpm.sh - building packages examples on
      how to invoke docker
    
    Separate Makefile defined, which can help to speed up building
    packages in parallel:
    Examples:
      * make help           - to get list of variables
      * make packages -j8   - build all rpm and deb packages
    
    Pure mock approach:
    ~~~~~~~~~~~~~~~~~~~
    Build RPM packages using pure mock ( need pigz, lzop pkgs)
    Mock configuration stored in mock/configs
    Examples:
      * make astute         - build astute rpm package(s)
      * make packages -j5   - build rpm package in parallel
    
    Change-Id: Ic97dbe5bbef3bc79eadb91cb221bcae5734f4ce8
    Related-bug: #1427239
    Related-bug: #1433653
    Related-blueprint:  package-fuel-components

---
+++ utils/build_fuel_packages/docker/create_debbuild_env.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+set -ex
+
+# This script will create docker image, that can be used
+# for building deb packages
+
+# Some part of the code was adopted from
+# https://github.com/docker/docker/blob/master/contrib/mkimage.sh
+
+if [ -z ${PRODUCT_VERSION} ]; then
+  echo "PRODUCT_VERSION should be defined directly"
+  exit -1
+fi
+
+# product mirror
+PRODUCT_NAME=mos
+MIRROR_UBUNTU_ROOT="/${PRODUCT_NAME}/ubuntu/"
+
+# docker image name
+TAG=fuel-${PRODUCT_VERSION}/debbuild_env
+# packages
+SANDBOX_PACKAGES="apt-utils build-essential bzip2 devscripts debhelper fakeroot python-setuptools wget"
+
+
+# ubuntu mirror
+MIRROR=${UBUNTU_MIRROR:-http://mirror.fuel-infra.org}
+UBUNTU_RELEASE=trusty
+UBUNTU_MIRROR_SUFFIX=/pkgs/ubuntu
+
+# path where we create our chroot and build docker
+TMPDIR=/var/tmp/docker_ubuntu
+
+# we need to add user who is going to build packages, usually jenkins
+GID=$(id -g)
+nGID=$(id -gn)
+nUID=$(id -un)
+
+BUILD_USER=${JENKINS_USER:-$nGID}
+BUILD_GROUP=${JENKINS_GROUP:-$nUID}
+BUILD_UID=${JENKINS_UID:-$GID}
+BUILD_GID=${JENKINS_GID:-$UID}
+
+mkdir -p "${TMPDIR}"
+
+# let's make all stuff on tmpfs
+sudo mount -n -t tmpfs -o size=2048M docker_chroot "${TMPDIR}"
+
+# creating chroot env
+dir="$(mktemp -d ${TMPDIR}/docker-image.XXXXXXXXXX)"
+
+rootfsDir="${dir}/rootfs"
+sudo mkdir -p "${rootfsDir}"
+
+sudo mkdir -p ${rootfsDir}/usr/sbin
+sudo bash -c "cat > ${rootfsDir}/usr/sbin/policy-rc.d" <<EOF
+#!/bin/sh
+# suppress services start in the staging chroots
+exit 101
+EOF
+sudo chmod 755 ${rootfsDir}/usr/sbin/policy-rc.d
+sudo mkdir -p ${rootfsDir}/etc/init.d
+sudo touch ${rootfsDir}/etc/init.d/.legacy-bootordering
+
+echo "Running debootstrap"
+sudo debootstrap --no-check-gpg --arch=amd64 ${UBUNTU_RELEASE} ${rootfsDir} ${MIRROR}${UBUNTU_MIRROR_SUFFIX}
+
+sudo cp /etc/resolv.conf ${rootfsDir}/etc/resolv.conf
+
+echo "Adding build user to sudo"
+echo "${BUILD_USER}  ALL=(ALL)       NOPASSWD: ALL" | sudo tee ${rootfsDir}/etc/sudoers.d/build_pkgs
+
+echo "Generating utf8 locale"
+sudo chroot ${rootfsDir} /bin/sh -c 'locale-gen en_US.UTF-8; dpkg-reconfigure -f noninteractive locales'
+
+echo "Allowing using unsigned repos"
+echo "APT::Get::AllowUnauthenticated 1;" | sudo tee ${rootfsDir}/etc/apt/apt.conf.d/02mirantis-unauthenticated
+
+echo "Adding mirrors && Updating apt package database"
+sudo bash -c "cat > ${rootfsDir}/etc/apt/sources.list" <<EOF
+#Upstream Ubuntu mirrors
+deb ${MIRROR}${UBUNTU_MIRROR_SUFFIX} ${UBUNTU_RELEASE} main universe multiverse restricted
+deb ${MIRROR}${UBUNTU_MIRROR_SUFFIX} ${UBUNTU_RELEASE}-updates main universe multiverse restricted
+deb ${MIRROR}${UBUNTU_MIRROR_SUFFIX} ${UBUNTU_RELEASE}-security main universe multiverse restricted
+#MOS mirrors
+deb ${MIRROR}${MIRROR_UBUNTU_ROOT} ${PRODUCT_NAME}${PRODUCT_VERSION} main restricted
+deb ${MIRROR}${MIRROR_UBUNTU_ROOT} ${PRODUCT_NAME}${PRODUCT_VERSION}-security main restricted
+deb ${MIRROR}${MIRROR_UBUNTU_ROOT} ${PRODUCT_NAME}${PRODUCT_VERSION}-proposed main restricted
+deb ${MIRROR}${MIRROR_UBUNTU_ROOT} ${PRODUCT_NAME}${PRODUCT_VERSION}-updates main restricted
+deb ${MIRROR}${MIRROR_UBUNTU_ROOT} ${PRODUCT_NAME}${PRODUCT_VERSION}-holdback main restricted
+
+EOF
+
+sudo bash -c "cat > ${rootfsDir}/etc/apt/preferences.d/fuel" <<EOF
+Package: *
+Pin: origin download.mirantis.com
+Pin-Priority: 1001
+
+Package: *
+Pin: release o=Mirantis, n=${UBUNTU_RELEASE}
+Pin-Priority: 1101
+
+# to install packages from unmerged fuel-infra requests
+Package: *
+Pin: release l=${UBUNTU_RELEASE}-fuel-${PRODUCT_VERSION}-stable*
+Pin-Priority: 1101
+
+Package: *
+Pin: release o=Open Build Service ${UBUNTU_RELEASE}-fuel-${PRODUCT_VERSION}-stable*
+Pin-Priority: 1101
+
+EOF
+
+sudo chroot ${rootfsDir} apt-get update && sudo chroot ${rootfsDir} \
+  env DEBIAN_FRONTEND=noninteractive \
+  DEBCONF_NONINTERACTIVE_SEEN=true \
+  LC_ALL=C LANG=C LANGUAGE=C \
+  apt-get --yes dist-upgrade
+
+echo "Installing additional packages: ${SANDBOX_PACKAGES})"
+test -n "${SANDBOX_PACKAGES}" && sudo chroot ${rootfsDir} \
+  env DEBIAN_FRONTEND=noninteractive \
+  DEBCONF_NONINTERACTIVE_SEEN=true \
+  LC_ALL=C LANG=C LANGUAGE=C \
+  apt-get install --yes ${SANDBOX_PACKAGES}
+echo "chroot: done"
+
+# put building script in docker
+sudo mkdir ${rootfsDir}/opt/sandbox
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+sudo cp ${DIR}/build_deb_in_docker.sh ${rootfsDir}/opt/sandbox
+
+# let's pack rootfs
+tarFile="${dir}/rootfs.tar.xz"
+sudo touch "${tarFile}"
+
+sudo tar --numeric-owner -caf "${tarFile}" -C "${rootfsDir}" --transform='s,^./,,' .
+
+# prepare for building docker
+cat > "${dir}/Dockerfile" <<EOF
+FROM scratch
+ADD rootfs.tar.xz /
+
+RUN groupadd --gid ${BUILD_GID} ${BUILD_GROUP} && \
+    useradd --system --uid ${BUILD_UID} --gid ${BUILD_GID} --home /opt/sandbox --shell /bin/bash ${BUILD_USER} && \
+    mkdir -p /opt/sandbox && \
+    chown -R ${BUILD_UID}:${BUILD_GID} /opt/sandbox
+EOF
+
+# cleaning rootfs
+sudo rm -rf "$rootfsDir"
+
+# creating docker image
+docker build -t "${TAG}" "${dir}"
+
+# cleaning all
+rm -rf "${dir}"
+sudo umount "${TMPDIR}"
+
+# saving image
+if [ "${SAVE_DOCKER_IMAGE}" == "yes" ]; then
+  docker save "${TAG}" | xz > ${ARTS_DIR:-/var/tmp}/fuel-${PRODUCT_VERSION}-debbuild_env.tar.xz
+fi
