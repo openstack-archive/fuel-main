85cf3d4000f4cb2d6802ac994a0e968ec5a00d47
asledzinskiy <asledzinskiy@mirantis.com>
2015-03-27 18:13:52 +0200

    Add check on swift rings health
    - Add check that swift rings balance value is equal
    to 0 to all ha neutron and nova tests
    
    Change-Id: I439519a05419f79192630e9db0319a1249e756c0
    Closes-Bug: #1405783

--- fuelweb_test/models/fuel_web_client.py
+++ fuelweb_test/models/fuel_web_client.py
@@ -1283,3 +1283,17 @@ class FuelWebClient(object):
         if self.environment.domain not in hostname:
             hostname += "." + self.environment.domain
         return hostname
+
+    @logwrap
+    def get_nailgun_primary_controller(self, slave):
+        #returns controller that is primary in nailgun
+        remote = self.get_ssh_for_node(slave.name)
+        data = yaml.load(''.join(
+            remote.execute('cat /etc/astute.yaml')['stdout']))
+        node_name = [node['fqdn'] for node in data['nodes']
+                     if node['role'] == 'primary-controller'][0]
+        logger.debug("node name is {0}".format(node_name))
+        fqdn = self.get_fqdn_by_hostname(node_name)
+        devops_node = self.find_devops_node_by_nailgun_fqdn(
+            fqdn, self.environment.nodes().slaves)
+        return devops_node
