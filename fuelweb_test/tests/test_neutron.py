f1f6a23d25ef9f84a4983787a3e6faf67fbce71b
Andrey Sledzinskiy <asledzinskiy@mirantis.com>
2015-04-01 10:30:37 +0000

    Revert "Add check on swift rings health"
    This reverts commit 85cf3d4000f4cb2d6802ac994a0e968ec5a00d47.
    
    Change-Id: I0a109f344a57714d2d6306c656016f2a92c825d1

--- fuelweb_test/tests/test_neutron.py
+++ fuelweb_test/tests/test_neutron.py
@@ -15,13 +15,11 @@
 from proboscis.asserts import assert_equal
 from proboscis import test
 
-from fuelweb_test.helpers import checkers
 from fuelweb_test.helpers.decorators import log_snapshot_on_error
 from fuelweb_test.settings import DEPLOYMENT_MODE_HA
 from fuelweb_test.settings import DEPLOYMENT_MODE_SIMPLE
 from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
-from fuelweb_test import logger
 
 
 @test(groups=["thread_1", "neutron", "bvt_1"])
@@ -191,11 +189,6 @@ class NeutronGreHa(TestBasic):
         # assert_equal(str(cluster['net_segment_type']), segment_type)
 
         self.fuel_web.verify_network(cluster_id)
-        devops_node = self.fuel_web.get_nailgun_primary_controller(
-            self.env.nodes().slaves[0])
-        logger.debug("devops node name is {0}".format(devops_node.name))
-        remote = self.env.get_ssh_to_remote_by_name(devops_node.name)
-        checkers.check_swift_ring(remote)
 
         self.fuel_web.run_ostf(
             cluster_id=cluster_id)
@@ -319,11 +312,6 @@ class NeutronVlanHa(TestBasic):
             cluster_id, self.env.get_ssh_to_remote_by_name('slave-01'))
 
         self.fuel_web.verify_network(cluster_id)
-        devops_node = self.fuel_web.get_nailgun_primary_controller(
-            self.env.nodes().slaves[0])
-        logger.debug("devops node name is {0}".format(devops_node.name))
-        remote = self.env.get_ssh_to_remote_by_name(devops_node.name)
-        checkers.check_swift_ring(remote)
 
         self.fuel_web.run_ostf(
             cluster_id=cluster_id)
