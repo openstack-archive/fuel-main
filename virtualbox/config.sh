db490d4cb000f4bbb34ad4dccb1a256e28317b3e
Fabrizio Soppelsa <fsoppelsa@mirantis.com>
2015-04-15 13:46:25 +0300

    Enable Internet for slave nodes
    Adds NAT features to the slaves in the VirtualBox scripts, now
    for Linux and OS X (Yosemite) systems:
    
    * It requires the sudo password (giving explantions on the reasons)
    * For OS X, it backups the original pf.conf before setting the NAT
      using pfctl, and does some cleanups
    * For Linux, inserts the masquerade rule in the nat chain
    
    Change-Id: Id37cb55241682f530a7e5378cadd556cccc1db90
    Closes-Bug: #1442988

--- virtualbox/config.sh
+++ virtualbox/config.sh
@@ -83,7 +83,7 @@ vm_master_memory_mb=1536
 vm_master_disk_mb=65535
 
 # Master node access to the internet through the host system, using VirtualBox NAT adapter
-vm_master_nat_network=192.168.200/24
+vm_master_nat_network=192.168.200.0/24
 vm_master_nat_gateway=192.168.200.2
 
 # These settings will be used to check if master node has installed or not.
