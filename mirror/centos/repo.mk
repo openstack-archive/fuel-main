b2b5a7f959f31911f076aec2c0aa0c8de5b69a40
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2015-04-21 15:28:46 +0300

    Added priorities for centos repos
    We need to be able to download packages from
    various repositories taking into account repository
    priority. We need this for patching story.
    
    This patch introduces MULTI_MIRROR_CENTOS variable
    which is supposed to have the following format:
    
    "name1,pri1,url1 name2,pri2,url2"
    
    Besides, this patch introduces uniformity for all those
    cases USE_MIRROR=msk|srt|none removing unused variables.
    
    It is supposed that USE_MIRROR=none is only used for
    building Centos mirror not for building ISO.
    
    Change-Id: I84a893a32194f0d6f5c2727af373f680fae1aaae
    Closes-Bug: #1446293

--- mirror/centos/repo.mk
+++ mirror/centos/repo.mk
@@ -1,4 +1,39 @@
-include $(SOURCE_DIR)/mirror/centos/yum_repos.mk
+define yum_conf
+[main]
+cachedir=$(BUILD_DIR)/mirror/centos/cache
+keepcache=0
+debuglevel=6
+logfile=$(BUILD_DIR)/mirror/centos/yum.log
+exclude=*.i686.rpm ntp-dev*
+exactarch=1
+obsoletes=1
+gpgcheck=0
+plugins=1
+pluginpath=$(BUILD_DIR)/mirror/centos/etc/yum-plugins
+pluginconfpath=$(BUILD_DIR)/mirror/centos/etc/yum/pluginconf.d
+reposdir=$(BUILD_DIR)/mirror/centos/etc/yum.repos.d
+endef
+
+get_repo_name=$(shell echo $1 | cut -d ',' -f 1)
+get_repo_priority=$(shell echo $1 | cut -d ',' -f 2)
+get_repo_url=$(shell echo $1 | cut -d ',' -f 3)
+
+# It's a callable object.
+# Usage: $(call create_repo,repo)
+# where:
+# repo=repo_name,repo_priority,http://path_to_the_repo
+# repo_priority is a number from 1 to 99
+define create_repo
+[$(call get_repo_name,$($1))]
+name = Repo "$(call get_repo_name,$($1))"
+baseurl = $(call get_repo_url,$($1))
+gpgcheck = 0
+enabled = 1
+priority = $(call get_repo_priority,$($1))
+endef
+
+# First repository is used to download comps.xml files.
+MIRROR_CENTOS_OS_BASEURL?=$(shell echo $(firstword $(MULTI_MIRROR_CENTOS)) | cut -d ',' -f 3)
 
 .PHONY: show-yum-urls-centos
 
@@ -17,17 +52,10 @@ $(BUILD_DIR)/mirror/centos/etc/yum/pluginconf.d/priorities.conf:
 	mkdir -p $(@D)
 	/bin/echo -e "[main]\nenabled=1\ncheck_obsoletes=1\nfull_match=1" > $@
 
-$(BUILD_DIR)/mirror/centos/etc/yum.repos.d/base.repo: $(call depv,YUM_REPOS)
-$(BUILD_DIR)/mirror/centos/etc/yum.repos.d/base.repo: \
-		export contents:=$(foreach repo,$(YUM_REPOS),\n$(yum_repo_$(repo))\n)
-$(BUILD_DIR)/mirror/centos/etc/yum.repos.d/base.repo:
-	@mkdir -p $(@D)
-	/bin/echo -e "$${contents}" > $@
-
-$(BUILD_DIR)/mirror/centos/etc/yum.repos.d/extra.repo: $(call depv,EXTRA_RPM_REPOS)
-$(BUILD_DIR)/mirror/centos/etc/yum.repos.d/extra.repo: \
-		export contents:=$(foreach repo,$(EXTRA_RPM_REPOS),\n$(call create_extra_repo,repo)\n)
-$(BUILD_DIR)/mirror/centos/etc/yum.repos.d/extra.repo:
+$(BUILD_DIR)/mirror/centos/etc/yum.repos.d/all.repo: $(call depv,MULTI_MIRROR_CENTOS)
+$(BUILD_DIR)/mirror/centos/etc/yum.repos.d/all.repo: \
+		export contents:=$(foreach repo,$(MULTI_MIRROR_CENTOS),$(NEWLINE)$(call create_repo,repo)$(NEWLINE))
+$(BUILD_DIR)/mirror/centos/etc/yum.repos.d/all.repo:
 	@mkdir -p $(@D)
 	/bin/echo -e "$${contents}" > $@
 
@@ -35,8 +63,7 @@ centos_empty_installroot:=$(BUILD_DIR)/mirror/centos/dummy_installroot
 
 $(BUILD_DIR)/mirror/centos/yum-config.done: \
 		$(BUILD_DIR)/mirror/centos/etc/yum.conf \
-		$(BUILD_DIR)/mirror/centos/etc/yum.repos.d/base.repo \
-		$(BUILD_DIR)/mirror/centos/etc/yum.repos.d/extra.repo \
+		$(BUILD_DIR)/mirror/centos/etc/yum.repos.d/all.repo \
 		$(BUILD_DIR)/mirror/centos/etc/yum-plugins/priorities.py \
 		$(BUILD_DIR)/mirror/centos/etc/yum/pluginconf.d/priorities.conf
 	rm -rf $(centos_empty_installroot)
