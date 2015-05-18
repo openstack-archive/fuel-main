c7bf69b3a81b7f8a25083c3df29d573687f90124
Sergey Kulanov <skulanov@mirantis.com>
2015-05-18 22:29:57 +0300

    Fix packages changelog generation
    We can have two types of packages changelog:
    1. The only packages from mirrors:
      make target: mirror-pkgs-changelog
      should be defined explicitly, for example:
    
          make iso mirror-pkgs-changelog
    
      the result will be stored in:
       $(LOCAL_MIRROR)/centos-mirror-packages.changelog
       $(LOCAL_MIRROR)/ubuntu-mirror-packages.changelog
    
    2. Packages from mirrors plus those that were built during
       make iso:
      * docker images;
      * rpm late packages;
      make target will be build automatically during make iso,
      the result will be stored in:
       $(LOCAL_MIRROR)/centos-pkgs-packages.changelog
       $(LOCAL_MIRROR)/ubuntu-pkgs-packages.changelog
    
    Change-Id: I4468219cef025658b99315129cd860b00391d16c
    Closes-bug: #1452284

--- mirror/module.mk
+++ mirror/module.mk
@@ -1,7 +1,7 @@
-.PHONY: mirror clean clean-mirror make-changelog
+.PHONY: mirror clean clean-mirror mirror-pkgs-changelog
 
 mirror: $(BUILD_DIR)/mirror/build.done
-make-changelog: $(BUILD_DIR)/mirror/make-changelog.done
+mirror-pkgs-changelog: $(BUILD_DIR)/mirror/mirror-pkgs-changelog.done
 
 clean: clean-mirror
 
@@ -18,7 +18,12 @@ $(BUILD_DIR)/mirror/build.done: \
 		$(BUILD_DIR)/mirror/docker/build.done
 	$(ACTION.TOUCH)
 
-$(BUILD_DIR)/mirror/make-changelog.done: $(BUILD_DIR)/mirror/build.done
-	sudo bash -c "export LOCAL_MIRROR=$(LOCAL_MIRROR); \
+# generate list of packages from mirrors
+$(BUILD_DIR)/mirror/mirror-pkgs-changelog.done: \
+		$(BUILD_DIR)/mirror/ubuntu/mirror.done \
+		$(BUILD_DIR)/mirror/centos/build.done
+	bash -c "export LOCAL_MIRROR=$(LOCAL_MIRROR); \
+		export CENTOS_FILE=centos-mirror-packages.changelog; \
+		export UBUNTU_FILE=ubuntu-mirror-packages.changelog; \
 		$(SOURCE_DIR)/report-changelog.sh"
 	$(ACTION.TOUCH)
