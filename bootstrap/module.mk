1ae65f37a86d323403f37851e6b139465c887aba
Albert <asyriy@mirantis.com>
2015-06-23 14:46:05 +0300

    Build bootstrap with kernel 2.6.32
    Change-Id: I475732c1f818e17f857d416b867bbc45eab5fa06

--- bootstrap/module.mk
+++ bootstrap/module.mk
@@ -29,6 +29,7 @@ BOOTSTRAP_RPMS:=\
 	ql2100-firmware \
 	ql2200-firmware \
 	ql23xx-firmware \
+	megaraid_sas\
 	crontabs \
 	dhclient \
 	dmidecode \
@@ -85,7 +86,7 @@ endef
 #FIXME Partial-Bug: #1403088
 YUM:=sudo yum -c $(BUILD_DIR)/bootstrap/etc/yum.conf --exclude=ruby-2.1.1  --exclude=ruby21 --installroot=$(INITRAMROOT) -y --nogpgcheck
 
-KERNEL_PATTERN:=kernel-lt-3.10.*
+KERNEL_PATTERN:=kernel-2.6.*
 KERNEL_FIRMWARE_PATTERN:=linux-firmware*
 
 clean: clean-bootstrap
