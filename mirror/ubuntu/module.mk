1bb1027351a034dade6aa52237712bcd5c5d3020
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2015-07-28 19:02:12 +0300

    Switch to perestroika v4 repositories
    Perestroika builds both rpm and deb fuel repos.
    We use perestroika rpm repository to build fwm
    repository which includes fuel packages as well
    as those packages which are listed in requirements-rpm.txt.
    Deb repository can be used directly.
    
    Closes-Bug: #1479761
    Change-Id: I236144759eb38e71977d175226f81772d23de9b3

--- mirror/ubuntu/module.mk
+++ mirror/ubuntu/module.mk
@@ -57,7 +57,8 @@ $(BUILD_DIR)/mirror/ubuntu/reprepro.done: \
 		$(BUILD_DIR)/mirror/ubuntu/mirror.done \
 		$(BUILD_DIR)/mirror/ubuntu/reprepro_config.done
 	# Import existing Ubuntu repository
-	cd $(LOCAL_MIRROR_UBUNTU) && reprepro --confdir=$(REPREPRO_CONF_DIR) -V update
+	cd $(LOCAL_MIRROR_UBUNTU) && reprepro --distdir dists --outdir dists/$(PRODUCT_NAME)$(PRODUCT_VERSION) --confdir=$(REPREPRO_CONF_DIR) -V update
+	cd $(LOCAL_MIRROR_UBUNTU) && $(SOURCE_DIR)/mirror/ubuntu/prepend_pool_path.sh dists/$(PRODUCT_NAME)$(PRODUCT_VERSION) dists/$(PRODUCT_NAME)$(PRODUCT_VERSION)/
 	$(ACTION.TOUCH)
 
 $(BUILD_DIR)/mirror/ubuntu/repo.done: \
@@ -65,7 +66,8 @@ $(BUILD_DIR)/mirror/ubuntu/repo.done: \
 		$(BUILD_DIR)/mirror/ubuntu/reprepro.done
 	# FIXME(aglarendil): do not touch upstream repo. instead - build new repo
 	# Import newly built packages
-	cd $(LOCAL_MIRROR_UBUNTU) && reprepro --confdir=$(REPREPRO_CONF_DIR) -V includedeb $(PRODUCT_NAME)$(PRODUCT_VERSION) $(BUILD_DIR)/packages/deb/packages/*.deb
+	cd $(LOCAL_MIRROR_UBUNTU) && reprepro --distdir dists --outdir dists/$(PRODUCT_NAME)$(PRODUCT_VERSION) --confdir=$(REPREPRO_CONF_DIR) -V includedeb $(PRODUCT_NAME)$(PRODUCT_VERSION) $(BUILD_DIR)/packages/deb/packages/*.deb
+	cd $(LOCAL_MIRROR_UBUNTU) && $(SOURCE_DIR)/mirror/ubuntu/prepend_pool_path.sh dists/$(PRODUCT_NAME)$(PRODUCT_VERSION) dists/$(PRODUCT_NAME)$(PRODUCT_VERSION)/
 	# Clean up reprepro data
 	rm -rf $(LOCAL_MIRROR_UBUNTU)/db
 	rm -rf $(LOCAL_MIRROR_UBUNTU)/lists
@@ -73,12 +75,15 @@ $(BUILD_DIR)/mirror/ubuntu/repo.done: \
 
 $(BUILD_DIR)/mirror/ubuntu/mirror.done:
 	mkdir -p $(LOCAL_MIRROR_UBUNTU)
-	set -ex; debmirror --method=$(MIRROR_UBUNTU_METHOD) --progress --checksums --nocleanup --host=$(MIRROR_FUEL_UBUNTU) --root=$(MIRROR_UBUNTU_ROOT) \
-	--arch=$(UBUNTU_ARCH) --dist=$(UBUNTU_RELEASE) --nosource --ignore-release-gpg --rsync-extra=none \
-	--section=$(MIRROR_UBUNTU_SECTION) $(LOCAL_MIRROR_UBUNTU)/
+	set -ex; debmirror --progress --checksums --nocleanup \
+	--nosource --ignore-release-gpg --rsync-extra=none
+	--method=$(MIRROR_MOS_UBUNTU_METHOD) \
+	--host=$(MIRROR_MOS_UBUNTU) \
+	--root=$(MIRROR_MOS_UBUNTU_ROOT) \
+	--dist=$(MIRROR_MOS_UBUNTU_SUITE) \
+	--section=$(subst $(space),$(comma),$(MIRROR_MOS_UBUNTU_SECTION))
+	--arch=$(UBUNTU_ARCH) \
+	$(LOCAL_MIRROR_UBUNTU)/
 	rm -rf $(LOCAL_MIRROR_UBUNTU)/.temp $(LOCAL_MIRROR_UBUNTU)/project
-	mv $(LOCAL_MIRROR_UBUNTU)/dists/trusty $(LOCAL_MIRROR_UBUNTU)/dists/mos7.0
-	sed -i 's/trusty/mos7.0/g' $(LOCAL_MIRROR_UBUNTU)/dists/mos7.0/Release
 	rm -f $(LOCAL_MIRROR_UBUNTU)/dists/mos7.0/main/binary-amd64/*bz2
-	$(SOURCE_DIR)/regenerate_ubuntu_repo.sh $(LOCAL_MIRROR_UBUNTU)/ mos7.0
 	$(ACTION.TOUCH)
