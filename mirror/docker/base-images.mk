b1a35b21749ce9f55a1d0fb756c773a63ad3c556
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2015-07-28 19:02:12 +0300

    Use new rpm repository structure
    Perestroika builds fuel repository which
    we then use to create fwm mirror. Recently
    we have switched to the new mos
    repository structure and this change reflects
    this fact.
    
    Change-Id: I236144759eb38e71977d175226f81772d23de9b3

--- mirror/docker/base-images.mk
+++ mirror/docker/base-images.mk
@@ -1,5 +1,7 @@
 BASE_IMAGE_FILES:=centos.tar.xz busybox.tar.xz
 
+MIRROR_DOCKER_BASEURL?=$(MIRROR_DOCKER)
+
 # docker base image files
 $(addprefix $(LOCAL_MIRROR_DOCKER_BASEURL)/,$(BASE_IMAGE_FILES)):
 	@mkdir -p $(@D)
