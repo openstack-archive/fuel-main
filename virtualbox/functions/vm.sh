f58400b542ade501ba34670d19a5d453de9c900d
Stanislav Makar <smakar@mirantis.com>
2015-08-26 17:27:26 +0300

    Use right way to skip fuelmenu
    Use virtualbox built-in keyboardputscancode instead of using
    ssh to master node and kill fuelmenu.
    
    Change-Id: Ie3a12b7ca54e4c606f4c88145cc3cbad5822480a
    Closes-bug: #1501796

--- virtualbox/functions/vm.sh
+++ virtualbox/functions/vm.sh
@@ -207,6 +207,17 @@ start_vm() {
     else
         execute VBoxManage startvm $name
     fi
+
+    if [ -n "$boot_line" ]; then
+        sleep 3
+        # Pressing/releasing escape key
+        execute VBoxManage controlvm ${name} keyboardputscancode 01 81
+
+        for letter in ${boot_line}; do
+            execute VBoxManage controlvm ${name} keyboardputscancode ${letter}
+        done
+    fi
+
 }
 
 mount_iso_to_vm() {
