4ff28c19e5c5ec0ea1967094d5e458e45fc294c2
bgaifullin <bgaifullin@mirantis.com>
2015-09-03 20:16:57 +0300

    Added support to deploy product on remote machine via ssh
    It is useful in case if there is 2 machines:
    the main that has limited resources and used only for development and
    the second, that has enough resources to use it for deployment.
    enable feature VRDP for virtual machine
    
    Change-Id: Ib34a188311cc04250d29e685bf5b9f12ac2aeb05
    Closes-Bug: #1491925

---
+++ virtualbox/functions/shell.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# This file contains the functions for abstracting over running command over ssh or directly
+# depends on environment settings
+
+shell_ssh_options=""
+shell_scp_options=""
+
+if [ -n "$REMOTE_PORT" ]; then
+  shell_ssh_options="-p $REMOTE_PORT $shell_ssh_options"
+  shell_scp_options="-P $REMOTE_PORT $shell_scp_options"
+fi
+
+
+function execute() {
+  if [ -n "$REMOTE_HOST" ]; then
+    eval "ssh $shell_ssh_options $REMOTE_HOST \"${@//\ /\\ }\""
+  else
+    eval "${@//\ /\\ }"
+  fi
+}
+
+# copy file to remote machine if needed
+# be careful, do nothing in case if file with same name exists on target machine
+function copy_if_required() {
+  local source_path=$1
+  if [ -n "$REMOTE_HOST" ]; then
+    if ! execute test -f $source_path; then
+      # the scripts always find iso in original path
+      # reconstruct same path on remote machine
+      local source_dir=$(dirname $source_path)
+      echo "Copying $source_path to $REMOTE_HOST:$source_path..."
+      execute mkdir -p $source_dir && copy $source_path $source_path || exit 1
+    else
+      echo "Skip copying the file $source_path, it already exists on $REMOTE_HOST."
+    fi
+  fi
+}
+
+
+function copy() {
+  local src=$1
+  local dst=$2
+
+  if [ -n "$REMOTE_HOST" ]; then
+    eval "scp $shell_scp_options $src $REMOTE_HOST:$dst"
+  else
+    cp $src $dst
+  fi
+}
