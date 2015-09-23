5b36d48ac120404604d6b57049e089ff58e1b2f6
Nikita Koshikov <mk455v@att.com>
2015-09-23 17:05:26 -0500

    Dynamic dnsmasq for fuelweb_admin networks
    Install 'deamonize' package in mcollective container for puppet
    mcollective agent. Also configure Hiera inside mcollective container.
    
    We need this for puppet task which is being executed in 'mcollective'
    container and which updates dnsmasq dhcp ip ranges when we change
    fuelweb_admin networks in any nodegroup.
    
    Co-Authored-By: Aleksandr Didenko <adidenko@mirantis.com>
    
    Blueprint: dynamic-dnsmasq
    Partial-bug: #1495593
    
    Change-Id: I3309b32e3ede4be2f6758ff756b4ed15e3b20d66

--- docker/cobbler/start.sh
+++ docker/cobbler/start.sh
@@ -30,6 +30,18 @@ puppet apply -v /etc/puppet/modules/nailgun/examples/cobbler-only.pp
 /etc/init.d/dnsmasq stop
 /etc/init.d/cobblerd stop
 
+# Check if we have any dhcp-ranges configured in dnsmasq. If not, then
+# we need to create default dhcp-range for fuelweb_admin network that
+# was configured via fuelmenu and stored in /etc/fuel/astute.yaml
+
+# TODO: remove 'test -f' when I9d7428c9fc21c705a1aee7fbca8003829a81e1d0
+# is merged
+ls /etc/dnsmasq.d/*.conf || \
+  ( \
+    test -f /etc/puppet/modules/nailgun/examples/dhcp-default-range.pp && \
+    puppet apply -d -v /etc/puppet/modules/nailgun/examples/dhcp-default-range.pp \
+  )
+
 # Running services
 /etc/init.d/dnsmasq restart
 cobblerd -F
--- docker/mcollective/start.sh
+++ docker/mcollective/start.sh
@@ -5,6 +5,11 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
+# TODO: remove 'test -f' when I9d7428c9fc21c705a1aee7fbca8003829a81e1d0
+# is merged
+test -f /etc/puppet/modules/nailgun/examples/hiera-for-container.pp && \
+  puppet apply -d -v /etc/puppet/modules/nailgun/examples/hiera-for-container.pp
+
 puppet apply -d -v /etc/puppet/modules/mcollective/examples/mcollective-server-only.pp
 
 #Stop daemon and restart it in the foreground
