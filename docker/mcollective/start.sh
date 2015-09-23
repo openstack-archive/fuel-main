0e82f2a80c9ff1e84d7dc6a088535ace8156febc
Aleksandr Didenko <adidenko@mirantis.com>
2015-09-23 17:05:26 -0500

    Dynamic dnsmasq for fuelweb_admin networks
    Install 'deamonize' package in mcollective container for puppet
    mcollective agent. Also configure Hiera inside mcollective container.
    
    We need this for puppet task which is being executed in 'mcollective'
    container and which updates dnsmasq dhcp ip ranges when we change
    fuelweb_admin networks in any nodegroup.
    
    Change-Id: I3309b32e3ede4be2f6758ff756b4ed15e3b20d66
    Requires: I9d7428c9fc21c705a1aee7fbca8003829a81e1d0
    Partial-bug: #1495593

--- docker/mcollective/start.sh
+++ docker/mcollective/start.sh
@@ -5,6 +5,7 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
+puppet apply -d -v /etc/puppet/modules/nailgun/examples/hiera-for-container.pp
 puppet apply -d -v /etc/puppet/modules/mcollective/examples/mcollective-server-only.pp
 sed -e 's/daemonize[[:space:]]*=[[:space:]]*1/daemonize = 0/g' -i /etc/mcollective/server.cfg
 /usr/sbin/mcollectived --pid=/var/run/mcollectived.pid --config=/etc/mcollective/server.cfg
