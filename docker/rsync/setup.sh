68ecdd097fb442d57017b958536292dc5d027b60
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] Swith to centos7
    Based on https://review.openstack.org/#/c/232182/1
    
    Change-Id: Ie45676656ce24b05ece799e6b2340996238092f7

---
+++ docker/rsync/setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+source /etc/fuel/functions.sh
+set -o errexit
+trap 'print_debug_info' ERR
+set -o xtrace
+
+rm -rf /etc/yum.repos.d/*
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=http://$(route -n | awk '/^0.0.0.0/ {print $2}'):${DOCKER_PORT}/os/x86_64/
+gpgcheck=0
+EOF
+
+yum clean expire-cache
+yum update -y
+
+sed -i '/requiretty/s/^/#/g' /etc/sudoers
+
+puppet apply --color false --detailed-exitcodes --debug --verbose \
+  /etc/puppet/modules/nailgun/examples/puppetsync-only.pp || [[ $? == 2 ]]
+
+mkdir -p /etc/systemd/system/rsyncd.service.d/
+cat << EOF > /etc/systemd/system/rsyncd.service.d/restart.conf
+[Service]
+Restart=always
+RestartSec=5
+FailureAction=reboot-force
+EOF
+
+systemctl set-default multi-user.target
+systemctl enable xinetd.service
+systemctl enable rsyncd.service
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=file:/var/www/nailgun/centos/x86_64
+gpgcheck=0
+EOF
+
+yum clean all
