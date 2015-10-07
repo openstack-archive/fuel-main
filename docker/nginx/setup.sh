9eb0c50e3e7943ab28f7d795a505ca4328c5f247
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] Swith to centos7
    Based on https://review.openstack.org/#/c/232182/1
    
    Change-Id: Ie45676656ce24b05ece799e6b2340996238092f7

---
+++ docker/nginx/setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -o errexit
+set -o xtrace
+
+rm -rf /etc/yum.repos.d/*
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=http://$(route -n | awk '/^0.0.0.0/ {print $2}'):${DOCKER_PORT}/os/x86_64/
+gpgcheck=0
+EOF
+
+yum clean expire-cache
+yum update -y 
+
+mkdir -p /var/www/nailgun
+chmod 755 /var/www/nailgun
+
+puppet apply --detailed-exitcodes -d -v /etc/puppet/modules/nailgun/examples/nginx-only.pp || [[ $? == 2 ]]
+
+mkdir -p /etc/systemd/system/nginx.service.d/
+cat << EOF > /etc/systemd/system/nginx.service.d/restart.conf
+[Service]
+Restart=always
+RestartSec=5
+FailureAction=reboot-force
+EOF
+
+systemctl set-default multi-user.target
+systemctl enable nginx.service
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=file:/var/www/nailgun/centos/x86_64
+gpgcheck=0
+EOF
+
+yum clean all
+
+systemctl enable start-container.service
