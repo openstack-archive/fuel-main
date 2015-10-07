fc1ef3ddac3a56e0dc9873034ce1ceb42b6a6e75
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] CentOS7 Support (supervisord)
    Based on https://review.openstack.org/#/c/232222/140
    
    Migrate from systemd to supervisord
    
    Change-Id: Idbbd418fba83de468529dbd7114ac655ba0c7cb9

--- docker/mcollective/start.sh
+++ docker/mcollective/start.sh
@@ -5,21 +5,13 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
-# TODO: remove 'test -f' when I9d7428c9fc21c705a1aee7fbca8003829a81e1d0
-# is merged
-test -f /etc/puppet/modules/nailgun/examples/hiera-for-container.pp && \
-  puppet apply -d -v /etc/puppet/modules/nailgun/examples/hiera-for-container.pp
-
-# TODO(bpiotrowski): remove old file path after new ISO is used on CI
-if [[ -f /etc/puppet/modules/mcollective/examples/mcollective-server-only.pp ]]; then
-  puppet apply -d -v /etc/puppet/modules/mcollective/examples/mcollective-server-only.pp
-else
-  puppet apply -d -v /etc/puppet/modules/nailgun/examples/mcollective-only.pp
-fi
-
-#Stop daemon and restart it in the foreground
-service mcollective stop
+puppet apply --debug --verbose \
+    /etc/puppet/modules/nailgun/examples/hiera-for-container.pp
 
 sed -e 's/daemonize[[:space:]]*=[[:space:]]*1/daemonize = 0/g' -i /etc/mcollective/server.cfg
-/usr/sbin/mcollectived --pid=/var/run/mcollectived.pid --config=/etc/mcollective/server.cfg
+puppet apply --debug --verbose \
+    /etc/puppet/modules/mcollective/examples/mcollective-server-only.pp
 
+if [ ! -f /etc/hiera/networks.yaml ]; then
+    python /usr/local/bin/default-admin-networks.py
+fi
