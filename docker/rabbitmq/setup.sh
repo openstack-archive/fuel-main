1e84ceec48d2738461a81ab86c37537483ccdd4d
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] Swith to centos7 (systemd)
    Based on https://review.openstack.org/#/c/232182/1
    
    PatchSet 111:
    - add 'systemctl daemon-reload' before each puppet apply
    
    PatchSet 114:
    - use LVM for docker, based on https://review.openstack.org/#/c/153211
    
    PatchSet 115:
    - add missed -c option
    
    PatchSet 116:
    - move disable getty and firewalld into kickstart
    
    PatchSet 117:
    - move requiretty sudo settings into kickstart
    - new mcollective with systemd support
    
    PatchSet 118:
    - revert PatchSet 114, it's not ready
    
    Change-Id: Ie45676656ce24b05ece799e6b2340996238092f7

---
+++ docker/rabbitmq/setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+source /etc/fuel/functions.sh
+set -o errexit
+trap 'print_debug_info' ERR
+set -o xtrace
+
+rm -rf /etc/yum.repos.d/*
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=http://$(route -n | awk '/^0.0.0.0/ {print $2}'):${DOCKER_PORT}/os/x86_64/
+gpgcheck=0
+EOF
+
+yum clean expire-cache
+yum update -y
+
+mkdir -p /var/lib/hiera /var/log/rabbitmq
+touch /var/lib/hiera/common.yaml
+
+systemctl daemon-reload
+puppet apply --color false --detailed-exitcodes --debug --verbose \
+  /etc/puppet/modules/nailgun/examples/rabbitmq-only.pp || [[ $? == 2 ]]
+
+mkdir -p /etc/systemd/system/rabbitmq-server.service.d/
+cat << EOF > /etc/systemd/system/rabbitmq-server.service.d/restart.conf
+[Service]
+Restart=on-failure
+RestartSec=5
+EOF
+systemctl enable rabbitmq-server.service
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=file:/var/www/nailgun/centos/x86_64
+gpgcheck=0
+EOF
+
+yum clean all
+
+systemctl enable start-container.service
+rm -Rf /var/lib/rabbitmq/mnesia/*
