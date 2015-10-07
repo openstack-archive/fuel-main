837fee316125727578a8d41b64c0fc0b4665c184
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] CentOS7 Support (supervisord)
    Based on https://review.openstack.org/#/c/232222/140
    
    Migrate from systemd to supervisord
    
    Change-Id: Idbbd418fba83de468529dbd7114ac655ba0c7cb9

---
+++ docker/astute/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+source /etc/fuel/functions.sh
+set -o errexit
+trap 'print_debug_info' ERR
+set -o xtrace
+
+rm -rf /etc/yum.repos.d/*
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=http://$(route -n | awk '/^0.0.0.0/ {print $2}'):${DOCKER_PORT}/os/x86_64/
+gpgcheck=0
+EOF
+
+yum clean expire-cache
+yum update -y
+yum install -y --quiet \
+  python-editor \
+  nailgun-mcagents \
+  sysstat \
+  rubygem-amqp \
+  rubygem-amq-protocol \
+  rubygem-i18n \
+  rubygem-tzinfo \
+  rubygem-minitest \
+  rubygem-open4 \
+  rubygem-Platform \
+  rubygem-symboltable \
+  rubygem-thread_safe \
+  rubygem-eventmachine
+
+#FIXME(dteselkin): use correct versions of rubygem packages
+sed -i '/amq-protocol/ s/~>/>=/' /usr/share/gems/specifications/amqp-*.gemspec
+
+puppet apply --color false --detailed-exitcodes --debug --verbose \
+  /etc/puppet/modules/nailgun/examples/astute-only.pp || [[ $? == 2 ]]
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=file:/var/www/nailgun/centos/x86_64
+gpgcheck=0
+EOF
+
+yum clean all
