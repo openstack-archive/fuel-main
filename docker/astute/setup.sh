a0b1a11d344a7eb489cd0bfc6fb696231e052e7b
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] Swith to centos7
    Based on https://review.openstack.org/#/c/232182/1
    
    PatchSet 111:
    - add 'systemctl daemon-reload' before each puppet apply
    
    PatchSet 114:
    - use LVM for docker, based on https://review.openstack.org/#/c/153211
    
    PatchSet 115:
    - add missed -c option
    
    Change-Id: Ie45676656ce24b05ece799e6b2340996238092f7

---
+++ docker/astute/setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+source /etc/fuel/functions.sh
+set -o errexit
+trap 'print_debug_info' ERR
+set -o xtrace
+
+rm -rf /etc/yum.repos.d/*
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=http://$(route -n | awk '/^0.0.0.0/ {print $2}'):${DOCKER_PORT}/os/x86_64/
+gpgcheck=0
+EOF
+
+yum clean expire-cache
+yum update -y
+yum install -y --quiet nailgun-mcagents sysstat
+
+sed -i '/requiretty/s/^/#/g' /etc/sudoers
+
+systemctl daemon-reload
+puppet apply --color false --detailed-exitcodes --debug --verbose \
+  /etc/puppet/modules/nailgun/examples/astute-only.pp || [[ $? == 2 ]]
+
+mkdir -p /etc/systemd/system/supervisord.service.d/
+cat << EOF > /etc/systemd/system/supervisord.service.d/restart.conf
+[Service]
+Restart=always
+RestartSec=5
+FailureAction=reboot-force
+EOF
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=file:/var/www/nailgun/centos/x86_64
+gpgcheck=0
+EOF
+
+yum clean all
+
+systemctl set-default multi-user.target
+systemctl enable supervisord.service
+systemctl enable start-container.service
