2aea547999f0023f6fe646e466216f851b1d020a
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] Swith to centos7
    Based on https://review.openstack.org/#/c/232182/1
    
    Change-Id: Ie45676656ce24b05ece799e6b2340996238092f7

--- docker/rsync/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-# Clean rpm locks before puppet run.
-# See ticket https://bugs.launchpad.net/fuel/+bug/1339236
-rm -f /var/lib/rpm/__db.*
-rpm --rebuilddb
-
-/usr/sbin/xinetd -dontfork
