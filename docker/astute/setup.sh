f551ed81f1d09ddc01c062cf510d89850d643af6
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] Swith to centos7
    Based on https://review.openstack.org/#/c/232182/1
    
    Change-Id: Ie45676656ce24b05ece799e6b2340996238092f7

---
+++ docker/astute/setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source /etc/env.vars
+
+rm -rf /etc/yum.repos.d/*
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=http://$(route -n | awk '/^0.0.0.0/ {print $2}'):${DOCKER_PORT}/os/x86_64/
+gpgcheck=0
+EOF
+
+yum clean expire-cache
+yum update -y
+yum install -y --quiet nailgun-mcagents sysstat
+
+
+puppet apply --detailed-exitcodes -d -v /etc/puppet/modules/nailgun/examples/astute-only.pp || [[ $? == 2 ]]
+
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=file:/var/www/nailgun/centos/x86_64
+gpgcheck=0
+EOF
+
+yum clean all
+
+
+systemctl enable start-container.service
