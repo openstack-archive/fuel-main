d5c3d9abb873715dba0aac282e258dafbf1c961c
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] Swith to centos7
    Based on https://review.openstack.org/#/c/232182/1
    
    PatchSet 111:
    - add 'systemctl daemon-reload' before each puppet apply
    
    PatchSet 114:
    - use LVM for docker, based on https://review.openstack.org/#/c/153211
    
    PatchSet 115:
    - add missed -c option
    
    PatchSet 116:
    - move disable getty and firewalld into kickstart
    
    PatchSet 117:
    - move requiretty sudo settings into kickstart
    - new mcollective with systemd support
    
    PatchSet 118:
    - revert PatchSet 114, it's not ready
    
    Change-Id: Ie45676656ce24b05ece799e6b2340996238092f7

--- docker/astute/start.sh
+++ docker/astute/start.sh
@@ -5,7 +5,7 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
-puppet apply -v /etc/puppet/modules/nailgun/examples/astute-only.pp
-pgrep supervisord >/dev/null && /usr/bin/supervisorctl shutdown
 mkdir -p /var/log/astute
-/usr/bin/supervisord -n
+
+systemctl daemon-reload
+puppet apply -v /etc/puppet/modules/nailgun/examples/astute-only.pp
