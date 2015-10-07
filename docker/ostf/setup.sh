67f20f671c104eaf80dcdc4017101432b02eed99
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] Switch to centos7
    Based on https://review.openstack.org/#/c/232222/140
    
    Migrate from systemd to supervisord
    
    Change-Id: Idbbd418fba83de468529dbd7114ac655ba0c7cb9

---
+++ docker/ostf/setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source /etc/fuel/functions.sh
+set -o errexit
+trap 'print_debug_info' ERR
+set -o xtrace
+
+rm -rf /etc/yum.repos.d/*
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=http://$(route -n | awk '/^0.0.0.0/ {print $2}'):${DOCKER_PORT}/os/x86_64/
+gpgcheck=0
+EOF
+
+yum clean expire-cache
+yum update -y
+
+yum install -y --quiet python-fuelclient supervisor postgresql-libs
+
+puppet apply --color false --detailed-exitcodes --debug --verbose \
+  /etc/puppet/modules/nailgun/examples/ostf-only.pp || [[ $? == 2 ]]
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=file:/var/www/nailgun/centos/x86_64
+gpgcheck=0
+EOF
+
+yum clean all
