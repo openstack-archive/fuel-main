214b1d5878df380c23780d1ffc4fd18ba9f21d8e
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] Switch to centos7
    Based on https://review.openstack.org/#/c/232222/140
    
    Migrate from systemd to supervisord
    
    Change-Id: Idbbd418fba83de468529dbd7114ac655ba0c7cb9

--- docker/nailgun/start.sh
+++ docker/nailgun/start.sh
@@ -9,6 +9,3 @@ rpm --rebuilddb
 ln -sf /etc/fuel/version.yaml /etc/nailgun/version.yaml
 #Run puppet to apply custom config
 puppet apply -v /etc/puppet/modules/nailgun/examples/nailgun-only.pp
-
-service supervisord stop
-/usr/bin/supervisord -n
