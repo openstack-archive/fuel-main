8cb0e59a3ba908cd594f285d2c4d2f5be6709e9b
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] Swith to centos7
    Based on https://review.openstack.org/#/c/232182/1
    
    Change-Id: Ie45676656ce24b05ece799e6b2340996238092f7

--- docker/postgres/start.sh
+++ docker/postgres/start.sh
@@ -5,6 +5,14 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
+mkdir -p /etc/systemd/system/postgresql.service.d/
+cat << EOF > /etc/systemd/system/postgresql.service.d/restart.conf
+[Service]
+Restart=always
+RestartSec=5
+FailureAction=reboot-force
+EOF
+
 # Because /var/lib/pgsql is mounted as a volume, reinstall if its files are
 # missing
 if rpm -V postgresql-server | grep -q missing; then
@@ -12,11 +20,15 @@ if rpm -V postgresql-server | grep -q missing; then
 fi
 
 puppet apply -v /etc/puppet/modules/nailgun/examples/postgres-only.pp
-if [ -f '/etc/init.d/postgresql' ]; then
-  service postgresql stop
-  sudo -u postgres /usr/bin/postmaster -p 5432 -D /var/lib/pgsql/data
-else
-  pgver=$(rpm -q --queryformat '%{VERSION}' postgresql | cut -c'1-3')
-  service "postgresql-${pgver}" stop
-  sudo -u postgres "/usr/pgsql-${pgver}/bin/postmaster" -p 5432 -D "/var/lib/pgsql/${pgver}/data"
-fi
+
+
+systemctl enable postgresql.service
+
+#if [ -f '/etc/init.d/postgresql' ]; then
+#  service postgresql stop
+#  sudo -u postgres /usr/bin/postmaster -p 5432 -D /var/lib/pgsql/data
+#else
+#  pgver=$(rpm -q --queryformat '%{VERSION}' postgresql | cut -c'1-3')
+#  service "postgresql-${pgver}" stop
+#  sudo -u postgres "/usr/pgsql-${pgver}/bin/postmaster" -p 5432 -D "/var/lib/pgsql/${pgver}/data"
+#fi
