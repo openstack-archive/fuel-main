7c867cc0c88b0a193c76f55737997cd398b601d0
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] Swith to centos7
    Based on https://review.openstack.org/#/c/232182/1
    
    Change-Id: Ie45676656ce24b05ece799e6b2340996238092f7

---
+++ docker/nailgun/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -o errexit
+set -o xtrace
+
+rm -rf /etc/yum.repos.d/*
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=http://$(route -n | awk '/^0.0.0.0/ {print $2}'):${DOCKER_PORT}/os/x86_64/
+gpgcheck=0
+EOF
+
+yum clean expire-cache
+yum update -y
+
+mkdir -p /opt/gateone/users/ANONYMOUS/ /var/log/nailgun
+
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+touch /root/.ssh/id_rsa.pub
+chmod 600 /root/.ssh/id_rsa.pub
+
+puppet apply --detailed-exitcodes -d -v /etc/puppet/modules/nailgun/examples/nailgun-only.pp || [[ $? == 2 ]]
+
+mkdir -p /var/log/remote /var/www/nailgun
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=file:/var/www/nailgun/centos/x86_64
+gpgcheck=0
+EOF
+
+yum clean all
+
+mkdir -p /etc/systemd/system/supervisord.service.d/
+cat << EOF > /etc/systemd/system/supervisord.service.d/restart.conf
+[Service]
+Restart=always
+RestartSec=5
+FailureAction=reboot-force
+EOF
+
+systemctl set-default multi-user.target
+systemctl enable start-container.service
