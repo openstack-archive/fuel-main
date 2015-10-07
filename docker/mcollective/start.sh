75996c0ed546b60e76ac1afdd4a43c7eee53d723
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-08 00:15:14 +0300

    [WIP] Swith to centos7
    Based on https://review.openstack.org/#/c/232182/1
    
    PatchSet 111:
    - add 'systemctl daemon-reload' before each puppet apply
    
    PatchSet 114:
    - use LVM for docker, based on https://review.openstack.org/#/c/153211
    
    PatchSet 115:
    - add missed -c option
    
    PatchSet 116:
    - move disable getty and firewalld into kickstart
    
    PatchSet 117:
    - move requiretty sudo settings into kickstart
    - new mcollective with systemd support
    
    PatchSet 118:
    - revert PatchSet 114, it's not ready
    
    Change-Id: Ie45676656ce24b05ece799e6b2340996238092f7

--- docker/mcollective/start.sh
+++ docker/mcollective/start.sh
@@ -5,21 +5,9 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
-# TODO: remove 'test -f' when I9d7428c9fc21c705a1aee7fbca8003829a81e1d0
-# is merged
-test -f /etc/puppet/modules/nailgun/examples/hiera-for-container.pp && \
-  puppet apply -d -v /etc/puppet/modules/nailgun/examples/hiera-for-container.pp
-
-# TODO(bpiotrowski): remove old file path after new ISO is used on CI
-if [[ -f /etc/puppet/modules/mcollective/examples/mcollective-server-only.pp ]]; then
-  puppet apply -d -v /etc/puppet/modules/mcollective/examples/mcollective-server-only.pp
-else
-  puppet apply -d -v /etc/puppet/modules/nailgun/examples/mcollective-only.pp
-fi
+systemctl daemon-reload
+puppet apply -d -v /etc/puppet/modules/mcollective/examples/mcollective-server-only.pp
 
 #Stop daemon and restart it in the foreground
-service mcollective stop
-
-sed -e 's/daemonize[[:space:]]*=[[:space:]]*1/daemonize = 0/g' -i /etc/mcollective/server.cfg
-/usr/sbin/mcollectived --pid=/var/run/mcollectived.pid --config=/etc/mcollective/server.cfg
+systemctl restart mcollective.service
 
