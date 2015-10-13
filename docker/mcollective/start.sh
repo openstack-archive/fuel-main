b4ee14a25d277c46cc5b1ed7be46b4f5b28a1163
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-10-13 12:12:19 +0000

    Improve readability of dockerfiles and cleanup
    * Added newlines to long RUN commands in dockerfiles.
    * Removed storage-* containers (obsolete since 6.0)
    * Removed shotgun workaround in mcollective container.
    * Change disable of mcollective service in start.sh,
      rather than blocking init script in Dockerfile
    * Cleaned up duplicate packages already included in
      fuel/centos base container.
    * Moved hiera default yaml workaround to fuel/centos
    
    Change-Id: Ifd8740735c676f6e500795aa05123ede4d72ae92
    Closes-Bug: #1482232

--- docker/mcollective/start.sh
+++ docker/mcollective/start.sh
@@ -6,6 +6,10 @@ rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
 puppet apply -d -v /etc/puppet/modules/mcollective/examples/mcollective-server-only.pp
+
+#Stop daemon and restart it in the foreground
+service mcollective stop
+
 sed -e 's/daemonize[[:space:]]*=[[:space:]]*1/daemonize = 0/g' -i /etc/mcollective/server.cfg
 /usr/sbin/mcollectived --pid=/var/run/mcollectived.pid --config=/etc/mcollective/server.cfg
 
