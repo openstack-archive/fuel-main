1ce996adc8667437dae94784ffb2e37b55147b32
Aleksandr Didenko <adidenko@mirantis.com>
2015-11-16 12:58:43 +0100

    Dnsmasq config clean up
    Remove no longer needed conditions in docker start scripts.
    
    Partial blueprint: dynamic-dnsmasq
    
    Change-Id: I7f88d6fa99927b2b3e761a5733ecf0284166a5b9

--- docker/mcollective/start.sh
+++ docker/mcollective/start.sh
@@ -5,10 +5,7 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
-# TODO: remove 'test -f' when I9d7428c9fc21c705a1aee7fbca8003829a81e1d0
-# is merged
-test -f /etc/puppet/modules/nailgun/examples/hiera-for-container.pp && \
-  puppet apply -d -v /etc/puppet/modules/nailgun/examples/hiera-for-container.pp
+puppet apply -d -v /etc/puppet/modules/nailgun/examples/hiera-for-container.pp
 
 # TODO(bpiotrowski): remove old file path after new ISO is used on CI
 if [[ -f /etc/puppet/modules/mcollective/examples/mcollective-server-only.pp ]]; then
