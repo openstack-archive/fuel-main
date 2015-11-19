34e5603de767f72f81e614a96bc58dddd3e255a6
Dmitry Teselkin <dteselkin@mirantis.com>
2015-11-19 17:02:52 +0300

    Update docker containers for CentOS7
    In order to migrate docker containers to CentOS7 the following
    fixes and improvements were made:
    
    * systemd service is a PID 1 service in each container instead
      of 'start.sh'
    * 'start.sh' script converted to systemd service and started
      automatically every time a container is launched
    * Dockerfiles simplified by removing all setup stuff to
      separate 'setup.sh' file which should be launched explicitely
      during container preparation phase
    
    Blueprint: master-on-centos7
    
    Is NOT compatible with CentOS6 master node
    
    Co-Authored-By: Ivan Suzdal <isuzdal@mirantis.com>
    
    Change-Id: I5f51f473a6fa783c0a85985a5bf0412b30bcb1c2

--- docker/fuel-centos-build/start.sh
+++ docker/fuel-centos-build/start.sh
@@ -5,24 +5,52 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
-#FIXME(mattymo): Update CentOS to 6.6
-yum downgrade -y libcom_err libselinux libselinux-utils
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=file:///repo/os/x86_64/
+gpgcheck=0
+
+[mos]
+name=MOS Local Repo
+baseurl=file:///mos-repo/
+gpgcheck=0
+EOF
+
+for repo in ${EXTRA_RPM_REPOS}; do
+  IFS=, repo_args=($repo)
+  cat << EOF >> /etc/yum.repos.d/nailgun.repo
+
+[extra-repo-${repo_args[0]}]
+name=MOS Extra Repo ${repo_args[0]}
+baseurl=file:///extra-repos/${repo_args[0]}
+gpgcheck=0
+EOF
+done
 
 # Install necessary packages
-yum install -y sudo ami-creator python-daemon httpd
+yum install -y --quiet \
+  sudo \
+  ami-creator \
+  python-daemon \
+  httpd
 
 sed -i '/requiretty/s/^/#/g' /etc/sudoers
 
+
 # Create loop devices if needed for ami-creator to setup image
-for loopdev in `seq 1 9`; do
+for loopdev in $(seq 1 9); do
   mknod "/dev/loop${loopdev}" -m0660 b 7 ${loopdev} || :
 done
+
 
 # Start webserver and wait for it to be up
 mkdir -p /var/www/html/repo
 ln -s /repo/os /var/www/html/repo/os
 ln -s /mos-repo /var/www/html/
-service httpd start
+
+/usr/sbin/httpd &
+/usr/lib/systemd/systemd-localed &
 
 cd /export
 ami-creator -c /root/fuel-centos.ks -n fuel-centos
