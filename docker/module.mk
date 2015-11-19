305e783e8f8c105b93f94c2a33c12a975ecde46c
Dmitry Teselkin <dteselkin@mirantis.com>
2015-11-19 17:02:52 +0300

    Update docker containers for CentOS7
    In order to migrate docker containers to CentOS7 the following
    fixes and improvements were made:
    
    * systemd service is a PID 1 service in each container instead
      of 'start.sh'
    * 'start.sh' script converted to systemd service and started
      automatically every time a container is launched
    * Dockerfiles simplified by removing all setup stuff to
      separate 'setup.sh' file which should be launched explicitely
      during container preparation phase
    
    Blueprint: master-on-centos7
    
    Is NOT compatible with CentOS6 master node
    
    Co-Authored-By: Ivan Suzdal <isuzdal@mirantis.com>
    
    Change-Id: I5f51f473a6fa783c0a85985a5bf0412b30bcb1c2

--- docker/module.mk
+++ docker/module.mk
@@ -23,7 +23,7 @@ else
 $(BUILD_DIR)/docker/build.done: \
 		$(BUILD_DIR)/docker/fuel-centos.done \
 		$(BUILD_DIR)/docker/sources.done
-	sudo docker save fuel/centos busybox $(foreach cnt,$(containers), fuel/$(cnt)_$(PRODUCT_VERSION)) > $(BUILD_DIR)/docker/fuel-images.tar
+	sudo docker save fuel/centos busybox $(foreach cnt,$(containers), fuel/$(cnt)_$(PRODUCT_VERSION):latest) > $(BUILD_DIR)/docker/fuel-images.tar
 	lrzip -L2 -U -D -f $(BUILD_DIR)/docker/fuel-images.tar -o $(BUILD_DIR)/docker/$(DOCKER_ART_NAME)
 	rm -f $(BUILD_DIR)/docker/fuel-images.tar
 	sudo docker rm -f "$(REPO_CONTAINER)" || true
@@ -41,16 +41,29 @@ $(BUILD_DIR)/docker/$1.done: \
 		$(BUILD_DIR)/docker/fuel-centos.done \
 		$(BUILD_DIR)/iso/isoroot/$(VERSION_YAML_ART_NAME) \
 		$(BUILD_DIR)/docker/repo-container-up.done
-	mkdir -p "$(BUILD_DIR)/docker/containers" && \
-	rm -rf $(BUILD_DIR)/docker/$1 && \
-	cp -a $(SOURCE_DIR)/docker/$1 $(BUILD_DIR)/docker/$1 && \
+	@echo ""
+	@echo "========================================"
+	@echo "Building container '$1'"
+	mkdir -p "$(BUILD_DIR)/docker/containers"
+	rm -rf $(BUILD_DIR)/docker/$1
+	cp -a $(SOURCE_DIR)/docker/$1 $(BUILD_DIR)/docker/$1
 	REPO_PORT=`sudo docker port $(REPO_CONTAINER) 80 | cut -d':' -f2` && \
-	sed -e "s/_PORT_/$$$${REPO_PORT}/" -i $(BUILD_DIR)/docker/$1/Dockerfile && \
-	mkdir -p $(BUILD_DIR)/docker/$1/etc/fuel && \
-	cp $(BUILD_DIR)/iso/isoroot/version.yaml $(BUILD_DIR)/docker/$1/etc/fuel/version.yaml && \
-	sed -e 's/production:.*/production: "docker-build"/' -i $(BUILD_DIR)/docker/$1/etc/fuel/version.yaml && \
-	cp $(SOURCE_DIR)/docker/docker-astute.yaml $(BUILD_DIR)/docker/$1/etc/fuel/astute.yaml && \
+		sed -e "s/_PORT_/$$$${REPO_PORT}/" -i $(BUILD_DIR)/docker/$1/Dockerfile
+	sed -e "s/_EXTRA_RPM_REPOS_/$(EXTRA_RPM_REPOS)/" -i $(BUILD_DIR)/docker/$1/Dockerfile
+	mkdir -p $(BUILD_DIR)/docker/$1/etc/fuel
+	cp $(BUILD_DIR)/iso/isoroot/version.yaml $(BUILD_DIR)/docker/$1/etc/fuel/version.yaml
+	sed -e 's/production:.*/production: "docker-build"/' -i $(BUILD_DIR)/docker/$1/etc/fuel/version.yaml
+	cp $(SOURCE_DIR)/docker/docker-astute.yaml $(BUILD_DIR)/docker/$1/etc/fuel/astute.yaml
 	sudo docker build --force-rm -t fuel/$1_$(PRODUCT_VERSION) $(BUILD_DIR)/docker/$1
+	sudo docker run --name=FUEL_$1_$(PRODUCT_VERSION) \
+		--net=bridge -d -i -t --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro fuel/$1_$(PRODUCT_VERSION)
+	sudo docker exec -i FUEL_$1_$(PRODUCT_VERSION) /usr/local/bin/setup.sh 2>&1 \
+		|| (echo "CONTAINER BUILD FAILED, container '$1'"; exit 1)
+	sudo docker commit FUEL_$1_$(PRODUCT_VERSION) fuel/$1_$(PRODUCT_VERSION):latest
+	sudo docker stop FUEL_$1_$(PRODUCT_VERSION)
+	@echo "CONTAINER BUILD SUCCEEDED, container '$1'."
+	@echo "========================================"
+	@echo ""
 	$$(ACTION.TOUCH)
 endef
 
@@ -63,19 +76,31 @@ $(BUILD_DIR)/docker/fuel-centos.done: \
 		$(BUILD_DIR)/docker/base-images.done \
 		$(BUILD_DIR)/mirror/centos/build.done \
 		$(BUILD_DIR)/packages/rpm/build.done
-	rm -rf $(BUILD_DIR)/docker/fuel-centos-build && \
-	cp -a $(SOURCE_DIR)/docker/fuel-centos-build $(BUILD_DIR)/docker/fuel-centos-build && \
-	sudo docker build -t fuel/fuel-centos-build $(BUILD_DIR)/docker/fuel-centos-build && \
-	mkdir -p $(BUILD_DIR)/docker/fuel-centos/ && \
-	echo "Generating fuel/centos base image. Refer to $(BUILD_DIR)/docker/fuel-centos-build.log if it fails." && \
-	sudo docker -D run --net=bridge --rm -a stdout -a stderr -i -t --privileged -v $(LOCAL_MIRROR_CENTOS):/repo:ro -v $(LOCAL_MIRROR_MOS_CENTOS):/mos-repo:ro -v $(BUILD_DIR)/docker/fuel-centos:/export fuel/fuel-centos-build 2>&1 > $(BUILD_DIR)/docker/fuel-centos-build.log && \
+	rm -rf $(BUILD_DIR)/docker/fuel-centos-build
+	cp -a $(SOURCE_DIR)/docker/fuel-centos-build $(BUILD_DIR)/docker/fuel-centos-build
+	sed -e "s/_EXTRA_RPM_REPOS_/$(EXTRA_RPM_REPOS)/" -i $(BUILD_DIR)/docker/fuel-centos-build/Dockerfile
+	sudo docker build -t fuel/fuel-centos-build $(BUILD_DIR)/docker/fuel-centos-build
+	mkdir -p $(BUILD_DIR)/docker/fuel-centos/
+	echo ">>> Generating fuel/centos base image..."
+	sudo docker -D run --name=FUEL_CENTOS_$(PRODUCT_VERSION) --net=bridge -d -i -t --privileged \
+		-v $(LOCAL_MIRROR_CENTOS):/repo:ro \
+		-v $(LOCAL_MIRROR_MOS_CENTOS):/mos-repo:ro \
+		-v $(LOCAL_MIRROR)/extra-repos:/extra-repos:ro \
+		-v $(BUILD_DIR)/docker/fuel-centos:/export \
+		fuel/fuel-centos-build
+	sudo docker exec -i FUEL_CENTOS_$(PRODUCT_VERSION) /usr/local/bin/start.sh 2>&1
+	echo "<<< Image generated successfully."
+	echo ">>> Converting image..."
 	sudo $(SOURCE_DIR)/docker/fuel-centos-build/img2docker.sh $(BUILD_DIR)/docker/fuel-centos/fuel-centos.img fuel/centos
+	echo "<<< Image converted successfully."
+	echo "$@ done."
 	$(ACTION.TOUCH)
 
 $(BUILD_DIR)/docker/repo-container-up.done: \
 		$(BUILD_DIR)/docker/fuel-centos.done
 	-sudo docker rm -f "$(REPO_CONTAINER)"
-	sudo docker -D run -d -p 80 -v $(LOCAL_MIRROR_CENTOS):/var/www/html/repo -v $(LOCAL_MIRROR_MOS_CENTOS):/var/www/html/mos-repo --name "$(REPO_CONTAINER)" fuel/centos /usr/sbin/apachectl -DFOREGROUND
+	sudo docker -D run --name "$(REPO_CONTAINER)" -d -p 80 \
+		-v $(LOCAL_MIRROR_CENTOS):/var/www/html/repo -v $(LOCAL_MIRROR_MOS_CENTOS):/var/www/html/mos-repo/fuel/centos /usr/sbin/apachectl -DFOREGROUND
 	REPO_PORT=`sudo docker port $(REPO_CONTAINER) 80 | cut -d':' -f2` && \
 	wget -t10 -T1 -O /dev/null --waitretry 1 --retry-connrefused --no-proxy http://127.0.0.1:$${REPO_PORT}/repo/os/x86_64/repodata/repomd.xml && \
 	wget -t10 -T1 -O /dev/null --waitretry 1 --retry-connrefused --no-proxy http://127.0.0.1:$${REPO_PORT}/mos-repo/repodata/repomd.xml
