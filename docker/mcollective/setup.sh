374d39fa0261b0afe41ece83a4e5c4d01f799b65
Dmitry Teselkin <dteselkin@mirantis.com>
2015-11-19 17:02:52 +0300

    Update docker containers for CentOS7
    In order to migrate docker containers to CentOS7 the following
    fixes and improvements were made:
    
    * systemd service is a PID 1 service in each container instead
      of 'start.sh'
    * 'start.sh' script converted to systemd service and started
      automatically every time a container is launched
    * Dockerfiles simplified by removing all setup stuff to
      separate 'setup.sh' file which should be launched explicitely
      during container preparation phase
    
    Blueprint: master-on-centos7
    
    Is NOT compatible with CentOS6 master node
    
    Co-Authored-By: Ivan Suzdal <isuzdal@mirantis.com>
    
    Change-Id: I5f51f473a6fa783c0a85985a5bf0412b30bcb1c2

---
+++ docker/mcollective/setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -xe
+
+rm -rf /etc/yum.repos.d/*
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=http://$(route -n | awk '/^0.0.0.0/ {print $2}'):${DOCKER_PORT}/repo/os/x86_64/
+gpgcheck=0
+
+[mos]
+name=MOS Local Repo
+baseurl=http://$(route -n | awk '/^0.0.0.0/ {print $2}'):${DOCKER_PORT}/mos-repo/
+gpgcheck=0
+EOF
+
+yum clean expire-cache
+yum update -y
+
+yum install -y --quiet \
+  sudo \
+  mcollective \
+  shotgun \
+  fuel-agent \
+  fuel-provisioning-scripts \
+  psmisc
+
+# /var/lib/fuel/ibp is a mount point for IBP host volume
+mkdir -p /var/lib/hiera /var/lib/fuel/ibp
+touch /etc/puppet/hiera.yaml /var/lib/hiera/common.yaml
+
+
+systemctl daemon-reload
+puppet apply --debug --verbose --color false --detailed-exitcodes \
+  /etc/puppet/modules/nailgun/examples/mcollective-only.pp || [[ $? == 2 ]]
+
+
+#FIXME(mattymo): Workaround to make diagnostic snapshots work
+mkdir -p /opt/nailgun/bin /var/www/nailgun/dump
+ln -s /usr/bin/nailgun_dump /opt/nailgun/bin/nailgun_dump
+
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=file:/var/www/nailgun/centos/x86_64
+gpgcheck=0
+
+[mos]
+name=MOS Local Repo
+baseurl=file:/var/www/nailgun/mos-centos/x86_64
+gpgcheck=0
+EOF
+
+yum clean all
+
+
+systemctl enable start-container.service
