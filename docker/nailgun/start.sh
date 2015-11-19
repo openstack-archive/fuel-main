cdd5ba46236ba7ed7db28162a8eee45e1a0a38d0
Dmitry Teselkin <dteselkin@mirantis.com>
2015-11-19 17:02:52 +0300

    Update docker containers for CentOS7
    In order to migrate docker containers to CentOS7 the following
    fixes and improvements were made:
    
    * systemd service is a PID 1 service in each container instead
      of 'start.sh'
    * 'start.sh' script converted to systemd service and started
      automatically every time a container is launched
    * Dockerfiles simplified by removing all setup stuff to
      separate 'setup.sh' file which should be launched explicitely
      during container preparation phase
    
    Co-Authored-By: Ivan Suzdal <isuzdal@mirantis.com>
    
    Change-Id: I5f51f473a6fa783c0a85985a5bf0412b30bcb1c2

--- docker/nailgun/start.sh
+++ docker/nailgun/start.sh
@@ -5,10 +5,25 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
+
 #Workaround so nailgun can see version.yaml
 ln -sf /etc/fuel/version.yaml /etc/nailgun/version.yaml
-#Run puppet to apply custom config
-puppet apply -v /etc/puppet/modules/nailgun/examples/nailgun-only.pp
 
-service supervisord stop
-/usr/bin/supervisord -n
+
+#Run puppet to apply custom config
+systemctl daemon-reload
+
+puppet apply --debug --verbose --color false --detailed-exitcodes \
+  /etc/puppet/modules/nailgun/examples/nailgun-only.pp
+
+
+#FIXME(dteselkin): for some funny reason /usr/share/nailgun/static
+#                  is empty after puppet apply, despite the fact that
+#                  package contains the files
+#                  Reinstall package as a dirty workaround
+if rpm -V fuel-nailgun | grep -q missing; then
+    yum -y --quiet reinstall fuel-nailgun
+fi
+
+
+systemctl restart supervisord.service
