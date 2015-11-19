e9067bf230b029a56524f7001c72c5f07663c19e
Dmitry Teselkin <dteselkin@mirantis.com>
2015-11-19 17:02:52 +0300

    Update docker containers for CentOS7
    In order to migrate docker containers to CentOS7 the following
    fixes and improvements were made:
    
    * systemd service is a PID 1 service in each container instead
      of 'start.sh'
    * 'start.sh' script converted to systemd service and started
      automatically every time a container is launched
    * Dockerfiles simplified by removing all setup stuff to
      separate 'setup.sh' file which should be launched explicitely
      during container preparation phase
    
    Blueprint: master-on-centos7
    
    Is NOT compatible with CentOS6 master node
    
    Co-Authored-By: Ivan Suzdal <isuzdal@mirantis.com>
    
    Change-Id: I5f51f473a6fa783c0a85985a5bf0412b30bcb1c2

--- docker/postgres/start.sh
+++ docker/postgres/start.sh
@@ -5,18 +5,18 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
-# Because /var/lib/pgsql is mounted as a volume, reinstall if its files are
-# missing
+
+# Because /var/lib/pgsql is mounted as a volume,
+# reinstall if its files are missing
 if rpm -V postgresql-server | grep -q missing; then
   yum reinstall -q -y postgresql-server
 fi
 
-puppet apply -v /etc/puppet/modules/nailgun/examples/postgres-only.pp
-if [ -f '/etc/init.d/postgresql' ]; then
-  service postgresql stop
-  sudo -u postgres /usr/bin/postmaster -p 5432 -D /var/lib/pgsql/data
-else
-  pgver=$(rpm -q --queryformat '%{VERSION}' postgresql | cut -c'1-3')
-  service "postgresql-${pgver}" stop
-  sudo -u postgres "/usr/pgsql-${pgver}/bin/postmaster" -p 5432 -D "/var/lib/pgsql/${pgver}/data"
-fi
+
+systemctl daemon-reload
+
+puppet apply --debug --verbose --color false --detailed-exitcodes \
+  /etc/puppet/modules/nailgun/examples/postgres-only.pp
+
+
+systemctl enable postgresql.service
--- docker/rabbitmq/start.sh
+++ docker/rabbitmq/start.sh
@@ -5,19 +5,18 @@
 rm -f /var/lib/rpm/__db.*
 rpm --rebuilddb
 
+
 mkdir -p /var/log/rabbitmq
 chown -R rabbitmq:rabbitmq /var/log/rabbitmq
 
-exitcode=0
-puppet apply --detailed-exitcodes -v /etc/puppet/modules/nailgun/examples/rabbitmq-only.pp || exitcode=$?
-if [[ $exitcode != 0 && $exitcode != 2 ]]; then
-  echo Puppet apply failed with exit code: $exitcode
-  exit $exitcode
-fi
 
-set +e
-service rabbitmq-server stop
+systemctl daemon-reload
+puppet apply --debug --verbose --color false --detailed-exitcodes \
+  /etc/puppet/modules/nailgun/examples/rabbitmq-only.pp || [[ $? == 2 ]]
+
+
+systemctl stop rabbitmq-server.service
 # Just in case stopping service fails
 pkill -u rabbitmq
 
-/usr/sbin/rabbitmq-server
+systemctl start rabbitmq-server.service
