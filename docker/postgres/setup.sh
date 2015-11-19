e3160228978216342018773510a3baeb4e111e69
Dmitry Teselkin <dteselkin@mirantis.com>
2015-11-19 17:02:52 +0300

    Update docker containers for CentOS7
    In order to migrate docker containers to CentOS7 the following
    fixes and improvements were made:
    
    * systemd service is a PID 1 service in each container instead
      of 'start.sh'
    * 'start.sh' script converted to systemd service and started
      automatically every time a container is launched
    * Dockerfiles simplified by removing all setup stuff to
      separate 'setup.sh' file which should be launched explicitely
      during container preparation phase
    
    Co-Authored-By: Ivan Suzdal <isuzdal@mirantis.com>
    
    Change-Id: I5f51f473a6fa783c0a85985a5bf0412b30bcb1c2

---
+++ docker/postgres/setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -xe
+
+rm -rf /etc/yum.repos.d/*
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=http://$(route -n | awk '/^0.0.0.0/ {print $2}'):${DOCKER_PORT}/os/x86_64/
+gpgcheck=0
+EOF
+
+yum clean expire-cache
+yum update -y
+
+
+touch /etc/sysconfig/network
+
+mkdir -p /var/lib/hiera
+touch /var/lib/hiera/common.yaml
+
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=file:/var/www/nailgun/centos/x86_64
+gpgcheck=0
+EOF
+
+yum clean all
+
+
+systemctl enable start-container.service
