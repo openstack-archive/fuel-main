fca6415653d91f1bd422fb91f21110f45b1e238b
Dmitry Teselkin <dteselkin@mirantis.com>
2015-11-19 17:02:52 +0300

    Update docker containers for CentOS7
    In order to migrate docker containers to CentOS7 the following
    fixes and improvements were made:
    
    * systemd service is a PID 1 service in each container instead
      of 'start.sh'
    * 'start.sh' script converted to systemd service and started
      automatically every time a container is launched
    * Dockerfiles simplified by removing all setup stuff to
      separate 'setup.sh' file which should be launched explicitely
      during container preparation phase
    
    Change-Id: I5f51f473a6fa783c0a85985a5bf0412b30bcb1c2

---
+++ docker/rsync/setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -xe
+
+rm -rf /etc/yum.repos.d/*
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=http://$(route -n | awk '/^0.0.0.0/ {print $2}'):${DOCKER_PORT}/os/x86_64/
+gpgcheck=0
+EOF
+
+yum clean expire-cache
+yum update -y
+
+
+systemctl daemon-reload
+puppet apply --debug --verbose --color false --detailed-exitcodes \
+  /etc/puppet/modules/nailgun/examples/puppetsync-only.pp || [[ $? == 2 ]]
+
+
+mkdir -p /etc/systemd/system/rsyncd.service.d/
+cat << EOF > /etc/systemd/system/rsyncd.service.d/restart.conf
+[Service]
+Restart=always
+RestartSec=5
+StartLimitAction=reboot-force
+EOF
+
+systemctl enable xinetd.service rsyncd.service
+
+
+cat << EOF > /etc/yum.repos.d/nailgun.repo
+[nailgun]
+name=Nailgun Local Repo
+baseurl=file:/var/www/nailgun/centos/x86_64
+gpgcheck=0
+EOF
+
+yum clean all
+
+
+systemctl mask start-container.service
