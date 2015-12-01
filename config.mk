51276fffd547f11fbd6dfaee09e4cd831e811369
Roman Vyalov <rvyalov@mirantis.com>
2015-12-01 19:27:24 +0300

    Move to the CentOS7 on master node
    Change centos verison from 6.6 to 7.1.1503
    
    Blueprint: master-on-centos7
    
    Change-Id: I788dace124e53e7c5ed5a511ec5fb6efdd0258d9
    Breaks: centos6

--- config.mk
+++ config.mk
@@ -63,8 +63,8 @@ MASTER_DNS?=10.20.0.1
 MASTER_NETMASK?=255.255.255.0
 MASTER_GW?=10.20.0.1
 
-CENTOS_MAJOR?=6
-CENTOS_MINOR?=6
+CENTOS_MAJOR?=7
+CENTOS_MINOR?=1
 CENTOS_RELEASE:=$(CENTOS_MAJOR).$(CENTOS_MINOR)
 CENTOS_ARCH:=x86_64
 CENTOS_IMAGE_RELEASE:=$(CENTOS_MAJOR)$(CENTOS_MINOR)
@@ -160,7 +160,7 @@ USE_MIRROR?=ext
 
 ifeq ($(USE_MIRROR),ext)
 MIRROR_FUEL?=http://mirror.fuel-infra.org/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)-fuel/os/x86_64/
-MIRROR_CENTOS?=http://vault.centos.org/$(CENTOS_RELEASE)
+MIRROR_CENTOS?=http://mirror.centos.org/centos/$(CENTOS_MAJOR)
 MIRROR_CENTOS_KERNEL?=$(MIRROR_CENTOS)
 SANDBOX_MIRROR_CENTOS_UPSTREAM?=$(MIRROR_CENTOS)
 MIRROR_UBUNTU?=mirror.fuel-infra.org
@@ -208,10 +208,10 @@ endif
 # The actual name will be constracted prepending "yum_repo_" prefix.
 # Example: YUM_REPOS?=official epel => yum_repo_official and yum_repo_epel
 # will be used.
-YUM_REPOS?=official fuel subscr_manager
-MIRROR_CENTOS?=http://mirrors-local-msk.msk.mirantis.net/centos-$(PRODUCT_VERSION)/$(CENTOS_RELEASE)
+YUM_REPOS?=official extras fuel
+MIRROR_CENTOS?=http://mirror.centos.org/centos/$(CENTOS_MAJOR)
 MIRROR_CENTOS_KERNEL?=$(MIRROR_CENTOS)
-SANDBOX_MIRROR_CENTOS_UPSTREAM?=http://mirrors-local-msk.msk.mirantis.net/centos-$(PRODUCT_VERSION)/$(CENTOS_RELEASE)
+SANDBOX_MIRROR_CENTOS_UPSTREAM?=$(MIRROR_CENTOS)
 SANDBOX_MIRROR_EPEL?=http://mirror.yandex.ru/epel
 MIRROR_UBUNTU_METHOD?=http
 MIRROR_UBUNTU?=osci-mirror-msk.msk.mirantis.net
@@ -272,3 +272,14 @@ SANDBOX_COPY_CERTS?=0
 #  \apt/metadata.json
 #  \concat/metadata.json
 USE_PREDEFINED_FUEL_LIB_PUPPET_MODULES?=
+
+# If the URL given ended with target.txt then is't a pointer to a snapshot that
+# should be unlinked. If it is not - return it as is.
+expand_repo_url=$(shell url=$1; echo $${url} | grep -q -e '.*\.target\.txt$$' && echo "$${url%/*}/$$(curl $$url)/x86_64/" || echo $${url})
+
+# Expand repo URLs now
+MIRROR_CENTOS=$(call expand_repo_url,$(MIRROR_CENTOS))
+MIRROR_CENTOS_KERNEL=$(call expand_repo_url,$(MIRROR_CENTOS_KERNEL))
+SANDBOX_MIRROR_CENTOS_UPSTREAM?=$(call expand_repo_url,$(SANDBOX_MIRROR_CENTOS_UPSTREAM))
+MIRROR_FUEL=$(call expand_repo_url,$(MIRROR_FUEL))
+
