4523b1ef235c74e2721290d9b0cd10f652bdc562
Sergey Kulanov <skulanov@mirantis.com>
2015-12-01 22:20:39 +0200

    [WIP] Install packages from build group
    Change-Id: Ifd9d7c6a35c4977e78eba10ffcd0b1a8dcfee4ca

--- sandbox.mk
+++ sandbox.mk
@@ -63,7 +63,6 @@ EOF
 cp /etc/resolv.conf $(SANDBOX)/etc/resolv.conf
 cp /etc/hosts $(SANDBOX)/etc/hosts
 cat > $(SANDBOX)/etc/yum.repos.d/base.repo <<EOF
-$(yum_upstream_repo)
 $(yum_epel_repo)
 $(yum_local_repo)
 $(yum_local_mos_repo)
@@ -79,7 +78,6 @@ full_match=1
 EOF
 sudo rpm -i --root=$(SANDBOX) `find $(LOCAL_MIRROR_CENTOS_OS_BASEURL) -name "centos-release*rpm" | head -1` || \
 echo "centos-release already installed"
-sudo rm -f $(SANDBOX)/etc/yum.repos.d/Cent*
 echo 'Rebuilding RPM DB'
 sudo rpm --root=$(SANDBOX) --rebuilddb
 echo 'Installing packages for Sandbox'
