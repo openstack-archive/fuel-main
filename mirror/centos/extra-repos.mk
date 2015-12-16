11b2473aa599d2e3fcbfbe2671620e0196def514
Dmitry Teselkin <dteselkin@mirantis.com>
2015-12-16 16:40:18 +0300

    [WIP] hardcode MIRROR_FUEL
    Change-Id: I4fb9c6fad709f54d95719fd39d9cad30eff49755

--- mirror/centos/extra-repos.mk
+++ mirror/centos/extra-repos.mk
@@ -18,7 +18,12 @@ $(BUILD_DIR)/mirror/centos/extra-repos.done:
 	$(ACTION.TOUCH)
 
 define extra_repo_download
-set -ex ; reposync --downloadcomps --plugins --delete --arch=$(CENTOS_ARCH) \
+set -ex ; \
+    cat $(BUILD_DIR)/mirror/centos/etc/yum.conf; \
+    yum -c $(BUILD_DIR)/mirror/centos/etc/yum.conf clean all; \
+    yum -c $(BUILD_DIR)/mirror/centos/etc/yum.conf makecache; \
+    yum -c $(BUILD_DIR)/mirror/centos/etc/yum.conf repolist -v; \
+    reposync --downloadcomps --plugins --delete --arch=$(CENTOS_ARCH) \
     -c $(BUILD_DIR)/mirror/centos/etc/yum.conf --repoid=$(call get_repo_name,$1) \
     -p $(LOCAL_MIRROR)/extra-repos/
 endef
