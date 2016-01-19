c3812ca4e4bef926b8a66152340567b2a4b1b595
Ivan Suzdal <isuzdal@mirantis.com>
2016-01-19 13:38:19 +0300

    Enable to use CR centos repo
    Change-Id: Ied882df73ef811d83c23f9663bc4479fb2c063e3

--- mirror/centos/module.mk
+++ mirror/centos/module.mk
@@ -6,18 +6,21 @@ include $(SOURCE_DIR)/mirror/centos/boot.mk
 include $(SOURCE_DIR)/mirror/centos/mos-repo.mk
 # This module downloads extra rpm repositories
 include $(SOURCE_DIR)/mirror/centos/extra-repos.mk
-
+# This module downloads cr rpm repository
+include $(SOURCE_DIR)/mirror/centos/cr-repo.mk
 
 $(BUILD_DIR)/mirror/centos/build.done: \
 		$(BUILD_DIR)/mirror/centos/repo.done \
 		$(BUILD_DIR)/mirror/centos/boot.done \
 		$(BUILD_DIR)/mirror/centos/mos-repo.done \
-		$(BUILD_DIR)/mirror/centos/extra-repos.done
+		$(BUILD_DIR)/mirror/centos/extra-repos.done \
+		$(BUILD_DIR)/mirror/centos/cr-repo.done
 	$(ACTION.TOUCH)
 
 mirror-centos: $(BUILD_DIR)/mirror/centos/build.done
 repo-centos: $(BUILD_DIR)/mirror/centos/repo.done
 repo-mos-centos: $(BUILD_DIR)/mirror/centos/mos-repo.done
 extra-repos-centos: $(BUILD_DIR)/mirror/centos/extra-repos.done
+cr-repo: $(BUILD_DIR)/mirror/centos/cr-repo.done
 
-.PHONY: mirror-centos repo-centos repo-mos-centos extra-repos-centos
+.PHONY: mirror-centos repo-centos repo-mos-centos extra-repos-centos cr-repo
