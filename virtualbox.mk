c03e81255c083ebb3f0a235a648eeb7400615c96
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2016-02-23 00:40:50 +0300

    Removed virtualbox scripts
    Virtualbox scripts have been moved to a separate git
    repository https://review.openstack.org/#/c/279074/
    
    Change-Id: Ib7d38ff46fc9c632115c833287a45ac4f5b30ab2

--- virtualbox.mk
+++
@@ -1,9 +0,0 @@
-########################
-# VBOX-SCRIPTS ARTIFACT
-########################
-vbox-scripts: $(VBOX_SCRIPTS_PATH)
-
-$(VBOX_SCRIPTS_PATH): \
-		$(call find-files,$(SOURCE_DIR)/virtualbox)
-	mkdir -p $(@D)
-	cd $(SOURCE_DIR) && zip -r $@ virtualbox
--- virtualbox/iso/README.md
+++
@@ -1,1 +0,0 @@
-Put ISO in this folder
