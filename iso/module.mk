709028dc35e03da0567006609a7dd35b555c4d96
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2016-02-23 19:30:09 +0300

    [WIP] Do not review (for testing)
    Change-Id: I62b19a36509216f75f48754822e54061d8fd846c
    Depends-On: I6025c18fa6f8b65adb1bd25e6c5061e871494c82

--- iso/module.mk
+++ iso/module.mk
@@ -3,119 +3,149 @@
 
 all: iso listing
 
+ISO_PATH?=$(ARTS_DIR)/$(ISO_NAME).iso
 ISOROOT:=$(BUILD_DIR)/iso/isoroot
 
 iso: $(ISO_PATH)
 
-listing: $(BUILD_DIR)/iso/isoroot.done $(BUILD_DIR)/mirror/build.done
-	-find $(BUILD_DIR)/iso/isoroot \
+##################
+# INFRA ARTIFACTS
+##################
+$(ARTS_DIR)/listing-build.txt: $(BUILD_DIR)/iso/isoroot.done
+	mkdir -p $(ARTS_DIR)
+	find $(BUILD_DIR) > $@.tmp
+	mv $@.tmp $@
+
+$(ARTS_DIR)/config.yaml: $(CONFIG_YAML); $(ACTION.COPY)
+$(ARTS_DIR)/mirror.yaml: $(MIRROR_YAML); $(ACTION.COPY)
+
+$(ARTS_DIR)/listing-package-changelog.txt: $(BUILD_DIR)/iso/isoroot.done
+	mkdir -p $(ARTS_DIR)
+	find $(BUILD_DIR)/iso/isoroot \
 		-regextype posix-egrep \
 		-regex '.*(fuel|astute|network-checker|nailgun|packetary|shotgun).*\.rpm' | \
 			while read package_file; do \
 				echo; \
 				echo $$(basename $$package_file); \
 				rpm -q --changelog -p $$package_file | head -12; \
-			done > $(BUILD_DIR)/listing-package-changelog.txt
+			done > $@.tmp
+	mv $@.tmp $@
+
+listing: \
+		$(ARTS_DIR)/listing-build.txt \
+		$(ARTS_DIR)/listing-package-changelog.txt \
+		$(ARTS_DIR)/config.yaml \
+		$(ARTS_DIR)/mirror.yaml
 
 ###################
 # BUILD IDENTIFIERS
 ###################
-
 ifdef BUILD_NUMBER
 $(BUILD_DIR)/iso/isoroot.done: $(ISOROOT)/fuel_build_number
 $(ISOROOT)/fuel_build_number:
+	@mkdir -p $(@D)
 	echo "$(BUILD_NUMBER)" > $@
 endif
 
 ifdef BUILD_ID
 $(BUILD_DIR)/iso/isoroot.done: $(ISOROOT)/fuel_build_id
 $(ISOROOT)/fuel_build_id:
+	@mkdir -p $(@D)
 	echo "$(BUILD_ID)" > $@
 endif
 
-###############
-# CENTOS MIRROR
-###############
-$(BUILD_DIR)/iso/isoroot-centos.done: \
-		$(BUILD_DIR)/mirror/build.done \
-		$(BUILD_DIR)/mirror/make-changelog.done \
-		$(BUILD_DIR)/packages/build.done \
-		$(BUILD_DIR)/iso/isoroot-dotfiles.done
-	mkdir -p $(ISOROOT)
-	rsync -rp $(LOCAL_MIRROR_CENTOS_OS_BASEURL)/ $(ISOROOT)
-	rsync -rp $(LOCAL_MIRROR_MOS_CENTOS) $(ISOROOT)
-	rsync -rp $(LOCAL_MIRROR)/extra-repos $(ISOROOT)
-	rsync -rp $(LOCAL_MIRROR)/centos-packages.changelog $(ISOROOT)
+#########
+# IMAGES
+#########
+# TODO(kozhukalov): implement downloading images in python
+IMAGES_BASEURL?=$(call $(notdir $(MIRROR_YAML))_0_url)
+IMAGES?=\
+isolinux/boot.msg \
+isolinux/grub.conf \
+isolinux/initrd.img \
+isolinux/isolinux.bin \
+isolinux/memtest \
+isolinux/vesamenu.c32 \
+isolinux/vmlinuz \
+images/efiboot.img \
+images/boot.iso \
+images/pxeboot/initrd.img \
+images/pxeboot/vmlinuz \
+EFI/BOOT/BOOTX64.EFI \
+EFI/BOOT/MokManager.efi \
+EFI/BOOT/grub.cfg \
+EFI/BOOT/grubx64.efi \
+LiveOS/squashfs.img
+
+# centos boot images
+$(addprefix $(ISOROOT)/,$(IMAGES)):
+	@mkdir -p $(@D)
+	wget -nv -O $@.tmp $(IMAGES_BASEURL)/$(subst $(ISOROOT)/,,$@)
+	mv $@.tmp $@
+
+$(BUILD_DIR)/iso/isoroot-images.done: $(addprefix $(ISOROOT)/,$(IMAGES))
 	$(ACTION.TOUCH)
 
-###############
-# UBUNTU MIRROR
-###############
-$(BUILD_DIR)/iso/isoroot-ubuntu.done: \
-		$(BUILD_DIR)/mirror/build.done \
-		$(BUILD_DIR)/mirror/make-changelog.done \
-		$(BUILD_DIR)/packages/build.done \
-		$(BUILD_DIR)/iso/isoroot-dotfiles.done
-	mkdir -p $(ISOROOT)/ubuntu
-	rsync -rp $(LOCAL_MIRROR_UBUNTU_OS_BASEURL)/ $(ISOROOT)/ubuntu/
-	rsync -rp $(LOCAL_MIRROR)/ubuntu-packages.changelog $(ISOROOT)
+#########
+# MIRROR
+#########
+$(BUILD_DIR)/mirror-changelog.done: $(BUILD_DIR)/mirror.done
+	bash -c "export LOCAL_MIRROR=$(ISOROOT); $(SOURCE_DIR)/report-changelog.sh"
+	$(ACTION.TOUCH)
+
+$(BUILD_DIR)/mirror.done:
+	@mkdir -p $(ISOROOT)
+	packetary --threads-num 10 serial-clone -f $(MIRROR_YAML) -d $(ISOROOT)
+	$(ACTION.TOUCH)
+
+$(BUILD_DIR)/iso/isoroot-mirror.done: \
+		$(BUILD_DIR)/mirror.done \
+		$(BUILD_DIR)/mirror-changelog.done
 	$(ACTION.TOUCH)
 
 ########################
-# Extra files
+# EXTRA FILES
 ########################
-
-$(BUILD_DIR)/iso/isoroot-dotfiles.done: \
-		$(ISOROOT)/.discinfo \
-		$(ISOROOT)/.treeinfo
-	$(ACTION.TOUCH)
-
-$(BUILD_DIR)/iso/isoroot-files.done: \
-		$(BUILD_DIR)/iso/isoroot-dotfiles.done \
-		$(ISOROOT)/isolinux/isolinux.cfg \
-		$(ISOROOT)/isolinux/splash.jpg \
-		$(ISOROOT)/ks.cfg
-	$(ACTION.TOUCH)
-
 $(ISOROOT)/.discinfo: $(SOURCE_DIR)/iso/.discinfo ; $(ACTION.COPY)
 $(ISOROOT)/.treeinfo: $(SOURCE_DIR)/iso/.treeinfo ; $(ACTION.COPY)
+$(ISOROOT)/isolinux/splash.jpg: $(SOURCE_DIR)/iso/isolinux/splash.jpg; $(ACTION.COPY)
 
-$(ISOROOT)/ks.yaml:
+$(ISOROOT)/isolinux/isolinux.cfg:
 	@mkdir -p $(@D)
-	cp $(KSYAML) $@
-
-$(ISOROOT)/isolinux/isolinux.cfg: $(SOURCE_DIR)/iso/isolinux/isolinux.cfg ; $(ACTION.COPY)
-$(ISOROOT)/isolinux/splash.jpg: $(SOURCE_DIR)/iso/isolinux/splash.jpg ; $(ACTION.COPY)
-$(ISOROOT)/ks.cfg: $(SOURCE_DIR)/iso/ks.template $(SOURCE_DIR)/iso/ks.py $(ISOROOT)/ks.yaml
-	python $(SOURCE_DIR)/iso/ks.py \
-		-t $(SOURCE_DIR)/iso/ks.template \
-		-c $(ISOROOT)/ks.yaml \
-		-u '{"CENTOS_RELEASE": "$(CENTOS_RELEASE)", "PRODUCT_VERSION": "$(PRODUCT_VERSION)"}' \
+	python $(SOURCE_DIR)/evaluator.py \
+		-t $(SOURCE_DIR)/iso/isolinux/isolinux.cfg \
+		-c $(CONFIG_YAML) \
 		-o $@.tmp
 	mv $@.tmp $@
 
+$(ISOROOT)/ks.cfg: $(SOURCE_DIR)/iso/ks.cfg
+	@mkdir -p $(@D)
+	python $(SOURCE_DIR)/evaluator.py \
+		-t $(SOURCE_DIR)/iso/ks.cfg \
+		-c $(CONFIG_YAML) \
+		-o $@.tmp
+	mv $@.tmp $@
+
+$(BUILD_DIR)/iso/isoroot-files.done: \
+		$(ISOROOT)/.discinfo \
+		$(ISOROOT)/.treeinfo \
+		$(ISOROOT)/isolinux/splash.jpg \
+		$(ISOROOT)/isolinux/isolinux.cfg \
+		$(ISOROOT)/ks.cfg
+	$(ACTION.TOUCH)
 
 ########################
 # Iso image root file system.
 ########################
-
 $(BUILD_DIR)/iso/isoroot.done: \
-		$(BUILD_DIR)/iso/isoroot-centos.done \
-		$(BUILD_DIR)/iso/isoroot-ubuntu.done \
+		$(BUILD_DIR)/iso/isoroot-images.done \
+		$(BUILD_DIR)/iso/isoroot-mirror.done \
 		$(BUILD_DIR)/iso/isoroot-files.done
 	$(ACTION.TOUCH)
 
 ########################
 # Building CD and USB stick images
 ########################
-
-# ISO_VOLUME_ID can't have whitespaces or other non-alphanumeric characters 'as is'.
-# They must be represented as \xNN, where NN is the hexadecimal representation of the character.
-# For example, \x20 is a white space (" ").
-# This is the limitation of kickstart boot options.
-
-ISO_VOLUME_ID:=OpenStack_Fuel
-ISO_VOLUME_PREP:="Fuel team"
 
 # keep in mind that mkisofs touches some files inside directory
 # from which it builds iso image
@@ -125,14 +155,6 @@ $(ISO_PATH): $(BUILD_DIR)/iso/isoroot.done
 	rm -f $@
 	mkdir -p $(BUILD_DIR)/iso/isoroot-mkisofs $(@D)
 	rsync -a --delete $(ISOROOT)/ $(BUILD_DIR)/iso/isoroot-mkisofs
-	sudo sed -r -i -e "s/ip=[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/ip=$(MASTER_IP)/" $(BUILD_DIR)/iso/isoroot-mkisofs/isolinux/isolinux.cfg
-	sudo sed -r -i -e "s/dns1=[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/dns1=$(MASTER_DNS)/" $(BUILD_DIR)/iso/isoroot-mkisofs/isolinux/isolinux.cfg
-	sudo sed -r -i -e "s/netmask=[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/netmask=$(MASTER_NETMASK)/" $(BUILD_DIR)/iso/isoroot-mkisofs/isolinux/isolinux.cfg
-	sudo sed -r -i -e "s/gw=[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/gw=$(MASTER_GW)/" $(BUILD_DIR)/iso/isoroot-mkisofs/isolinux/isolinux.cfg
-	sudo sed -r -i -e "s/will_be_substituted_with_PRODUCT_VERSION/$(PRODUCT_VERSION)/" $(BUILD_DIR)/iso/isoroot-mkisofs/isolinux/isolinux.cfg
-	sudo sed -r -i -e 's/will_be_substituted_with_ISO_VOLUME_ID/$(ISO_VOLUME_ID)/g' $(BUILD_DIR)/iso/isoroot-mkisofs/isolinux/isolinux.cfg
-	sudo sed -r -i -e 's/will_be_substituted_with_ISO_VOLUME_ID/$(ISO_VOLUME_ID)/g' $(BUILD_DIR)/iso/isoroot-mkisofs/ks.cfg
-
 
 	mkdir -p $(BUILD_DIR)/iso/efi_tmp/efi_image
 	# We need to have a partition which will be pointed from ISO as efi partition
@@ -143,22 +165,11 @@ $(ISO_PATH): $(BUILD_DIR)/iso/isoroot.done
 	sudo umount -l $(BUILD_DIR)/iso/efi_tmp/efi_image || true
 	sudo mount $(BUILD_DIR)/iso/efi_tmp/efiboot.img $(BUILD_DIR)/iso/efi_tmp/efi_image
 
-	# This needs to be edited in place due to some strange implementations of UEFI
-	# For example, Tianocore OVMF will not use efiboot.img. Instead, it looks for
-	# bootloader and it conffiles in /EFI/BOOT/* on main ISO partition (with ISO9660 fs)
-	echo > $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf
-	echo "default=0" >> $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf
-	#echo "splashimage=/EFI/BOOT/splash.xpm.gz" >> $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf
-	echo "timeout 300" >> $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf
-	echo "hiddenmenu" >> $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf
-	echo "title DVD Fuel Install (Static IP)" >> $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf
-	# efiboot.img is a partition with filesystem now and /vmlinuz there will be pointed
-	# to root of it
-	echo "  kernel /vmlinuz biosdevname=0 ks=cdrom:/ks.cfg ip=$(MASTER_IP) gw=$(MASTER_GW) dns1=$(MASTER_DNS) netmask=$(MASTER_NETMASK) hostname=fuel.domain.tld showmenu=yes" >> $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf
-	echo "  initrd /initrd.img" >> $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf
-	echo "title USB Fuel Install (Static IP)" >> $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf
-	echo "  kernel /vmlinuz biosdevname=0 repo=hd:LABEL=\"$(ISO_VOLUME_ID)\":/ ks=hd:LABEL=\"$(ISO_VOLUME_ID)\":/ks.cfg ip=$(MASTER_IP) gw=$(MASTER_GW) dns1=$(MASTER_DNS) netmask=$(MASTER_NETMASK) hostname=fuel.domain.tld showmenu=yes" >> $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf
-	echo "  initrd /initrd.img" >> $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf
+	mkdir -p $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT
+	python $(SOURCE_DIR)/evaluator.py \
+		-t $(SOURCE_DIR)/iso/EFI/BOOT/BOOTX64.conf \
+		-c $(CONFIG_YAML) \
+		-o $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf
 
 	# But many UEFI implementations will use our efiboot.img and if we want to boot from it,
 	# we also need to place kernel and initrd there (and bootloader and conffile to it too)
@@ -167,13 +178,12 @@ $(ISO_PATH): $(BUILD_DIR)/iso/isoroot.done
 	sudo mkdir -p $(BUILD_DIR)/iso/efi_tmp/efi_image/EFI/BOOT/
 	sudo cp -f $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.conf $(BUILD_DIR)/iso/efi_tmp/efi_image/EFI/BOOT/
 	sudo cp -f $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/BOOTX64.EFI $(BUILD_DIR)/iso/efi_tmp/efi_image/EFI/BOOT/
-	#sudo cp -f $(BUILD_DIR)/iso/isoroot-mkisofs/EFI/BOOT/splash.xpm.gz $(BUILD_DIR)/iso/efi_tmp/efi_image/EFI/BOOT/
 	sudo umount $(BUILD_DIR)/iso/efi_tmp/efi_image
 	cp -f $(BUILD_DIR)/iso/efi_tmp/efiboot.img $(BUILD_DIR)/iso/isoroot-mkisofs/images/
 	sudo rm -rf $(BUILD_DIR)/iso/efi_tmp/
 
 	xorriso -as mkisofs \
-		-V $(ISO_VOLUME_ID) -p $(ISO_VOLUME_PREP) \
+		-V "$(ISO_VOLUME_ID)" -p "$(ISO_VOLUME_PREP)" \
 		-J -R \
 		-graft-points \
 		-b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table \
