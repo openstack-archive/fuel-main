b4911720924862e31e2e4a25cbeb38f789d9e95e
Vitaly Parakhin <vparakhin@mirantis.com>
2016-03-17 17:04:51 +0200

    Remove debian-installer placeholders
    In Fuel 7.0 we dropped classical provisioning mode,
    fully switching to IBP. Since then, placeholders
    created for Ubuntu cobbler profile, are no longer
    needed.
    
    Closes-Bug: #1549346
    Change-Id: I36ff56c1e4a0437b33fc8e31da3df6f311607307

--- iso/bootstrap_admin_node.sh
+++ iso/bootstrap_admin_node.sh
@@ -229,13 +229,6 @@ echo $FUEL_PACKAGES | xargs -n1 yum install -y
 # /etc/fuel_openstack_version is provided by 'fuel-openstack-metadata' package
 OPENSTACK_VERSION=$(cat /etc/fuel_openstack_version)
 
-# We do not ship debian-installer kernel and initrd on ISO.
-# But we still need to be able to create ubuntu cobbler distro
-# which requires kernel and initrd to be available. So, we
-# just touch these files to work around cobbler's limitation.
-mkdir -p ${wwwdir}/${OPENSTACK_VERSION}/ubuntu/x86_64/images/
-touch ${wwwdir}/${OPENSTACK_VERSION}/ubuntu/x86_64/images/linux
-touch ${wwwdir}/${OPENSTACK_VERSION}/ubuntu/x86_64/images/initrd.gz
 
 # Disable online base MOS repo if we run an ISO installation
 [ -f /etc/fuel_build_id ] && yum-config-manager --disable mos${FUEL_RELEASE}-base --save
