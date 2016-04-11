8bd082d3fda90289d2ec1a00cd512f82c3661ef6
Sergey Kulanov <skulanov@mirantis.com>
2016-04-11 12:44:22 +0300

    Align mirror path for CentOS repos
    Change-Id: I5ad54343f10800cca60e03eecd65686365af5ebd
    Related-bug: #1563870
    (cherry picked from commit e124d5fe5379d8602e22eba3323934bfdc1c7ed7)

--- config.mk
+++ config.mk
@@ -136,7 +136,7 @@ LOCAL_MIRROR_UBUNTU_OS_BASEURL:=$(LOCAL_MIRROR_UBUNTU)
 USE_MIRROR?=ext
 
 ifeq ($(USE_MIRROR),ext)
-MIRROR_FUEL?=http://mirror.fuel-infra.org/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)-fuel/os/x86_64/
+MIRROR_FUEL?=http://mirror.fuel-infra.org/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)/os/x86_64/
 MIRROR_CENTOS?=http://mirror.centos.org/centos/$(CENTOS_MAJOR)
 MIRROR_CENTOS_KERNEL?=$(MIRROR_CENTOS)
 SANDBOX_MIRROR_CENTOS_UPSTREAM?=$(MIRROR_CENTOS)
@@ -145,31 +145,31 @@ MIRROR_MOS_UBUNTU?=$(MIRROR_UBUNTU)
 endif
 
 ifeq ($(USE_MIRROR),srt)
-MIRROR_FUEL?=http://osci-mirror-srt.srt.mirantis.net/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)-fuel/os/x86_64/
+MIRROR_FUEL?=http://osci-mirror-srt.srt.mirantis.net/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)/os/x86_64/
 MIRROR_UBUNTU?=osci-mirror-srt.srt.mirantis.net
 MIRROR_MOS_UBUNTU?=$(MIRROR_UBUNTU)
 endif
 
 ifeq ($(USE_MIRROR),msk)
-MIRROR_FUEL?=http://osci-mirror-msk.msk.mirantis.net/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)-fuel/os/x86_64/
+MIRROR_FUEL?=http://osci-mirror-msk.msk.mirantis.net/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)/os/x86_64/
 MIRROR_UBUNTU?=osci-mirror-msk.msk.mirantis.net
 MIRROR_MOS_UBUNTU?=$(MIRROR_UBUNTU)
 endif
 
 ifeq ($(USE_MIRROR),hrk)
-MIRROR_FUEL?=http://osci-mirror-kha.kha.mirantis.net/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)-fuel/os/x86_64/
+MIRROR_FUEL?=http://osci-mirror-kha.kha.mirantis.net/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)/os/x86_64/
 MIRROR_UBUNTU?=osci-mirror-kha.kha.mirantis.net
 MIRROR_MOS_UBUNTU?=$(MIRROR_UBUNTU)
 endif
 
 ifeq ($(USE_MIRROR),usa)
-MIRROR_FUEL?=http://mirror.seed-us1.fuel-infra.org/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)-fuel/os/x86_64/
+MIRROR_FUEL?=http://mirror.seed-us1.fuel-infra.org/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)/os/x86_64/
 MIRROR_UBUNTU?=mirror.seed-us1.fuel-infra.org
 MIRROR_MOS_UBUNTU?=$(MIRROR_UBUNTU)
 endif
 
 ifeq ($(USE_MIRROR),cz)
-MIRROR_FUEL?=http://mirror.seed-cz1.fuel-infra.org/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)-fuel/os/x86_64/
+MIRROR_FUEL?=http://mirror.seed-cz1.fuel-infra.org/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)/os/x86_64/
 MIRROR_UBUNTU?=mirror.seed-cz1.fuel-infra.org
 MIRROR_MOS_UBUNTU?=$(MIRROR_UBUNTU)
 endif
@@ -197,7 +197,7 @@ MIRROR_MOS_UBUNTU_SECTION?=main restricted
 
 # MIRROR_FUEL affects build process only if YUM_REPOS variable contains 'fuel'.
 # Otherwise it is ignored entirely.
-MIRROR_FUEL?=http://mirror.fuel-infra.org/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)-fuel
+MIRROR_FUEL?=http://mirror.fuel-infra.org/mos-repos/centos/$(PRODUCT_NAME)$(PRODUCT_VERSION)-centos$(CENTOS_MAJOR)
 
 # Additional CentOS repos.
 # Each repo must be comma separated tuple with repo-name and repo-path.
