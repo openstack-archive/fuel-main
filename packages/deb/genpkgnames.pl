bb079c030a9a71694cc5191520c3670febd7c8d3
dmburmistrov <dmburmistrov@mirantis.com>
2016-06-02 15:37:14 +0300

    [WIP] don't fail on comment lines
    Change-Id: I4e9a235956e5d08ab23a02ee52750e233da5f6d9

--- packages/deb/genpkgnames.pl
+++ packages/deb/genpkgnames.pl
@@ -1,7 +1,17 @@
 use Parse::DebControl;
 
 $parser = new Parse::DebControl;
- $data = $parser->parse_file($ARGV[0]);
+
+ open(my $fh, '<:encoding(UTF-8)', $ARGV[0])
+  or die "Could not open file '$ARGV[0]' $!";
+
+ $control_data = "";
+ while (my $line = <$fh>) {
+     if ($line =~ /^#/) { next; }
+     $control_data = $control_data . $line;
+ }
+
+ $data = $parser->parse_mem($control_data);
  foreach my $a (@$data) {
      next if exists $a->{"Source"};
      print ${a}->{"Package"}."\n";
