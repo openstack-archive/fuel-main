fa3905a063c61ec53755d177bb9c5da0bd7d9aaa
dmburmistrov <dmburmistrov@mirantis.com>
2016-06-02 15:37:14 +0300

    Don't fail on comment lines
    Change-Id: I4e9a235956e5d08ab23a02ee52750e233da5f6d9

--- packages/deb/genpkgnames.pl
+++ packages/deb/genpkgnames.pl
@@ -1,7 +1,13 @@
 use Parse::DebControl;
 
 $parser = new Parse::DebControl;
- $data = $parser->parse_file($ARGV[0]);
+
+%options = (
+    type => 'debian/control',
+    stripComments => 'true',
+    );
+
+ $data = $parser->parse_file($ARGV[0], \%options);
  foreach my $a (@$data) {
      next if exists $a->{"Source"};
      print ${a}->{"Package"}."\n";
