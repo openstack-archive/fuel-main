5e97420219055ef02d48dcac594f852528b46c92
azvyagintsev <azvyagintsev@mirantis.com>
2017-07-31 13:51:04 +0300

    Add package listing into bootstrap
    Change-Id: Ic1994fd6164bfea617f59d7048f663271df473ca

--- bootstrap/module.mk
+++ bootstrap/module.mk
@@ -152,14 +152,22 @@ $(BUILD_DIR)/bootstrap/customize-initram-root.done: \
 	# Copying bash init files
 	sudo cp -f $(INITRAMROOT)/etc/skel/.bash* $(INITRAMROOT)/root/
 
+	# Save list of installed packages
+#	sudo sh -c "rpm --root=$(INITRAMROOT) --dbpath $(INITRAMROOT)/root/.rpmdb -qa >> $(INITRAMROOT)/bootstrap_packages"
+	$(YUM) install list | sort >> $(INITRAMROOT)/bootstrap_packages
+	sleep 60m
+	-sudo sh -c "find $(LOCAL_MIRROR_CENTOS_OS_BASEURL) -name '$(KERNEL_PATTERN)' | xargs basename -a >> $(INITRAMROOT)/bootstrap_kernel"
+	-sudo sh -c "find $(LOCAL_MIRROR_CENTOS_OS_BASEURL) -name '$(KERNEL_FIRMWARE_PATTERN)' | xargs basename -a >> $(INITRAMROOT)/bootstrap_kernel"
+
 	# Removing garbage
 	sudo rm -rf $(INITRAMROOT)/home/*
 	sudo rm -rf \
 		$(INITRAMROOT)/var/cache/yum \
+#		$(INITRAMROOT)/root/.rpmdb
 		$(INITRAMROOT)/var/lib/yum \
 		$(INITRAMROOT)/usr/share/doc \
-        $(INITRAMROOT)/usr/share/locale \
-	sudo rm -rf $(INITRAMROOT)/tmp/*
+		$(INITRAMROOT)/usr/share/locale \
+		$(INITRAMROOT)/tmp/*
 
 	$(ACTION.TOUCH)
 
